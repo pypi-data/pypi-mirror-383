e97e0d9023ee1092b0ed55752e13b22ca42336eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:27 +0000

    Add package shudaodao/0.1.43/shudaodao-0.1.43-cp312-cp312-musllinux_1_2_x86_64.whl

---
+++ packages/shudaodao/shudaodao-0.1.43-cp312-cp312-musllinux_1_2_x86_64.whl/shudaodao_core/portal_enum/controller_table/t_enum_field.pyi
@@ -0,0 +1,11 @@
+from .. import get_engine_name as get_engine_name, get_schema_name as get_schema_name
+from ...controller.table_controller import AuthController as AuthController
+from ..entity_table.t_enum_field import (
+    TEnumField as TEnumField,
+    TEnumFieldCreate as TEnumFieldCreate,
+    TEnumFieldResponse as TEnumFieldResponse,
+    TEnumFieldUpdate as TEnumFieldUpdate,
+)
+from _typeshed import Incomplete
+
+TEnumField_Router: Incomplete
