4929d6852caa5a20b340caed30b35da89c3d8ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 15:05:53 +0000

    Add package ruff-format/0.2.0/ruff_format-0.2.0.tar.gz

---
+++ packages/ruff-format/ruff_format-0.2.0.tar.gz/ruff_format-0.2.0/src/lib.rs
@@ -0,0 +1,20 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn format_string(code: &str) -> PyResult<String> {
+    ruff_python_formatter::format_module_source(
+        code,
+        ruff_python_formatter::PyFormatOptions::default(),
+    )
+    .map_err(|e| {
+        PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("Failed to format code: {}", e))
+    })
+    .map(|formatted| formatted.into_code())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule(gil_used=false)]
+fn ruff_format(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(format_string, m)?)?;
+    Ok(())
+}
