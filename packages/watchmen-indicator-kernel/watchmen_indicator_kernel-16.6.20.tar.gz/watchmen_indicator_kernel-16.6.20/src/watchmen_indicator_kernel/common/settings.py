911cb48e73d79afa45acf19581f3d62758631ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:10 +0000

    Add package watchmen-indicator-kernel/16.6.20/watchmen_indicator_kernel-16.6.20.tar.gz

---
+++ packages/watchmen-indicator-kernel/watchmen_indicator_kernel-16.6.20.tar.gz/watchmen_indicator_kernel-16.6.20/src/watchmen_indicator_kernel/common/settings.py
@@ -0,0 +1,17 @@
+from logging import getLogger
+
+from pydantic import BaseSettings
+
+logger = getLogger(__name__)
+
+
+class IndicatorKernelSettings(BaseSettings):
+	PLUGIN_HOST: str = None
+
+
+settings = IndicatorKernelSettings()
+# logger.info(f'Indicator kernel settings[{settings.dict()}].')
+
+
+def ask_plugin_host() -> str:
+	return settings.PLUGIN_HOST
