365a3510c378a3f0f6086b86fb3f19e84b74f177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 14:06:30 +0000

    Add package fastapi-custom-responses/0.0.9/fastapi_custom_responses-0.0.9-py3-none-any.whl

---
+++ packages/fastapi-custom-responses/fastapi_custom_responses-0.0.9-py3-none-any.whl/fastapi_custom_responses/__init__.py
@@ -0,0 +1,2 @@
+from .errors import EXCEPTION_HANDLERS, ErrorResponse
+from .responses import PaginatedResponse, PaginationMeta, Response
---
+++ packages/fastapi-custom-responses/fastapi_custom_responses-0.0.9-py3-none-any.whl/fastapi_custom_responses/responses.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+from pydantic_super_model import SuperModel
+
+
+class Response[T](SuperModel):
+    """Response model."""
+
+    success: bool
+    data: T | None = None
+    error: str | None = None
+
+
+class PaginationMeta(SuperModel):
+    """Pagination metadata model."""
+
+    offset: int
+    limit: int
+    total: int
+
+
+class PaginatedResponse[T: Any](Response[list[T]]):
+    """Paginated response model."""
+
+    meta: PaginationMeta
