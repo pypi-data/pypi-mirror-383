2be2a6687c7819afc633819c14f2edd9cd5b945f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 14:03:45 +0000

    Add package fretsonfire/1.6.1/fretsonfire-1.6.1.tar.gz

---
+++ packages/fretsonfire/fretsonfire-1.6.1.tar.gz/fretsonfire-1.6.1/src/fretsonfire/__init__.py
@@ -0,0 +1,10 @@
+"""Frets on Fire game package."""
+
+from importlib import import_module
+
+try:
+    __version__ = import_module(".Version", __name__).version()
+except Exception:  # pragma: no cover - metadata only
+    __version__ = "0.0.dev0"
+
+__all__ = []
