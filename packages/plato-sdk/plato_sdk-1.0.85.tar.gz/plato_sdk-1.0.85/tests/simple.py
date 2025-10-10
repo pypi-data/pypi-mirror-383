7e8d48b8a56b793a5285ebe48ae184583c2fcc12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 14:04:43 +0000

    Add package plato-sdk/1.0.85/plato_sdk-1.0.85.tar.gz

---
+++ packages/plato-sdk/plato_sdk-1.0.85.tar.gz/plato_sdk-1.0.85/tests/simple.py
@@ -0,0 +1,46 @@
+import logging
+import os
+from plato.sdk import Plato
+from dotenv import load_dotenv
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+load_dotenv()
+BASE_URL = os.getenv("PLATO_BASE_URL")
+API_KEY = os.getenv("PLATO_API_KEY")
+
+async def main():
+    client = Plato(base_url=BASE_URL, api_key=API_KEY)
+    running_sessions_count = await client.get_running_sessions_count()
+    env = await client.make_environment(
+        "espocrm",
+        fast=True,
+        interface_type=None,
+        # dataset="wintergreen",
+        artifact_id="56f85a14-8e82-4053-a7df-8490c31a14e3",
+        # version="latest"
+    )
+    tasks = await client.load_tasks("espocrm")
+    task = tasks[0]
+    await env.wait_for_ready()
+    await env.reset(task=task)
+    public_url = await env.get_public_url()
+    print(public_url)
+    input("Press Enter to continue...")
+
+    state = await env.get_state()
+    print(state)
+
+    input("Press Enter to continue...")
+    # tasks = await client.load_tasks("firefly")
+    # task = tasks[0]
+    # await env.reset(task=task)
+    # breakpoint()
+    # state = await env.get_state()
+    await env.close()
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
+
+
