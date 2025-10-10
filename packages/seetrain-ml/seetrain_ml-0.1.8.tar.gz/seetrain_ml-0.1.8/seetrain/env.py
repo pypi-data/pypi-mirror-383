048caf10d00468f949f3f284d086b33d75d983e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:03:59 +0000

    Add package seetrain-ml/0.1.8/seetrain_ml-0.1.8.tar.gz

---
+++ packages/seetrain-ml/seetrain_ml-0.1.8.tar.gz/seetrain_ml-0.1.8/seetrain/env.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import enum
+
+
+def get_env(key: str, default: str = None) -> str:
+    return os.getenv(key, default)
+
+
+class Env(enum.Enum):
+    BaseURL = get_env("BASE_URL", "http://localhost:8088/api/v1")
+    TaskID = get_env("TASK_ID", "0")
+    Project = get_env("PROJECT", "default")
+    ViewURL = get_env("VIEW_URL", "http://localhost:5173/")
