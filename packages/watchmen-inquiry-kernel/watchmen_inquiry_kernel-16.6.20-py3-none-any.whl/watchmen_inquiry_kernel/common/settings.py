f78044e96234dc3d49c2e24e4ab2f4bd5e3a2e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:10 +0000

    Add package watchmen-inquiry-kernel/16.6.20/watchmen_inquiry_kernel-16.6.20-py3-none-any.whl

---
+++ packages/watchmen-inquiry-kernel/watchmen_inquiry_kernel-16.6.20-py3-none-any.whl/watchmen_inquiry_kernel/common/settings.py
@@ -0,0 +1,28 @@
+from logging import getLogger
+
+from pydantic import BaseSettings
+
+logger = getLogger(__name__)
+
+
+class KernelSettings(BaseSettings):
+	USE_STORAGE_DIRECTLY: bool = True  # use storage directly when all topics in subject are from one data source
+	TRINO: bool = True  # trino
+
+	class Config:
+		# secrets_dir = '/var/run'
+		env_file = '.env'
+		env_file_encoding = 'utf-8'
+		case_sensitive = True
+
+
+settings = KernelSettings()
+# logger.info(f'Inquiry kernel settings[{settings.dict()}].')
+
+
+def ask_use_storage_directly() -> bool:
+	return settings.USE_STORAGE_DIRECTLY
+
+
+def ask_trino_enabled() -> bool:
+	return settings.TRINO
