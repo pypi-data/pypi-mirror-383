f9c38bf0c061c67d2b3745aaa98af3a8db913a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:19 +0000

    Add package phase-cli/1.21.1/phase_cli-1.21.1-py3-none-any.whl

---
+++ packages/phase-cli/phase_cli-1.21.1-py3-none-any.whl/phase_cli/utils/const.py
@@ -0,0 +1,51 @@
+import os
+import re
+
+__version__ = "1.21.1"
+__ph_version__ = "v1"
+
+description = (
+    "Securely manage application secrets and environment variables with Phase."
+)
+
+phaseASCii = f"""
+           /$$
+          | $$
+  /$$$$$$ | $$$$$$$   /$$$$$$   /$$$$$$$  /$$$$$$
+ /$$__  $$| $$__  $$ |____  $$ /$$_____/ /$$__  $$
+| $$  \ $$| $$  \ $$  /$$$$$$$|  $$$$$$ | $$$$$$$$
+| $$  | $$| $$  | $$ /$$__  $$ \____  $$| $$_____/
+| $$$$$$$/| $$  | $$|  $$$$$$$ /$$$$$$$/|  $$$$$$$
+| $$____/ |__/  |__/ \_______/|_______/  \_______/
+| $$
+|__/
+"""
+
+SECRET_REF_REGEX = re.compile(r"\$\{([^}]+)\}")
+
+# Define paths to Phase configs
+PHASE_ENV_CONFIG = ".phase.json"  # Holds project and environment contexts in users repo, unique to each application.
+
+PHASE_SECRETS_DIR = os.path.expanduser(
+    "~/.phase/secrets"
+)  # Holds local encrypted caches of secrets and environment variables, common to all applications. (only if offline mode is enabled)
+CONFIG_FILE = os.path.join(
+    PHASE_SECRETS_DIR, "config.json"
+)  # Holds local user account configurations
+
+
+PHASE_CLOUD_API_HOST = "https://console.phase.dev"
+
+# AWS Config
+AWS_DEFAULT_GLOBAL_STS_ENDPOINT = "https://sts.amazonaws.com"
+AWS_DEFAULT_GLOBAL_STS_REGION = "us-east-1"
+
+pss_user_pattern = re.compile(
+    r"^pss_user:v(\d+):([a-fA-F0-9]{64}):([a-fA-F0-9]{64}):([a-fA-F0-9]{64}):([a-fA-F0-9]{64})$"
+)
+pss_service_pattern = re.compile(
+    r"^pss_service:v(\d+):([a-fA-F0-9]{64}):([a-fA-F0-9]{64}):([a-fA-F0-9]{64}):([a-fA-F0-9]{64})$"
+)
+
+cross_env_pattern = re.compile(r"\$\{(.+?)\.(.+?)\}")
+local_ref_pattern = re.compile(r"\$\{([^.]+?)\}")
