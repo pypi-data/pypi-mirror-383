948490a69d452c2fe3233991d38e7436a3754143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:19 +0000

    Add package hkfdb2/5.3.1/hkfdb2-5.3.1.tar.gz

---
+++ packages/hkfdb2/hkfdb2-5.3.1.tar.gz/hkfdb2-5.3.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hkfdb2",
+    version="5.3.1",
+    author="Hong Kong Finance Database Team",
+    author_email="info@hkfdb.net",
+    description="Hong Kong Finance Database.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.hkfdb.net",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'pandas == 1.5.3',
+        'requests',
+        'beautifulsoup4',
+        'pymongo',
+        'lxml',
+    ]
+)
