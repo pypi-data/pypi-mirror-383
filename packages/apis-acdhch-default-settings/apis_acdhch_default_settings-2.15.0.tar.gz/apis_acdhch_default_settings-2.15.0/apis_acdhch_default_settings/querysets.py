8cf2c753803dd0186dd2f0c7e9145147e0597c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:01 +0000

    Add package apis-acdhch-default-settings/2.15.0/apis_acdhch_default_settings-2.15.0.tar.gz

---
+++ packages/apis-acdhch-default-settings/apis_acdhch_default_settings-2.15.0.tar.gz/apis_acdhch_default_settings-2.15.0/apis_acdhch_default_settings/querysets.py
@@ -0,0 +1,54 @@
+import os
+
+from apis_core.utils.autocomplete import (
+    ExternalAutocomplete,
+    TypeSenseAutocompleteAdapter,
+    LobidAutocompleteAdapter,
+)
+
+
+class E74_GroupExternalAutocomplete(ExternalAutocomplete):
+    adapters = [
+        LobidAutocompleteAdapter(
+            params={
+                "filter": "type:CorporateBody",
+                "format": "json:preferredName,geographicAreaCode,dateOfEstablishment,broaderTermInstantial",
+            }
+        ),
+    ]
+
+
+class E53_PlaceExternalAutocomplete(ExternalAutocomplete):
+    adapters = [
+        TypeSenseAutocompleteAdapter(
+            collections=[
+                "prosnet-wikidata-place-index",
+                "prosnet-geonames-place-index",
+            ],
+            template="e53_place_from_typesense_autocomplete_result.html",
+            token=os.getenv("TYPESENSE_TOKEN", None),
+            server=os.getenv("TYPESENSE_SERVER", None),
+        ),
+        LobidAutocompleteAdapter(
+            params={
+                "filter": "type:PlaceOrGeographicName",
+                "format": "json:preferredName",
+            }
+        ),
+    ]
+
+
+class E21_PersonExternalAutocomplete(ExternalAutocomplete):
+    adapters = [
+        TypeSenseAutocompleteAdapter(
+            collections="prosnet-wikidata-person-index",
+            token=os.getenv("TYPESENSE_TOKEN", None),
+            server=os.getenv("TYPESENSE_SERVER", None),
+        ),
+        LobidAutocompleteAdapter(
+            params={
+                "filter": "type:Person",
+                "format": "json:preferredName,professionOrOccupation",
+            }
+        ),
+    ]
