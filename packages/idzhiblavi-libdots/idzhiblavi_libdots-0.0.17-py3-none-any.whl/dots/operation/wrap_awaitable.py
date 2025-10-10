c1aeb1a0e1e6c32cae915b69ebcf97b3f060deb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 16:05:19 +0000

    Add package idzhiblavi-libdots/0.0.17/idzhiblavi_libdots-0.0.17-py3-none-any.whl

---
+++ packages/idzhiblavi-libdots/idzhiblavi_libdots-0.0.17-py3-none-any.whl/dots/operation/wrap_awaitable.py
@@ -0,0 +1,14 @@
+from dots.operation import Operation, Target
+
+
+class _Op(Operation):
+    def __init__(self, awaitable):
+        self._awaitable = awaitable
+
+    async def apply(self, target: Target):
+        op = await self._awaitable
+        return await op.apply(target)
+
+
+def wrap_awaitable(awaitable) -> Operation:
+    return _Op(awaitable)
