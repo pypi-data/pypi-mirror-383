d9e9170b99bf9b9e6679260ba2436c87dc9b9a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:23 +0000

    Add package aiovodafone/2.0.0/aiovodafone-2.0.0.tar.gz

---
+++ packages/aiovodafone/aiovodafone-2.0.0.tar.gz/aiovodafone-2.0.0/src/aiovodafone/__init__.py
@@ -0,0 +1,29 @@
+"""aiovodafone library."""
+
+__version__ = "2.0.0"
+
+from .api import (
+    VodafoneStationDevice,
+    VodafoneStationSercommApi,
+    VodafoneStationTechnicolorApi,
+)
+from .exceptions import (
+    AlreadyLogged,
+    CannotAuthenticate,
+    CannotConnect,
+    CsrfError,
+    ModelNotSupported,
+    VodafoneError,
+)
+
+__all__ = [
+    "AlreadyLogged",
+    "CannotAuthenticate",
+    "CannotConnect",
+    "CsrfError",
+    "ModelNotSupported",
+    "VodafoneError",
+    "VodafoneStationDevice",
+    "VodafoneStationSercommApi",
+    "VodafoneStationTechnicolorApi",
+]
