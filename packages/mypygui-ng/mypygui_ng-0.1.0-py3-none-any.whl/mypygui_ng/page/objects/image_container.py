00b7b4d5e4ef842d2582f673b8fcdc59327413c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 14:05:42 +0000

    Add package mypygui-ng/0.1.0/mypygui_ng-0.1.0-py3-none-any.whl

---
+++ packages/mypygui-ng/mypygui_ng-0.1.0-py3-none-any.whl/mypygui_ng/page/objects/image_container.py
@@ -0,0 +1,38 @@
+import PIL.Image
+import PIL.ImageTk
+from math import ceil
+from ...logging import console
+
+class Image:
+    '''Ye'''
+    def __init__(self, image : PIL.Image.Image):
+        self.true_image = image
+        self.true_size = image.size
+
+        self.photo_image = PIL.ImageTk.PhotoImage(image)
+        self.size = self.true_size
+
+    def resize(self, new_width, new_height):
+        new_width = ceil(new_width)
+        new_height = ceil(new_height)
+        if self.size == (new_width, new_height):return
+        if self.true_size == (new_width, new_height):
+            self.photo_image = PIL.ImageTk.PhotoImage(self.true_image)
+            self.size = self.true_size
+            return
+        
+        self.photo_image = PIL.ImageTk.PhotoImage(self.true_image.resize((new_width, new_height))) if new_height > 0 and new_width > 0 else None
+
+        self.size = (new_width, new_height)
+
+    def __repr__(self):
+        return f'<Image size = {self.size} true_size = {self.true_size}>'
+
+    @staticmethod
+    def handle_resource_request(data):
+        from io import BytesIO
+        try:
+            return PIL.Image.open(BytesIO(data))
+        except:
+            console.warn('Image data did not come :(')
+            return PIL.Image.new('RGB', (0, 0))
