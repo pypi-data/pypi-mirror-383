21deae842b916b5271fe1157d6b02541d76db0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:34 +0000

    Add package phylogenie/2.1.28/phylogenie-2.1.28.tar.gz

---
+++ packages/phylogenie/phylogenie-2.1.28.tar.gz/phylogenie-2.1.28/phylogenie/io/__init__.py
@@ -0,0 +1,5 @@
+from phylogenie.io.fasta import load_fasta
+from phylogenie.io.newick import dump_newick, load_newick
+from phylogenie.io.nexus import load_nexus
+
+__all__ = ["load_fasta", "load_newick", "dump_newick", "load_nexus"]
