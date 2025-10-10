ab02121e26c0bcbc8a19a6cdbf2927d4b846081e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 14:05:07 +0000

    Add package anacal/0.7.1/anacal-0.7.1-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/anacal/anacal-0.7.1-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/anacal/_anacalLib.cc
@@ -0,0 +1,19 @@
+#include "anacal.h"
+
+
+namespace anacal {
+    PYBIND11_MODULE(_anacal, m)
+    {
+        pyExportModel(m);
+        pyExportImage(m);
+        pyExportFpfs(m);
+        math::pyExportMath(m);
+        noise::pyExportNoise(m);
+        mask::pyExportMask(m);
+        ngmix::pyExportNgmix(m);
+        table::pyExportTable(m);
+        detector::pyExportDetector(m);
+        geometry::pyExportGeometry(m);
+        task::pyExportTask(m);
+    }
+}
