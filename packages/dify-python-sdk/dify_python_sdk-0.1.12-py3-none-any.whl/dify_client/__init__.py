c8be7e88d75708bdadbf3256caa98c54f6039952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 16:05:02 +0000

    Add package dify-python-sdk/0.1.12/dify_python_sdk-0.1.12-py3-none-any.whl

---
+++ packages/dify-python-sdk/dify_python_sdk-0.1.12-py3-none-any.whl/dify_client/__init__.py
@@ -0,0 +1,17 @@
+from dify_client.client import (
+    ChatClient,
+    CompletionClient,
+    DifyClient,
+    KnowledgeBaseClient,
+    WorkflowClient,
+    WorkspaceClient,
+)
+
+__all__ = [
+    "ChatClient",
+    "CompletionClient",
+    "DifyClient",
+    "KnowledgeBaseClient",
+    "WorkflowClient",
+    "WorkspaceClient",
+]
