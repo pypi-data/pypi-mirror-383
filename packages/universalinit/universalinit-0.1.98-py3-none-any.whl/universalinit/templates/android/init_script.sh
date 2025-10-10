7f6afc3f93b116842a0905d24df0ffb1e6e07d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 15:05:58 +0000

    Add package universalinit/0.1.98/universalinit-0.1.98-py3-none-any.whl

---
+++ packages/universalinit/universalinit-0.1.98-py3-none-any.whl/universalinit/templates/android/init_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/opt/gradle/gradle-9.0.0/bin/gradle init \
+  -Dorg.gradle.buildinit.specs=org.gradle.experimental.android-ecosystem-init:0.1.44 \
+  -Dorg.gradle.buildinit.language=java \
+  --use-defaults \
+  --overwrite
