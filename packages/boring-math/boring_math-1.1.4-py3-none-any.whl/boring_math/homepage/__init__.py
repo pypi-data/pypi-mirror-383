229950200a02541c58a9d4510ef92319dcb37cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 14:06:21 +0000

    Add package boring-math/1.1.4/boring_math-1.1.4-py3-none-any.whl

---
+++ packages/boring-math/boring_math-1.1.4-py3-none-any.whl/boring_math/homepage/__init__.py
@@ -0,0 +1,28 @@
+"""
+Purpose boring-math PyPI Project
+================================
+
+The boring-math PyPI project is a central component of my
+Boring Math hobby projects.
+
+- it is the PyPI project for the ``boring_math.homepage`` module
+- claims the boring-math name on PyPI for the overall effort
+- hosts on *GitHub Pages*
+
+  - the Boring Math homepage
+  - Sphinx API & usage documentation for each Boring Math project
+
+- provides locations for overarching CHANGELOG and README files
+
+  - each Boring Math project has its on CHANGELOG and README files
+
+.. tip::
+
+    There is no need to install boring-math itself, but does not hurt
+    anything if it is installed.
+
+"""
+
+__author__ = 'Geoffrey R. Scheller'
+__copyright__ = 'Copyright (c) 2016-2025 Geoffrey R. Scheller'
+__license__ = 'Apache License 2.0'
---
+++ packages/boring-math/boring_math-1.1.4-py3-none-any.whl/boring_math/homepage/overview.py
@@ -0,0 +1,11 @@
+"""
+The overall effort's name is
+`Boring Math <https://pypi.org/project/boring-math/>`_.
+
+- it is a collection of my mathematical hobby projects
+- each project's PyPI and GitHub names begin with *"boring-math-"*
+- each project implements a top-level package in the ``boring_math`` Python namespace
+- for links to all the Boring Math project PyPI and GitHub repos `click here
+  <https://github.com/grscheller/boring-math/blob/main/README.md>`_
+
+"""
