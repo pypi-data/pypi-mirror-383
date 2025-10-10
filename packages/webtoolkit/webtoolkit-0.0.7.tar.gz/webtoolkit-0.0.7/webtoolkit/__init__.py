981dce0183ab8ba86d5bbcf0d9b8a7a5df508a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 16:04:35 +0000

    Add package webtoolkit/0.0.7/webtoolkit-0.0.7.tar.gz

---
+++ packages/webtoolkit/webtoolkit-0.0.7.tar.gz/webtoolkit-0.0.7/webtoolkit/__init__.py
@@ -0,0 +1,28 @@
+"""
+Similar project: https://pypi.org/project/abstract-webtools/
+"""
+
+from .webtools import *
+from .webconfig import WebConfig
+
+from .contentinterface import ContentInterface
+from .contentlinkparser import ContentLinkParser
+from .urllocation import UrlLocation
+from .remoteserver import RemoteServer
+from .url import Url
+
+from .pages import (
+   DefaultContentPage,
+   HtmlPage,
+   RssPage,
+   RssContentReader,
+   OpmlPage,
+   JsonPage,
+   PageFactory,
+)
+
+from .contentmoderation import (
+    UrlPropertyValidator,
+    UrlPropertyValidator,
+    UrlAgeModerator,
+)
