97eeac26ad0e93568986acb6753ddaa6a89f6eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 15:06:13 +0000

    Add package evotoolkit/0.3.0b2/evotoolkit-0.3.0b2-py3-none-any.whl

---
+++ packages/evotoolkit/evotoolkit-0.3.0b2-py3-none-any.whl/evotoolkit/task/python_task/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025 Ping Guo
+# Licensed under the MIT License
+
+
+from .python_task import PythonTask
+
+from .method_interface import (
+    FunSearchPythonInterface,
+    EoHPythonInterface,
+    EvoEngineerPythonInterface,
+)
+
+from .adversarial_attack import AdversarialAttackTask
