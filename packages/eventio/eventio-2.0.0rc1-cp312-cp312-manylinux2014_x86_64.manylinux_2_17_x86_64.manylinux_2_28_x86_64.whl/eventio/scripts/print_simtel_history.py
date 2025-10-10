f97ea619424c37707e594aa8bf922956833aeca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:30 +0000

    Add package eventio/2.0.0rc1/eventio-2.0.0rc1-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/eventio/eventio-2.0.0rc1-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/eventio/scripts/print_simtel_history.py
@@ -0,0 +1,31 @@
+from eventio import EventIOFile
+from argparse import ArgumentParser
+from eventio.simtel import History
+from datetime import datetime
+
+parser = ArgumentParser()
+parser.add_argument('inputfile')
+
+
+def print_history():
+    args = parser.parse_args()
+
+    with EventIOFile(args.inputfile) as f:
+        o = next(f)
+        while isinstance(o, History):
+            for subo in o:
+                t, line = subo.parse()
+                t = datetime.fromtimestamp(t)
+                print(f'{t:%Y-%m-%dT%H:%M:%S}', line.decode('utf-8').strip())
+            o = next(f)
+
+
+def main():
+    try:
+        print_history()
+    except BrokenPipeError:
+        pass
+
+
+if __name__ == '__main__':
+    main()
