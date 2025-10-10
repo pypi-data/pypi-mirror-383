d82e315b969e7cfb86d36c3af42097a498a309ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:03:58 +0000

    Add package haiway/0.35.0/haiway-0.35.0.tar.gz

---
+++ packages/haiway/haiway-0.35.0.tar.gz/haiway-0.35.0/src/haiway/attributes/__init__.py
@@ -0,0 +1,16 @@
+from haiway.attributes.annotations import AttributeAnnotation
+from haiway.attributes.path import AttributePath
+from haiway.attributes.requirement import AttributeRequirement
+from haiway.attributes.state import State
+from haiway.attributes.validation import Validating, ValidationContext, ValidationError, Validator
+
+__all__ = (
+    "AttributeAnnotation",
+    "AttributePath",
+    "AttributeRequirement",
+    "State",
+    "Validating",
+    "ValidationContext",
+    "ValidationError",
+    "Validator",
+)
