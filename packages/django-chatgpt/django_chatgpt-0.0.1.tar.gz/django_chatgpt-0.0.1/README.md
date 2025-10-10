b91d56b941da7377c563d184d0bcb689029b3140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 15:06:22 +0000

    Add package django-chatgpt/0.0.1/django_chatgpt-0.0.1.tar.gz

---
+++ packages/django-chatgpt/django_chatgpt-0.0.1.tar.gz/django_chatgpt-0.0.1/README.md
@@ -0,0 +1,13 @@
+# django-chatgpt
+
+A placeholder package to reserve the name `django-chatgpt`.
+
+## Installation
+
+```bash
+pip install django-chatgpt
+```
+
+## License
+
+MIT
---
+++ packages/django-chatgpt/django_chatgpt-0.0.1.tar.gz/django_chatgpt-0.0.1/django_chatgpt/__init__.py
@@ -0,0 +1,9 @@
+"""
+django-chatgpt: A placeholder package to reserve the name
+"""
+
+__version__ = "0.0.1"
+
+def get_package_name():
+    """Return the package name"""
+    return "django-chatgpt"
