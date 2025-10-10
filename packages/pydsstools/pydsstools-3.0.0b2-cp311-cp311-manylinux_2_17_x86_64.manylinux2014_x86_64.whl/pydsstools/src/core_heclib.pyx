14a0a1d9fba496b3b51b935cb052fae4f84a4349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 14:05:50 +0000

    Add package pydsstools/3.0.0b2/pydsstools-3.0.0b2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pydsstools/pydsstools-3.0.0b2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pydsstools/src/core_heclib.pyx
@@ -0,0 +1,17 @@
+#cython: c_string_type=str, c_string_encoding=ascii
+#cython: embedsignature=True
+DssTimeArray_PythonArrayType = 'l'
+include "core_heclib_imports.pyx"
+include "constants.pyx"
+include "time_series.pyx"
+include "paired_data.pyx"
+include "open.pyx"
+include "utils.pyx"
+include "exceptions.pyx"
+include "hectime.pyx"
+include "_hec_compression.pyx"
+#include "_zcompression.pyx"
+include "_chelper.pyx"
+include "grid.pyx"
+include "catalog.pyx"
+
