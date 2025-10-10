07e8556c9aceb8ff7e2fa9451a2100671f21ec71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 16:04:22 +0000

    Add package essspectroscopy/25.10.0/essspectroscopy-25.10.0.tar.gz

---
+++ packages/essspectroscopy/essspectroscopy-25.10.0.tar.gz/essspectroscopy-25.10.0/src/ess/bifrost/data.py
@@ -0,0 +1,65 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
+
+"""Data for tests and documentation with BIFROST."""
+
+_version = "3"
+
+
+def _make_pooch():
+    import pooch
+
+    return pooch.create(
+        path=pooch.os_cache("ess/bifrost"),
+        env="ESS_DATA_DIR",
+        base_url="https://public.esss.dk/groups/scipp/ess/bifrost/{version}/",
+        version=_version,
+        registry={
+            "BIFROST_20240914T053723.h5": "md5:0f2fa5c9a851f8e3a4fa61defaa3752e",
+            "computed_energy_data_simulated.h5": "blake2b:3c398443cb85c8294d283c7212255bc695c2520f2332c2c99d041a0760b6bcbb9937e19bcd8a498daf306d279c88d2ea911c510c1ce3b3a7f1e6b7e54022a943",  # noqa: E501
+            "BIFROST-simulation-tof-lookup-table.h5": "blake2b:682021920a355f789da37b18029719fe20569d86db26cdaf5f3d916d2f76f9360907960ba86903be4cab489d39f1b6f9f265f3a4ab3f82c5e095afa4a2c456af",  # noqa: E501
+        },
+        retry_if_failed=3,
+    )
+
+
+_pooch = _make_pooch()
+
+
+def get_path(name: str) -> str:
+    """
+    Return the path to a data file bundled with ess.bifrost.
+
+    This function only works with example data and cannot handle
+    paths to custom files.
+    """
+    return _pooch.fetch(name)
+
+
+def simulated_elastic_incoherent_with_phonon() -> str:
+    """Simulated data for elastic incoherent scattering including a phonon."""
+    return get_path("BIFROST_20240914T053723.h5")
+
+
+def tof_lookup_table_simulation() -> str:
+    """Time-of-flight lookup table for the simulated BIFROST data.
+
+    This table was computed with `tof <https://github.com/scipp/tof>`_
+    using `Create a time-of-flight lookup table for BIFROST
+    <../../user-guide/bifrost/bifrost-make-tof-lookup-table.rst>`_
+    with ``NumberOfSimulatedNeutrons = 5_000_000``.
+    """
+    return get_path("BIFROST-simulation-tof-lookup-table.h5")
+
+
+def computed_energy_data_simulated() -> str:
+    """Energy and momentum transfer computed from the simulated BIFROST data."""
+    return get_path("computed_energy_data_simulated.h5")
+
+
+__all__ = [
+    "computed_energy_data_simulated",
+    "get_path",
+    "simulated_elastic_incoherent_with_phonon",
+    "tof_lookup_table_simulation",
+]
---
+++ packages/essspectroscopy/essspectroscopy-25.10.0.tar.gz/essspectroscopy-25.10.0/src/ess/bifrost/detector.py
@@ -0,0 +1,116 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
+
+"""Detector handling for BIFROST."""
+
+import scipp as sc
+import scippnexus as snx
+
+from ess.spectroscopy.types import (
+    BeamlineWithSpectrometerCoords,
+    CalibratedDetector,
+    DetectorPositionOffset,
+    NeXusComponent,
+    NeXusTransformation,
+    RunType,
+)
+
+from .types import ArcNumber
+
+
+def arc_number(
+    beamline: BeamlineWithSpectrometerCoords[RunType],
+) -> ArcNumber[RunType]:
+    """Calculate BIFROST arc index number from pixel final energy
+
+    The BIFROST analyzers are each set to diffract an
+    energy in the set (2.7, 3.2, 3.8, 4.4. 5.0) meV.
+    This energy is only valid for the central point of the center
+    tube of the associated detector triplet. All other pixels
+    will have a final energy slightly higher or lower.
+
+    This function assigns the closest arc number indexing the
+    ordered set above.
+
+    Parameters
+    ----------
+    beamline:
+        A data array with a 'final_energy' coordinate which is the
+        per-pixel (or event) final neutron energy.
+
+    Returns
+    -------
+    :
+        The arc index of the analyzer from which the neutron scattered
+    """
+    minimum = sc.scalar(2.7, unit='meV')
+    step = sc.scalar(0.575, unit='meV')
+    final_energy = beamline.coords['final_energy']
+    return ArcNumber[RunType](sc.round((final_energy - minimum) / step).to(dtype='int'))
+
+
+def get_calibrated_detector_bifrost(
+    detector: NeXusComponent[snx.NXdetector, RunType],
+    *,
+    transform: NeXusTransformation[snx.NXdetector, RunType],
+    offset: DetectorPositionOffset[RunType],
+) -> CalibratedDetector[RunType]:
+    """Extract the data array corresponding to a detector's signal field.
+
+    The returned data array includes coords and masks pertaining directly to the
+    signal values array, but not additional information about the detector.
+    The data array is reshaped to the logical detector shape.
+
+    This function is specific to BIFROST and differs from the generic
+    :func:`ess.reduce.nexus.workflow.get_calibrated_detector` in that it does not
+    fold the detectors into logical dimensions because the files already contain
+    the detectors in the correct shape.
+
+    Parameters
+    ----------
+    detector:
+        Loaded NeXus detector.
+    transform:
+        Transformation that determines the detector position.
+    offset:
+        Offset to add to the detector position.
+
+    Returns
+    -------
+    :
+        Detector data.
+    """
+
+    from ess.reduce.nexus.types import DetectorBankSizes
+    from ess.reduce.nexus.workflow import get_calibrated_detector
+
+    da = get_calibrated_detector(
+        detector=detector,
+        transform=transform,
+        offset=offset,
+        # The detectors are folded in the file, no need to do that here.
+        bank_sizes=DetectorBankSizes({}),
+    )
+    da = da.rename(dim_0='tube', dim_1='length')
+    return CalibratedDetector[RunType](da)
+
+
+def merge_triplets(
+    *triplets: sc.DataArray,
+) -> sc.DataArray:
+    """Merge BIFROST detector triplets into a single data array.
+
+    Parameters
+    ----------
+    triplets:
+        Data arrays to merge.
+
+    Returns
+    -------
+    :
+        Input data arrays stacked along the "triplet" dimension.
+    """
+    return sc.concat(triplets, dim="triplet")
+
+
+providers = (arc_number, get_calibrated_detector_bifrost)
