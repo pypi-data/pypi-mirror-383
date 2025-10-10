ded2586ceb8a52d637d14118733b6dfef1e97c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 16:04:25 +0000

    Add package ssb-befolkning-fagfunksjoner/0.0.12/ssb_befolkning_fagfunksjoner-0.0.12.tar.gz

---
+++ packages/ssb-befolkning-fagfunksjoner/ssb_befolkning_fagfunksjoner-0.0.12.tar.gz/ssb_befolkning_fagfunksjoner-0.0.12/src/ssb_befolkning_fagfunksjoner/__init__.py
@@ -0,0 +1,34 @@
+"""Befolkning Fagfunksjoner."""
+
+# Expose modules for convenient access
+from . import date_utils
+from . import demographics
+from . import generelle_sjekker
+from . import klass_utils
+from . import kommnr
+from . import versions
+from .generelle_sjekker import dublettsjekk
+from .generelle_sjekker import sml_rader
+from .kommnr import get_kommnr_changes
+from .kommnr import update_kommnr
+from .kommnr import validate_kommnr
+from .versions import get_next_version_number
+
+# Frequently used functions
+from .versions import write_versioned_pandas
+
+__all__ = [
+    "date_utils",
+    "demographics",
+    "dublettsjekk",
+    "generelle_sjekker",
+    "get_kommnr_changes",
+    "get_next_version_number",
+    "klass_utils",
+    "kommnr",
+    "sml_rader",
+    "update_kommnr",
+    "validate_kommnr",
+    "versions",
+    "write_versioned_pandas",
+]
