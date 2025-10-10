95e7862feb86696bd959a3dc4f810e469db6ea39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 15:05:54 +0000

    Add package boring-math/1.2.1/boring_math-1.2.1-py3-none-any.whl

---
+++ packages/boring-math/boring_math-1.2.1-py3-none-any.whl/boring_math/homepage/compatible_versions.py
@@ -0,0 +1,33 @@
+"""
+Known Boring Math compatible versions
+=====================================
+
+2025-10-TBD - 2.0.0
+-------------------
+
+========================= ===================================== =======
+Name                      PyPI Project                          Version
+========================= ===================================== =======
+Combinatorics             boring-math-combinatorics             2.0.0  
+Number Theory             boring-math-number-theory             2.0.0  
+Probability Distributions boring-math-probability-distributions 0.9.0  
+Pythagorean Triples       boring-math-pythagorean-triples       0.9.0  
+Recursive Functions       boring-math-recursive-functions       0.9.0  
+Special Functions         boring-math-special-functions         0.1.0  
+========================= ===================================== =======
+
+----
+
+2025-08-04 - 1.1.3
+------------------
+
+========================= ===================================== =======
+Name                      PyPI Project                          Version
+========================= ===================================== =======
+Integer Math              boring-math-integer-math              1.0.2  
+Probability Distributions boring-math-probability-distributions 0.8.0  
+Pythagorean Triples       boring-math-pythagorean-triples       0.8.2  
+Recursive Functions       boring-math-recursive-functions       0.8.1  
+========================= ===================================== =======
+
+"""
