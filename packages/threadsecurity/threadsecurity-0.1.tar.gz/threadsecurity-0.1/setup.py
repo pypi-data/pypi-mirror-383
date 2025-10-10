862d2c435db9d9f202b211f8e76c1d9ced53642d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 16:04:35 +0000

    Add package threadsecurity/0.1/threadsecurity-0.1.tar.gz

---
+++ packages/threadsecurity/threadsecurity-0.1.tar.gz/threadsecurity-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="threadsecurity",
+    version="0.1",
+    author="Vishal Kumar",
+    author_email="vishal@example.com",
+    description="A fun CTF-style package with a hidden flag",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
