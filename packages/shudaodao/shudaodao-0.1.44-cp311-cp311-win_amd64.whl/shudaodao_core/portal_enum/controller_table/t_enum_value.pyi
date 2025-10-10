9387608db0b0599ba284ac4431582688be0a7aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:05 +0000

    Add package shudaodao/0.1.44/shudaodao-0.1.44-cp311-cp311-win_amd64.whl

---
+++ packages/shudaodao/shudaodao-0.1.44-cp311-cp311-win_amd64.whl/shudaodao_core/portal_enum/controller_table/t_enum_value.pyi
@@ -0,0 +1,12 @@
+from .. import get_engine_name as get_engine_name, get_schema_name as get_schema_name
+from ...controller.router_config import RouterConfig as RouterConfig
+from ...controller.table_controller import AuthController as AuthController
+from ..entity_table.t_enum_value import (
+    TEnumValue as TEnumValue,
+    TEnumValueCreate as TEnumValueCreate,
+    TEnumValueResponse as TEnumValueResponse,
+    TEnumValueUpdate as TEnumValueUpdate,
+)
+from _typeshed import Incomplete
+
+TEnumValue_Router: Incomplete
