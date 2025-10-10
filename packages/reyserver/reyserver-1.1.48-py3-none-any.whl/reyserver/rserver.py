12b0939c6254b55ca431e2f53cf2e170dac93a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 16:04:27 +0000

    Add package reyserver/1.1.48/reyserver-1.1.48-py3-none-any.whl

---
+++ packages/reyserver/reyserver-1.1.48-py3-none-any.whl/reyserver/rserver.py
@@ -0,0 +1,217 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2025-10-05
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Server methods.
+"""
+
+
+from typing import Literal
+from collections.abc import Sequence, Callable, Coroutine
+from inspect import iscoroutinefunction
+from uvicorn import run as uvicorn_run
+from starlette.middleware.base import _StreamingResponse
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from reydb import DatabaseAsync
+from reykit.rbase import CallableT, CoroutineFunctionSimple, Singleton, throw
+
+from .rbase import ServerBase, ServerConfig, Bind
+
+
+__all__ = (
+    'Server',
+)
+
+
+class Server(ServerBase, Singleton):
+    """
+    Server type, singleton mode.
+    Based on `fastapi` and `uvicorn` package.
+    Can view document api '/docs', '/redoc', '/openapi.json'.
+    """
+
+
+    def __init__(
+        self,
+        db: DatabaseAsync,
+        public: str | None = None,
+        depend: CoroutineFunctionSimple | Sequence[CoroutineFunctionSimple] | None = None,
+        before: CoroutineFunctionSimple | Sequence[CoroutineFunctionSimple] | None = None,
+        after: CoroutineFunctionSimple | Sequence[CoroutineFunctionSimple] | None = None,
+        ssl_cert: str | None = None,
+        ssl_key: str | None = None,
+        debug: bool = False
+    ) -> None:
+        """
+        Build instance attributes.
+
+        Parameters
+        ----------
+        db : Asynchronous database, must include database engines with APIs.
+        public : Public directory.
+        depend : Global api dependencies.
+        before : Execute before server start.
+        after : Execute after server end.
+        ssl_cert : SSL certificate file path.
+        ssl_key : SSL key file path.
+        debug : Whether use development mode debug server.
+        """
+
+        # Parameter.
+        if type(ssl_cert) != type(ssl_key):
+            throw(AssertionError, ssl_cert, ssl_key)
+        lifespan = Bind.create_lifespan(before, after)
+        if depend is None:
+            depend = ()
+        elif iscoroutinefunction(depend):
+            depend = (depend,)
+        depend = [
+            Bind.Depend(task)
+            for task in depend
+        ]
+
+        # Build.
+        ServerConfig.server = self
+        self.db = db
+        self.ssl_cert = ssl_cert
+        self.ssl_key = ssl_key
+
+        ## App.
+        self.app = FastAPI(
+            dependencies=depend,
+            lifespan=lifespan,
+            debug=debug
+        )
+
+        if public is not None:
+            subapp = StaticFiles(directory=public, html=True)
+            self.app.mount('/', subapp)
+        self.wrap_middleware = self.app.middleware('http')
+        'Decorator, add middleware to APP.'
+
+        # Middleware
+        self.app.add_middleware(GZipMiddleware)
+        if not debug:
+            self.app.add_middleware(HTTPSRedirectMiddleware)
+        self.__add_default_middleware()
+
+        # API.
+        self.api_file_dir: str
+        'File API store directory path.'
+
+
+    def __add_default_middleware(self) -> None:
+        """
+        Add default handle middleware.
+        """
+
+        # Add.
+        @self.wrap_middleware
+        async def foo(
+            request: Request,
+            call_next: Callable[[Request], Coroutine[None, None, _StreamingResponse]]
+        ) -> _StreamingResponse:
+            """
+            Default handle middleware.
+
+            Parameters
+            ----------
+            Reqeust : Request instance.
+            call_next : Next middleware.
+            """
+
+            # Before.
+            ...
+
+            # Next.
+            response = await call_next(request)
+
+            # After.
+            if request.method == 'POST':
+                response.status_code = 201
+
+            return response
+
+
+    def run(self) -> None:
+        """
+        Run server.
+        """
+
+        # Run.
+        uvicorn_run(
+            self.app,
+            ssl_certfile=self.ssl_cert,
+            ssl_keyfile=self.ssl_key
+        )
+
+
+    def set_doc(
+        self,
+        version: str | None = None,
+        title: str | None = None,
+        summary: str | None = None,
+        desc: str | None = None,
+        contact: dict[Literal['name', 'email', 'url'], str] | None = None
+    ) -> None:
+        """
+        Set server document.
+
+        Parameters
+        ----------
+        version : Server version.
+        title : Server title.
+        summary : Server summary.
+        desc : Server description.
+        contact : Server contact information.
+        """
+
+        # Parameter.
+        set_dict = {
+            'version': version,
+            'title': title,
+            'summary': summary,
+            'description': desc,
+            'contact': contact
+        }
+
+        # Set.
+        for key, value in set_dict.items():
+            if value is not None:
+                setattr(self.app, key, value)
+
+
+    def add_api_auth(self):
+        """
+        Add Authentication API.
+        Note: must include database engine of `auth` name.
+        """
+
+        ...
+
+
+    def add_api_file(self, file_dir: str = 'file') -> None:
+        """
+        Add file API.
+        Note: must include database engine of `file` name.
+
+        Parameters
+        ----------
+        file_dir : File API store directory path.
+        prefix : File API path prefix.
+        """
+
+        from .rfile import file_router, build_file_db
+
+        # Build database.
+        build_file_db()
+
+        # Add.
+        self.api_file_dir = file_dir
+        self.app.include_router(file_router, prefix='/files', tags=['file'])
