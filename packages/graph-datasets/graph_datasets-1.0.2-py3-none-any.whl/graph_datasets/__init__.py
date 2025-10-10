9ca5d316a238436b50d3195e6f60721f1855a793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-10 17:04:05 +0000

    Add package graph-datasets/1.0.2/graph_datasets-1.0.2-py3-none-any.whl

---
+++ packages/graph-datasets/graph_datasets-1.0.2-py3-none-any.whl/graph_datasets/__init__.py
@@ -0,0 +1,7 @@
+"""Graph Datasets
+"""
+
+__version__ = "1.0.2"
+
+from .load_data import load_data
+from .utils import *
---
+++ packages/graph-datasets/graph_datasets-1.0.2-py3-none-any.whl/graph_datasets/utils/__init__.py
@@ -0,0 +1,7 @@
+"""Utils
+"""
+
+# pylint:disable=invalid-name
+from .common import *
+from .logging import get_logger
+from .statistics import edge_homo, node_homo, statistics
