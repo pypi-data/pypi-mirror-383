8663e16c865e9ffda36ca980f17a437d065bc605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 07:05:20 +0000

    Add package zonoopt/2.0.1/zonoopt-2.0.1-cp311-cp311-win_amd64.whl

---
+++ packages/zonoopt/zonoopt-2.0.1-cp311-cp311-win_amd64.whl/zonoopt/__init__.pyi
@@ -0,0 +1,45 @@
+from ._core import *
+from ._core import __doc__ as __doc__
+from .zono_plot import get_vertices as get_vertices, plot as plot
+
+__all__ = ['Box', 'ConZono', 'EQUAL', 'EmptySet', 'GREATER', 'GREATER_OR_EQUAL', 'HybZono', 'IneqTerm', 'IneqType', 'Inequality', 'Interval', 'LESS', 'LESS_OR_EQUAL', 'OptSettings', 'OptSolution', 'Point', 'Zono', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'affine_map', 'cartesian_product', 'constrain', 'halfspace_intersection', 'intersection', 'intersection_over_dims', 'interval_2_zono', 'make_regular_zono_2D', 'minkowski_sum', 'pontry_diff', 'project_onto_dims', 'set_diff', 'union_of_many', 'vrep_2_conzono', 'vrep_2_hybzono', 'zono_union_2_hybzono', 'plot', 'get_vertices']
+
+# Names in __all__ with no definition:
+#   Box
+#   ConZono
+#   EQUAL
+#   EmptySet
+#   GREATER
+#   GREATER_OR_EQUAL
+#   HybZono
+#   IneqTerm
+#   IneqType
+#   Inequality
+#   Interval
+#   LESS
+#   LESS_OR_EQUAL
+#   OptSettings
+#   OptSolution
+#   Point
+#   Zono
+#   __file__
+#   __loader__
+#   __name__
+#   __package__
+#   __spec__
+#   affine_map
+#   cartesian_product
+#   constrain
+#   halfspace_intersection
+#   intersection
+#   intersection_over_dims
+#   interval_2_zono
+#   make_regular_zono_2D
+#   minkowski_sum
+#   pontry_diff
+#   project_onto_dims
+#   set_diff
+#   union_of_many
+#   vrep_2_conzono
+#   vrep_2_hybzono
+#   zono_union_2_hybzono
