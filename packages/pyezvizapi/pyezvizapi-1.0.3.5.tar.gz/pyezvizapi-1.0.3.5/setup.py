88db1cb025eccbc48792cb84e3d1695dfa247aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:05:27 +0000

    Add package pyezvizapi/1.0.3.5/pyezvizapi-1.0.3.5.tar.gz

---
+++ packages/pyezvizapi/pyezvizapi-1.0.3.5.tar.gz/pyezvizapi-1.0.3.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyezvizapi',
+    version="1.0.3.5",
+    license='Apache Software License 2.0',
+    author='Renier Moorcroft',
+    author_email='RenierM26@users.github.com',
+    description='Pilot your Ezviz cameras',
+    long_description="Pilot your Ezviz cameras with this module. Please view readme on github",
+    url='https://github.com/RenierM26/pyEzvizApi/',
+    packages=setuptools.find_packages(),
+    setup_requires=[
+        'requests',
+        'setuptools'
+    ],
+    install_requires=[
+        'requests',
+        'pandas',
+        'paho-mqtt',
+        'xmltodict',
+        'pycryptodome'
+    ],
+    entry_points={
+    'console_scripts': ['pyezvizapi = pyezvizapi.__main__:main']
+    },
+    python_requires = '>=3.11'
+)
