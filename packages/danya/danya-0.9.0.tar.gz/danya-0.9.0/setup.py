55b0b345f74dff7557835275fc74642606caf36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 07:05:29 +0000

    Add package danya/0.9.0/danya-0.9.0.tar.gz

---
+++ packages/danya/danya-0.9.0.tar.gz/danya-0.9.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='danya',     
+    version='0.9.0',         
+    packages=find_packages(),
+    description='Катаем всё!', 
+    python_requires='>=3.6',
+    include_package_data=True
+)
