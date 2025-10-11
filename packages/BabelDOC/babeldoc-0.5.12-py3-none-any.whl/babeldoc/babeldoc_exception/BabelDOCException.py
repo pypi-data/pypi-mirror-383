82fd993a4a7261c6990cfadd25f61e3ca8e8923f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:04:55 +0000

    Add package BabelDOC/0.5.12/babeldoc-0.5.12-py3-none-any.whl

---
+++ packages/BabelDOC/babeldoc-0.5.12-py3-none-any.whl/babeldoc/babeldoc_exception/BabelDOCException.py
@@ -0,0 +1,13 @@
+class ScannedPDFError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class ExtractTextError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class InputFileGeneratedByBabelDOCError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
