5a7c983dfefcaa480c15d0de71ec16fc9503e52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 04:03:59 +0000

    Add package abstract-hugpy/0.0.0.120/abstract_hugpy-0.0.0.120.tar.gz

---
+++ packages/abstract-hugpy/abstract_hugpy-0.0.0.120.tar.gz/abstract_hugpy-0.0.0.120/src/abstract_hugpy/hugpy_console/hugging_face_models/__init__.py
@@ -0,0 +1,6 @@
+from .bigbird_module import *
+from .config import *
+from .falcon_flan_t5_summarizers import *
+from .generation import *
+from .keybert_model import *
+from .whisper_model import *
---
+++ packages/abstract-hugpy/abstract_hugpy-0.0.0.120.tar.gz/abstract_hugpy-0.0.0.120/src/abstract_hugpy/hugpy_console/video_console/imports.py
@@ -0,0 +1,7 @@
+from ..imports import *
+from ..hugging_face_models.google_flan import *
+from ..hugging_face_models.keybert_model import *
+from ..hugging_face_models.summarizer_model import *
+from ..hugging_face_models.whisper_model import *
+from ..hugging_face_models.generation import *
+from ..hugging_face_models.bigbird_module import *
