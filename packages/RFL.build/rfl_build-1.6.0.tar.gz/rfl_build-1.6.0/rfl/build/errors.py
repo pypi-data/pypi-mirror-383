2560bd2e151dc86bf3340fa204c61e8918bab0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 04:04:14 +0000

    Add package RFL.build/1.6.0/rfl_build-1.6.0.tar.gz

---
+++ packages/RFL.build/rfl_build-1.6.0.tar.gz/rfl_build-1.6.0/rfl/build/errors.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2024 Rackslab
+#
+# This file is part of RFL.
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+from rfl.core.errors import RFLError
+
+
+class BuildError(RFLError):
+    pass
