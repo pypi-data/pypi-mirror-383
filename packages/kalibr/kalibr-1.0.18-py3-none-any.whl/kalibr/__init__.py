0f016126e1e7364b4655c75674021a57ae1d9299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 04:04:05 +0000

    Add package kalibr/1.0.18/kalibr-1.0.18-py3-none-any.whl

---
+++ packages/kalibr/kalibr-1.0.18-py3-none-any.whl/kalibr/__init__.py
@@ -0,0 +1,8 @@
+"""Kalibr SDK - Multi-Model AI Integration Framework"""
+
+from kalibr.kalibr import Kalibr
+# KalibrApp will be imported once it's properly implemented
+# from kalibr.kalibr_app import KalibrApp
+
+__version__ = "1.0.18"
+__all__ = ["Kalibr"]  # "KalibrApp" will be added once implemented
