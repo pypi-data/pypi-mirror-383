229f94f32f9284a409b6e5ef93df03785af38207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:25 +0000

    Add package euno/0.3.0/euno-0.3.0-py3-none-any.whl

---
+++ packages/euno/euno-0.3.0-py3-none-any.whl/euno/__init__.py
@@ -0,0 +1,15 @@
+"""
+Euno SDK - A Python library and CLI tool for interacting with Euno instances.
+
+This package provides both programmatic access to Euno functionality
+and a command-line interface for common operations.
+"""
+
+from .version import __version__
+
+__all__ = ["__version__", "hello_world", "get_version", "config", "api_client", "list_resources"]
+
+from .core import hello_world, get_version
+from .config import config
+from .api import api_client
+from .resources import list_resources
