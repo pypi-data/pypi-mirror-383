38e139ad8876e5276da28c0f3f58518070cbf375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 07:05:12 +0000

    Add package rusty-manga-image-translator/0.12.1/rusty_manga_image_translator-0.12.1-cp39-abi3-win_amd64.whl

---
+++ packages/rusty-manga-image-translator/rusty_manga_image_translator-0.12.1-cp39-abi3-win_amd64.whl/rusty_manga_image_translator/__init__.py
@@ -0,0 +1,5 @@
+from .rusty_manga_image_translator import *
+
+__doc__ = rusty_manga_image_translator.__doc__
+if hasattr(rusty_manga_image_translator, "__all__"):
+    __all__ = rusty_manga_image_translator.__all__
