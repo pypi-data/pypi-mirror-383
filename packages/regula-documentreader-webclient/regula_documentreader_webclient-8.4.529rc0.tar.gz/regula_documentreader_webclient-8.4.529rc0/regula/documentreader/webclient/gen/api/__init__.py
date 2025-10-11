a6f354ee1edbcedaaa8937e0d41c8276f98ad30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:37 +0000

    Add package regula-documentreader-webclient/8.4.529rc0/regula_documentreader_webclient-8.4.529rc0.tar.gz

---
+++ packages/regula-documentreader-webclient/regula_documentreader_webclient-8.4.529rc0.tar.gz/regula_documentreader_webclient-8.4.529rc0/regula/documentreader/webclient/gen/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from regula.documentreader.webclient.gen.api.healthcheck_api import HealthcheckApi
+from regula.documentreader.webclient.gen.api.process_api import ProcessApi
+from regula.documentreader.webclient.gen.api.transaction_api import TransactionApi
+
