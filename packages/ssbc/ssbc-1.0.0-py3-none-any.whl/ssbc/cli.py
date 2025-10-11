a4610c832dec09eac456070076024ab0794bd3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 06:04:51 +0000

    Add package ssbc/1.0.0/ssbc-1.0.0-py3-none-any.whl

---
+++ packages/ssbc/ssbc-1.0.0-py3-none-any.whl/ssbc/cli.py
@@ -0,0 +1,21 @@
+"""Console script for ssbc."""
+
+import typer
+from rich.console import Console
+
+from ssbc import utils
+
+app = typer.Typer()
+console = Console()
+
+
+@app.command()
+def main():
+    """Console script for ssbc."""
+    console.print("Replace this message by putting your code into ssbc.cli.main")
+    console.print("See Typer documentation at https://typer.tiangolo.com/")
+    utils.do_something_useful()
+
+
+if __name__ == "__main__":
+    app()
