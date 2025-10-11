ea3863f545df15da4d5a1601a71aea9d2ee9facd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 05:05:21 +0000

    Add package longsora/0.1.4/longsora-0.1.4.tar.gz

---
+++ packages/longsora/longsora-0.1.4.tar.gz/longsora-0.1.4/src/longsora/__init__.py
@@ -0,0 +1,3 @@
+from longsora._client import AsyncOpenAI, OpenAI
+
+__all__ = ["AsyncOpenAI", "OpenAI"]
---
+++ packages/longsora/longsora-0.1.4.tar.gz/longsora-0.1.4/src/longsora/openai/types/shared/reasoning_effort.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+
+from typing_extensions import Literal, TypeAlias
+
+__all__ = ["ReasoningEffort"]
+
+ReasoningEffort: TypeAlias = Optional[Literal["minimal", "low", "medium", "high"]]
