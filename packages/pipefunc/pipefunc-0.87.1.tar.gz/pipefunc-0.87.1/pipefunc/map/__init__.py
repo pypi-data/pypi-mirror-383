0d116cce3d1e7ab227c0d41e16344977a2f661d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 05:04:53 +0000

    Add package pipefunc/0.87.1/pipefunc-0.87.1.tar.gz

---
+++ packages/pipefunc/pipefunc-0.87.1.tar.gz/pipefunc-0.87.1/pipefunc/map/__init__.py
@@ -0,0 +1,37 @@
+"""pipefunc.map: Modules that handle MapSpecs and its runs."""
+
+from contextlib import suppress as _suppress
+
+from ._load import load_all_outputs, load_dataframe, load_outputs, load_xarray_dataset
+from ._mapspec import MapSpec
+from ._run import run_map, run_map_async
+from ._run_info import RunInfo
+from ._storage_array._base import StorageBase, register_storage, storage_registry
+from ._storage_array._dict import DictArray, SharedMemoryDictArray
+from ._storage_array._file import FileArray
+
+__all__ = [
+    "DictArray",
+    "FileArray",
+    "MapSpec",
+    "RunInfo",
+    "SharedMemoryDictArray",
+    "StorageBase",
+    "load_all_outputs",
+    "load_dataframe",
+    "load_outputs",
+    "load_xarray_dataset",
+    "register_storage",
+    "run_map",
+    "run_map_async",
+    "storage_registry",
+]
+
+with _suppress(ImportError, AttributeError):
+    # ImportError: zarr not installed
+    # AttributeError: zarr v3 is not compatible with pipefunc
+    #   Blocked by: https://github.com/zarr-developers/zarr-python/issues/2617
+    #   TODO: Remove this block once zarr v3 is compatible with pipefunc
+    from ._storage_array._zarr import ZarrFileArray, ZarrMemoryArray, ZarrSharedMemoryArray
+
+    __all__ += ["ZarrFileArray", "ZarrMemoryArray", "ZarrSharedMemoryArray"]
