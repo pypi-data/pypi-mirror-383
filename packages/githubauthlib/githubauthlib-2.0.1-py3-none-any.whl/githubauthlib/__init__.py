6aac737a00bd1fb8147336336e47cde6e3645eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 07:04:59 +0000

    Add package githubauthlib/2.0.1/githubauthlib-2.0.1-py3-none-any.whl

---
+++ packages/githubauthlib/githubauthlib-2.0.1-py3-none-any.whl/githubauthlib/__init__.py
@@ -0,0 +1,28 @@
+"""
+GitHub authentication library for retrieving tokens from system keychains.
+
+This library provides a unified interface for retrieving GitHub tokens
+from various system-specific secure storage solutions.
+"""
+
+from .github_auth import (
+    CredentialHelperError,
+    GitHubAuthError,
+    InvalidTokenError,
+    PlatformNotSupportedError,
+    TokenNotFoundError,
+    get_github_token,
+)
+
+__version__ = "2.0.1"
+__author__ = "garotm"
+__license__ = "MIT"
+
+__all__ = [
+    "get_github_token",
+    "GitHubAuthError",
+    "TokenNotFoundError",
+    "InvalidTokenError",
+    "PlatformNotSupportedError",
+    "CredentialHelperError",
+]
