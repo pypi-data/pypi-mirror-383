a52b5ecddd5f6be85f00d3c5ecf73977ac1f5c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:42 +0000

    Add package flowtask/5.9.18/flowtask-5.9.18-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/flowtask/flowtask-5.9.18-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/flowtask/components/Workday/types/__init__.py
@@ -0,0 +1,19 @@
+from .time_blocks import TimeBlockType
+from .locations import LocationType
+from .workers import WorkerType
+from .time_requests import TimeRequestType
+from .organizations import OrganizationType
+from .cost_centers import CostCenterType
+from .applicants import ApplicantType
+from .candidates import CandidateType
+
+__all__ = [
+    "TimeBlockType",
+    "LocationType",
+    "WorkerType", 
+    "TimeRequestType",
+    "OrganizationType",
+    "CostCenterType",
+    "ApplicantType",
+    "CandidateType"
+]
