2cb92f1109f1c38d24c5e2846bdcfd5993327f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 06:04:24 +0000

    Add package agentex-sdk/0.4.25/agentex_sdk-0.4.25-py3-none-any.whl

---
+++ packages/agentex-sdk/agentex_sdk-0.4.25-py3-none-any.whl/agentex/lib/utils/iterables.py
@@ -0,0 +1,14 @@
+from typing import Any
+from collections.abc import AsyncGenerator
+
+
+async def async_enumerate(
+    aiterable: AsyncGenerator, start: int = 0
+) -> AsyncGenerator[tuple[int, Any], None]:
+    """
+    Enumerate an async generator.
+    """
+    i = start
+    async for item in aiterable:
+        yield i, item
+        i += 1
