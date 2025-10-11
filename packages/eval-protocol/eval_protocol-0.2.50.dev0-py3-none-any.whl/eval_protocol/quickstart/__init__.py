5aecb19797af3d7735bc17b2e03f2ff382217e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:05:14 +0000

    Add package eval-protocol/0.2.50.dev0/eval_protocol-0.2.50.dev0-py3-none-any.whl

---
+++ packages/eval-protocol/eval_protocol-0.2.50.dev0-py3-none-any.whl/eval_protocol/quickstart/__init__.py
@@ -0,0 +1,4 @@
+from .llm_judge import aha_judge
+from .utils import multi_turn_assistant_to_ground_truth, assistant_to_ground_truth
+
+__all__ = ["aha_judge", "multi_turn_assistant_to_ground_truth", "assistant_to_ground_truth"]
