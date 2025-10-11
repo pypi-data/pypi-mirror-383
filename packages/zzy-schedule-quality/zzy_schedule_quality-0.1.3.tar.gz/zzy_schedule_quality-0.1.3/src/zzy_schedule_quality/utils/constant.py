59605e5925a1764c51825513a4b047d63ce94ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:05:25 +0000

    Add package zzy-schedule-quality/0.1.3/zzy_schedule_quality-0.1.3.tar.gz

---
+++ packages/zzy-schedule-quality/zzy_schedule_quality-0.1.3.tar.gz/zzy_schedule_quality-0.1.3/src/zzy_schedule_quality/utils/constant.py
@@ -0,0 +1,8 @@
+DOMAIN = 'https://cm.aizzyun.com'
+
+QUALITY_RECTIFICATION = '/bsp/user/ugs/bps/api/quality/qualityRectificationRecord'
+
+#质量整改相关
+QUALITY_RECTIFICATION_LIST_URL = DOMAIN + QUALITY_RECTIFICATION + '/queryQualityRectificationRecord'
+QUALITY_RECTIFICATION_DETAIL_URL = DOMAIN + QUALITY_RECTIFICATION + '/queryRecordById/'
+QUALITY_RECTIFICATION_ADD = DOMAIN + QUALITY_RECTIFICATION + '/saveQualityRectificationRecordList'
