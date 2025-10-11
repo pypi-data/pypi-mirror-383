b0c72e8aa14467b079fd665f78c910f83a28311c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 06:04:46 +0000

    Add package python-base-toolkit/0.0.1/python_base_toolkit-0.0.1-py3-none-any.whl

---
+++ packages/python-base-toolkit/python_base_toolkit-0.0.1-py3-none-any.whl/python_base_toolkit/utils/venv_details.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+
+def get_venv_details() -> dict:
+    return {
+        'python_executable_path': sys.executable,
+        'python_version': sys.version,
+        'venv_path': os.getenv('VIRTUAL_ENV'),
+    }
