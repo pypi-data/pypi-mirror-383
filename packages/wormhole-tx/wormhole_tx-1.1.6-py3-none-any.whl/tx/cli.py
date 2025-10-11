e4adec494f3e000daa77a5ce9cf1d5fd416f0438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:40 +0000

    Add package wormhole-tx/1.1.6/wormhole_tx-1.1.6-py3-none-any.whl

---
+++ packages/wormhole-tx/wormhole_tx-1.1.6-py3-none-any.whl/tx/cli.py
@@ -0,0 +1,207 @@
+# pylint: disable=consider-using-with
+
+"""
+A wrapper around magic-wormhole send to more easily send a file
+"""
+
+import argparse
+import os
+import secrets
+import subprocess
+import sys
+import warnings
+from dataclasses import dataclass
+from pathlib import Path
+from typing import IO, Optional
+
+from colorama import just_fix_windows_console
+
+KEY_LENGTH = 32
+
+just_fix_windows_console()
+
+
+def gen_code(key_length: int) -> str:
+    """Returns a random number string of the given length."""
+    # only numbers
+    return "".join([str(secrets.randbelow(10)) for _ in range(key_length)])
+
+
+class ArgumentParser(argparse.ArgumentParser):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def print_help(self, file: Optional[IO[str]] = None) -> None:  # type: ignore
+        """Prints the help message."""
+        msg = self.format_help()
+        print(msg)
+        stdout = subprocess.run(
+            ["wormhole", "--help", "--no-qr"],
+            capture_output=True,
+            text=True,
+            check=False,
+        ).stdout
+
+        print(
+            "\n\n"
+            + "###########################################################\n"
+            + '## Any unknown options will be passed to "wormhole send" ##\n'
+            + "###########################################################\n\n"
+            + stdout
+        )
+
+
+@dataclass
+class Args:
+    file_or_dir: Path
+    code_length: int | None
+    multi: bool
+    code: str | None
+
+    def __post_init__(self):
+        assert isinstance(
+            self.file_or_dir, Path
+        ), f"Expected Path, got {type(self.file_or_dir)}"
+        assert self.code_length is None or isinstance(
+            self.code_length, int
+        ), f"Expected int, got {type(self.code_length)}"
+        assert isinstance(self.multi, bool), f"Expected bool, got {type(self.multi)}"
+        assert self.code is None or isinstance(
+            self.code, str
+        ), f"Expected str, got {type(self.code)}"
+
+
+def parse_args() -> tuple[Args, list[str]]:
+    parser = ArgumentParser(
+        description="Sends a file using magic-wormhole",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("file_or_dir", help="The file or directory to send")
+    parser.add_argument(
+        "--code-length",
+        type=int,
+        default=None,
+        help="The code length to generate",
+    )
+    parser.add_argument("--multi", action="store_true", help="Send multiple files")
+    parser.add_argument("--code", type=str, default=None, help="The code to use")
+    tmp, unknown = parser.parse_known_args()
+    args = Args(
+        file_or_dir=Path(tmp.file_or_dir),
+        code_length=tmp.code_length,
+        multi=tmp.multi,
+        code=tmp.code,
+    )
+    return args, unknown
+
+
+def gen_wormhole_receive_command(code: str) -> str:
+    return f"wormhole recieve --accept-file {code}"
+
+
+def run(
+    file_or_dir: str,
+    cwd: Optional[str] = None,
+    code: Optional[str] = None,
+    code_length: Optional[int] = None,
+    wormhole_args: Optional[list[str]] = None,
+    multi_file: bool = False,
+) -> int:
+    cwd = cwd or os.getcwd()
+    code = code or None
+    wormhole_args = wormhole_args or []
+    if not os.path.exists(os.path.join(cwd or "", file_or_dir)):
+        print(f"File or directory {file_or_dir} does not exist.")
+        return 1
+
+    if code is not None and code_length is not None:
+        raise ValueError(
+            "Cannot specify both --code and --code-length. Either specify --code or --code-length."
+        )
+    if code_length is None:
+        code_length = KEY_LENGTH
+
+    code = code or gen_code(code_length)
+    recieve_cmd = gen_wormhole_receive_command(code)
+    cmd_list = [
+        "wormhole",
+        "send",
+        "--code",
+        code,
+        "--no-qr",
+        file_or_dir,
+    ] + wormhole_args
+
+    if sys.platform == "win32":
+        # On windows, we need to use chcp 65501 to support UTF-8 or else
+        # we get an error when sending files with non-ascii characters
+        cmd_list = ["chcp", "65001", "&&"] + cmd_list
+
+    cmd = subprocess.list2cmdline(cmd_list)
+
+    while True:
+
+        print(f'\nSending "{file_or_dir}"...')
+        print("On the other computer, run the following command:\n")
+        print("    " + recieve_cmd)
+        print("")
+
+        proc = subprocess.Popen(
+            cmd,
+            cwd=cwd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=True,
+        )
+        assert proc.stdout is not None
+        assert proc.stderr is None
+
+        # stream out stdout line by line
+        # for line in iter(proc.stdout.readline, ""):
+        for raw_line in proc.stdout:
+            try:
+                line = raw_line.decode("utf-8", errors="replace")
+            except UnicodeDecodeError:
+                print(f"Error decoding line: {raw_line}")  # type: ignore
+                continue
+            if "Sending" in line and "kB file" in line:
+                continue
+            if line == "\n":
+                continue
+            if "Wormhole code is" in line:
+                continue
+            if "On the other computer" in line:
+                continue
+            if "wormhole receive" in line:
+                continue
+            print(line, end="")
+
+        proc.stdout.close()
+        rtn_code = proc.wait()
+        if multi_file:
+            continue
+        return rtn_code
+
+
+def main() -> int:
+    try:
+        args, unknown = parse_args()
+        print(args, unknown)
+        if "--appid" in unknown:
+            warnings.warn("The --appid option is not supported. Use --code instead.")
+            return 1
+        return run(
+            file_or_dir=args.file_or_dir.as_posix(),
+            cwd=os.getcwd(),
+            code=args.code,
+            code_length=args.code_length,
+            wormhole_args=unknown,
+            multi_file=args.multi,
+        )
+    except KeyboardInterrupt:
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
