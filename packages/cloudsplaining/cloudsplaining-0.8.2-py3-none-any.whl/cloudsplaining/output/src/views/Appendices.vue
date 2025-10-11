29b0106be696336cb9a657547c34d7b6ffd78aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 07:04:32 +0000

    Add package cloudsplaining/0.8.2/cloudsplaining-0.8.2-py3-none-any.whl

---
+++ packages/cloudsplaining/cloudsplaining-0.8.2-py3-none-any.whl/cloudsplaining/output/src/views/Appendices.vue
@@ -0,0 +1,21 @@
+<template>
+    <b-tab>
+        <div v-if="appendices_content === 'default'">
+            <Glossary />
+        </div>
+        <div v-else v-html="appendices_content"></div>
+    </b-tab>
+</template>
+
+<script>
+import Glossary from "../components/Glossary";
+export default {
+    components: { Glossary },
+    data() {
+        return {
+            // eslint-disable-next-line no-undef
+            appendices_content: appendices_content,
+        };
+    },
+};
+</script>
