774c0d2fe2bb27ab012ee2161f506c96eebeebd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:22 +0000

    Add package safe-earth/0.1.4/safe_earth-0.1.4-py2.py3-none-any.whl

---
+++ packages/safe-earth/safe_earth-0.1.4-py2.py3-none-any.whl/safe_earth/utils/stats.py
@@ -0,0 +1,20 @@
+from sklearn.neighbors import LocalOutlierFactor as LOF
+import numpy as np
+import pandas as pd
+
+def filter_outliers(df: pd.DataFrame, error_metric: str = 'rmse_weighted_l2') -> pd.DataFrame:
+    for variable in df.variable.unique():
+        for model in df.model.unique():
+            for lt in df.lead_time.unique():
+                for attribute in df.attribute.unique():
+                    mask = (
+                        (df['attribute'] == attribute) &
+                        (df['variable'] == variable) &
+                        (df['model'] == model) &
+                        (df['lead_time'] == lt)
+                    )
+                    samples = df.loc[mask, error_metric].tolist()
+                    estimator = LOF()
+                    outliers = (estimator.fit_predict(np.array(samples).reshape(-1, 1)) == -1)
+                    df.drop(df.loc[mask][outliers].index, inplace=True)
+    return df
