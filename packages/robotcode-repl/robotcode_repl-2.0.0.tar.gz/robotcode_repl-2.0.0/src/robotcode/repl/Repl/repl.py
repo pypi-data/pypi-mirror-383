609d8024cc87cca204c44a0981ff79aed184a981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 03:12:18 +0000

    Add package robotcode-repl/2.0.0/robotcode_repl-2.0.0.tar.gz

---
+++ packages/robotcode-repl/robotcode_repl-2.0.0.tar.gz/robotcode_repl-2.0.0/src/robotcode/repl/Repl/repl.py
@@ -0,0 +1,14 @@
+import sys
+
+from ..__version__ import __version__
+
+
+class Repl:
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+    ROBOT_LIBRARY_VERSION = __version__
+
+    def repl(self) -> None:
+        pass
+
+    def exit(self, exit_code: int = 0) -> None:
+        sys.exit(exit_code)
