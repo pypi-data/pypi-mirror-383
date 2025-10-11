afbe6b6cb60b53be0de7f39a629f775a4509eb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 07:05:13 +0000

    Add package ekogram/2.0/ekogram-2.0.tar.gz

---
+++ packages/ekogram/ekogram-2.0.tar.gz/ekogram-2.0/ekogram/__init__.py
@@ -0,0 +1,2 @@
+from .ekogram import Bot, Markup, InputFile, InputMediaAudio, InputMediaDocument, InputMediaVideo, InputMediaAnimation, InputMediaPhoto, MessageEntity, InlineQuery, FreeGpt, Uncensored_Ai, FreeImg, Deef, ChatGPT
+__all__ = ['Bot', 'Markup', 'InputFile', 'InputMediaAnimation', 'InputMediaPhoto', 'InputMediaVideo', 'InputMediaAudio', 'InputMediaDocument', 'MessageEntity', 'InlineQuery', 'FreeGpt', 'Uncensored_Ai', 'FreeImg', 'Deef', 'ChatGPT']
