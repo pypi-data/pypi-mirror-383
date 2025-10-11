59fb885cfb813638b1b2b805eda91eb4fa41e018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 03:12:38 +0000

    Add package 1d-qt-ideal-solver/0.0.7/1d_qt_ideal_solver-0.0.7.tar.gz

---
+++ packages/1d-qt-ideal-solver/1d_qt_ideal_solver-0.0.7.tar.gz/1d_qt_ideal_solver-0.0.7/src/qt1d_ideal/__init__.py
@@ -0,0 +1,11 @@
+"""1D Quantum Tunneling Solver - Idealized Version"""
+
+__version__ = "0.0.7"
+__author__ = "Siti N. Kaban, Sandy H. S. Herho, Sonny Prayogo, Iwan P. Anwar, Nurjanna J. Trilaksono"
+
+from .core.solver import QuantumTunneling1D
+from .core.initial_conditions import GaussianWavePacket
+from .io.config_manager import ConfigManager
+from .io.data_handler import DataHandler
+
+__all__ = ["QuantumTunneling1D", "GaussianWavePacket", "ConfigManager", "DataHandler"]
