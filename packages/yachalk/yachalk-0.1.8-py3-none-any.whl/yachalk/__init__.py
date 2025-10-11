67d1e5c427ed0c2eb2696c6b4623467cdf2490b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 05:04:50 +0000

    Add package yachalk/0.1.8/yachalk-0.1.8-py3-none-any.whl

---
+++ packages/yachalk/yachalk-0.1.8-py3-none-any.whl/yachalk/__init__.py
@@ -0,0 +1,12 @@
+from .chalk_factory import ChalkFactory
+from .chalk_instance import chalk, create_chalk
+from .types import ColorMode
+
+VERSION = "0.1.8"
+
+__all__ = [
+    "chalk",
+    "create_chalk",
+    "ChalkFactory",
+    "ColorMode",
+]
