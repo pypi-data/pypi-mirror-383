91811891bc06669d4c3f288ae4241a0a95201c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 05:05:32 +0000

    Add package couchbase-columnar/1.0.1/couchbase_columnar-1.0.1-cp311-cp311-macosx_10_15_x86_64.whl

---
+++ packages/couchbase-columnar/couchbase_columnar-1.0.1-cp311-cp311-macosx_10_15_x86_64.whl/couchbase_columnar/protocol/core/client_adapter.py
@@ -0,0 +1,191 @@
+#  Copyright 2016-2024. Couchbase, Inc.
+#  All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from __future__ import annotations
+
+import sys
+from functools import wraps
+from typing import (Any,
+                    Callable,
+                    Dict,
+                    Literal,
+                    Optional,
+                    TypeVar,
+                    Union)
+
+if sys.version_info < (3, 10):
+    from typing_extensions import TypeAlias
+else:
+    from typing import TypeAlias
+
+from couchbase_columnar.common.credential import Credential
+from couchbase_columnar.common.deserializer import Deserializer
+from couchbase_columnar.common.errors import ColumnarError, InternalSDKError
+from couchbase_columnar.protocol.connection import _ConnectionDetails
+from couchbase_columnar.protocol.core.client import _CoreClient
+from couchbase_columnar.protocol.core.request import CloseConnectionRequest, ConnectRequest
+from couchbase_columnar.protocol.core.result import CoreResult
+from couchbase_columnar.protocol.errors import CoreColumnarError, ErrorMapper
+from couchbase_columnar.protocol.options import OptionsBuilder
+
+ReqT = TypeVar('ReqT', ConnectRequest, CloseConnectionRequest)
+
+
+class BlockingWrapper:
+    """
+        **INTERNAL**
+    """
+
+    @classmethod
+    def block(cls,  # noqa: C901
+              return_cls: Literal[True]
+              ) -> Callable[[InputWrappedFunc[ReqT]], OutputWrappedFunc[ReqT]]:
+        """
+            **INTERNAL**
+        """
+
+        def decorator(fn: InputWrappedFunc[ReqT]) -> OutputWrappedFunc[ReqT]:  # noqa: C901:
+            @wraps(fn)
+            def wrapped_fn(self: _ClientAdapter, req: ReqT) -> Optional[CoreResult]:
+                try:
+                    ret = fn(self, req)
+                    if isinstance(ret, CoreColumnarError):
+                        raise ErrorMapper.build_error(ret)
+                    if return_cls is None:
+                        return None
+                    elif return_cls is True:
+                        return ret
+                    elif ret is None:
+                        raise InternalSDKError('Expected return value to be non-empty.')
+
+                    return return_cls(ret)
+                except (ColumnarError, InternalSDKError) as err:
+                    raise err
+                except CoreColumnarError as err:
+                    raise ErrorMapper.build_error(err) from None
+                except Exception as ex:
+                    raise InternalSDKError(str(ex))
+
+            return wrapped_fn
+        return decorator
+
+
+class _ClientAdapter:
+    """
+        **INTERNAL**
+    """
+
+    def __init__(self,
+                 connstr: str,
+                 credential: Credential,
+                 options: Optional[object] = None,
+                 **kwargs: object) -> None:
+
+        self._cluster_info = None
+        self._server_version = None
+        self._opts_builder = OptionsBuilder()
+        self._conn_details = _ConnectionDetails.create(self._opts_builder,
+                                                       connstr,
+                                                       credential,
+                                                       options,
+                                                       **kwargs)
+
+    @property
+    def client(self) -> _CoreClient:
+        """
+            **INTERNAL**
+        """
+        return self._client
+
+    @property
+    def has_connection(self) -> bool:
+        """
+            **INTERNAL**
+        """
+        if not hasattr(self, '_client'):
+            return False
+        return self._client.has_connection
+
+    @property
+    def connection_details(self) -> _ConnectionDetails:
+        """
+            **INTERNAL**
+        """
+        return self._conn_details
+
+    @property
+    def default_deserializer(self) -> Deserializer:
+        """
+            **INTERNAL**
+        """
+        return self._conn_details.default_deserializer
+
+    @property
+    def options_builder(self) -> OptionsBuilder:
+        """
+            **INTERNAL**
+        """
+        return self._opts_builder
+
+    @BlockingWrapper.block(True)
+    def close_connection(self, req: CloseConnectionRequest) -> None:
+        """
+            **INTERNAL**
+        """
+
+        try:
+            self.client.close_connection(req)
+        except Exception as ex:
+            # suppress context, we know we have raised an error from the bindings
+            if isinstance(ex, CoreColumnarError):
+                raise ErrorMapper.build_error(ex) from None
+            raise InternalSDKError(str(ex)) from None
+
+    @BlockingWrapper.block(True)
+    def connect(self, req: ConnectRequest) -> None:
+        """
+            **INTERNAL**
+        """
+        if not hasattr(self, '_client'):
+            self._client = _CoreClient()
+
+        ret = self.client.connect(req)
+        if isinstance(ret, CoreColumnarError):
+            raise ErrorMapper.build_error(ret)
+        self._client.connection = ret
+
+    def reset_client(self) -> None:
+        """
+            **INTERNAL**
+        """
+        if hasattr(self, '_client'):
+            del self._client
+
+    def _test_connect(self, req: ConnectRequest) -> Dict[str, Any]:
+        """
+            **INTERNAL**
+        """
+        if not hasattr(self, '_client'):
+            self._client = _CoreClient()
+
+        try:
+            return self.client._test_connect(req)
+        except CoreColumnarError as err:
+            raise ErrorMapper.build_error(err)
+
+
+InputWrappedFunc: TypeAlias = Callable[[_ClientAdapter, ReqT],
+                                       Optional[Union[CoreColumnarError, CoreResult]]]
+OutputWrappedFunc: TypeAlias = Callable[[_ClientAdapter, ReqT], Optional[CoreResult]]
