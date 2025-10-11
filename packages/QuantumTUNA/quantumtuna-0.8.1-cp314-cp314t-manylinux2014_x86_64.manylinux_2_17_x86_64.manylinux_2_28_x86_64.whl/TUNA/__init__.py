73f78a8a8649819e690b4f82bcf72af26efc9b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:40 +0000

    Add package QuantumTUNA/0.8.1/quantumtuna-0.8.1-cp314-cp314t-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/QuantumTUNA/quantumtuna-0.8.1-cp314-cp314t-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/TUNA/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+TUNA
+
+~~~~ Theoretical Unification of Nuclear Arrangements ~~~~
+
+A user-friendly quantum chemistry program for diatomics.
+
+"""
+
+__version__ = "0.8.1"
+__author__ = "Harry Brough"
