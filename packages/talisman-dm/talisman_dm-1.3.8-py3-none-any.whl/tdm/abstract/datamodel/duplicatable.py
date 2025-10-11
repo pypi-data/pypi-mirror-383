6c7080c990f54b37b70f860d895a75c1f0e15ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 04:03:59 +0000

    Add package talisman-dm/1.3.8/talisman_dm-1.3.8-py3-none-any.whl

---
+++ packages/talisman-dm/talisman_dm-1.3.8-py3-none-any.whl/tdm/abstract/datamodel/duplicatable.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta, abstractmethod
+from typing import Callable, Hashable, Iterable, Optional, Tuple
+
+from .identifiable import EnsureIdentifiable
+
+
+class Duplicatable(metaclass=ABCMeta):
+    @abstractmethod
+    def duplicate_hash(self) -> Hashable:
+        pass
+
+    @abstractmethod
+    def choose_one(
+            self, other, related: Callable[[str], Iterable[str]], restore_element: Callable[[str], EnsureIdentifiable]
+    ) -> Optional[Tuple[str, str]]:
+        pass
