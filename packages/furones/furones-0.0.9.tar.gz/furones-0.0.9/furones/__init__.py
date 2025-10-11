8321cf922daa5cf77778539b22096ca8d2729e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:04:46 +0000

    Add package furones/0.0.9/furones-0.0.9.tar.gz

---
+++ packages/furones/furones-0.0.9.tar.gz/furones-0.0.9/furones/__init__.py
@@ -0,0 +1,4 @@
+# Furones: Approximate Independent Set Solver https://pypi.org/project/furones
+# Author: Frank Vega
+
+__all__ = ["utils", "algorithm", "parser", "applogger", "test", "app", "batch"]
