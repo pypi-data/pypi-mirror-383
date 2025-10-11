3d6895005edc2c594d1eaf23b33db0a3c78ca382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:04:56 +0000

    Add package nmea2000/2025.10.9/nmea2000-2025.10.9-py3-none-any.whl

---
+++ packages/nmea2000/nmea2000-2025.10.9-py3-none-any.whl/nmea2000/cli.py
@@ -0,0 +1,243 @@
+import argparse
+import asyncio
+import sys
+import logging
+
+from .message import NMEA2000Message
+from .ioclient import ActisenseNmea2000Gateway, AsyncIOClient, EByteNmea2000Gateway, State, Type, WaveShareNmea2000Gateway, YachtDevicesNmea2000Gateway
+from .decoder import NMEA2000Decoder
+from .encoder import NMEA2000Encoder
+
+logger = logging.getLogger(__name__)
+
+# Define receive callback as a standalone function
+async def handle_received_message(message: NMEA2000Message):
+    """Callback function for received data."""
+    print(f"Received: {message}")
+
+# Define status change callback as a standalone function
+async def handle_status_change(state: State):
+    """Callback function for status changes."""
+    print(f"Connection status: {state}")
+
+async def interactive_client(client: AsyncIOClient):
+    """Interactive client function to handle user input."""
+    client.set_receive_callback(handle_received_message)
+    client. set_status_callback(handle_status_change)
+    await client.connect()
+    
+    print("Connected to NMEA2000 gateway. Enter NMEA2000 messages in JSON format.")
+    print("Type 'exit' to quit.")
+
+    try:
+        while True:
+            line = await asyncio.to_thread(sys.stdin.readline)
+            if not line:
+                break
+                
+            line = line.strip()
+            if line.lower() == "exit":
+                break
+            else:
+                try:
+                    message = NMEA2000Message.from_json(line)
+                    await client.send(message)
+                except Exception as e:
+                    print(f"Error: {e}")
+                    print("Not valid NMEA2000Message json")
+                    continue
+    except KeyboardInterrupt:
+        sys.stdout.flush()
+        pass
+    finally:
+        await client.close()
+        print("Connection closed.")
+
+def parse(filename: str, decoder: NMEA2000Decoder, single_line: bool):
+    try:
+        with open(filename, 'r') as file:
+            while True:
+                line = file.readline()
+                if not line:
+                    break
+                if line.startswith('#') or len(line) <= 1:
+                    continue
+                line = line.strip()
+                logger.info(f'Processing: {line}')
+                try:
+                    decoder.decode_basic_string(line, single_line)
+                except Exception as e:
+                    print(f"Error: {e}")
+    except KeyboardInterrupt:
+        sys.stdout.flush()
+        pass
+
+async def async_main():
+    logging.basicConfig(filename='parser.log', level=logging.NOTSET)
+    parser = argparse.ArgumentParser(description="NMEA 2000 CLI Tool")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose mode")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    # Decode command
+    decode_parser = subparsers.add_parser("decode", help="Decode an NMEA 2000 frame")
+    
+    # Either provide a frame string or a file
+    decode_parser.add_argument(
+        "--frame", 
+        type=str, 
+        help="Hex-encoded NMEA 2000 frame (optional if file is provided)"
+    )
+    decode_parser.add_argument(
+        "--file", 
+        type=str, 
+        help="Path to a file containing a Hex-encoded NMEA 2000 frame"
+    )
+    decode_parser.add_argument(
+        "--single_line", 
+        action="store_true", 
+        help="Fast frame is already merged to single line"
+    )
+
+    # Encode command
+    encode_parser = subparsers.add_parser("encode", help="Encode an NMEA 2000 frame")
+    
+    # Either provide a frame string or a file
+    encode_parser.add_argument(
+        "--frame", 
+        type=str, 
+        help="json NMEA 2000 frame (optional if file is provided)"
+    )
+    encode_parser.add_argument(
+        "--file", 
+        type=str, 
+        help="Path to a file containing a JSON NMEA 2000 frame"
+    )
+
+    # TCP client command
+    tcp_client_parser = subparsers.add_parser("tcp_client", help="start TCP client to CANBUS gateway (for example, ECAN-E01 or ECAN-W01)")
+    tcp_client_parser.add_argument(
+        "--server", 
+        type=str,
+        required=True, 
+        help="Server IP address"
+    )
+    tcp_client_parser.add_argument(
+        "--port", 
+        type=int, 
+        required=True, 
+        help="Server port number"
+    )
+    tcp_client_parser.add_argument(
+        "--dump_file", 
+        type=str, 
+        help="Record frames to a given file"
+    )
+    tcp_client_parser.add_argument(
+        "--dump_pgns", 
+        type=str, 
+        help="Record only specific pgns, comma seperated"
+    )
+
+    def parse_type(value):
+        try:
+            return Type[value.upper()]
+        except KeyError:
+            valid = ", ".join(t.name for t in Type)
+            raise argparse.ArgumentTypeError(f"Invalid type: {value}. Valid options are: {valid}.")
+
+    tcp_client_parser.add_argument(
+        "--type",
+        type=parse_type,
+        required=True,
+        help="Type of TCP server (EBYTE, ACTISENSE, or YACHT_DEVICES)"
+    )
+
+    # USB client command
+    usb_client_parser = subparsers.add_parser("usb_client", help="start USB client to CANBUS gateway (for example, Waveshare USB-CAN-A)")
+    usb_client_parser.add_argument(
+        "--port", 
+        type=str, 
+        required=True, 
+        help="Serial port"
+    )
+    usb_client_parser.add_argument(
+        "--dump_file", 
+        type=str, 
+        help="Record frames to a given file"
+    )
+    usb_client_parser.add_argument(
+        "--dump_pgns", 
+        type=str, 
+        help="Record only specific pgns, comma seperated"
+    )
+    # Parse arguments
+    args = parser.parse_args()
+
+    if args.verbose:
+        root_logger = logging.getLogger()
+        root_logger.setLevel(logging.DEBUG)  # Set the global log level
+        root_logger.addHandler(logging.StreamHandler())
+
+    if args.command == "decode":
+        decoder = NMEA2000Decoder()
+
+        # Decode from a frame string if provided
+        if args.frame:
+            decoded = decoder.decode_actisense_string(args.frame)
+            if decoded is not None:
+                print(decoded.to_json())
+
+        # Decode from a file if provided
+        elif args.file:
+            parse(args.file, decoder, args.single_line)
+        else:
+            print("Error: You must provide either a frame or a file to decode.")
+            exit(1)
+
+    elif args.command == "encode":
+        encoder = NMEA2000Encoder()
+
+        # Encode from a frame json
+        if args.frame:
+            frame_str = args.frame
+            print(f"Encoding frame: {frame_str}")
+            encoded = encoder.encode_actisense(NMEA2000Message.from_json(args.frame))
+            print(encoded)
+
+        # Encode from a json file
+        elif args.file:
+            with open(args.file, 'r') as file:
+                json_string = file.read()
+            encoded = encoder.encode_actisense(NMEA2000Message.from_json(json_string))
+            print(encoded)
+        else:
+            print("Error: You must provide either a frame or a file to encode.")
+            exit(1)
+            
+    elif args.command == "tcp_client":
+        # Create TCP client passing callbacks in constructor
+        if args.type == Type.EBYTE:
+            logger.info("Using EByteNmea2000Gateway with server: %s, port: %d", args.server, args.port)
+            client = EByteNmea2000Gateway(args.server, args.port)
+        elif args.type == Type.ACTISENSE:
+            logger.info("Using ActisenseNmea2000Gateway with server: %s, port: %d", args.server, args.port)
+            client = ActisenseNmea2000Gateway(args.server, args.port)            
+        elif args.type == Type.YACHT_DEVICES:
+            logger.info("Using YachtDevicesNmea2000Gateway with server: %s, port: %d", args.server, args.port)
+            client = YachtDevicesNmea2000Gateway(args.server, args.port)            
+        await interactive_client(client)
+    elif args.command == "usb_client":
+        # Create USB client passing callbacks in constructor
+        logger.info("Using WaveShareNmea2000Gateway with port: %s", args.port)
+        client = WaveShareNmea2000Gateway(port=args.port, dump_to_file=args.dump_file, dump_pgns=args.dump_pgns)
+        await interactive_client(client)
+
+def main():
+    try:
+        asyncio.run(async_main())
+    except KeyboardInterrupt:
+        print("Interrupted by user")
+
+if __name__ == "__main__":
+    main()
+
---
+++ packages/nmea2000/nmea2000-2025.10.9-py3-none-any.whl/nmea2000/message.py
@@ -0,0 +1,230 @@
+from __future__ import annotations
+
+import binascii
+from datetime import date, datetime, time, timedelta
+from dataclasses import dataclass, field
+import hashlib
+import logging
+from typing import Any
+import orjson
+from .consts import PhysicalQuantities, FieldTypes
+from .utils import kelvin_to_celsius, kelvin_to_fahrenheit, mps_to_knots, pascal_to_bar, pascal_to_PSI, radians_to_degrees
+
+logger = logging.getLogger(__name__)
+
+# Helper function
+def int_to_bytes(value):
+    # Determine the number of bytes needed
+    byte_length = (value.bit_length() + 8) // 8 or 1
+    return value.to_bytes(byte_length, byteorder="big", signed=False)
+
+# NMEA2000Message class represents a single NMEA 2000 PGN
+@dataclass
+class NMEA2000Message:
+    PGN: int
+    id: str = ''
+    description: str = ''
+    ttl: timedelta | None = None
+    fields: list[NMEA2000Field] = field(default_factory=list)
+    source: int = 0
+    destination: int = 0
+    priority: int = 0
+    timestamp: datetime = datetime.now()
+    source_iso_name: IsoName | None = None
+    hash: str | None = None
+    raw_can_data: bytes | str | None = None
+
+    def add_data(self, src:int, dest: int, priority:int, timestamp: datetime, source_iso_name: IsoName | None, build_network_map: bool, raw_can_data: bytes | str):
+        self.source = src
+        self.destination = dest
+        self.priority = priority
+        self.timestamp = timestamp
+        self.source_iso_name = source_iso_name
+        self.hash = None
+        self.raw_can_data = raw_can_data
+
+        if build_network_map:
+            # Using MD5 as we don't need secure hashing and speed matters
+            # For now, we will NOT include the ISO name in the primary key
+            #primary_key = f"{self.id}_{self.source_iso_name.name if self.source_iso_name else '#'}"
+            primary_key = f"{self.id}"
+            for nmea_field in self.fields:
+                if nmea_field.part_of_primary_key:
+                    primary_key += "_" + str(nmea_field.raw_value)
+            logger.debug("primary key: %s. iso name: %s", primary_key, self.source_iso_name)
+            self.hash = hashlib.md5(primary_key.encode()).hexdigest()
+
+
+    def apply_preferred_units(self, preferred_units: dict[PhysicalQuantities, str]):
+        if len(preferred_units) == 0:
+            return
+        for f in self.fields:
+            if f.physical_quantities == PhysicalQuantities.TEMPERATURE:
+                requested_unit = preferred_units.get(PhysicalQuantities.TEMPERATURE, None)
+                if requested_unit == "c":
+                    f.unit_of_measurement = "C"
+                    f.value = kelvin_to_celsius(f.value)
+                elif requested_unit == "f":
+                    f.unit_of_measurement = "F"
+                    f.value = kelvin_to_fahrenheit(f.value)
+            if f.physical_quantities == PhysicalQuantities.PRESSURE:
+                requested_unit = preferred_units.get(PhysicalQuantities.PRESSURE, None)
+                if requested_unit == "bar":
+                    f.unit_of_measurement = "Bar"
+                    f.value = pascal_to_bar(f.value)
+                elif requested_unit == "psi":
+                    f.unit_of_measurement = "PSI"
+                    f.value = pascal_to_PSI(f.value)
+            if f.physical_quantities == PhysicalQuantities.ANGLE:
+                requested_unit = preferred_units.get(PhysicalQuantities.ANGLE, None)
+                if requested_unit == "deg":
+                    f.unit_of_measurement = "Deg"
+                    f.value = radians_to_degrees(f.value)
+            if f.physical_quantities == PhysicalQuantities.SPEED:
+                requested_unit = preferred_units.get(PhysicalQuantities.SPEED, None)
+                if requested_unit == "kts":
+                    f.unit_of_measurement = "kts"
+                    f.value = mps_to_knots(f.value)
+
+    def __str__(self):
+        return f"NMEA2000Message(PGN={self.PGN}, id={self.id}, pri={self.priority}, src={self.source}, source_iso_name={self.source_iso_name}, dest={self.destination}, description={self.description}, fields={self.fields})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def to_string_test_style(self):
+        fields_str = ', '.join([field.to_string_test_style() for field in self.fields])
+        return f"{self.PGN} {self.description}: {fields_str}"
+
+    def to_json(self):
+        def default(obj: Any) -> Any:
+            if isinstance(obj, (bytes, bytearray)):
+                return obj.hex()
+            if isinstance(obj, timedelta):
+                return obj.total_seconds()
+            # ...existing code...
+            raise TypeError
+        return orjson.dumps(self.__dict__, default=default).decode()
+
+    @staticmethod
+    def from_json(json_str):
+        data = orjson.loads(json_str)
+        msg = NMEA2000Message(**data)
+        msg.fields = [NMEA2000Field(**field) for field in data.get("fields", [])]
+        return msg
+    
+    def get_field_by_id(self, id: str) -> NMEA2000Field:
+        field = next((f for f in self.fields if f.id == id), None)
+        if field is None:
+            raise ValueError(f"PGN: {self.id}: Field with id '{id}' is missing.")
+        return field
+
+    def get_field_int_value_by_id(self, id: str, default_value: int | None = None) -> int:
+        field = self.get_field_by_id(id)
+        if not isinstance(field.value, int):
+            if default_value is not None:
+                return default_value
+            raise ValueError(f"PGN: {self.id}: Field with id '{id}' is not an integer. It is {type(field.value).__name__}.")
+        return field.value
+
+    def get_field_str_value_by_id(self, id: str) -> str | None:
+        field = self.get_field_by_id(id)
+        if field.value is None:
+            logger.warning("PGN: %s: Field with id '%s' is None. Raw value is: %s", self.id, id, field.raw_value)
+            return None
+        if not isinstance(field.value, str):
+            raise ValueError(f"PGN: {self.id}: Field with id '{id}' is not a string. It is {type(field.value).__name__}.")
+        return field.value
+    
+# NMEA2000Field class represents a single NMEA 2000 field
+@dataclass
+class NMEA2000Field:
+    id: str
+    name: str | None = None
+    description: str | None = None
+    unit_of_measurement: str | None = None
+    value: str | int | float | bytes | time | date | None = 0
+    raw_value: int | float | str | bytes | None = 0
+    physical_quantities: PhysicalQuantities | None = None
+    type: FieldTypes = FieldTypes.NUMBER
+    part_of_primary_key: bool | None = None
+
+    def __str__(self):
+        return f"NMEA2000Field(id={self.id}, name={self.name}, description={self.description}, unit_of_measurement={self.unit_of_measurement}, value={self.value}, raw_value={self.raw_value}, physical_quantities={self.physical_quantities}, type={self.type}, part_of_primary_key = {self.part_of_primary_key})"
+
+    def __repr__(self):
+        return self.__str__()
+    
+    def to_string_test_style(self):
+        if isinstance(self.raw_value, int):
+            # Convert integer to bytes (big-endian)
+            value_bytes = int_to_bytes(self.raw_value)
+            # Use binascii.hexlify to convert bytes to hex
+            raw_value_hex = binascii.hexlify(value_bytes, " ").decode().upper()
+        else:
+            raw_value_hex = self.raw_value
+        return f'{self.name} = {self.value} (bytes = "{raw_value_hex}")'
+    
+class LookupFieldTypeEnumeration:
+    name: str
+    field_type: FieldTypes
+    resolution: float | None
+    unit: str | None
+    bits: int
+    lookup_enumeration: str | None
+
+    def __init__(self, name: str, field_type: FieldTypes, resolution: float | None, unit: str | None, bits: int, lookup_enumeration: str | None):
+        self.name = name
+        self.field_type = field_type
+        self.resolution = resolution
+        self.unit = unit
+        self.bits = bits
+        self.lookup_enumeration = lookup_enumeration
+
+@dataclass
+class IsoName:
+    unique_number: int
+    manufacturer_code: str
+    device_instance: int
+    device_function: str
+    device_class: str
+    system_instance: int
+    industry_group: str
+    arbitrary_address_capable: bool
+    name: int
+
+    def __init__(self, message: NMEA2000Message, name: int):
+        """
+        Initialize an IsoName object from an NMEA2000Message.
+
+        Args:
+            message: An NMEA2000Message object containing the NAME field data.
+            name: The 64-bit NAME field value as an integer.
+        """
+        self.name = name
+        self.unique_number = message.get_field_int_value_by_id('uniqueNumber', 0)
+        self.manufacturer_code = message.get_field_str_value_by_id('manufacturerCode')
+        self.device_instance = (
+            message.get_field_int_value_by_id('deviceInstanceUpper', 0) << 3
+        ) | message.get_field_int_value_by_id('deviceInstanceLower', 0)
+        self.device_function = message.get_field_str_value_by_id('deviceFunction')
+        self.device_class = message.get_field_str_value_by_id('deviceClass')
+        self.system_instance = message.get_field_int_value_by_id('systemInstance', 0)
+        self.industry_group = message.get_field_str_value_by_id('industryGroup')
+        self.arbitrary_address_capable = message.get_field_str_value_by_id('arbitraryAddressCapable') == "Yes"
+
+    def __str__(self) -> str:
+        return (
+            f"IsoName(unique_number={self.unique_number}, "
+            f"manufacturer_code='{self.manufacturer_code}', "
+            f"device_instance={self.device_instance}, "
+            f"device_function='{self.device_function}', "
+            f"device_class='{self.device_class}', "
+            f"system_instance={self.system_instance}, "
+            f"industry_group='{self.industry_group}', "
+            f"arbitrary_address_capable={self.arbitrary_address_capable}, "
+            f"name={self.name})"
+        )
+    
+    def __repr__(self):
+        return self.__str__()
