8bc1004e8540be376400a31b5c942e394d4e4cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 05:05:20 +0000

    Add package medsenger-api/0.1.86/medsenger_api-0.1.86.tar.gz

---
+++ packages/medsenger-api/medsenger_api-0.1.86.tar.gz/medsenger_api-0.1.86/medsenger_api/__init__.py
@@ -0,0 +1,12 @@
+"""
+medsenger_api.
+Python SDK for Medsenger.AI
+"""
+
+__version__ = "0.1.86"
+__author__ = 'Rostislav Borodin'
+__credits__ = 'TelePat LLC'
+
+from .api_client import AgentApiClient
+from .utils import *
+from .protocol import *
