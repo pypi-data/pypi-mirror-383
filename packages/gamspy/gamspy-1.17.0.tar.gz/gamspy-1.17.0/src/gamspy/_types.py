d75ca058f2d66f9930f85d8fb18056c8943eb23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 03:12:51 +0000

    Add package gamspy/1.17.0/gamspy-1.17.0.tar.gz

---
+++ packages/gamspy/gamspy-1.17.0.tar.gz/gamspy-1.17.0/src/gamspy/_types.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from typing import TypeAlias
+
+from gamspy._algebra.operable import Operable
+
+OperableType: TypeAlias = Operable | int | float
+EllipsisType: TypeAlias = type(...)  # type: ignore
