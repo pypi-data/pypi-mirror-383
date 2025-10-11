08124981327f4c79a1c4f5a025ccc8d638644753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:05:28 +0000

    Add package markitdown-docx-image-plugin/0.1.0/markitdown_docx_image_plugin-0.1.0.tar.gz

---
+++ packages/markitdown-docx-image-plugin/markitdown_docx_image_plugin-0.1.0.tar.gz/markitdown_docx_image_plugin-0.1.0/src/markitdown_docx_image_plugin/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024-present
+#
+# SPDX-License-Identifier: MIT
+
+from ._plugin import register_converters, __plugin_interface_version__
+
+__all__ = ["register_converters", "__plugin_interface_version__"]
+
