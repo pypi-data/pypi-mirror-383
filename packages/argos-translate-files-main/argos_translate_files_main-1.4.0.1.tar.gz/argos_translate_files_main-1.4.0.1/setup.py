6abb7f713687af63695f45e223e8e79dcdfd1e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 04:04:16 +0000

    Add package argos-translate-files-main/1.4.0.1/argos_translate_files_main-1.4.0.1.tar.gz

---
+++ packages/argos-translate-files-main/argos_translate_files_main-1.4.0.1.tar.gz/argos_translate_files_main-1.4.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="argos-translate-files-main",
+    version="1.4.0.1",
+    description="Translate files with Argos Translate",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="S. Thuret",
+    author_email="contact@sebastien-thuret.fr",
+    url="https://github.com/LibreTranslate/argos-translate-files",
+    packages=find_packages(),
+    install_requires=required_packages,
+)
