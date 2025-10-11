461c9200ccbbe5a490b6e81afac552147301d099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:05:06 +0000

    Add package types-aiofiles/25.1.0.20251011/types_aiofiles-25.1.0.20251011.tar.gz

---
+++ packages/types-aiofiles/types_aiofiles-25.1.0.20251011.tar.gz/types_aiofiles-25.1.0.20251011/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'aiofiles-stubs': ['__init__.pyi', 'base.pyi', 'os.pyi', 'ospath.pyi', 'tempfile/__init__.pyi', 'tempfile/temptypes.pyi', 'threadpool/__init__.pyi', 'threadpool/binary.pyi', 'threadpool/text.pyi', 'threadpool/utils.pyi', 'METADATA.toml', 'py.typed']})
