6a3bb9a061d413421b2e3ce1e738181a1a12c409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 16:05:42 +0000

    Add package async-dnsbl-client/0.0.5/async_dnsbl_client-0.0.5-py3-none-any.whl

---
+++ packages/async-dnsbl-client/async_dnsbl_client-0.0.5-py3-none-any.whl/async_dnsbl_client-0.0.5.data/scripts/adnsbl.py
@@ -0,0 +1,39 @@
+#!python
+
+import argparse
+import sys
+
+import asyncio
+
+import async_dnsbl_client
+
+def get_args():
+
+    me = sys.argv[0]
+    epilog = f'''\nExamples:
+    {me} 1.1.1.1
+    {me} -a dnsbl.example.com dnsbl.example.org -r dnsbl.example.net 1.1.1.1'''
+
+    parser = argparse.ArgumentParser(description='Sleep which can awake', epilog=epilog,  formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('host', metavar='HOST', help='host to check (IP or hostname)')
+    parser.add_argument('--append', '-a', nargs='+', default=list(), metavar='DNSBL', help='add this DNSBL to check')
+    parser.add_argument('--remove', '-r', nargs='+', default=list(), metavar='DNSBL', help='remove this DNSBL')
+    parser.add_argument('--nameserver', '-n', default=None, metavar='NS server', help='DNS server')
+
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    zones = async_dnsbl_client.dnsbl_zones
+
+    for r in args.remove:
+        zones.remove(r)
+
+    zones.extend(args.append)
+
+    result = asyncio.run(async_dnsbl_client.dnsbl(args.host, zones, nameserver=args.nameserver))
+    print('\n'.join(result))
+    return int(bool(len(result)))
+
+if __name__ == '__main__':
+    sys.exit(main())
