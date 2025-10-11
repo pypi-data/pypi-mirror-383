76421ae82d876919e4d0087ec6b72e42e00d3c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:24 +0000

    Add package argparse-from-file/1.0/argparse_from_file-1.0-py3-none-any.whl

---
+++ packages/argparse-from-file/argparse_from_file-1.0-py3-none-any.whl/argparse_from_file.py
@@ -0,0 +1,72 @@
+"""
+A very light wrapper around Python's argparse module to prepend a program's
+argument list with default options and arguments read from a user configuration
+file.
+"""
+
+import argparse
+import shlex
+import sys
+from pathlib import Path
+
+import platformdirs
+
+
+class ArgumentParser(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        self._argv = []
+
+        # from_file = 'path-to/file': Use this as "from file" name/path. If
+        #   relative then wrt platform specific user config dir.
+        # from_file = '': Do not use a "from file".
+        # from_file = None: Create default "from file" path.
+        if (from_file := kwargs.pop('from_file', None)) is None:
+            from_file = Path(sys.argv[0]).stem + '-flags.conf'
+
+        if from_file:
+            from_file_path = platformdirs.user_config_path(from_file)
+            from_file_path_str = str(from_file_path)
+            # epilog = 'text string': Set this as epilog, replacing any
+            #   '#FROM_FILE_PATH#' with the above determined "from file" path.
+            # epilog = '': Do not set an epilog.
+            # epilog = None: create default epilog with "from file" path.
+            if (epilog := kwargs.pop('epilog', None)) is None:
+                epilog = f'Note you can set default starting options in {from_file_path_str}.'
+            else:
+                epilog = epilog.replace('#FROM_FILE_PATH#', from_file_path_str)
+
+            if epilog:
+                kwargs['epilog'] = epilog
+
+            # Also replace any '#FROM_FILE_PATH#' in usage and description.
+            for kw in 'usage', 'description':
+                if v := kwargs.get(kw):
+                    kwargs[kw] = v.replace('#FROM_FILE_PATH#', from_file_path_str)
+
+            # Create list of default args from user file.
+            if from_file_path.is_file():
+                with from_file_path.open() as fp:
+                    self._argv.extend(
+                        ln
+                        for line in fp
+                        if (ln := line.strip()) and not ln.startswith('#')
+                    )
+        else:
+            from_file_path = None
+
+        self.from_file_path = from_file_path
+        return super().__init__(*args, **kwargs)
+
+    def parse_args(self, args=None, namespace=None):  # type: ignore[override]
+        if args is None and self._argv:
+            # Combine args from file and command line, to be parsed
+            argstr = ' '.join(self._argv).strip()
+            args = shlex.split(argstr) + sys.argv[1:]
+
+        del self._argv
+        return super().parse_args(args, namespace)
+
+
+def __getattr__(name: str):
+    "Proxy all other attributes from argparse module."
+    return getattr(argparse, name)
