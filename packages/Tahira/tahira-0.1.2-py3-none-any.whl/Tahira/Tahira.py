649d4e3f11ecbe19b223b1a611ed110f45afba21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 07:05:04 +0000

    Add package Tahira/0.1.2/tahira-0.1.2-py3-none-any.whl

---
+++ packages/Tahira/tahira-0.1.2-py3-none-any.whl/Tahira/Tahira.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+
+def debdoubi():
+    print("The best nickname ever")
+
+def ahmedsaid():
+    print("Tahira is my fav person ever")
+
+def time():
+    print("9alt mau dar khaaayba")
+
+def oursong():
+    webbrowser.open("https://www.youtube.com/watch?v=P1gnCQhoEYQ&list=RDP1gnCQhoEYQ&start_radio=1")
