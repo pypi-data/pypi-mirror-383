e7acf7e9ebe4d4578de8d6eca49bf562b5146dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 03:12:45 +0000

    Add package seismos-package/0.1.9/seismos_package-0.1.9-py3-none-any.whl

---
+++ packages/seismos-package/seismos_package-0.1.9-py3-none-any.whl/seismos_package/logging/__init__.py
@@ -0,0 +1,4 @@
+from seismos_package.logging.configuration import get_default_logging_conf
+from seismos_package.logging.logger import LoggingConfigurator
+
+__all__ = ["LoggingConfigurator", "get_default_logging_conf"]
