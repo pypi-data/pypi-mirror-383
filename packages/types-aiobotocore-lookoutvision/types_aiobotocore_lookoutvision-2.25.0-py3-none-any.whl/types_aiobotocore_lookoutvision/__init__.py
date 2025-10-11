76c5fdfe01e28c4bb53dd3cc84e72961988f82f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:19 +0000

    Add package types-aiobotocore-lookoutvision/2.25.0/types_aiobotocore_lookoutvision-2.25.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-lookoutvision/types_aiobotocore_lookoutvision-2.25.0-py3-none-any.whl/types_aiobotocore_lookoutvision/__init__.py
@@ -0,0 +1,52 @@
+"""
+Main interface for lookoutvision service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lookoutvision/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_lookoutvision import (
+        Client,
+        ListDatasetEntriesPaginator,
+        ListModelPackagingJobsPaginator,
+        ListModelsPaginator,
+        ListProjectsPaginator,
+        LookoutforVisionClient,
+    )
+
+    session = get_session()
+    async with session.create_client("lookoutvision") as client:
+        client: LookoutforVisionClient
+        ...
+
+
+    list_dataset_entries_paginator: ListDatasetEntriesPaginator = client.get_paginator("list_dataset_entries")
+    list_model_packaging_jobs_paginator: ListModelPackagingJobsPaginator = client.get_paginator("list_model_packaging_jobs")
+    list_models_paginator: ListModelsPaginator = client.get_paginator("list_models")
+    list_projects_paginator: ListProjectsPaginator = client.get_paginator("list_projects")
+    ```
+"""
+
+from .client import LookoutforVisionClient
+from .paginator import (
+    ListDatasetEntriesPaginator,
+    ListModelPackagingJobsPaginator,
+    ListModelsPaginator,
+    ListProjectsPaginator,
+)
+
+Client = LookoutforVisionClient
+
+
+__all__ = (
+    "Client",
+    "ListDatasetEntriesPaginator",
+    "ListModelPackagingJobsPaginator",
+    "ListModelsPaginator",
+    "ListProjectsPaginator",
+    "LookoutforVisionClient",
+)
