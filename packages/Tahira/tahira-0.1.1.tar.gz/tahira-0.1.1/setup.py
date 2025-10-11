5e9f3536c929863fb4a90e93f4952586163ae69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 07:05:13 +0000

    Add package Tahira/0.1.1/tahira-0.1.1.tar.gz

---
+++ packages/Tahira/tahira-0.1.1.tar.gz/tahira-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Tahira',
+    version='0.1.1',
+    packages=find_packages(),
+    description='A simple library for YOU',
+    author='Ahmed',
+    license='Free-to-use (proprietary)',
+)
