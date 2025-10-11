6471abbfab9f07f0f07656d9032c5717a88f95cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 06:04:47 +0000

    Add package spymot/2.1.2.dev0/spymot-2.1.2.dev0-py3-none-any.whl

---
+++ packages/spymot/spymot-2.1.2.dev0-py3-none-any.whl/spymot/v2/scripts/__init__.py
@@ -0,0 +1,6 @@
+"""
+Spymot V2 Scripts: Command-line interfaces for enhanced functionality.
+"""
+
+# This module provides access to V2 CLI scripts
+# The actual scripts are imported from the V2 directory
