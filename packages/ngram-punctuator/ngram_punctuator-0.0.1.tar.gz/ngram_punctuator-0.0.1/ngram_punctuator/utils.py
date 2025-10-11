50d2e09aae41171083792e4fc67aed5c2d3d82d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 16:05:36 +0000

    Add package ngram-punctuator/0.0.1/ngram_punctuator-0.0.1.tar.gz

---
+++ packages/ngram-punctuator/ngram_punctuator-0.0.1.tar.gz/ngram_punctuator-0.0.1/ngram_punctuator/utils.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2025, Zhendong Peng (pzd17@tsinghua.org.cn)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from functools import lru_cache
+
+import regex as re
+
+
+@lru_cache()
+def bytes_to_unicode():
+    bs = list(range(33, 127)) + list(range(161, 173)) + list(range(174, 256))
+    cs = bs[:]
+    n = 0
+    for b in range(2**8):
+        if b not in bs:
+            bs.append(b)
+            cs.append(2**8 + n)
+            n += 1
+    cs = [chr(n) for n in cs]
+    return dict(zip(bs, cs))
+
+
+def format_text(text: str) -> str:
+    """
+    Add space between Chinese and ASCII characters.
+    """
+    if not text.strip():
+        return text
+    text = re.sub(r"[ \u3000]+", " ", text)
+    text = re.sub(r"(\p{Han})([a-zA-Z0-9])", r"\1 \2", text, flags=re.UNICODE)
+    text = re.sub(r"([a-zA-Z0-9])(\p{Han})", r"\1 \2", text, flags=re.UNICODE)
+    return text
+
+
+def get_pairs(word):
+    """
+    Return set of symbol pairs in a word.
+
+    Word is represented as tuple of symbols (symbols being variable-length strings).
+    """
+    pairs = set()
+    prev_char = word[0]
+    for char in word[1:]:
+        pairs.add((prev_char, char))
+        prev_char = char
+    return pairs
