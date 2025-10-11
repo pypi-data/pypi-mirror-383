40bcb5dc633e742761cd20d2520ebe75e8a864bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 03:12:12 +0000

    Add package jellyjoin/0.2.3/jellyjoin-0.2.3-py3-none-any.whl

---
+++ packages/jellyjoin/jellyjoin-0.2.3-py3-none-any.whl/jellyjoin/type_definitions.py
@@ -0,0 +1,12 @@
+from collections.abc import Collection
+from typing import Callable, TypeAlias, Union
+
+import numpy as np
+
+# type descriptions
+SimilarityStrategyCallable: TypeAlias = Callable[
+    [Collection[str], Collection[str]], np.ndarray
+]
+SimilarityCallable: TypeAlias = Callable[[str, str], float]
+PreprocessorCallable: TypeAlias = Callable[[str], str]
+SimilarityIdentifier: TypeAlias = Union[None, str, SimilarityCallable]
