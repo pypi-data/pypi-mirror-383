c9edebc45d9c2317ad3aef220927c450ca63bd53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 05:04:56 +0000

    Add package mblack/25.6.1/mblack-25.6.1-py3-none-any.whl

---
+++ packages/mblack/mblack-25.6.1-py3-none-any.whl/mblack/debug.py
@@ -0,0 +1,62 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+# ===----------------------------------------------------------------------=== #
+#
+# File originates from:
+#   Repo:   git@github.com:psf/black.git
+#   Commit: d4a85643a465f5fae2113d07d22d021d4af4795a
+#   Path:   src/black/debug.py
+#
+# ===----------------------------------------------------------------------=== #
+
+from dataclasses import dataclass
+from typing import Iterator, TypeVar, Union
+
+from mblack.nodes import Visitor
+from mblack.output import out
+from mblack.parsing import lib2to3_parse
+from mblib2to3.pgen2 import token
+from mblib2to3.pytree import Leaf, Node, type_repr
+
+LN = Union[Leaf, Node]
+T = TypeVar("T")
+
+
+@dataclass
+class DebugVisitor(Visitor[T]):
+    tree_depth: int = 0
+
+    def visit_default(self, node: LN) -> Iterator[T]:
+        indent = " " * (2 * self.tree_depth)
+        if isinstance(node, Node):
+            _type = type_repr(node.type)
+            out(f"{indent}{_type}", fg="yellow")
+            self.tree_depth += 1
+            for child in node.children:
+                yield from self.visit(child)
+
+            self.tree_depth -= 1
+            out(f"{indent}/{_type}", fg="yellow", bold=False)
+        else:
+            _type = token.tok_name.get(node.type, str(node.type))
+            out(f"{indent}{_type}", fg="blue", nl=False)
+            if node.prefix:
+                # We don't have to handle prefixes for `Node` objects since
+                # that delegates to the first child anyway.
+                out(f" {node.prefix!r}", fg="green", bold=False, nl=False)
+            out(f" {node.value!r}", fg="blue", bold=False)
+
+    @classmethod
+    def show(cls, code: Union[str, Leaf, Node]) -> None:
+        """Pretty-print the lib2to3 AST of a given string of `code`.
+
+        Convenience method for debugging.
+        """
+        v: DebugVisitor[None] = DebugVisitor()
+        if isinstance(code, str):
+            code = lib2to3_parse(code)
+        list(v.visit(code))
