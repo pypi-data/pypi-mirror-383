d2094dd9b697651a032bf203fba20c5d5a78cbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:05:25 +0000

    Add package scraper-nuuuwan/1.0.3/scraper_nuuuwan-1.0.3-py3-none-any.whl

---
+++ packages/scraper-nuuuwan/scraper_nuuuwan-1.0.3-py3-none-any.whl/scraper/__init__.py
@@ -0,0 +1,19 @@
+# scraper (auto generate by build_inits.py)
+# flake8: noqa: F408
+
+from scraper.abstract_doc import (AbstractDoc, AbstractDocBase,
+                                  AbstractDocBasePathsMixin,
+                                  AbstractDocChartDocsByTimeAndLangMixin,
+                                  AbstractDocGeneratorMixin,
+                                  AbstractDocHuggingFaceMixin,
+                                  AbstractDocMetadataMixin,
+                                  AbstractDocPipelineCleanupMixin,
+                                  AbstractDocPipelineExtendedDataMixin,
+                                  AbstractDocPipelineMetadataMixin,
+                                  AbstractDocPipelineMixin,
+                                  AbstractDocReadMeMixin,
+                                  AbstractDocRemotePathMixin,
+                                  AbstractDocSummaryMixin,
+                                  AbstractDocTextMixin)
+from scraper.abstract_pdf_doc import AbstractPDFDoc
+from scraper.global_readme import GlobalReadMe, GlobalReadMeSummaryMixin
