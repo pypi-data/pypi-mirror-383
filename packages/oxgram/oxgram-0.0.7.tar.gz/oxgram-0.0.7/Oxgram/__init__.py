0ab34fe76561ed9825cbf931ca3ee42617fce62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:05:10 +0000

    Add package oxgram/0.0.7/oxgram-0.0.7.tar.gz

---
+++ packages/oxgram/oxgram-0.0.7.tar.gz/oxgram-0.0.7/Oxgram/__init__.py
@@ -0,0 +1,24 @@
+appname = 'oxgram'
+version = '0.0.7'
+
+caption = 'ㅤ'
+pythons = '~=3.10'
+clinton = 'Clinton Abraham'
+profile = 'https://github.com/Clinton-Abraham'
+
+install = ['pyrofork', 'tgcryptos']
+mention = ['bot', 'bots', 'telegram']
+
+DATA01 = 'clintonabrahamc@gmail.com'
+DATA02 = ['Natural Language :: English',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Programming Language :: Python :: 3.14',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Localization',
+          'Topic :: Software Development :: User Interfaces',
+          'Topic :: Software Development :: Version Control']
