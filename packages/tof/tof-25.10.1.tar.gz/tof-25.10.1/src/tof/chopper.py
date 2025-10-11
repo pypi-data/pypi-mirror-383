daba24106d672fbf5694cb7125b1773cbe61e471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 06:04:47 +0000

    Add package tof/25.10.1/tof-25.10.1.tar.gz

---
+++ packages/tof/tof-25.10.1.tar.gz/tof-25.10.1/src/tof/chopper.py
@@ -0,0 +1,223 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
+from __future__ import annotations
+
+from dataclasses import dataclass, replace
+from enum import Enum
+
+import scipp as sc
+
+from .reading import ComponentReading
+from .utils import two_pi
+
+
+class Direction(Enum):
+    CLOCKWISE = 1
+    ANTICLOCKWISE = 2
+
+
+Clockwise = Direction.CLOCKWISE
+AntiClockwise = Direction.ANTICLOCKWISE
+
+
+class Chopper:
+    """
+    A chopper is a rotating device with cutouts that blocks the beam at certain times.
+
+    Parameters
+    ----------
+    frequency:
+        The frequency of the chopper. Must be positive.
+    distance:
+        The distance from the source to the chopper.
+    name:
+        The name of the chopper.
+    phase:
+        The phase of the chopper. Because the phase offset implemented as a time delay
+        on real beamline choppers, it is applied in the opposite direction
+        to the chopper rotation direction. For example, if the chopper rotates
+        clockwise, a phase of 10 degrees will shift all window angles by 10 degrees
+        in the anticlockwise direction, which will result in the windows opening later.
+    open:
+        The opening angles of the chopper cutouts.
+    close:
+        The closing angles of the chopper cutouts.
+    centers:
+        The centers of the chopper cutouts.
+    widths:
+        The widths of the chopper cutouts.
+
+    Notes
+    -----
+    Either `open` and `close` or `centers` and `widths` must be provided, but not both.
+    """
+
+    def __init__(
+        self,
+        *,
+        frequency: sc.Variable,
+        distance: sc.Variable,
+        name: str,
+        phase: sc.Variable | None = None,
+        open: sc.Variable | None = None,
+        close: sc.Variable | None = None,
+        centers: sc.Variable | None = None,
+        widths: sc.Variable | None = None,
+        direction: Direction = Clockwise,
+    ):
+        if frequency <= (0.0 * frequency.unit):
+            raise ValueError(f"Chopper frequency must be positive, got {frequency:c}.")
+        self.frequency = frequency.to(dtype=float, copy=False)
+        if direction not in (Clockwise, AntiClockwise):
+            raise ValueError(
+                "Chopper direction must be Clockwise or AntiClockwise"
+                f", got {direction}."
+            )
+        if phase is None:
+            phase = sc.scalar(0.0, unit='deg')
+        self.direction = direction
+        # Check that either open/close or centers/widths are provided, but not both
+        if tuple(x for x in (open, close, centers, widths) if x is not None) not in (
+            (open, close),
+            (centers, widths),
+        ):
+            raise ValueError(
+                "Either open/close or centers/widths must be provided, got"
+                f" open={open}, close={close}, centers={centers}, widths={widths}."
+            )
+        if open is None:
+            half_width = widths * 0.5
+            open = centers - half_width
+            close = centers + half_width
+
+        self.open = (open if open.dims else open.flatten(to='cutout')).to(
+            dtype=float, copy=False
+        )
+        self.close = (close if close.dims else close.flatten(to='cutout')).to(
+            dtype=float, copy=False
+        )
+        self.distance = distance.to(dtype=float, copy=False)
+        self.phase = phase.to(dtype=float, copy=False)
+        self.name = name
+        super().__init__()
+
+    @property
+    def omega(self) -> sc.Variable:
+        """
+        The angular velocity of the chopper.
+        """
+        return two_pi * self.frequency
+
+    def open_close_times(
+        self, time_limit: sc.Variable | None = None, unit: str | None = None
+    ) -> tuple[sc.Variable, sc.Variable]:
+        """
+        The times at which the chopper opens and closes.
+
+        Parameters
+        ----------
+        time_limit:
+            Determines how many rotations the chopper needs to perform to reach the time
+            limit. If not specified, the chopper will perform a single rotation.
+        unit:
+            The unit of the returned times. If not specified, the unit of `time_limit`
+            is used.
+        """
+        if time_limit is None:
+            time_limit = sc.scalar(0.0, unit='us')
+        if unit is None:
+            unit = time_limit.unit
+        nrot = max(int(sc.ceil((time_limit * self.frequency).to(unit='')).value), 1)
+        # We make a unique dim name that is different from self.open.dim and
+        # self.close.dim to make use of automatic broadcasting below.
+        # We also start at -1 to catch early openings in case the phase or opening
+        # angles are large
+        phases = sc.arange(
+            f"{self.open.dim}-{self.close.dim}", -1, nrot
+        ) * two_pi + self.phase.to(unit='rad')
+
+        open_times = self.open.to(unit='rad', copy=False)
+        close_times = self.close.to(unit='rad', copy=False)
+        # If the chopper is rotating anti-clockwise, we mirror the openings because the
+        # first cutout will be the last to open.
+        if self.direction == AntiClockwise:
+            open_times, close_times = (
+                sc.array(
+                    dims=close_times.dims,
+                    values=(two_pi - close_times).values[::-1],
+                    unit=close_times.unit,
+                ),
+                sc.array(
+                    dims=open_times.dims,
+                    values=(two_pi - open_times).values[::-1],
+                    unit=open_times.unit,
+                ),
+            )
+        # Note that the order is important here: we need (phases + open/close) to get
+        # the correct dimension order when we flatten.
+        open_times = (phases + open_times).flatten(to=self.open.dim)
+        close_times = (phases + close_times).flatten(to=self.close.dim)
+        open_times /= self.omega
+        close_times /= self.omega
+        return (
+            open_times.to(unit=unit, copy=False),
+            close_times.to(unit=unit, copy=False),
+        )
+
+    def __repr__(self) -> str:
+        return (
+            f"Chopper(name={self.name}, distance={self.distance:c}, "
+            f"frequency={self.frequency:c}, phase={self.phase:c}, "
+            f"direction={self.direction.name}, cutouts={len(self.open)})"
+        )
+
+    def as_dict(self):
+        return {
+            'frequency': self.frequency,
+            'open': self.open,
+            'close': self.close,
+            'distance': self.distance,
+            'phase': self.phase,
+            'name': self.name,
+            'direction': self.direction,
+        }
+
+
+@dataclass(frozen=True)
+class ChopperReading(ComponentReading):
+    """
+    Read-only container for the neutrons that reach the chopper.
+    """
+
+    distance: sc.Variable
+    name: str
+    frequency: sc.Variable
+    open: sc.Variable
+    close: sc.Variable
+    phase: sc.Variable
+    open_times: sc.Variable
+    close_times: sc.Variable
+    data: sc.DataArray
+
+    def _repr_stats(self) -> str:
+        return (
+            f"visible={int(self.data.sum().value)}, "
+            f"blocked={int(self.data.masks['blocked_by_me'].sum().value)}"
+        )
+
+    def __repr__(self) -> str:
+        return f"""ChopperReading: '{self.name}'
+  distance: {self.distance:c}
+  frequency: {self.frequency:c}
+  phase: {self.phase:c}
+  cutouts: {len(self.open)}
+  neutrons: {self._repr_stats()}
+"""
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    def __getitem__(self, val: int | slice | tuple[str, int | slice]) -> ChopperReading:
+        if isinstance(val, int):
+            val = ('pulse', val)
+        return replace(self, data=self.data[val])
---
+++ packages/tof/tof-25.10.1.tar.gz/tof-25.10.1/tests/chopper_test.py
@@ -0,0 +1,356 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+
+import numpy as np
+import pytest
+import scipp as sc
+
+import tof
+
+Hz = sc.Unit('Hz')
+deg = sc.Unit('deg')
+rad = sc.Unit('rad')
+meter = sc.Unit('m')
+sec = sc.Unit('s')
+two_pi = 2.0 * np.pi * rad
+
+
+def test_angular_speed():
+    f = 10.0 * Hz
+    chopper = tof.Chopper(
+        frequency=f,
+        open=0.0 * deg,
+        close=10.0 * deg,
+        phase=0.0 * deg,
+        distance=5.0 * meter,
+        name='chopper',
+    )
+    assert chopper.omega == two_pi * f
+
+
+def test_open_close_times_one_rotation():
+    f = 10.0 * Hz
+    chopper = tof.Chopper(
+        frequency=f,
+        open=sc.array(dims=['cutout'], values=[10.0], unit='deg'),
+        close=sc.array(dims=['cutout'], values=[20.0], unit='deg'),
+        phase=0.0 * deg,
+        distance=5.0 * meter,
+        name='chopper',
+    )
+
+    topen, tclose = chopper.open_close_times(0 * sec)
+    # Note that choppers perform one rotation before t=0 to make sure
+    # no openings are missed at early times.
+    assert sc.identical(topen[0], ((10.0 * deg).to(unit='rad') - two_pi) / (two_pi * f))
+    assert sc.identical(
+        tclose[0], ((20.0 * deg).to(unit='rad') - two_pi) / (two_pi * f)
+    )
+    assert sc.identical(topen[1], (10.0 * deg).to(unit='rad') / (two_pi * f))
+    assert sc.identical(tclose[1], (20.0 * deg).to(unit='rad') / (two_pi * f))
+
+
+def test_open_close_times_three_rotations():
+    f = 10.0 * Hz
+    chopper = tof.Chopper(
+        frequency=f,
+        open=sc.array(dims=['cutout'], values=[10.0], unit='deg'),
+        close=sc.array(dims=['cutout'], values=[20.0], unit='deg'),
+        phase=0.0 * deg,
+        distance=5.0 * meter,
+        name='chopper',
+    )
+
+    topen, tclose = chopper.open_close_times(0.21 * sec)
+    open0 = (10.0 * deg).to(unit='rad')
+    close0 = (20.0 * deg).to(unit='rad')
+    assert sc.identical(
+        topen,
+        sc.concat(
+            [open0 - two_pi, open0, open0 + two_pi, open0 + 2 * two_pi], dim='cutout'
+        )
+        / (two_pi * f),
+    )
+    assert sc.identical(
+        tclose,
+        sc.concat(
+            [close0 - two_pi, close0, close0 + two_pi, close0 + 2 * two_pi],
+            dim='cutout',
+        )
+        / (two_pi * f),
+    )
+
+
+def test_open_close_angles_scalars_converted_to_arrays():
+    f = 10.0 * Hz
+    chopper = tof.Chopper(
+        frequency=f,
+        open=10.0 * deg,
+        close=20.0 * deg,
+        phase=0.0 * deg,
+        distance=5.0 * meter,
+        name='chopper',
+    )
+    topen, tclose = chopper.open_close_times(0.0 * sec)
+    assert sc.identical(topen[1], (10.0 * deg).to(unit='rad') / (two_pi * f))
+    assert sc.identical(tclose[1], (20.0 * deg).to(unit='rad') / (two_pi * f))
+
+
+def test_phase():
+    f = 10.0 * Hz
+    chopper1 = tof.Chopper(
+        frequency=f,
+        open=sc.array(dims=['cutout'], values=[10.0], unit='deg'),
+        close=sc.array(dims=['cutout'], values=[20.0], unit='deg'),
+        phase=0.0 * deg,
+        distance=10.0 * meter,
+        name='chopper1',
+    )
+    topen1, tclose1 = chopper1.open_close_times(0.0 * sec)
+    chopper2 = tof.Chopper(
+        frequency=f,
+        open=sc.array(dims=['cutout'], values=[10.0], unit='deg'),
+        close=sc.array(dims=['cutout'], values=[20.0], unit='deg'),
+        phase=30.0 * deg,
+        distance=10.0 * meter,
+        name='chopper2',
+    )
+    topen2, tclose2 = chopper2.open_close_times(0.0 * sec)
+    assert sc.allclose(topen2, topen1 + (30.0 * deg).to(unit='rad') / chopper2.omega)
+    assert sc.allclose(tclose2, tclose1 + (30.0 * deg).to(unit='rad') / chopper2.omega)
+
+
+def test_phase_int():
+    f = 10.0 * Hz
+    op = sc.array(dims=['cutout'], values=[10.0], unit='deg')
+    cl = sc.array(dims=['cutout'], values=[20.0], unit='deg')
+    d = 10.0 * meter
+    chopper1 = tof.Chopper(
+        frequency=f,
+        open=op,
+        close=cl,
+        phase=30.0 * deg,
+        distance=d,
+        name='chopper1',
+    )
+    chopper2 = tof.Chopper(
+        frequency=f,
+        open=op,
+        close=cl,
+        phase=30 * deg,
+        distance=d,
+        name='chopper2',
+    )
+    topen1, tclose1 = chopper1.open_close_times(0.0 * sec)
+    topen2, tclose2 = chopper2.open_close_times(0.0 * sec)
+    assert sc.allclose(topen1, topen2)
+    assert sc.allclose(tclose1, tclose2)
+
+
+def test_frequency_must_be_positive():
+    with pytest.raises(ValueError, match="Chopper frequency must be positive"):
+        tof.Chopper(
+            frequency=-1.0 * Hz,
+            open=0.0 * deg,
+            close=10.0 * deg,
+            phase=0.0 * deg,
+            distance=5.0 * meter,
+            name='chopper',
+        )
+
+
+def test_open_close_times_counter_rotation():
+    f = 10.0 * Hz
+    d = 10.0 * meter
+    ph = 0.0 * deg
+    chopper1 = tof.Chopper(
+        frequency=f,
+        open=sc.array(dims=['cutout'], values=[10.0, 90.0], unit='deg'),
+        close=sc.array(dims=['cutout'], values=[20.0, 130.0], unit='deg'),
+        phase=ph,
+        distance=d,
+        name='chopper1',
+    )
+    chopper2 = tof.Chopper(
+        frequency=f,
+        open=sc.array(
+            dims=['cutout'], values=[360.0 - 130.0, 360.0 - 20.0], unit='deg'
+        ),
+        close=sc.array(
+            dims=['cutout'], values=[360.0 - 90.0, 360.0 - 10.0], unit='deg'
+        ),
+        phase=ph,
+        distance=d,
+        direction=tof.AntiClockwise,
+        name='chopper2',
+    )
+
+    topen1, tclose1 = chopper1.open_close_times(0.0 * sec)
+    topen2, tclose2 = chopper2.open_close_times(0.0 * sec)
+    assert sc.allclose(topen1, topen2)
+    assert sc.allclose(tclose1, tclose2)
+
+
+def test_open_close_times_counter_rotation_with_phase():
+    f = 10.0 * Hz
+    chopper1 = tof.Chopper(
+        frequency=f,
+        open=sc.array(dims=['cutout'], values=[80.0], unit='deg'),
+        close=sc.array(dims=['cutout'], values=[90.0], unit='deg'),
+        phase=0.0 * deg,
+        distance=10.0 * meter,
+        direction=tof.AntiClockwise,
+        name='chopper1',
+    )
+    topen1, tclose1 = chopper1.open_close_times(0.0 * sec)
+    chopper2 = tof.Chopper(
+        frequency=f,
+        open=sc.array(dims=['cutout'], values=[80.0], unit='deg'),
+        close=sc.array(dims=['cutout'], values=[90.0], unit='deg'),
+        phase=30.0 * deg,
+        distance=10.0 * meter,
+        direction=tof.AntiClockwise,
+        name='chopper2',
+    )
+    topen2, tclose2 = chopper2.open_close_times(0.0 * sec)
+    assert sc.allclose(
+        topen2, topen1 + (30.0 * deg).to(unit='rad') / abs(chopper2.omega)
+    )
+    assert sc.allclose(
+        tclose2, tclose1 + (30.0 * deg).to(unit='rad') / abs(chopper2.omega)
+    )
+
+
+def test_open_close_anticlockwise_multiple_rotations():
+    chopper = tof.Chopper(
+        frequency=10.0 * Hz,
+        open=sc.array(dims=["cutout"], values=[10.0, 90.0], unit="deg"),
+        close=sc.array(dims=["cutout"], values=[20.0, 130.0], unit="deg"),
+        distance=10.0 * meter,
+        phase=0 * deg,
+        direction=tof.AntiClockwise,
+        name='chopper',
+    )
+
+    two_rotations_open, two_rotations_close = chopper.open_close_times(0.0 * sec)
+    three_rotations_open, three_rotations_close = chopper.open_close_times(0.2 * sec)
+    four_rotations_open, four_rotations_close = chopper.open_close_times(0.3 * sec)
+
+    assert len(two_rotations_open) == 4
+    assert len(three_rotations_open) == 6
+    assert len(four_rotations_open) == 8
+    assert len(two_rotations_close) == 4
+    assert len(three_rotations_close) == 6
+    assert len(four_rotations_close) == 8
+
+    assert sc.allclose(two_rotations_open, three_rotations_open[:-2])
+    assert sc.allclose(two_rotations_close, three_rotations_close[:-2])
+    assert sc.allclose(three_rotations_open, four_rotations_open[:-2])
+    assert sc.allclose(three_rotations_close, four_rotations_close[:-2])
+    assert sc.allclose(two_rotations_open, four_rotations_open[:-4])
+    assert sc.allclose(two_rotations_close, four_rotations_close[:-4])
+
+
+def test_bad_direction_raises():
+    f = 10.0 * Hz
+    op = sc.array(dims=['cutout'], values=[10.0], unit='deg')
+    cl = sc.array(dims=['cutout'], values=[20.0], unit='deg')
+    d = 10.0 * meter
+    ph = 0.0 * deg
+    tof.Chopper(
+        frequency=f,
+        open=op,
+        close=cl,
+        phase=ph,
+        distance=d,
+        direction=tof.Clockwise,
+        name='chopper',
+    )
+    tof.Chopper(
+        frequency=f,
+        open=op,
+        close=cl,
+        phase=ph,
+        distance=d,
+        direction=tof.AntiClockwise,
+        name='chopper',
+    )
+    with pytest.raises(
+        ValueError, match="Chopper direction must be Clockwise or AntiClockwise"
+    ):
+        tof.Chopper(
+            frequency=f,
+            open=op,
+            close=cl,
+            phase=ph,
+            distance=d,
+            direction='clockwise',
+            name='chopper',
+        )
+    with pytest.raises(
+        ValueError, match="Chopper direction must be Clockwise or AntiClockwise"
+    ):
+        tof.Chopper(
+            frequency=f,
+            open=op,
+            close=cl,
+            phase=ph,
+            distance=d,
+            direction='anti-clockwise',
+            name='chopper',
+        )
+    with pytest.raises(
+        ValueError, match="Chopper direction must be Clockwise or AntiClockwise"
+    ):
+        tof.Chopper(
+            frequency=f,
+            open=op,
+            close=cl,
+            phase=ph,
+            distance=d,
+            direction=1,
+            name='chopper',
+        )
+
+
+def test_chopper_create_from_centers_and_widths():
+    f = 10.0 * Hz
+    centers = sc.array(dims=['cutout'], values=[15.0, 46.0], unit='deg')
+    widths = sc.array(dims=['cutout'], values=[10.0, 16.0], unit='deg')
+    chopper = tof.Chopper(
+        frequency=f,
+        centers=centers,
+        widths=widths,
+        phase=0.0 * deg,
+        distance=5.0 * meter,
+        name='chopper',
+    )
+    assert sc.allclose(chopper.open, centers - widths / 2)
+    assert sc.allclose(chopper.close, centers + widths / 2)
+
+    expected = tof.Chopper(
+        frequency=f,
+        open=sc.array(dims=['cutout'], values=[10.0, 38.0], unit='deg'),
+        close=sc.array(dims=['cutout'], values=[20.0, 54.0], unit='deg'),
+        phase=0.0 * deg,
+        distance=5.0 * meter,
+        name='chopper',
+    )
+    assert sc.allclose(chopper.open, expected.open)
+    assert sc.allclose(chopper.close, expected.close)
+
+
+def test_chopper_create_raises_when_both_edges_and_centers_are_supplied():
+    with pytest.raises(
+        ValueError, match="Either open/close or centers/widths must be provided"
+    ):
+        tof.Chopper(
+            frequency=10.0 * Hz,
+            open=10.0 * deg,
+            close=20.0 * deg,
+            centers=15.0 * deg,
+            widths=10.0 * deg,
+            phase=0.0 * deg,
+            distance=5.0 * meter,
+            name='chopper',
+        )
