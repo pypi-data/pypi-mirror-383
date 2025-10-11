c49a5318f9a5a767a25da8bcd2f1b20b54296618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:18 +0000

    Add package types-aiobotocore-ec2-instance-connect/2.25.0/types_aiobotocore_ec2_instance_connect-2.25.0.tar.gz

---
+++ packages/types-aiobotocore-ec2-instance-connect/types_aiobotocore_ec2_instance_connect-2.25.0.tar.gz/types_aiobotocore_ec2_instance_connect-2.25.0/types_aiobotocore_ec2_instance_connect/client.pyi
@@ -0,0 +1,136 @@
+"""
+Type annotations for ec2-instance-connect service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ec2_instance_connect/client/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_ec2_instance_connect.client import EC2InstanceConnectClient
+
+    session = get_session()
+    async with session.create_client("ec2-instance-connect") as client:
+        client: EC2InstanceConnectClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from types import TracebackType
+from typing import Any
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .type_defs import (
+    SendSerialConsoleSSHPublicKeyRequestTypeDef,
+    SendSerialConsoleSSHPublicKeyResponseTypeDef,
+    SendSSHPublicKeyRequestTypeDef,
+    SendSSHPublicKeyResponseTypeDef,
+)
+
+if sys.version_info >= (3, 9):
+    from builtins import type as Type
+    from collections.abc import Mapping
+else:
+    from typing import Mapping, Type
+if sys.version_info >= (3, 12):
+    from typing import Self, Unpack
+else:
+    from typing_extensions import Self, Unpack
+
+__all__ = ("EC2InstanceConnectClient",)
+
+class Exceptions(BaseClientExceptions):
+    AuthException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    EC2InstanceNotFoundException: Type[BotocoreClientError]
+    EC2InstanceStateInvalidException: Type[BotocoreClientError]
+    EC2InstanceTypeInvalidException: Type[BotocoreClientError]
+    EC2InstanceUnavailableException: Type[BotocoreClientError]
+    InvalidArgsException: Type[BotocoreClientError]
+    SerialConsoleAccessDisabledException: Type[BotocoreClientError]
+    SerialConsoleSessionLimitExceededException: Type[BotocoreClientError]
+    SerialConsoleSessionUnavailableException: Type[BotocoreClientError]
+    SerialConsoleSessionUnsupportedException: Type[BotocoreClientError]
+    ServiceException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+
+class EC2InstanceConnectClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2-instance-connect.html#EC2InstanceConnect.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ec2_instance_connect/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        EC2InstanceConnectClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2-instance-connect.html#EC2InstanceConnect.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ec2_instance_connect/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2-instance-connect/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ec2_instance_connect/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2-instance-connect/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ec2_instance_connect/client/#generate_presigned_url)
+        """
+
+    async def send_ssh_public_key(
+        self, **kwargs: Unpack[SendSSHPublicKeyRequestTypeDef]
+    ) -> SendSSHPublicKeyResponseTypeDef:
+        """
+        Pushes an SSH public key to the specified EC2 instance for use by the specified
+        user.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2-instance-connect/client/send_ssh_public_key.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ec2_instance_connect/client/#send_ssh_public_key)
+        """
+
+    async def send_serial_console_ssh_public_key(
+        self, **kwargs: Unpack[SendSerialConsoleSSHPublicKeyRequestTypeDef]
+    ) -> SendSerialConsoleSSHPublicKeyResponseTypeDef:
+        """
+        Pushes an SSH public key to the specified EC2 instance.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2-instance-connect/client/send_serial_console_ssh_public_key.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ec2_instance_connect/client/#send_serial_console_ssh_public_key)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2-instance-connect.html#EC2InstanceConnect.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ec2_instance_connect/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: Type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2-instance-connect.html#EC2InstanceConnect.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ec2_instance_connect/client/)
+        """
