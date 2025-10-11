6e534bc01b2b5b900918a8ac51e90ee7ee93c0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 03:12:42 +0000

    Add package whitebox-plugin-device-insta360/0.1.14/whitebox_plugin_device_insta360-0.1.14-py3-none-any.whl

---
+++ packages/whitebox-plugin-device-insta360/whitebox_plugin_device_insta360-0.1.14-py3-none-any.whl/whitebox_plugin_device_insta360/plugin.py
@@ -0,0 +1,70 @@
+import django_rq
+
+from whitebox import import_whitebox_model, Plugin
+from .tasks import handle_recording_for_flight_session
+
+DeviceConnection = import_whitebox_model("device.DeviceConnection")
+
+
+class WhiteboxPluginDeviceInsta360(Plugin):
+    """
+    A plugin that enables support for Insta360 cameras.
+
+    Attributes:
+        name: The name of the plugin.
+    """
+
+    name = "Insta360 Camera Support"
+    registered = False
+
+    def get_device_classes(self) -> list:
+        # Defer loading of device classes to allow for `device-manager` to first
+        # register its base classes into the class registry, as base classes
+        # cannot proxied
+        from whitebox_plugin_device_insta360.devices import (
+            Insta360X3,
+            Insta360X4,
+            RealInsta360X4,
+        )
+
+        return [Insta360X3, Insta360X4, RealInsta360X4]
+
+    def on_load(self):
+        self.whitebox.register_event_callback(
+            "flight.start",
+            self.on_flight_start,
+        )
+
+    def on_unload(self):
+        self.whitebox.unregister_event_callback(
+            "flight.start",
+            self.on_flight_start,
+        )
+
+    async def on_flight_start(self, data, ctx):
+        flight_session = ctx["flight_session"]
+
+        targets = [device_class.codename for device_class in self.get_device_classes()]
+
+        connection_ids = DeviceConnection.objects.filter(
+            codename__in=targets,
+            connection_type="network",
+        ).values_list("pk", flat=True)
+
+        device_count = await connection_ids.acount()
+        self.logger.info(
+            f"Flight session started (ID: {flight_session.id}), "
+            f"initializing {device_count} Insta360 devices...",
+        )
+
+        async for connection_id in connection_ids:
+            django_rq.enqueue(
+                handle_recording_for_flight_session,
+                device_connection_id=connection_id,
+                flight_session_id=flight_session.id,
+                # RQ queue settings
+                job_timeout="24h",
+            )
+
+
+plugin_class = WhiteboxPluginDeviceInsta360
