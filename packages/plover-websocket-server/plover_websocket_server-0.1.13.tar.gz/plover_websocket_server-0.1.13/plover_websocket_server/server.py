e1b0b578aa4956242de88996f4ac8bf85959b02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 07:04:30 +0000

    Add package plover-websocket-server/0.1.13/plover_websocket_server-0.1.13.tar.gz

---
+++ packages/plover-websocket-server/plover_websocket_server-0.1.13.tar.gz/plover_websocket_server-0.1.13/plover_websocket_server/server.py
@@ -0,0 +1,110 @@
+"""Core engine server definitions."""
+
+from asyncio import Future, run_coroutine_threadsafe
+from enum import Enum, auto
+from threading import Thread
+
+from plover_websocket_server.listens import Listens
+
+
+class ServerStatus(Enum):
+    """Represents the status of the server.
+
+    Attributes:
+        Stopped: The server is stopped.
+        Running: The server is running.
+
+    """
+
+    Stopped = auto()
+    Running = auto()
+
+
+class EngineServer:
+    """A server for the Plover engine.
+
+    Attributes:
+        status: The current status of the server.
+
+    """
+
+    listened: Listens = Listens()
+    data: Listens = Listens()
+
+    def __init__(self, host: str, port: str) -> None:
+        """Initialize the server.
+
+        Args:
+            host: The host address for the server to run on.
+            port: The port for the server to run on.
+
+        """
+        self._thread = Thread(target=self._start, name="ServerThread")
+        # it's not recommended to subclass Thread because some of its methods
+        # might be accidentally overloaded, for example _stop.
+        # Documented in Python documentation: https://docs.python.org/3/library/threading.html#thread-objects
+
+        self._host = host
+        self._port = port
+
+        self._loop = None
+        self._callbacks = []
+        self.listened.status = ServerStatus.Stopped
+
+    def start(self) -> None:
+        """Function to start the server (external thread)."""
+        self._thread.start()
+
+    def join(self) -> None:
+        """Function to stop the underlying thread."""
+        self._thread.join()
+
+    def queue_message(self, data: dict) -> Future:
+        """Queues a message for the server to broadcast.
+
+        Assumes it is called from a thread different from the event loop.
+
+        Args:
+            data: The data in JSON format to broadcast.
+
+        """
+        if not self._loop:
+            return
+
+        return run_coroutine_threadsafe(self._broadcast_message(data), self._loop)
+
+    def queue_stop(self) -> Future:
+        """Queues the server to stop.
+
+        Assumes it is called from a thread different from the event loop.
+        """
+        if not self._loop:
+            return
+
+        return run_coroutine_threadsafe(self._stop(), self._loop)
+
+    def _start(self) -> None:
+        """Starts the server.
+
+        Will create a blocking event loop.
+        """
+        raise NotImplementedError()
+
+    async def _stop(self) -> None:
+        """Stops the server.
+
+        Performs any clean up operations as needed.
+        """
+        raise NotImplementedError()
+
+    async def _broadcast_message(self, data: dict) -> None:
+        """Broadcasts a message to connected clients.
+
+        Args:
+            data: The data in JSON format to broadcast.
+
+        """
+        raise NotImplementedError()
+
+    def register_message_callback(self, callback) -> None:
+        self.data.add_listener(callback)
---
+++ packages/plover-websocket-server/plover_websocket_server-0.1.13.tar.gz/plover_websocket_server-0.1.13/tests/test_server.py
@@ -0,0 +1,113 @@
+from asyncio import get_event_loop
+from collections.abc import Awaitable
+
+from plover import log
+from pytest import mark
+
+from plover_websocket_server.manager import EngineServerManager, ServerStatus
+from tests.client_example import Client
+
+
+async def websocket(client: Client):
+    """Callback function for handling websocket connections.
+
+    Args:
+        client (Client): The client object representing the websocket connection.
+
+    Returns:
+        None
+
+    """
+    await client.connect_to_websocket({"messageTwo": "testTwo"})
+    message = {"name": "Georgia"}
+    await client.send_websocket_message(message)
+    reply = await client.receive_json()
+    assert reply == message
+
+
+async def ping_pong(client: Client):
+    """Callback function for testing websocket heartbeat (ping-pong).
+
+    Args:
+        client (Client): The client object representing the websocket connection.
+
+    Returns:
+        None
+
+    """
+    await client.connect_to_websocket({"message": "ping-pong test"})
+    await client.ping()
+    reply = await client.receive_raw()
+    assert reply == "pong"
+
+
+async def http(client: Client):
+    """Handle the callback for HTTP requests.
+
+    Args:
+        client (Client): The client making the HTTP request.
+
+    Returns:
+        None
+
+    """
+    pass
+
+
+@mark.parametrize("callback", [http, websocket, ping_pong])
+def test_server(callback: Awaitable) -> None:
+    """Test the server functionality.
+
+    Args:
+        callback (Awaitable): The callback function to be executed.
+
+    Returns:
+        None
+
+    """
+    esm = EngineServerManager(None, True)
+
+    async def server_loop_handler() -> None:
+        """Handles the server loop by starting the server.
+
+        Waiting for it to run, and then executing the callback function when the server is running.
+
+        Args:
+            callback (Awaitable): The callback function to be executed when the server is running.
+
+        Returns:
+            None
+
+        """
+
+        async def status_change_listener(status) -> None:
+            """Listens for status changes and performs actions based on the status.
+
+            Args:
+                status: The status of the server.
+
+            Returns:
+                None
+
+            """
+            if status == ServerStatus.Running:
+                client = Client(
+                    esm._config.host,
+                    esm._config.port,
+                    esm._config.public_key,
+                    esm._config.ssl,
+                )
+                data = await client.send_message({"messageOne": "testOne"})
+                assert data == f"ws{client.protocol()}://"
+                await callback(client)
+                await client.disconnect()
+                esm.raw_stop()
+
+        esm.add_listener(status_change_listener)
+        esm.start()
+        esm.join()
+        log.info("Joining main thread...")
+
+    loop = get_event_loop()
+    loop.run_until_complete(server_loop_handler())
+    log.info("Joined!")
