88d4355f462f3648efd907c7434d09f2c32e796d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 15:04:56 +0000

    Add package juq-py/0.5.2/juq_py-0.5.2.tar.gz

---
+++ packages/juq-py/juq_py-0.5.2.tar.gz/juq_py-0.5.2/tests/test_papermill_clean.py
@@ -0,0 +1,65 @@
+import json
+import sys
+from os.path import join
+from subprocess import check_call, CalledProcessError
+from tempfile import TemporaryDirectory
+
+from papermill import execute_notebook, PapermillExecutionError
+from pytest import mark, raises
+from utz import env
+
+from juq.papermill.clean import papermill_clean_cmd
+from juq.papermill.run import papermill_run_cmd
+from tests.utils import MERGE_OUTPUTS_DIR, TEST_DIR
+
+
+@mark.xfail(sys.version_info[:3] != (3, 11, 13), reason="Test fixtures are Python 3.11.13-specific")
+def test_papermill_clean():
+    nb_path = join(MERGE_OUTPUTS_DIR, "merged-outputs.ipynb")
+    with TemporaryDirectory() as tmpdir:
+        post_papermill_path = join(tmpdir, 'post-papermill.ipynb')
+        execute_notebook(nb_path, post_papermill_path)
+        with open(post_papermill_path, 'r') as f:
+            post_papermill = json.load(f)
+        assert 'papermill' in post_papermill['cells'][0]['metadata']
+        assert 'papermill' in post_papermill['metadata']
+
+        cleaned_path = join(tmpdir, 'cleaned.ipynb')
+        papermill_clean_cmd.callback(post_papermill_path, cleaned_path)
+        with open(cleaned_path, 'r') as f:
+            cleaned_nb = json.load(f)
+        split_outputs_nb_path = join(MERGE_OUTPUTS_DIR, "split-outputs.ipynb")
+        with open(split_outputs_nb_path, 'r') as f:
+            split_outputs_nb = json.load(f)
+    assert cleaned_nb == split_outputs_nb
+
+
+@mark.xfail(sys.version_info[:3] != (3, 11, 13), reason="Test fixtures are Python 3.11.13-specific")
+def test_papermill_error():
+    nb_path = join(TEST_DIR, "test-err.ipynb")
+    with TemporaryDirectory() as tmpdir, env(NO_COLOR="1"):
+        out_path1 = join(tmpdir, 'out1.ipynb')
+        with raises(PapermillExecutionError):
+            papermill_run_cmd.callback(
+                nb_path, out_path1,
+                keep_ids=False,
+                keep_tags=None,
+                parameter_strs=(),
+                request_save_on_cell_execute=False,
+                autosave_cell_every=False,
+            )
+
+        with open(join(TEST_DIR, "test-err-out.ipynb"), 'rt') as f:
+            expected = f.read()
+
+        with open(out_path1, 'rt') as f:
+            actual1 = f.read()
+        assert actual1 == expected
+
+        out_path2 = join(tmpdir, 'out2.ipynb')
+        with raises(CalledProcessError):
+            check_call([ 'juq', 'papermill', 'run', '-n1', nb_path, out_path2 ])
+
+        with open(out_path2, 'rt') as f:
+            actual2 = f.read()
+        assert actual2 == expected
---
+++ packages/juq-py/juq_py-0.5.2.tar.gz/juq_py-0.5.2/tests/test_papermill_run.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import json
+import sys
+from os.path import join, basename
+from tempfile import TemporaryDirectory
+
+from pytest import mark
+
+from juq.papermill.run import papermill_run_cmd
+from tests.test_papermill_clean import TEST_DIR
+
+
+def check(
+    in_name: str,
+    expected_name: str | None = None,
+    **kwargs,
+):
+    in_path = join(TEST_DIR, in_name)
+    if expected_name is None:
+        expected_name = in_name
+    expected_path = join(TEST_DIR, expected_name)
+    name = basename(expected_path)
+    with TemporaryDirectory() as tmpdir:
+        out_path = join(tmpdir, name)
+        papermill_run_cmd.callback(in_path, out_path, **kwargs)
+        with open(out_path, 'r') as f:
+            actual = json.load(f)
+    with open(expected_path, 'r') as f:
+        expected = json.load(f)
+    assert actual == expected
+
+
+@mark.xfail(sys.version_info[:3] != (3, 11, 13), reason="Test fixtures are Python 3.11.13-specific")
+def test_mixed_tags():
+    check("mixed-tags.ipynb")
+
+
+@mark.xfail(sys.version_info[:3] != (3, 11, 13), reason="Test fixtures are Python 3.11.13-specific")
+def test_mixed_tags_params():
+    check(
+        "mixed-tags-params.ipynb",
+        "mixed-tags-params-222.ipynb",
+        parameter_strs=("num=222",),
+    )
+
+
+@mark.xfail(sys.version_info[:3] != (3, 11, 13), reason="Test fixtures are Python 3.11.13-specific")
+def test_run_previously_run_nb():
+    check(
+        "mixed-tags-params-222.ipynb",
+        "mixed-tags-params-333.ipynb",
+        parameter_strs=("num=333",),
+    )
+
+
+@mark.xfail(sys.version_info[:3] != (3, 11, 13), reason="Test fixtures are Python 3.11.13-specific")
+def test_mixed_tags_keep():
+    check(
+        "mixed-tags.ipynb",
+        "mixed-tags-keep.ipynb",
+        keep_tags=True,
+    )
+
+
+@mark.xfail(sys.version_info[:3] != (3, 11, 13), reason="Test fixtures are Python 3.11.13-specific")
+def test_mixed_tags_drop():
+    check(
+        "mixed-tags.ipynb",
+        "mixed-tags-drop.ipynb",
+        keep_tags=False,
+    )
