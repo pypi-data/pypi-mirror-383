2bf6baa83e82fd446dcffd75b19fc18f26f6cf6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:32 +0000

    Add package meltanolabs-target-snowflake/0.12.3/meltanolabs_target_snowflake-0.12.3-py3-none-any.whl

---
+++ packages/meltanolabs-target-snowflake/meltanolabs_target_snowflake-0.12.3-py3-none-any.whl/target_snowflake/snowflake_types.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import datetime as dt
+import typing as t
+
+import snowflake.sqlalchemy.custom_types as sct
+from sqlalchemy.types import DateTime
+
+
+class TIMESTAMP_NTZ(sct.TIMESTAMP_NTZ):  # noqa: N801
+    """Snowflake TIMESTAMP_NTZ type."""
+
+    def __init__(self, *args: t.Any, **kwargs: t.Any) -> None:
+        super().__init__(*args, **kwargs)
+
+    @property
+    def python_type(self):
+        return dt.datetime
+
+    def as_generic(self, **kwargs: t.Any):  # noqa: ARG002
+        return DateTime()
+
+
+class NUMBER(sct.NUMBER):
+    """Snowflake NUMBER type."""
+
+    def __init__(self, *args: t.Any, **kwargs: t.Any) -> None:
+        super().__init__(*args, **kwargs)
+
+    @property
+    def python_type(self):
+        return float
+
+
+class VARIANT(sct.VARIANT):
+    """Snowflake VARIANT type."""
+
+    def __init__(self, *args: t.Any, **kwargs: t.Any) -> None:
+        super().__init__(*args, **kwargs)
+
+    @property
+    def python_type(self):
+        return dict
