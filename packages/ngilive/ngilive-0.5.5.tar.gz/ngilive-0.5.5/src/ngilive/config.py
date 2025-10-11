67aefbca3cb46c17ce050321ba29120a3823cdc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 14:04:41 +0000

    Add package ngilive/0.5.5/ngilive-0.5.5.tar.gz

---
+++ packages/ngilive/ngilive-0.5.5.tar.gz/ngilive-0.5.5/src/ngilive/config.py
@@ -0,0 +1,6 @@
+CLIENT_ID = "nl-public"
+AUTHORIZE_URL = "https://keycloak.ngiapi.no/auth/realms/tenant-geohub-public/protocol/openid-connect/auth"
+TOKENS_URL = "https://keycloak.ngiapi.no/auth/realms/tenant-geohub-public/protocol/openid-connect/token"
+BASE_URL = "https://api.ngilive.no"
+
+APP_NAME = "NGILive"
