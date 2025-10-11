6691fee0f4ff7b76230bbc8c77fcf8adbe79478d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-11 04:04:07 +0000

    Add package pylint-per-file-ignores/3.0.0/pylint_per_file_ignores-3.0.0-py3-none-any.whl

---
+++ packages/pylint-per-file-ignores/pylint_per_file_ignores-3.0.0-py3-none-any.whl/pylint_per_file_ignores/__init__.py
@@ -0,0 +1,7 @@
+"""pylint-per-file-ignores plugin."""
+
+from __future__ import annotations
+
+from pylint_per_file_ignores._plugin import load_configuration, register
+
+__all__ = ("load_configuration", "register")
