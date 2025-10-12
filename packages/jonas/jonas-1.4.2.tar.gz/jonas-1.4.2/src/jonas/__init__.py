b14357545d8b2ab7473a8224d26f3f55b2c9c75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:43 +0000

    Add package jonas/1.4.2/jonas-1.4.2.tar.gz

---
+++ packages/jonas/jonas-1.4.2.tar.gz/jonas-1.4.2/src/jonas/__init__.py
@@ -0,0 +1,9 @@
+"""
+JONAS - Just Operate Nicely And Securely
+
+Ein intelligenter Shell-Assistent mit OpenAI Integration.
+"""
+
+__version__ = "1.4.2"
+__author__ = "Peter Filz"
+__email__ = "peter.filz@googlemail.com"
