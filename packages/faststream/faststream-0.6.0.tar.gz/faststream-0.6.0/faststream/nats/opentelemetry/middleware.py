826ca124bee21bc8e201a3568d40cbe0069bd2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:40 +0000

    Add package faststream/0.6.0/faststream-0.6.0.tar.gz

---
+++ packages/faststream/faststream-0.6.0.tar.gz/faststream-0.6.0/faststream/nats/opentelemetry/middleware.py
@@ -0,0 +1,23 @@
+from opentelemetry.metrics import Meter, MeterProvider
+from opentelemetry.trace import TracerProvider
+
+from faststream.nats.opentelemetry.provider import telemetry_attributes_provider_factory
+from faststream.nats.response import NatsPublishCommand
+from faststream.opentelemetry.middleware import TelemetryMiddleware
+
+
+class NatsTelemetryMiddleware(TelemetryMiddleware[NatsPublishCommand]):
+    def __init__(
+        self,
+        *,
+        tracer_provider: TracerProvider | None = None,
+        meter_provider: MeterProvider | None = None,
+        meter: Meter | None = None,
+    ) -> None:
+        super().__init__(
+            settings_provider_factory=telemetry_attributes_provider_factory,
+            tracer_provider=tracer_provider,
+            meter_provider=meter_provider,
+            meter=meter,
+            include_messages_counters=True,
+        )
