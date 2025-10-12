02883d786f65255fb40b0d006581a19fa6aa3a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:12 +0000

    Add package django-ledger/0.8.2.3/django_ledger-0.8.2.3.tar.gz

---
+++ packages/django-ledger/django_ledger-0.8.2.3.tar.gz/django_ledger-0.8.2.3/django_ledger/views/data_import.py
@@ -0,0 +1,290 @@
+"""
+Django Ledger created by Miguel Sanda <msanda@arrobalytics.com>.
+Copyright© EDMA Group Inc licensed under the GPLv3 Agreement.
+
+Contributions to this module:
+    * Miguel Sanda <msanda@arrobalytics.com>
+"""
+
+from datetime import datetime, time
+from typing import Optional
+
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+from django.utils.timezone import make_aware
+from django.utils.translation import gettext_lazy as _
+from django.views import View
+from django.views.generic import DeleteView, DetailView, FormView, ListView, UpdateView
+
+from django_ledger.forms.data_import import (
+    ImportJobModelCreateForm,
+    ImportJobModelUpdateForm,
+    StagedTransactionModelFormSet,
+)
+from django_ledger.io.ofx import OFXFileManager
+from django_ledger.models import StagedTransactionModelValidationError
+from django_ledger.models.data_import import ImportJobModel, StagedTransactionModel
+from django_ledger.views.mixins import DjangoLedgerSecurityMixIn
+
+
+class ImportJobModelViewBaseView(DjangoLedgerSecurityMixIn):
+    queryset = None
+
+    def get_queryset(self):
+        if self.queryset is None:
+            self.queryset = (
+                ImportJobModel.objects.for_entity(
+                    entity_model=self.AUTHORIZED_ENTITY_MODEL,
+                )
+                .order_by('-created')
+                .select_related(
+                    'bank_account_model',
+                    'bank_account_model__entity_model',
+                    'bank_account_model__account_model',
+                    'bank_account_model__account_model__coa_model',
+                )
+            )
+        return self.queryset
+
+
+class ImportJobModelCreateView(ImportJobModelViewBaseView, FormView):
+    template_name = 'django_ledger/data_import/import_job_create.html'
+    PAGE_TITLE = _('Create Import Job')
+    extra_context = {'page_title': PAGE_TITLE, 'header_title': PAGE_TITLE}
+    form_class = ImportJobModelCreateForm
+
+    def get_form(self, form_class=None, **kwargs):
+        return self.form_class(entity_model=self.get_authorized_entity_instance(), **self.get_form_kwargs())
+
+    def get_success_url(self):
+        return reverse(
+            'django_ledger:data-import-jobs-list',
+            kwargs={'entity_slug': self.kwargs['entity_slug']},
+        )
+
+    def form_valid(self, form):
+        ofx_manager = OFXFileManager(ofx_file_or_path=form.files['ofx_file'])
+        import_job: ImportJobModel = form.save(commit=False)
+        import_job.configure(commit=False)
+        import_job.save()
+
+        txs_to_stage = ofx_manager.get_account_txs()
+        staged_txs_model_list = [
+            StagedTransactionModel(
+                date_posted=make_aware(value=datetime.combine(date=tx.dtposted.date(), time=time.min)),
+                fit_id=tx.fitid,
+                amount=tx.trnamt,
+                import_job=import_job,
+                name=tx.name,
+                memo=tx.memo,
+            )
+            for tx in txs_to_stage
+        ]
+        for tx in staged_txs_model_list:
+            tx.clean()
+
+        StagedTransactionModel.objects.bulk_create(staged_txs_model_list)
+        return super().form_valid(form=form)
+
+
+class ImportJobModelListView(ImportJobModelViewBaseView, ListView):
+    PAGE_TITLE = _('Data Import Jobs')
+    extra_context = {'page_title': PAGE_TITLE, 'header_title': PAGE_TITLE}
+    context_object_name = 'import_jobs'
+    template_name = 'django_ledger/data_import/data_import_job_list.html'
+
+
+class ImportJobModelUpdateView(ImportJobModelViewBaseView, UpdateView):
+    template_name = 'django_ledger/data_import/import_job_update.html'
+    context_object_name = 'import_job_model'
+    pk_url_kwarg = 'job_pk'
+    form_class = ImportJobModelUpdateForm
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        import_job_model: ImportJobModel = self.object
+        ctx['page_title'] = 'Import Job Update'
+        ctx['header_title'] = 'Import Job Update'
+        ctx['header_subtitle'] = import_job_model.description
+        ctx['header_subtitle_icon'] = 'solar:import-bold'
+        return ctx
+
+    def get_success_url(self):
+        entity_model = self.get_authorized_entity_instance()
+        return reverse(
+            viewname='django_ledger:data-import-jobs-list',
+            kwargs={'entity_slug': entity_model.slug},
+        )
+
+    def form_valid(self, form):
+        messages.add_message(
+            self.request,
+            level=messages.SUCCESS,
+            message=_(f'Successfully updated Import Job {self.object.description}'),
+            extra_tags='is-success',
+        )
+        return super().form_valid(form=form)
+
+
+class ImportJobModelDeleteView(ImportJobModelViewBaseView, DeleteView):
+    template_name = 'django_ledger/data_import/import_job_delete.html'
+    context_object_name = 'import_job_model'
+    pk_url_kwarg = 'job_pk'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['page_title'] = 'Delete Import Job'
+        ctx['header_title'] = 'Delete Import Job'
+        ctx['header_subtitle'] = self.object.description
+        ctx['header_subtitle_icon'] = 'solar:import-bold'
+        return ctx
+
+    def get_success_url(self):
+        return reverse(
+            viewname='django_ledger:data-import-jobs-list',
+            kwargs={'entity_slug': self.AUTHORIZED_ENTITY_MODEL.slug},
+        )
+
+
+class DataImportJobDetailView(ImportJobModelViewBaseView, DetailView):
+    template_name = 'django_ledger/data_import/data_import_job_txs.html'
+    PAGE_TITLE = _('Import Job Staged Txs')
+    context_object_name = 'import_job'
+    pk_url_kwarg = 'job_pk'
+    import_transactions = False
+    form_class = StagedTransactionModelFormSet
+    http_method_names = ['get', 'post']
+
+    def get_form_kwargs(self):
+        return {
+            'entity_model': self.get_authorized_entity_instance(),
+            'import_job_model': self.get_object(),
+        }
+
+    def get_context_data(self, txs_formset: Optional[StagedTransactionModelFormSet] = None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        import_job_model: ImportJobModel = self.object
+
+        context['page_title'] = import_job_model.description
+        context['header_title'] = self.PAGE_TITLE
+        context['header_subtitle'] = import_job_model.description
+        context['header_subtitle_icon'] = 'tabler:table-import'
+
+        staged_txs_formset = (
+            StagedTransactionModelFormSet(
+                entity_model=self.get_authorized_entity_instance(),
+                import_job_model=import_job_model,
+            )
+            if not txs_formset
+            else txs_formset
+        )
+
+        context['staged_txs_formset'] = staged_txs_formset
+
+        return context
+
+    def post(self, request, **kwargs):
+        self.object = self.get_object()
+        import_job_model: ImportJobModel = self.object
+
+        txs_formset = StagedTransactionModelFormSet(
+            entity_model=self.AUTHORIZED_ENTITY_MODEL,
+            import_job_model=import_job_model,
+            data=request.POST,
+        )
+
+        if txs_formset.has_changed():
+            for tx_form in txs_formset:
+                if any(
+                    [
+                        'account_model' in tx_form.changed_data,
+                        'tx_split' in tx_form.changed_data,
+                    ]
+                ):
+                    staged_transaction_model: StagedTransactionModel = tx_form.instance
+                    staged_transaction_model.activity = None
+
+            if txs_formset.is_valid():
+                txs_formset.save()
+                for tx_form in txs_formset:
+                    if tx_form.has_changed():
+                        staged_transaction_model: StagedTransactionModel = tx_form.instance
+
+                        is_split = tx_form.cleaned_data['tx_split'] is True
+                        is_import = tx_form.cleaned_data['tx_import'] is True
+                        is_bundled = tx_form.cleaned_data['bundle_split'] is True
+
+                        if is_split:
+                            staged_transaction_model.add_split()
+                        elif is_import:
+                            if staged_transaction_model.can_migrate_receipt():
+                                staged_transaction_model.migrate_receipt(
+                                    receipt_date=staged_transaction_model.date_posted,
+                                    split_amount=not is_bundled,
+                                )
+                            else:
+                                staged_transaction_model.migrate_transactions(split_txs=not is_bundled)
+            else:
+                context = self.get_context_data(txs_formset=txs_formset, **kwargs)
+                return self.render_to_response(context)
+
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            'Successfully saved transactions.',
+            extra_tags='is-success',
+        )
+
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context=context)
+
+
+class StagedTransactionUndoView(ImportJobModelViewBaseView, View):
+    http_method_names = ['post']
+
+    def post(self, request, entity_slug, job_pk, staged_tx_pk, *args, **kwargs):
+        import_job_model = get_object_or_404(self.get_queryset(), uuid__exact=job_pk)
+        staged_txs_qs = import_job_model.stagedtransactionmodel_set.all()
+        staged_tx = get_object_or_404(staged_txs_qs, uuid__exact=staged_tx_pk)
+        try:
+            staged_tx.undo_import()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                _('Successfully undone import for transaction %(name)s.')
+                % {'name': staged_tx.name or staged_tx.fit_id},
+                extra_tags='is-success',
+            )
+        except StagedTransactionModelValidationError as e:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                e.message,
+                extra_tags='is-danger',
+            )
+        return redirect(
+            reverse(
+                'django_ledger:data-import-job-txs',
+                kwargs={'entity_slug': entity_slug, 'job_pk': job_pk},
+            )
+        )
+
+
+class ImportJobModelResetView(ImportJobModelViewBaseView, DetailView):
+    pk_url_kwarg = 'job_pk'
+    http_method_names = ['post']
+
+    def post(self, request, **kwargs):
+        import_job_model: ImportJobModel = self.get_object()
+        imported_staged_txs = import_job_model.stagedtransactionmodel_set.all()
+        for staged_tx in imported_staged_txs:
+            staged_tx.undo_import(raise_exception=False)
+            if staged_tx.is_children():
+                staged_tx.delete()
+
+
+        return redirect(
+            to=import_job_model.get_data_import_url(),
+            permanent=False,
+        )
