6f98216e82d4ec68ec8f8e34de9cc79d4d07ebcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:39 +0000

    Add package hyperscale-ozone/0.16.3/hyperscale_ozone-0.16.3.tar.gz

---
+++ packages/hyperscale-ozone/hyperscale_ozone-0.16.3.tar.gz/hyperscale_ozone-0.16.3/tests/test_cloudtrail.py
@@ -0,0 +1,20 @@
+import cfnlint
+
+from hyperscale.ozone.cloudtrail import OrganizationalCloudTrail
+
+
+def test_organizational_cloudtrail():
+    ct = OrganizationalCloudTrail()
+    t = ct.create_template()
+    errors = cfnlint.lint(
+        t.to_json(),
+    )
+    print(t.to_json())
+    assert not errors
+    d = t.to_dict()
+    params = d["Parameters"]
+    assert "OrgId" in params
+    assert "LogArchiveBucketName" in params
+
+    resources = d["Resources"]
+    assert "OrgTrail" in resources
