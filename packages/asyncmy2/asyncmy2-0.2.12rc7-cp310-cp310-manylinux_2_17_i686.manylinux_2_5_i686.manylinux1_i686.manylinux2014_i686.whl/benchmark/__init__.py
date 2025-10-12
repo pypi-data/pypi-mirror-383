30fbe31d9a9160dc74821af2c3182fbfd5c79360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:59 +0000

    Add package asyncmy2/0.2.12rc7/asyncmy2-0.2.12rc7-cp310-cp310-manylinux_2_17_i686.manylinux_2_5_i686.manylinux1_i686.manylinux2014_i686.whl

---
+++ packages/asyncmy2/asyncmy2-0.2.12rc7-cp310-cp310-manylinux_2_17_i686.manylinux_2_5_i686.manylinux1_i686.manylinux2014_i686.whl/benchmark/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+import MySQLdb
+import pymysql
+import uvloop
+
+uvloop.install()
+
+connection_kwargs = dict(
+    host="127.0.0.1", port=3306, user="root", password=os.getenv("MYSQL_PASS", "123456"), autocommit=True
+)
+conn_mysqlclient = MySQLdb.connect(**connection_kwargs)
+conn_pymysql = pymysql.connect(**connection_kwargs)
+COUNT = 50000
+
+data = [
+    (
+        1,
+        "2021-01-01",
+        "2020-07-16 22:49:54",
+        1,
+        "asyncmy",
+        1,
+    )
+    for _ in range(COUNT)
+]
+sql = """INSERT INTO test.asyncmy(`decimal`, `date`, `datetime`, `float`, `string`, `tinyint`) VALUES (%s,%s,%s,%s,%s,%s)"""
