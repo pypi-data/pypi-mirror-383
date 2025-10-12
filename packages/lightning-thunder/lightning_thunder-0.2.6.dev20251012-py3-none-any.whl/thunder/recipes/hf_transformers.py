4fcae9f2915e515f676d994816ee8627bc7467e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:49 +0000

    Add package lightning-thunder/0.2.6.dev20251012/lightning_thunder-0.2.6.dev20251012-py3-none-any.whl

---
+++ packages/lightning-thunder/lightning_thunder-0.2.6.dev20251012-py3-none-any.whl/thunder/recipes/hf_transformers.py
@@ -0,0 +1,185 @@
+from distutils.version import LooseVersion
+from functools import partial
+import warnings
+
+import thunder
+from thunder.recipes import BaseRecipe
+from thunder import Recipe
+
+
+class InplaceIndexCopyTransform(thunder.Transform):
+    def __init__(self):
+        super().__init__()
+        self.executor = thunder.extend.OperatorExecutor("inplace_index_copy_ex")
+        thunder.extend.register_executor(self.executor)  # needed if you want to pickle traces
+
+        def inplace_index_copy_meta(buffer, dim, idx, val):
+            return thunder.TensorProxy(like=buffer)
+
+        def inplace_index_copy_impl(buffer, dim, idx, val):
+            return buffer.index_copy_(dim, idx, val)
+
+        self.inplace_index_copy = self.executor.register_operator(
+            "inplace_index_copy", fn=inplace_index_copy_impl, meta=inplace_index_copy_meta
+        )
+
+    def get_executor(self):
+        return self.executor
+
+    def transform_traces_pre_prologue(self, pro, comp, epi, **kwargs):
+        comp_new = thunder.core.trace.from_trace(comp)
+        for bsym in comp.bound_symbols:
+            if bsym.sym == thunder.torch.index_copy:
+                bsym.args[0].tags.add(thunder.core.proxies.ProxyTag.STATIC_MEMORY_LOCATION)
+                bsym = bsym.from_bsym(sym=self.inplace_index_copy)
+            else:
+                bsym = bsym.from_bsym()
+            comp_new.bound_symbols.append(bsym)
+        return pro, comp_new, epi
+
+
+@Recipe.register("hf-transformers")
+class HFTransformers(BaseRecipe):
+    """
+    Recipe tuned for Hugging Face ``transformers`` models.
+
+    Args:
+        show_progress (bool, optional): Forwarded to :class:`BaseRecipe`.
+        interpreter (str, optional): Thunder interpreter to use.
+        plugins (Iterable | None, optional): Extra Thunder plugins.
+    """
+
+    def __init__(
+        self,
+        show_progress=False,
+        interpreter="thunder.jit",
+        plugins=None,
+    ):
+        super().__init__(show_progress=show_progress, interpreter=interpreter, plugins=plugins)
+
+        # for kv-cache inplace ops
+        self.inplace_index_copy_transform = InplaceIndexCopyTransform()
+        self.executor_names.append(self.inplace_index_copy_transform.executor.name)
+
+    @classmethod
+    def validate(cls, model):
+        """
+        Emit warnings (or errors) if *model* falls outside the supported
+        transformer versions or base classes.
+
+        Args:
+            model (transformers.PreTrainedModel): Model instance to vet.
+
+        Raises:
+            ValueError: If *model* is not a ``PreTrainedModel``.
+        """
+        import transformers
+
+        version = LooseVersion(transformers.__version__)
+        min_version = LooseVersion("4.46.0")
+        max_version = LooseVersion("4.55.4")
+
+        if version < min_version or version > max_version:
+            warnings.warn(
+                f"`transformers` version {version} detected. The HFTransformers recipe supports {min_version} to {max_version}."
+            )
+
+        supported = [
+            transformers.BertPreTrainedModel,
+            transformers.LlamaPreTrainedModel,
+            transformers.Phi3PreTrainedModel,
+            transformers.Qwen2PreTrainedModel,
+        ]
+        supported_str = "\n".join(f"* {el.__name__}" for el in supported)
+
+        if not any(cls for cls in supported if isinstance(model, cls)):
+            warnings.warn(
+                f"instance of {type(model).__name__} found. The HFTransformers recipe supports:\n{supported_str}"
+            )
+
+        if not isinstance(model, transformers.PreTrainedModel):
+            raise ValueError(f"The model must be an instance of PreTrainedModel, found {type(model)}")
+
+    def setup_config(self):
+        """
+        Enable NV-kernelised linear, matmul and SDPA ops on top of the
+        base recipe’s debug configuration.
+
+        Returns:
+            dict[str, Any]: Thunder config dictionary augmented with
+            ``nv_enable_*`` flags.
+        """
+        config = super().setup_config()
+        config.update(nv_enable_linear=True, nv_enable_matmul=True, nv_enable_sdpa=True)
+        return config
+
+    def setup_lookasides(self):
+        """
+        Swap out the warning helper when running under
+        the non Thunder-FX interpreter.
+
+        Returns:
+            list[thunder.core.recipe.Lookaside] | None
+        """
+        if self.interpreter == thunder.core.recipe.Interpreter.THUNDER_FX:
+            return None
+
+        import transformers
+
+        warn_lookaside = thunder.core.recipe.Lookaside(
+            fn=transformers.modeling_utils.PreTrainedModel.warn_if_padding_and_no_attention_mask,
+            replace_with=lambda *args: None,
+        )
+
+        # HF transformers (4.52.4) wraps something in autocast(device, enabled=False)
+        # but PyTorch (2.7) does not like this call when device is meta.
+        # So to trace on the meta device and because don't care about much about the autocast,
+        # we replace it with the nullcontext.
+        # We might allow more cases (call autocast if iot is nt with meta or not enabled=False?)
+        def autocast_lookaside(*args, enabled=True, **kwargs):
+            from contextlib import nullcontext
+
+            if enabled:
+                raise RuntimeError("don't do autocast")
+            return nullcontext()
+
+        import torch
+
+        autocast_lookaside = thunder.core.recipe.Lookaside(fn=torch.autocast, replace_with=autocast_lookaside)
+        return [warn_lookaside, autocast_lookaside]
+
+    def setup_transforms(self):
+        """
+        Prepend the ``InplaceIndexCopyTransform`` to the default
+        transform list.
+
+        Returns:
+            list[thunder.Transform]: transform list.
+        """
+        transforms = super().setup_transforms()
+        return [self.inplace_index_copy_transform] + transforms
+
+    def apply(self, model):
+        """
+        Apply the recipe (compile the model) and patch ``generate`` / ``_sample``
+        so they work after tracing.
+
+        Args:
+            model (transformers.PreTrainedModel): The model to compile.
+
+        Returns:
+            transformers.PreTrainedModel: Thunder-compiled model ready
+            for inference.
+        """
+        thunder_model = super().apply(model)
+
+        if getattr(thunder_model, "generate", None):
+            thunder_model.generate = partial(thunder_model.generate.__func__, thunder_model)
+
+        if getattr(thunder_model, "_sample", None):
+            thunder_model._sample = partial(thunder_model._sample.__func__, thunder_model)
+
+        if getattr(thunder_model, "_valid_auto_compile_criteria", None):
+            thunder_model._valid_auto_compile_criteria = lambda *args, **kwargs: False
+
+        return thunder_model
---
+++ packages/lightning-thunder/lightning_thunder-0.2.6.dev20251012-py3-none-any.whl/thunder/tests/test_elementwise.py
@@ -0,0 +1,138 @@
+from functools import partial
+import builtins
+
+import torch
+from torch.testing import assert_close, make_tensor
+
+import thunder
+import thunder.clang as tlang
+import thunder.torch as ttorch
+import thunder.core.devices as devices
+from thunder.tests.framework import instantiate, NOTHING
+
+
+# TODO Enable the remaining elementwise unary operations (following the pattern of abs)
+# TODO Expand testing to elementwise binary operations (following a similar pattern)
+@instantiate(dtypes=NOTHING, devicetypes=(devices.DeviceType.CPU,))
+def test_elementwise_dunder_operations_on_numbers(executor, device, dtype):
+    # op, allowed types
+    elementwise_unary_ops = (
+        (builtins.abs, (bool, int, float, complex)),
+        # (math.ceil, (bool, int, float)),
+        # (math.floor, (bool, int, float)),
+        # (operator.inv, (bool, int)),
+        # (operator.neg, (bool, int, float, complex)),
+        # # TODO see issue "Implement positive operations"
+        # # operator.pos,
+        # (builtins.round, (bool, int, float)),
+        # (math.trunc, (bool, int, float)),
+    )
+
+    bool_inps = [False, True]
+    int_inps = [-1, 0, 2]
+    float_inps = [-0.7, 0, 0.3, 1.1]
+    complex_inps = [complex(1, 0.3), complex(-4.1, 0.9)]
+
+    _type_to_input_map = {
+        bool: bool_inps,
+        int: int_inps,
+        float: float_inps,
+        complex: complex_inps,
+    }
+
+    def gather_inputs(allowed_types):
+        inps = []
+
+        for typ in allowed_types:
+            inps.extend(_type_to_input_map[typ])
+
+        return inps
+
+    for op, allowed_types in elementwise_unary_ops:
+
+        def foo(a):
+            return op(a)
+
+        cfoo = executor.make_callable(foo)
+
+        for a in gather_inputs(allowed_types):
+            actual = cfoo(a)
+            expected = foo(a)
+
+            assert_close(actual, expected)
+
+
+# TODO: see issue "Test operator and method variants of operations using
+# OpInfos"
+@instantiate(dtypes=(thunder.float32,))
+def test_core_tensor_methods(executor, device, dtype):
+    def foo(a, b, c, d):
+        return a + b - c + (d - a)
+
+    traced_foo = executor.make_callable(foo)
+
+    tdtype = ttorch.to_torch_dtype(dtype)
+    a = make_tensor((4, 4), device=device, dtype=tdtype)
+    b = make_tensor((2, 1, 4), device=device, dtype=tdtype)
+    c = make_tensor((4, 1), device=device, dtype=tdtype)
+    d = make_tensor((1, 1, 4), device=device, dtype=tdtype)
+
+    thunder_result = traced_foo(a, b, c, d)
+    torch_result = a + b - c + (d - a)
+    assert_close(thunder_result, torch_result)
+
+
+# NOTE This cannot be an OpInfo test (as OpInfos work today, anyway)
+#   because it tests multiple dtypes for correct promotion behavior
+@instantiate(dtypes=NOTHING)
+def test_where(executor, device, dtype):
+    # Tests where type promotion and number support
+
+    thunder_fn = executor.make_callable(tlang.where)
+    torch_fn = torch.where
+
+    shape = (5, 5)
+    make = partial(make_tensor, device=device)
+    pred = make(shape, dtype=torch.bool)
+
+    # int64 x float32
+    i64 = make(shape, dtype=torch.int64)
+    f32 = make(shape, dtype=torch.float32)
+
+    thunder_result = thunder_fn(pred, i64, f32)
+    torch_result = torch_fn(pred, i64, f32)
+    assert_close(thunder_result, torch_result)
+
+    # int x float32
+    thunder_result = thunder_fn(pred, 5, f32)
+    torch_result = torch_fn(pred, 5, f32)
+    assert_close(thunder_result, torch_result)
+
+    # int64 x int
+    thunder_result = thunder_fn(pred, i64, 5)
+    torch_result = torch_fn(pred, i64, 5)
+    assert_close(thunder_result, torch_result)
+
+    # int x int
+    thunder_result = thunder_fn(pred, -1, 5)
+    torch_result = torch_fn(pred, -1, 5)
+    assert_close(thunder_result, torch_result)
+
+    # int64 x float
+    thunder_result = thunder_fn(pred, i64, -2.3)
+    torch_result = torch_fn(pred, i64, -2.3)
+    assert_close(thunder_result, torch_result)
+
+    # TODO fix issue "Currently nvFuser tensor x float operations result in
+    # float64 results"
+    # float x int
+    # thunder_result = thunder_fn(pred, 3., 5)
+    # torch_result = torch_fn(pred, 3., 5)
+    # assert_close(thunder_result, torch_result)
+
+    # TODO:
+    # float x float
+    # int x complex
+    # int64 x complex
+    # int32 x complex64
+    # predicate as number (True/False)
