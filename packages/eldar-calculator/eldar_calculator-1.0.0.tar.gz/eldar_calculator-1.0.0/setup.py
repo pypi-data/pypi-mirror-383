613e6319107b7bdad96733df8f8dedb5ef3ad4c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:19:22 +0000

    Add package eldar-calculator/1.0.0/eldar_calculator-1.0.0.tar.gz

---
+++ packages/eldar-calculator/eldar_calculator-1.0.0.tar.gz/eldar_calculator-1.0.0/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="eldar_calculator",  # Kitabxana adı burada PyPI tələblərinə uyğun yazıldı
+    version="1.0.0",          # Yeni əsas versiya
+    packages=find_packages(),
+    install_requires=[],
+    author="Eldar Eliyev",
+    author_email="digitalocean2005eldar@gmail.com",
+    description="A powerful calculator library for Python — supports basic, advanced, geometry, algebra, and unit conversions.",
+    url="https://github.com/username/calculator",  # GitHub linkini özün dəyiş
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
