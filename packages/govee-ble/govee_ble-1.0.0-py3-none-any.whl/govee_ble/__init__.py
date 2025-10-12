5abcd6bdc737e98ad7c9cd7b3bf8522844d9b706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:04:38 +0000

    Add package govee-ble/1.0.0/govee_ble-1.0.0-py3-none-any.whl

---
+++ packages/govee-ble/govee_ble-1.0.0-py3-none-any.whl/govee_ble/__init__.py
@@ -0,0 +1,46 @@
+"""Parser for Govee BLE advertisements.
+
+This file is shamelessly copied from the following repository:
+https://github.com/Ernst79/bleparser/blob/c42ae922e1abed2720c7fac993777e1bd59c0c93/package/bleparser/govee.py
+
+MIT License applies.
+"""
+
+from __future__ import annotations
+
+from sensor_state_data import (
+    BinarySensorDeviceClass,
+    BinarySensorValue,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceClass,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+from sensor_state_data import (
+    DeviceClass,
+)
+
+from .parser import GoveeBluetoothDeviceData, SensorType, get_model_info, ModelInfo
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "GoveeBluetoothDeviceData",
+    "BinarySensorDeviceClass",
+    "SensorDeviceClass",
+    "BinarySensorValue",
+    "SensorType",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+    "get_model_info",
+    "ModelInfo",
+]
