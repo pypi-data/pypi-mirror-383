ac569709c90075ec996eac4baa3d144562eb5c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:55 +0000

    Add package ctfbridge/0.8.3/ctfbridge-0.8.3.tar.gz

---
+++ packages/ctfbridge/ctfbridge-0.8.3.tar.gz/ctfbridge-0.8.3/ctfbridge/platforms/pwnablexyz/services/challenge.py
@@ -0,0 +1,28 @@
+import logging
+from typing import List
+
+from ctfbridge.core.services.challenge import CoreChallengeService
+from ctfbridge.exceptions.challenge import ChallengeFetchError
+from ctfbridge.models.challenge import Challenge
+from ctfbridge.platforms.pwnablexyz.utils.parsers import parse_challenges
+
+logger = logging.getLogger(__name__)
+
+
+class PwnableXYZChallengeService(CoreChallengeService):
+    def __init__(self, client):
+        self._client = client
+
+    @property
+    def base_has_details(self) -> bool:
+        return True
+
+    async def _fetch_challenges(self) -> List[Challenge]:
+        try:
+            response = await self._client.get("/challenges/")
+            response.raise_for_status()
+            challenges = parse_challenges(response.text)
+            return challenges
+        except Exception as e:
+            logger.debug("Error while fetching or parsing challenges", exc_info=e)
+            raise ChallengeFetchError("Failed to fetch or parse challenges from pwnable.xyz") from e
