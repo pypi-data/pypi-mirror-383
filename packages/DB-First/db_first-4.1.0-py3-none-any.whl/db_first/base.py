6b01c3360f52c7ba62e2d751df4c9224be9f5f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 16:05:13 +0000

    Add package DB-First/4.1.0/db_first-4.1.0-py3-none-any.whl

---
+++ packages/DB-First/db_first-4.1.0-py3-none-any.whl/db_first/base.py
@@ -0,0 +1,24 @@
+from typing import Any
+from typing import Optional
+
+from .exc import MetaNotFound
+from .exc import OptionNotFound
+
+
+class BaseCRUD:
+    @classmethod
+    def _get_option_from_meta(cls, name: str, default: Optional[Any] = ...) -> Any:
+        try:
+            meta = cls.Meta
+        except AttributeError:
+            raise MetaNotFound('You need add class Meta with options.')
+
+        try:
+            option = getattr(meta, name)
+        except AttributeError:
+            if default is Ellipsis:
+                raise OptionNotFound(f'Option <{name}> not set in Meta class.')
+            else:
+                option = default
+
+        return option
