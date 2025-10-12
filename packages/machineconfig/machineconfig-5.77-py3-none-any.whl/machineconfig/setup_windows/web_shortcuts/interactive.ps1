c8056e8d2a916545f780c1340c218efd280ecce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:34 +0000

    Add package machineconfig/5.77/machineconfig-5.77-py3-none-any.whl

---
+++ packages/machineconfig/machineconfig-5.77-py3-none-any.whl/machineconfig/setup_windows/web_shortcuts/interactive.ps1
@@ -0,0 +1,31 @@
+
+
+iex (iwr "https://raw.githubusercontent.com/thisismygitrepo/machineconfig/main/src/machineconfig/setup_windows/uv.ps1").Content
+function devops {
+    & "$HOME\.local\bin\uv.exe" run --python 3.14 --with "machineconfig>=5.74" devops $args
+}
+
+function cloud {
+    & "$HOME\.local\bin\uv.exe" run --python 3.14 --with "machineconfig>=5.74" cloud $args
+}
+
+function croshell {
+    & "$HOME\.local\bin\uv.exe" run --python 3.14 --with "machineconfig>=5.74" croshell $args
+}
+
+function agents {
+    & "$HOME\.local\bin\uv.exe" run --python 3.14 --with "machineconfig>=5.74" agents $args
+}
+
+function fire {
+    & "$HOME\.local\bin\uv.exe" run --python 3.14 --with "machineconfig>=5.74" fire $args
+}
+
+function ftpx {
+    & "$HOME\.local\bin\uv.exe" run --python 3.14 --with "machineconfig>=5.74" ftpx $args
+}
+
+function sessions {
+    & "$HOME\.local\bin\uv.exe" run --python 3.14 --with "machineconfig>=5.74" sessions $args
+}
+
