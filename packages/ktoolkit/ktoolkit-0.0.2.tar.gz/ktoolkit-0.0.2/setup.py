a5415e0f35fb24e303225a61bc65b3e3531fb768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:04:42 +0000

    Add package ktoolkit/0.0.2/ktoolkit-0.0.2.tar.gz

---
+++ packages/ktoolkit/ktoolkit-0.0.2.tar.gz/ktoolkit-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  return "123"
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='ktoolkit',
+  version='0.0.2',
+  author='kolyax',
+  author_email='example@gmail.com',
+  description='This is the simplest module for quick work with files.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://example.com/',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='files speedfiles ',
+  
+  python_requires='>=3.6'
+)
