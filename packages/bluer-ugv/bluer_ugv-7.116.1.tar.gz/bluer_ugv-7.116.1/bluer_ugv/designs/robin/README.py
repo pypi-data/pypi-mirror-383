d1e18616224dda168b2b73c7085bb445780dd871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 15:05:55 +0000

    Add package bluer-ugv/7.116.1/bluer_ugv-7.116.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-7.116.1.tar.gz/bluer_ugv-7.116.1/bluer_ugv/designs/robin/README.py
@@ -0,0 +1,24 @@
+from bluer_objects.README.items import ImageItems
+
+from bluer_ugv.README.robin.consts import robin_assets2, robin_design
+
+
+items = ImageItems(
+    {
+        f"{robin_assets2}/20250712_114819.jpg": "",
+        f"{robin_design}/wiring.png": f"{robin_design}/wiring.svg",
+        f"{robin_assets2}/20250713_172325.jpg": "",
+        f"{robin_assets2}/20250713_172413.jpg": "",
+        f"{robin_assets2}/20250713_172442_1.gif": "",
+        f"{robin_assets2}/20250723_095022.jpg": "",
+        f"{robin_assets2}/20250723_095155~2_1.gif": "",
+        f"{robin_assets2}/20250728_112123.jpg": "",
+        f"{robin_assets2}/20250726_134024.jpg": "",
+        f"{robin_assets2}/20250807_103534.jpg": "",
+        f"{robin_assets2}/20250807_171354.jpg": "",
+        f"{robin_assets2}/20250813_172023.jpg": "",
+        f"{robin_assets2}/20250827_170346.jpg": "",
+        f"{robin_assets2}/20250827_170319~3_1.gif": "",
+        f"{robin_assets2}/20250914_175506-2.jpg": "",
+    }
+)
