748c83e6408e80c932b79b723ed8d25e51d392c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:10 +0000

    Add package hv4gha/0.4.1/hv4gha-0.4.1.tar.gz

---
+++ packages/hv4gha/hv4gha-0.4.1.tar.gz/hv4gha-0.4.1/README.md
@@ -0,0 +1,136 @@
+# HashiCorp Vault for GitHub Apps
+
+Python library for using [HashiCorp Vault][1]'s [Transit Engine][2] to
+manage a GitHub App's private RSA key. More precisely, the library
+provides the following pieces of functionality.
+
+* Perform initial one-way import of the App's private key into Vault
+* Issue (short-lived) GitHub Access Token
+  * Have Vault sign a JWT using the App's private key
+  * Exchange that JWT for a GitHub Access Token
+
+Conceptually Vault here fills the role of an HSM or a Cloud KMS.
+
+See [Authenticating as a GitHub App installation (GitHub Docs)][3] for context.
+
+The library is also tested against [OpenBao][6].
+
+## Installation
+
+```shell
+pip install hv4gha
+```
+
+## Usage
+
+In addition to the examples below see also the
+[hv4gha/entry.py](https://github.com/andreaso/hv4gha/blob/main/hv4gha/entry.py) docstrings.
+
+### Import App key
+
+```python
+from hv4gha import import_app_key
+
+with open("/path/to/github-app.private-key.pem", "r") as akh:
+    my_app_key = akh.read()
+
+response = import_app_key(
+    pem_key=my_app_key,
+    key_name="my-github-app",
+    vault_addr="https://vault.example.com:8200",
+    vault_token="...",
+)
+
+key_version = response["key_version"]
+```
+
+### Issue Access Token
+
+```python
+from hv4gha import issue_access_token
+
+response = issue_access_token(
+    key_name="my-github-app",
+    vault_addr="https://vault.example.com:8200",
+    vault_token="...",
+    app_client_id="Iv1.bc01362e9d72c72a",
+    account="andreaso",
+)
+
+access_token = response["access_token"]
+token_expiry = response["expires_at"]
+```
+
+### Issue scoped Access Token
+
+```python
+from hv4gha import issue_access_token
+
+response = issue_access_token(
+    key_name="my-github-app",
+    vault_addr="https://vault.example.com:8200",
+    vault_token="...",
+    app_client_id="Iv1.bc01362e9d72c72a",
+    account="andreaso",
+    permissions={"contents": "read"},
+    repositories=["world-domination"],
+)
+
+access_token = response["access_token"]
+token_expiry = response["expires_at"]
+```
+
+## Vault requirements
+
+Somewhat simplified, this is what's required Vault wise.
+
+### Transit secrets engine
+
+First of all, the [Transit Engine][2] needs to be enabled.
+
+```shell
+vault secrets enable transit
+```
+
+Here we are sticking to the default `transit/` mount point.
+
+### Import policy
+
+```HCL
+path "transit/wrapping_key" {
+  capabilities = ["read"]
+}
+
+path "transit/keys/my-github-app" {
+  capabilities = ["read"]
+}
+
+path "transit/keys/my-github-app/import" {
+  capabilities = ["update"]
+}
+
+path "transit/keys/my-github-app/import_version" {
+  capabilities = ["update"]
+}
+```
+
+### Issue policy
+
+```HCL
+path "transit/sign/my-github-app" {
+  capabilities = ["update"]
+}
+```
+
+### Vault Token
+
+For obtaining the initial Vault Token, see the [hvac][4] Python
+library and its [Auth Methods][5] documentation.
+
+
+[1]: https://www.vaultproject.io/
+[2]: https://developer.hashicorp.com/vault/docs/secrets/transit
+[3]: https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/authenticating-as-a-github-app-installation
+[4]: https://github.com/hvac/hvac
+[5]: https://hvac.readthedocs.io/en/stable/usage/auth_methods/
+[6]: https://openbao.org/
---
+++ packages/hv4gha/hv4gha-0.4.1.tar.gz/hv4gha-0.4.1/hv4gha/gh.py
@@ -0,0 +1,248 @@
+"""GitHub specific code"""
+
+from datetime import datetime
+from typing import Annotated, Final, Literal
+
+import requests
+from pydantic import BaseModel, Field, TypeAdapter, ValidationError
+from typing_extensions import NotRequired, TypedDict
+
+PermARW = Literal["admin", "read", "write"]
+PermRW = Literal["read", "write"]
+PermR = Literal["read"]
+PermW = Literal["write"]
+
+RepoName = Annotated[str, Field(max_length=100, pattern=r"^[a-zA-Z0-9_\-\.]+$")]
+
+
+class GitHubAPIError(Exception):
+    """Any error response from the GitHub API"""
+
+
+class InstallationLookupError(GitHubAPIError):
+    """Failure to lookup the GitHub App installation ID"""
+
+
+class TokenIssueError(GitHubAPIError):
+    """Failure to issue GitHub Access Token"""
+
+
+class NotInstalledError(Exception):
+    """The GitHub App isn't installed in the specified account"""
+
+
+class AccountInfo(TypedDict):
+    """Part of Installation"""
+
+    login: Annotated[
+        str, Field(max_length=39, pattern=r"^[a-zA-Z0-9]([a-zA-Z0-9\-]*[a-zA-Z0-9])?$")
+    ]
+
+
+class Installation(BaseModel):
+    """
+    https://docs.github.com/en/rest/apps/apps?apiVersion=2022-11-28#list-installations-for-the-authenticated-app
+    """
+
+    id: int
+    account: AccountInfo
+
+
+class TokenPermissions(TypedDict, total=False):
+    """Part of AccessTokenResponse and AccessTokenRequest"""
+
+    # Repository permissions
+    actions: PermRW
+    administration: PermRW
+    checks: PermRW
+    contents: PermRW
+    deployments: PermRW
+    environments: PermRW
+    issues: PermRW
+    metadata: PermRW
+    packages: PermRW
+    pages: PermRW
+    pull_requests: PermRW
+    repository_hooks: PermRW
+    repository_projects: PermARW
+    secret_scanning_alerts: PermRW
+    secrets: PermRW
+    security_events: PermRW
+    single_file: PermRW
+    statuses: PermRW
+    vulnerability_alerts: PermRW
+    workflows: PermW
+    # Organizational permissions
+    members: PermRW
+    organization_administration: PermRW
+    organization_custom_roles: PermRW
+    organization_announcement_banners: PermRW
+    organization_hooks: PermRW
+    organization_personal_access_tokens: PermRW
+    organization_personal_access_token_requests: PermRW
+    organization_plan: PermR
+    organization_projects: PermARW
+    organization_packages: PermRW
+    organization_secrets: PermRW
+    organization_self_hosted_runners: PermRW
+    organization_user_blocking: PermRW
+    team_discussions: PermRW
+
+
+class Repository(TypedDict):
+    """Part of AccessTokenResponse"""
+
+    name: RepoName
+
+
+class AccessTokenResponse(BaseModel):
+    """
+    https://docs.github.com/en/rest/apps/apps?apiVersion=2022-11-28#create-an-installation-access-token-for-an-app
+    """
+
+    token: str
+    expires_at: datetime
+    permissions: TokenPermissions
+    repositories: None | list[Repository] = None
+
+
+class AccessTokenRequest(BaseModel, validate_assignment=True, extra="forbid"):
+    """
+    https://docs.github.com/en/rest/apps/apps?apiVersion=2022-11-28#create-an-installation-access-token-for-an-app
+    """
+
+    permissions: None | TokenPermissions = None
+    repositories: None | list[RepoName] = None
+
+
+class TokenResponse(TypedDict):
+    """Typing for customized Access Token response"""
+
+    access_token: str
+    expires_at: datetime
+    permissions: TokenPermissions
+    repositories: NotRequired[list[str]]
+
+
+class GitHubApp:
+    """GitHub App Access Tokens, etc"""
+
+    def __init__(self, *, account: str, jwt_token: str):
+        """
+        :param account: GitHub account to access
+        :param jwt_token: GitHub App JWT token
+        """
+
+        self.account: Final[str] = account
+        self.auth_headers: Final[dict[str, str]] = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {jwt_token}",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+
+    def __find_installation(self) -> str:
+        lookup_url = "https://api.github.com/app/installations"
+
+        pagination_params = {
+            "page": 1,
+            "per_page": 100,
+        }
+
+        more = True
+        while more:
+            more = False
+            try:
+                response = requests.get(
+                    lookup_url,
+                    headers=self.auth_headers,
+                    params=pagination_params,
+                    timeout=10,
+                )
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as http_error:
+                raise InstallationLookupError(http_error.response.text) from http_error
+
+            try:
+                ita = TypeAdapter(list[Installation])
+                installations = ita.validate_python(response.json())
+            except ValidationError as validation_error:
+                error_message = "<Failed to parse Installations API response>"
+                raise InstallationLookupError(error_message) from validation_error
+
+            for installation in installations:
+                if installation.account["login"].lower() == self.account.lower():
+                    return str(installation.id)
+
+            if "next" in response.links:
+                pagination_params["page"] += 1
+                more = True
+
+        failure = f'App appear not to be installed in the "{self.account}" account'
+        raise NotInstalledError(failure)
+
+    def issue_token(
+        self,
+        *,
+        permissions: None | TokenPermissions = None,
+        repositories: None | list[RepoName] = None,
+    ) -> TokenResponse:
+        """
+        Issue GitHub Access Token
+
+        :param permissions: Optionally scope (down) token permissions.
+        :param repositories: Optionally limit accessible repositories.
+
+        :return: The requested access token; together with its expiry
+            time, permission scope and optionally covered repositories.
+        """
+
+        params_bm = AccessTokenRequest()
+        if permissions:
+            try:
+                params_bm.permissions = permissions
+            except ValidationError as validation_error:
+                raise ValueError("Invalid GitHub permission(s)") from validation_error
+        if repositories:
+            try:
+                params_bm.repositories = repositories
+            except ValidationError as validation_error:
+                raise ValueError("Invalid repository name(s)") from validation_error
+
+        installation_id: str = self.__find_installation()
+        issue_url = "/".join(
+            [
+                "https://api.github.com/app/installations",
+                installation_id,
+                "access_tokens",
+            ]
+        )
+
+        try:
+            response = requests.post(
+                issue_url,
+                headers=self.auth_headers,
+                data=params_bm.model_dump_json(exclude_unset=True),
+                timeout=10,
+            )
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as http_error:
+            raise TokenIssueError(http_error.response.text) from http_error
+
+        try:
+            access_token_bm = AccessTokenResponse(**response.json())
+        except ValidationError as validation_error:
+            error_message = "<Failed to parse Token Issue API response>"
+            raise TokenIssueError(error_message) from validation_error
+
+        access_token: TokenResponse = {
+            "access_token": access_token_bm.token,
+            "expires_at": access_token_bm.expires_at,
+            "permissions": access_token_bm.permissions,
+        }
+
+        if access_token_bm.repositories is not None:
+            access_token["repositories"] = sorted(
+                [repo["name"] for repo in access_token_bm.repositories]
+            )
+
+        return access_token
