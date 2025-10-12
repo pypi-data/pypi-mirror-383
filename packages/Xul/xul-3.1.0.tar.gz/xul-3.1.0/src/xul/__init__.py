eccaad1ae2b034453104f3fd28751fea50795fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 15:06:07 +0000

    Add package Xul/3.1.0/xul-3.1.0.tar.gz

---
+++ packages/Xul/xul-3.1.0.tar.gz/xul-3.1.0/src/xul/__init__.py
@@ -0,0 +1,13 @@
+"""XML Utilities.
+
+XML scripts in Python.
+
+ * ppx: pretty print XML source in human readable form
+ * xp: select XML nodes with XPath
+ * transform: transform XML with XSLT
+ * validate: validate XML with XSD, DTD or RELAX NG
+"""
+
+# Xul version.
+__version_info__ = ("3", "1", "0")
+__version__ = ".".join(__version_info__)
