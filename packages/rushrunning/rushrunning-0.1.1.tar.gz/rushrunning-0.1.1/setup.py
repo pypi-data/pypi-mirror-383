69c7852924e48a23a4db92e5a472cdadd8f458b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:34 +0000

    Add package rushrunning/0.1.1/rushrunning-0.1.1.tar.gz

---
+++ packages/rushrunning/rushrunning-0.1.1.tar.gz/rushrunning-0.1.1/setup.py
@@ -0,0 +1,26 @@
+﻿from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='rushrunning',
+    version='0.1.1',
+    packages=find_packages(),
+    requires=[
+        'rushlib'
+    ],
+    description='python data',
+    author='ndrzy',
+    author_email='dandan0019@outlook.com',
+    python_requires='>=3.10',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/meatdumplings0019/rushrunning",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
