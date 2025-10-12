86e62b053d51dce940f9110376a9565dcd181c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:32 +0000

    Add package ab-auth-client/0.1.5/ab_auth_client-0.1.5.tar.gz

---
+++ packages/ab-auth-client/ab_auth_client-0.1.5.tar.gz/ab_auth_client-0.1.5/src/ab_core/auth_client/oauth2/schema/token.py
@@ -0,0 +1,11 @@
+
+from pydantic import BaseModel
+
+
+class OAuth2Token(BaseModel):
+    access_token: str
+    id_token: str | None = None
+    refresh_token: str | None = None
+    expires_in: int
+    scope: str | None = None
+    token_type: str
