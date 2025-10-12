07601659dcf3503426b3b7e614e46058c8392a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 15:06:07 +0000

    Add package Maclip/2.4/Maclip-2.4.tar.gz

---
+++ packages/Maclip/Maclip-2.4.tar.gz/Maclip-2.4/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import  setup
+import setuptools
+packages = ['ma-clip']
+setup(name='Maclip',
+	version='2.4',
+	author='Beyond Cosine Similarity: Magnitude-Aware CLIP for No-Reference Image Quality Assessment',
+    packages=packages, 
+    package_dir={'requests': 'requests'},)
+
