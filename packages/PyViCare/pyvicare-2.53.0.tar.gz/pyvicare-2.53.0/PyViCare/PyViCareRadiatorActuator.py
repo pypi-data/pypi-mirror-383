ed5c787fa3ecee77e527db94f0130f1dfe6a6091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:04:58 +0000

    Add package PyViCare/2.53.0/pyvicare-2.53.0.tar.gz

---
+++ packages/PyViCare/pyvicare-2.53.0.tar.gz/pyvicare-2.53.0/PyViCare/PyViCareRadiatorActuator.py
@@ -0,0 +1,29 @@
+from PyViCare.PyViCareDevice import ZigbeeBatteryDevice
+from PyViCare.PyViCareUtils import handleAPICommandErrors, handleNotSupported
+
+
+class RadiatorActuator(ZigbeeBatteryDevice):
+
+    @handleNotSupported
+    def getTemperature(self):
+        return self.service.getProperty("device.sensors.temperature")["properties"]["value"]["value"]
+
+    @handleNotSupported
+    def getValvePosition(self) -> int:
+        return int(self.service.getProperty("trv.valve.position")["properties"]["position"]["value"])
+
+    @handleNotSupported
+    def getChildLock(self) -> str:
+        return str(self.service.getProperty("trv.childLock")["properties"]["status"]["value"])
+
+    @handleNotSupported
+    def getMountingMode(self) -> bool:
+        return bool(self.service.getProperty("trv.mountingMode")["properties"]["active"]["value"])
+
+    @handleNotSupported
+    def getTargetTemperature(self):
+        return self.service.getProperty("trv.temperature")["properties"]["value"]["value"]
+
+    @handleAPICommandErrors
+    def setTargetTemperature(self, temperature):
+        return self.service.setProperty("trv.temperature", "setTargetTemperature", {'temperature': float(temperature)})
