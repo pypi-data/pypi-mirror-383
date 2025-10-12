989dd04e457efcb8505d62bf077315d1d92e89af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:14 +0000

    Add package cocina/0.0.1/cocina-0.0.1-py3-none-any.whl

---
+++ packages/cocina/cocina-0.0.1-py3-none-any.whl/cocina/__init__.py
@@ -0,0 +1,8 @@
+"""
+Project Kit Package
+
+A comprehensive collection of tools for building Python projects with
+sophisticated configuration management and job execution capabilities.
+
+License: CC-BY-4.0
+"""
