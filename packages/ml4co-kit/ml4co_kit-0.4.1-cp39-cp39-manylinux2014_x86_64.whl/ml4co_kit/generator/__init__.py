f14aa3950d1e2b32c7bb2d0f717f12ff2a4e5cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:05:03 +0000

    Add package ml4co-kit/0.4.1/ml4co_kit-0.4.1-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/ml4co-kit/ml4co_kit-0.4.1-cp39-cp39-manylinux2014_x86_64.whl/ml4co_kit/generator/__init__.py
@@ -0,0 +1,35 @@
+r"""
+Generator Module.
+"""
+
+# Copyright (c) 2024 Thinklab@SJTU
+# ML4CO-Kit is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+# http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+
+# Base Generator
+from .base import GeneratorBase
+
+# Graph Generator
+from .graph.base import (
+    GraphGeneratorBase, GRAPH_TYPE, GRAPH_WEIGHT_TYPE, GraphWeightGenerator
+)
+from .graph.mcl import MClGenerator
+from .graph.mcut import MCutGenerator
+from .graph.mis import MISGenerator
+from .graph.mvc import MVCGenerator
+
+# Routing Generator
+from .routing.base import RoutingGenerator
+from .routing.atsp import ATSPGenerator, ATSP_TYPE
+from .routing.cvrp import CVRPGenerator, CVRP_TYPE
+from .routing.op import OPGenerator, OP_TYPE
+from .routing.pctsp import PCTSPGenerator, PCTSP_TYPE
+from .routing.spctsp import SPCTSPGenerator, SPCTSP_TYPE
+from .routing.tsp import TSPGenerator, TSP_TYPE
