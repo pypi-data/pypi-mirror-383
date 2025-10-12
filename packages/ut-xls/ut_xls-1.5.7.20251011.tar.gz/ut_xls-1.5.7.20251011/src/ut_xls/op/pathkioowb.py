67a678da0c8ae5aca45fd0eafc97810caa69f777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:04:57 +0000

    Add package ut-xls/1.5.7.20251011/ut_xls-1.5.7.20251011.tar.gz

---
+++ packages/ut-xls/ut_xls-1.5.7.20251011.tar.gz/ut_xls-1.5.7.20251011/src/ut_xls/op/pathkioowb.py
@@ -0,0 +1,43 @@
+from typing import Any, TypeAlias
+
+import openpyxl as op
+
+from ut_path.pathk import PathK
+from ut_xls.op.pathioowb import PathIooWb
+
+TyWb: TypeAlias = op.workbook.workbook.Workbook
+
+TyArr = list[Any]
+TyDic = dict[Any, Any]
+TyAoA = list[TyArr]
+TyAoD = list[TyDic]
+TyDoAoA = dict[Any, TyAoA]
+TyDoAoD = dict[Any, TyAoD]
+TyPath = str
+TyPathK = str
+TySheet = int | str
+
+TnPath = None | TyPath
+TnWb = None | TyWb
+
+
+def sh_path(func):
+    def wrapper(pathk: TyPathK, kwargs: TyDic, *args):
+        _path: TyPath = PathK.sh_path(pathk, kwargs, *args)
+        return func(_path, kwargs, *args)
+    return wrapper
+
+
+class PathKIooWb:
+
+    @staticmethod
+    @sh_path
+    def write(pathk: TyPathK, kwargs: TyDic, wb: TnWb) -> None:
+        PathIooWb.write(pathk, wb)
+
+    @staticmethod
+    @sh_path
+    def write_wb_from_doaod(pathk: TyPathK, kwargs: TyDic, doaod: TyDoAoD) -> None:
+        if not doaod:
+            return
+        PathIooWb.write_wb_from_doaod(pathk, doaod)
