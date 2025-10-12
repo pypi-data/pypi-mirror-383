fc18e37eb23978c9b2691693b025e60152ab5868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 15:05:21 +0000

    Add package scons-msvc-env/1.0.0/scons_msvc_env-1.0.0.tar.gz

---
+++ packages/scons-msvc-env/scons_msvc_env-1.0.0.tar.gz/scons_msvc_env-1.0.0/README.md
@@ -0,0 +1,14 @@
+# scons_msvc_env
+
+[![PyPI - Version](https://img.shields.io/pypi/v/scons_msvc_env.svg)](https://pypi.org/project/scons_msvc_env)
+[![License - MIT](https://img.shields.io/badge/license-MIT-green)](https://spdx.org/licenses/MIT.html)
+
+-----
+
+scons_msvc_env is my way of using the [Microsoft Visual C++](
+https://visualstudio.microsoft.com/vs/features/cplusplus/) compiler in
+conjunction with [SCons](https://scons.org/).
+
+See [rgrep](https://github.com/RoccoMatano/rgrep/blob/master/bld.py) for an
+application example.
+
---
+++ packages/scons-msvc-env/scons_msvc_env-1.0.0.tar.gz/scons_msvc_env-1.0.0/bld.py
@@ -0,0 +1,40 @@
+import sys
+import subprocess
+import argparse
+from pathlib import Path
+import setuptools.build_meta as backend
+
+################################################################################
+
+THIS_SCRIPT = Path(__file__).resolve()
+
+################################################################################
+
+def perform_action(args):
+    if args.action not in ("build_sdist", "build_wheel"):
+        raise ValueError(f"unknown action: '{args.action}'")
+
+    getattr(backend, args.action)(str(THIS_SCRIPT.parent / "dist"))
+
+################################################################################
+
+def initiate_actions():
+    # distutils - that setuptools.build_meta is based on - currently do NOT
+    # support to execute more than one action per process. Therefore we
+    # have to use a child process for every action.
+    common = [sys.executable, str(THIS_SCRIPT), "--action"]
+    subprocess.run([*common, "build_sdist"], check=True)
+    subprocess.run([*common, "build_wheel"], check=True)
+
+################################################################################
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--action")
+    args = parser.parse_args()
+    if not args.action:
+        initiate_actions()
+    else:
+        perform_action(args)
+
+################################################################################
