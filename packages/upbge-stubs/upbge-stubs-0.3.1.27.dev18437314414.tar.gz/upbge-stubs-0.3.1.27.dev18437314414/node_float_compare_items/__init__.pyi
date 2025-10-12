6cd2792f284b576d64a3f21cc76cdf7e9322d31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:35 +0000

    Add package upbge-stubs/0.3.1.27.dev18437314414/upbge-stubs-0.3.1.27.dev18437314414.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev18437314414.tar.gz/upbge-stubs-0.3.1.27.dev18437314414/node_float_compare_items/__init__.pyi
@@ -0,0 +1,41 @@
+"""
+
+
+Node Float Compare Items
+^^^^^^^^^^^^^^^^^^^^^^^^
+
+:LESS_THAN:       
+  Less Than.
+
+  True when the first input is smaller than second input.
+
+:LESS_EQUAL:      
+  Less Than or Equal.
+
+  True when the first input is smaller than the second input or equal.
+
+:GREATER_THAN:    
+  Greater Than.
+
+  True when the first input is greater than the second input.
+
+:GREATER_EQUAL:   
+  Greater Than or Equal.
+
+  True when the first input is greater than the second input or equal.
+
+:EQUAL:           
+  Equal.
+
+  True when both inputs are approximately equal.
+
+:NOT_EQUAL:       
+  Not Equal.
+
+  True when both inputs are not approximately equal.
+
+.. _rna-enum-node-float-compare-items:
+
+"""
+
+import typing
