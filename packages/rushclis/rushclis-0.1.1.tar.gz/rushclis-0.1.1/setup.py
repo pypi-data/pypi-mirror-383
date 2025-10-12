322a34a95b9b68941af472a9a910cbf297fc1522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:30 +0000

    Add package rushclis/0.1.1/rushclis-0.1.1.tar.gz

---
+++ packages/rushclis/rushclis-0.1.1.tar.gz/rushclis-0.1.1/setup.py
@@ -0,0 +1,27 @@
+﻿from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='rushclis',
+    version='0.1.1',
+    packages=find_packages(),
+    requires=[
+        'colorama',
+        'rushlib'
+    ],
+    description='python cli tool',
+    author='ndrzy',
+    author_email='dandan0019@outlook.com',
+    python_requires='>=3.10',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/meatdumplings0019/rushcli",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
