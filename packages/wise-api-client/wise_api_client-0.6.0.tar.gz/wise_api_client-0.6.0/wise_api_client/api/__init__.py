9ecaf424c91e9d1de70e2698ffb14c8347e29637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:04:41 +0000

    Add package wise-api-client/0.6.0/wise_api_client-0.6.0.tar.gz

---
+++ packages/wise-api-client/wise_api_client-0.6.0.tar.gz/wise_api_client-0.6.0/wise_api_client/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from wise_api_client.api.activities_api import ActivitiesApi
+from wise_api_client.api.profiles_api import ProfilesApi
+from wise_api_client.api.quotes_api import QuotesApi
+from wise_api_client.api.recipients_api import RecipientsApi
+from wise_api_client.api.transfers_api import TransfersApi
+
