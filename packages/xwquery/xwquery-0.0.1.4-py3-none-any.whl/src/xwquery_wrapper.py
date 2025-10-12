df8593373d13c4945d7bce65cfcb3d6d79180abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:15 +0000

    Add package xwquery/0.0.1.4/xwquery-0.0.1.4-py3-none-any.whl

---
+++ packages/xwquery/xwquery-0.0.1.4-py3-none-any.whl/src/xwquery_wrapper.py
@@ -0,0 +1,15 @@
+"""
+Convenience wrapper for xwquery.
+
+This module provides the 'xwquery' package as a lightweight wrapper
+that imports from 'exonware-xwquery'. Install the full package as a dependency.
+
+Company: eXonware.com
+Author: Eng. Muhammad AlShehri
+Email: connect@exonware.com
+Version: 0.0.1.4
+"""
+
+# Import everything from the full package
+from exonware.xwquery import *  # noqa: F401, F403
+
