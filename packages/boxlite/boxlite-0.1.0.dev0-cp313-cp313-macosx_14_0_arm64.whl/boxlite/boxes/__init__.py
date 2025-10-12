18529989fbe5e5395055476a058fece36d936b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:01 +0000

    Add package boxlite/0.1.0.dev0/boxlite-0.1.0.dev0-cp313-cp313-macosx_14_0_arm64.whl

---
+++ packages/boxlite/boxlite-0.1.0.dev0-cp313-cp313-macosx_14_0_arm64.whl/boxlite/boxes/__init__.py
@@ -0,0 +1,11 @@
+"""
+Specialized BoxLite containers for specific use cases.
+"""
+
+from .codebox import CodeBox
+
+# Future specialized containers
+# from .browser import BrowserBox
+# from .claude_code import ClaudeCodeBox
+
+__all__ = ["CodeBox"]
