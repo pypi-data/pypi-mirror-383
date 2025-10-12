de3b4ac16895bdcf3fa42217de73316d7e4bd609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:04:40 +0000

    Add package ssc_codegen/0.12.0/ssc_codegen-0.12.0.tar.gz

---
+++ packages/ssc_codegen/ssc_codegen-0.12.0.tar.gz/ssc_codegen-0.12.0/ssc_codegen/ast_/nodes_cast.py
@@ -0,0 +1,83 @@
+from dataclasses import dataclass
+from typing import TypedDict, Type, TYPE_CHECKING, TypeAlias, ClassVar
+
+from ssc_codegen.tokens import TokenType, VariableType, StructType
+from ssc_codegen.ast_.base import BaseAstNode, T_EMPTY_KWARGS
+
+if TYPE_CHECKING:
+    from ssc_codegen.schema import BaseSchema
+
+
+@dataclass(kw_only=True)
+class ExprToInt(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.TO_INT
+    accept_type: VariableType = VariableType.STRING
+    ret_type: VariableType = VariableType.INT
+
+
+@dataclass(kw_only=True)
+class ExprToListInt(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.TO_INT_LIST
+    accept_type: VariableType = VariableType.LIST_STRING
+    ret_type: VariableType = VariableType.LIST_INT
+
+
+@dataclass(kw_only=True)
+class ExprToFloat(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.TO_FLOAT
+    accept_type: VariableType = VariableType.STRING
+    ret_type: VariableType = VariableType.FLOAT
+
+
+@dataclass(kw_only=True)
+class ExprToListFloat(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.TO_FLOAT_LIST
+    accept_type: VariableType = VariableType.LIST_STRING
+    ret_type: VariableType = VariableType.LIST_FLOAT
+
+
+@dataclass(kw_only=True)
+class ExprToBool(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.TO_BOOL
+    accept_type: VariableType = VariableType.ANY
+    ret_type: VariableType = VariableType.BOOL
+
+
+_T_SCHEMA: TypeAlias = Type["BaseSchema"]
+KW_EXPR_NESTED = TypedDict(
+    "KW_EXPR_NESTED", {"schema_name": str, "schema_type": StructType}
+)
+EXPR_NESTED_ARGS = tuple[str, StructType]
+
+
+@dataclass(kw_only=True)
+class ExprNested(BaseAstNode[KW_EXPR_NESTED, EXPR_NESTED_ARGS]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_NESTED
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.NESTED
+
+
+KW_EXPR_JSONIFY = TypedDict(
+    "KW_EXPR_JSONIFY", {"json_struct_name": str, "is_array": bool, "query": str}
+)
+ARGS_EXPR_JSONIFY = tuple[str, bool, str]
+
+
+@dataclass(kw_only=True)
+class ExprJsonify(BaseAstNode[KW_EXPR_JSONIFY, ARGS_EXPR_JSONIFY]):
+    kind: ClassVar[TokenType] = TokenType.TO_JSON
+    accept_type: VariableType = VariableType.STRING
+    ret_type: VariableType = VariableType.JSON
+
+
+KW_EXPR_JSONIFY_DYNAMIC = TypedDict("KW_EXPR_JSONIFY_DYNAMIC", {"query": str})
+ARGS_EXPR_JSONIFY_DYNAMIC = tuple[str]
+
+
+@dataclass(kw_only=True)
+class ExprJsonifyDynamic(
+    BaseAstNode[KW_EXPR_JSONIFY_DYNAMIC, ARGS_EXPR_JSONIFY_DYNAMIC]
+):
+    kind: ClassVar[TokenType] = TokenType.TO_JSON_DYNAMIC
+    accept_type: VariableType = VariableType.STRING
+    ret_type: VariableType = VariableType.ANY  # STUBBED
---
+++ packages/ssc_codegen/ssc_codegen-0.12.0.tar.gz/ssc_codegen-0.12.0/ssc_codegen/ast_/nodes_selectors.py
@@ -0,0 +1,118 @@
+from dataclasses import dataclass
+from typing import TypedDict, ClassVar
+
+from ssc_codegen.tokens import TokenType, VariableType
+from ssc_codegen.ast_.base import BaseAstNode, T_EMPTY_KWARGS
+
+KW_EXPR_SELECTOR_LIKE = TypedDict("KW_EXPR_SELECTOR_LIKE", {"query": str})
+ARGS_EXPR_SELECTOR_LIKE = tuple[str]
+
+
+@dataclass(kw_only=True)
+class ExprCss(BaseAstNode[KW_EXPR_SELECTOR_LIKE, ARGS_EXPR_SELECTOR_LIKE]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_CSS
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.DOCUMENT
+
+
+@dataclass(kw_only=True)
+class ExprCssAll(BaseAstNode[KW_EXPR_SELECTOR_LIKE, ARGS_EXPR_SELECTOR_LIKE]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_CSS_ALL
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.LIST_DOCUMENT
+
+
+@dataclass(kw_only=True)
+class ExprXpath(BaseAstNode[KW_EXPR_SELECTOR_LIKE, ARGS_EXPR_SELECTOR_LIKE]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_XPATH
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.DOCUMENT
+
+
+@dataclass(kw_only=True)
+class ExprXpathAll(BaseAstNode[KW_EXPR_SELECTOR_LIKE, ARGS_EXPR_SELECTOR_LIKE]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_XPATH_ALL
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.LIST_DOCUMENT
+
+
+KW_EXPR_ATTR = TypedDict("KW_EXPR_ATTR", {"key": tuple[str, ...]})
+ARGS_EXPR_ATTR = tuple[tuple[str, ...]]
+
+
+@dataclass(kw_only=True)
+class ExprGetHtmlAttr(BaseAstNode[KW_EXPR_ATTR, ARGS_EXPR_ATTR]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_ATTR
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.STRING
+
+
+@dataclass(kw_only=True)
+class ExprGetHtmlAttrAll(BaseAstNode[KW_EXPR_ATTR, ARGS_EXPR_ATTR]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_ATTR_ALL
+    accept_type: VariableType = VariableType.LIST_DOCUMENT
+    ret_type: VariableType = VariableType.LIST_STRING
+
+
+@dataclass(kw_only=True)
+class ExprGetHtmlText(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_TEXT
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.STRING
+
+
+@dataclass(kw_only=True)
+class ExprGetHtmlTextAll(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_TEXT_ALL
+    accept_type: VariableType = VariableType.LIST_DOCUMENT
+    ret_type: VariableType = VariableType.LIST_STRING
+
+
+@dataclass(kw_only=True)
+class ExprGetHtmlRaw(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_RAW
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.STRING
+
+
+@dataclass(kw_only=True)
+class ExprGetHtmlRawAll(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_RAW_ALL
+    accept_type: VariableType = VariableType.LIST_DOCUMENT
+    ret_type: VariableType = VariableType.LIST_STRING
+
+
+@dataclass(kw_only=True)
+class ExprMapAttrs(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_MAP_ATTRS
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.LIST_STRING
+
+
+@dataclass(kw_only=True)
+class ExprMapAttrsAll(BaseAstNode[T_EMPTY_KWARGS, tuple]):
+    kind: ClassVar[TokenType] = TokenType.EXPR_MAP_ATTRS_ALL
+    accept_type: VariableType = VariableType.LIST_DOCUMENT
+    ret_type: VariableType = VariableType.LIST_STRING
+
+
+KW_EXPR_SELECTOR_REMOVE = TypedDict("KW_EXPR_SELECTOR_REMOVE", {"query": str})
+ARGS_EXPR_SELECTOR_REMOVE = tuple[str]
+
+
+@dataclass(kw_only=True)
+class ExprCssElementRemove(
+    BaseAstNode[KW_EXPR_SELECTOR_REMOVE, ARGS_EXPR_SELECTOR_REMOVE]
+):
+    kind: ClassVar[TokenType] = TokenType.EXPR_CSS_REMOVE
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.DOCUMENT
+
+
+@dataclass(kw_only=True)
+class ExprXpathElementRemove(
+    BaseAstNode[KW_EXPR_SELECTOR_REMOVE, ARGS_EXPR_SELECTOR_REMOVE]
+):
+    kind: ClassVar[TokenType] = TokenType.EXPR_XPATH_REMOVE
+    accept_type: VariableType = VariableType.DOCUMENT
+    ret_type: VariableType = VariableType.DOCUMENT
