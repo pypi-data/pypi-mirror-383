1700deca77805e132f080b35cd6e10f9e8a5db8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:43 +0000

    Add package moves-cli/0.2.0/moves_cli-0.2.0.tar.gz

---
+++ packages/moves-cli/moves_cli-0.2.0.tar.gz/moves_cli-0.2.0/src/moves_cli/utils/id_generator.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import re
+import unicodedata
+import secrets
+
+from moves_cli.data.models import SpeakerId, HistoryId
+
+
+def generate_speaker_id(name: str) -> SpeakerId:
+    slug = (
+        re.sub(
+            r"\s+",
+            "-",
+            re.sub(
+                r"[^\w\s-]",
+                "",
+                unicodedata.normalize("NFKD", name)
+                .encode("ascii", "ignore")
+                .decode("ascii"),
+            ),
+        )
+        .strip("-")
+        .lower()
+    )
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    suffix = "".join(secrets.choice(alphabet) for _ in range(5))
+    speaker_id = f"{slug}-{suffix}"
+    return speaker_id
+
+
+def generate_history_id() -> HistoryId:
+    history_id = datetime.now().strftime("%Y%m%d_%H-%M-%S")
+    return history_id
