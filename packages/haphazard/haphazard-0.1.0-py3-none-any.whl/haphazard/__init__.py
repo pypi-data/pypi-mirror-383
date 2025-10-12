a5dc8dbba1f6cd9b6ca84096d032491cc30a4ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:19:17 +0000

    Add package haphazard/0.1.0/haphazard-0.1.0-py3-none-any.whl

---
+++ packages/haphazard/haphazard-0.1.0-py3-none-any.whl/haphazard/__init__.py
@@ -0,0 +1,25 @@
+"""
+haphazard
+---------
+Top-level imports for main API access.
+"""
+
+# User-facing APIs
+from .data import load_dataset
+from .models import load_model
+
+# Optional APIs for contributors
+from .data import BaseDataset, register_dataset
+from .models import BaseModel, register_model
+
+# -------------------------------------------------------------------------
+# Public exports
+# -------------------------------------------------------------------------
+__all__ = [
+    "load_dataset",
+    "load_model",
+    "BaseDataset",
+    "register_dataset",
+    "BaseModel",
+    "register_model",
+]
