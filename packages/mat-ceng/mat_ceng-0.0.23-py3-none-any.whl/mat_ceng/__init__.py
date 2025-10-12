4e29abb3caec84286be070173e35be84a7765306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:04:45 +0000

    Add package mat-ceng/0.0.23/mat_ceng-0.0.23-py3-none-any.whl

---
+++ packages/mat-ceng/mat_ceng-0.0.23-py3-none-any.whl/mat_ceng/__init__.py
@@ -0,0 +1,18 @@
+'''
+Package For Some Civil Engineering Tools
+'''
+
+__version__ = '0.0.22'
+
+
+
+from mat_ceng.mat_ceng import (say_hi)
+from mat_ceng.column import(
+    Material,Load_Case,Section_Dimensions,Column,
+    calculate_column_actual_moment_of_inertia,
+    calculate_minor_delta_ns,
+    calculate_major_delta_ns,
+    calculate_Cm,
+    calculate_Ise)
+from mat_ceng.column_area import (get_column_area_loads)
+import mat_ceng.csi as csi
---
+++ packages/mat-ceng/mat_ceng-0.0.23-py3-none-any.whl/mat_ceng/mat_ceng.py
@@ -0,0 +1,12 @@
+
+
+
+
+def say_hi():
+    '''
+    to say hi only
+    '''
+    msg ='Hello World'
+    print(msg)
+    return msg
+
