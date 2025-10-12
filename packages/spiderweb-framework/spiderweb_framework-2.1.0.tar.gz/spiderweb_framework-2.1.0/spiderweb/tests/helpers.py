7c5d5165659be090be9ec51193901558d628a398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:29 +0000

    Add package spiderweb-framework/2.1.0/spiderweb_framework-2.1.0.tar.gz

---
+++ packages/spiderweb-framework/spiderweb_framework-2.1.0.tar.gz/spiderweb_framework-2.1.0/spiderweb/tests/helpers.py
@@ -0,0 +1,28 @@
+from wsgiref.util import setup_testing_defaults
+
+from spiderweb import SpiderwebRouter
+
+
+class StartResponse:
+    def __init__(self):
+        self.status = None
+        self.headers = None
+
+    def __call__(self, status, headers):
+        self.status = status
+        self.headers = headers
+
+    def get_headers(self):
+        return {h[0]: h[1] for h in self.headers} if self.headers else {}
+
+
+def setup(**kwargs):
+    environ = {}
+    setup_testing_defaults(environ)
+    if "db" not in kwargs:
+        kwargs["db"] = "spiderweb-tests.db"
+    return (
+        SpiderwebRouter(**kwargs),
+        environ,
+        StartResponse(),
+    )
