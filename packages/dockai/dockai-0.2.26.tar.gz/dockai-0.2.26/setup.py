6bc62401151fdd6d03cf305b5d5591c3b8e7ab36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:05:07 +0000

    Add package dockai/0.2.26/dockai-0.2.26.tar.gz

---
+++ packages/dockai/dockai-0.2.26.tar.gz/dockai-0.2.26/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dockai",
+    version="0.2.26",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "docker",
+        "openai",
+        "requests",
+        "termcolor",
+        "fastapi",
+        "uvicorn",
+        "stripe",
+        "python-dotenv",
+        "jwt"
+    ],
+    entry_points={
+        "console_scripts": [
+            "dockai=dockai.cli.commands:cli",
+        ],
+    },
+    author="Ahmet Atakan",
+    author_email="ahmetatakan.tech@gmail.com",
+    description="DockAI – AI-powered Docker Log Analysis Tool (CLI + Cloud)",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="Apache License 2.0",
+    python_requires=">=3.9",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Monitoring",
+    ],
+)
