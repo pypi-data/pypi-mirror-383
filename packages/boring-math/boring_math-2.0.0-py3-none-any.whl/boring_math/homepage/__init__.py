0aecd57d7a2882e7a4b137f745f10daadddeac9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 15:05:15 +0000

    Add package boring-math/2.0.0/boring_math-2.0.0-py3-none-any.whl

---
+++ packages/boring-math/boring_math-2.0.0-py3-none-any.whl/boring_math/homepage/__init__.py
@@ -0,0 +1,28 @@
+"""
+Purpose boring-math PyPI Project
+================================
+
+The boring-math PyPI project is a central component of my
+Boring Math hobby projects.
+
+- It is the PyPI project for the ``boring_math.homepage`` module.
+- Claims the boring-math name on PyPI for the overall effort.
+- Hosts on *GitHub Pages*
+
+  - The Boring Math homepage.
+  - Sphinx API & usage documentation for each Boring Math project.
+
+- Provides locations for overarching CHANGELOG and README files.
+
+  - Each Boring Math project has its own CHANGELOG and README files.
+
+.. tip::
+
+    There is no need to install boring-math itself, but does not hurt
+    anything if it is installed.
+
+"""
+
+__author__ = 'Geoffrey R. Scheller'
+__copyright__ = 'Copyright (c) 2016-2025 Geoffrey R. Scheller'
+__license__ = 'Apache License 2.0'
---
+++ packages/boring-math/boring_math-2.0.0-py3-none-any.whl/boring_math/homepage/docs.py
@@ -0,0 +1,9 @@
+"""
+- `boring_math-combinatorics <https://grscheller.github.io/boring-math/combinatorics/development/build/html/>`_
+- `boring_math-number-theory <https://grscheller.github.io/boring-math/number-theory/development/build/html/>`_
+- `boring-math-probability-distributions <https://grscheller.github.io/boring-math/probability-distributions/development/build/html/>`_ 
+- `boring-math-pythagorean-triples <https://grscheller.github.io/boring-math/pythagorean-triples/development/build/html/>`_
+- `recursive-functions <https://grscheller.github.io/boring-math/recursive-functions/development/build/html/>`_
+- `boring-math-special-functions <https://grscheller.github.io/boring-math/special-functions/development/build/html/>`_
+
+"""
