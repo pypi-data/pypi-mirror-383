88f024129ec87beac97ec10a9660d76c062a0e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:19:12 +0000

    Add package xloft/0.6.7/xloft-0.6.7-py3-none-any.whl

---
+++ packages/xloft/xloft-0.6.7-py3-none-any.whl/xloft/__init__.py
@@ -0,0 +1,15 @@
+"""(XLOFT) X-Library of tools.
+
+Modules exported by this package:
+
+- `namedtuple`- Class imitates the behavior of the _named tuple_.
+- `converters` - Collection of tools for converting data.
+- `itis` - Tools for determining something.
+"""
+
+from __future__ import annotations
+
+__all__ = ("NamedTuple",)
+
+
+from xloft.namedtuple import NamedTuple
