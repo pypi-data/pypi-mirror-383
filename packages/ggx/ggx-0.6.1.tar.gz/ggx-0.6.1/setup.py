f7130c1659f25b6e02e6b1048dc0a3477e2dbd06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:53 +0000

    Add package ggx/0.6.1/ggx-0.6.1.tar.gz

---
+++ packages/ggx/ggx-0.6.1.tar.gz/ggx-0.6.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ggx",
+    version="0.6.1",
+    description="API for GoodGame Empire",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="mst4ck",
+    author_email="mst4ck@mailfence.com",
+    license="MIT",
+    packages=find_packages(),      
+    install_requires=[
+        "websockets>=15.0.1",
+        "loguru>=0.7.3",
+        "aiohttp>=3.13.0",
+    ],
+    python_requires=">=3.12",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
