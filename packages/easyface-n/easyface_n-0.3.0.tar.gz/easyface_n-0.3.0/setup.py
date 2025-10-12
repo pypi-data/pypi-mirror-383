46528d3f4785c2ef4a14f85dc34f1716dd8db0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:36 +0000

    Add package easyface-n/0.3.0/easyface_n-0.3.0.tar.gz

---
+++ packages/easyface-n/easyface_n-0.3.0.tar.gz/easyface_n-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="easyface-n",
+    version="0.3.0",
+    packages=find_packages(),
+    install_requires=[
+    "numpy>=1.26",
+    "opencv-python>=4.8",
+    "onnxruntime>=1.17"
+],
+    author="Nimalpranav",
+    description="A lightweight face recognition library without dlib(working)",
+    python_requires='>=3.10, <3.14',
+)
