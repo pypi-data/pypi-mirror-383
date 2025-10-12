bf2d1de69c13909f04c156bfb27b9df9a8854f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:19:12 +0000

    Add package merge-insertion/1.0.0/merge_insertion-1.0.0.tar.gz

---
+++ packages/merge-insertion/merge_insertion-1.0.0.tar.gz/merge_insertion-1.0.0/merge_insertion/__init__.py
@@ -0,0 +1,175 @@
+"""
+Merge-Insertion Sort a.k.a. Ford-Johnson Algorithm
+==================================================
+
+The Ford-Johnson algorithm[1], also known as the merge-insertion sort[2,3] uses the minimum
+number of possible comparisons for lists of 22 items or less, and at the time of writing has
+the fewest comparisons known for lists of 46 items or less. It is therefore very well suited
+for cases where comparisons are expensive, such as user input, and the API is implemented to
+take an async comparator function for this reason.
+
+>>> from merge_insertion import merge_insertion_sort
+>>> # A Comparator must return 0 if the first item is larger, or 1 if the second item is larger.
+>>> # It can use any criteria for comparison, in this example we'll use user input:
+>>> async def comparator(ab :tuple[str,str]):
+...     choice = None
+...     while choice not in ab:
+...         choice = input(f"Please choose {ab[0]!r} or {ab[1]!r}: ")
+...     return 0 if choice == ab[0] else 1
+...
+>>> # Sort five items in ascending order with a maximum of only seven comparisons:
+>>> sorted = merge_insertion_sort('DABEC', comparator)
+>>> # Since we can't `await` in the REPL, use asyncio to run the coroutine here:
+>>> import asyncio
+>>> asyncio.run(sorted)  # doctest: +SKIP
+Please choose 'D' or 'A': D
+...
+Please choose 'B' or 'A': B
+['A', 'B', 'C', 'D', 'E']
+
+**References**
+
+1. Ford, L. R., & Johnson, S. M. (1959). A Tournament Problem.
+   The American Mathematical Monthly, 66(5), 387-389. <https://doi.org/10.1080/00029890.1959.11989306>
+2. Knuth, D. E. (1998). The Art of Computer Programming: Volume 3: Sorting and Searching (2nd ed.).
+   Addison-Wesley. <https://cs.stanford.edu/~knuth/taocp.html#vol3>
+3. <https://en.wikipedia.org/wiki/Merge-insertion_sort>
+
+API
+---
+
+.. autoclass:: merge_insertion.T
+    :members:
+
+.. autoclass:: merge_insertion.Comparator
+    :members:
+
+.. autofunction:: merge_insertion.merge_insertion_sort
+
+.. autofunction:: merge_insertion.merge_insertion_max_comparisons
+
+Author, Copyright and License
+-----------------------------
+
+Copyright © 2025 Hauke Dämpfling (haukex@zero-g.net)
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""
+from collections.abc import Generator, Sequence, Callable, Awaitable
+from typing import TypeVar, Literal
+from math import floor, ceil, log2
+
+# NOTICE: This file contains very few code comments because it is a port of
+# https://github.com/haukex/merge-insertion.js/blob/main/src/merge-insertion.ts
+# Please see that file for detailed code comments and explanations.
+
+#: A type of object that can be compared by a :class:`Comparator` and therefore sorted by
+#: :func:`merge_insertion_sort`. Must have sensible support for the equality operators.
+T = TypeVar('T')
+
+def _group_sizes() -> Generator[int, None, None]:
+    prev :int = 0
+    i :int = 1
+    while True:
+        cur :int = 2**i - prev
+        yield cur
+        prev = cur
+        i += 1
+
+def _make_groups(array :Sequence[T]) -> Sequence[tuple[int, T]]:
+    items = list(enumerate(array))
+    rv :list[tuple[int, T]] = []
+    gen = _group_sizes()
+    i :int = 0
+    while True:
+        size = next(gen)
+        group = items[i:i+size]
+        group.reverse()
+        rv.extend(group)
+        if len(group)<size:
+            break
+        i += size
+    return rv
+
+#: A user-supplied function to compare two items.
+#: The argument is a tuple of the two items to be compared; they must not be equal.
+#: Must return a Promise resolving to 0 if the first item is ranked higher, or 1 if the second item is ranked higher.
+Comparator = Callable[[tuple[T, T]], Awaitable[Literal[0, 1]]]
+
+async def _bin_insert_index(array :Sequence[T], item :T, comp :Comparator) -> int:
+    if not array:
+        return 0
+    if item in array:
+        raise ValueError("item is already in target array")
+    if len(array)==1:
+        return 0 if await comp((item,array[0])) else 1
+    left, right = 0, len(array)-1
+    while left <= right:
+        mid = left + floor((right-left)/2)
+        if await comp((item, array[mid])):
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
+
+async def merge_insertion_sort(array :Sequence[T], comparator :Comparator) -> Sequence[T]:
+    """Merge-Insertion Sort (Ford-Johnson algorithm) with async comparison.
+
+    :param array: Array of to sort. Duplicate items are not allowed.
+    :param comparator: Async comparison function.
+    :return: A shallow copy of the array sorted in ascending order.
+    """
+    if len(array)<1:
+        return []
+    if len(array)==1:
+        return list(array)
+    if len(array) != len(set(array)):
+        raise ValueError('array may not contain duplicate items')
+    if len(array)==2:
+        return list(array) if await comparator((array[0], array[1])) else [array[1], array[0]]
+
+    pairs :dict[T, T] = {}
+    for i in range(0, len(array)-1, 2):
+        if await comparator((array[i], array[i+1])):
+            pairs[array[i+1]] = array[i]
+        else:
+            pairs[array[i]] = array[i+1]
+
+    larger = await merge_insertion_sort(list(pairs), comparator)
+
+    main_chain :list[list[T]] = [ [ pairs[larger[0]] ], [ larger[0] ] ] + [ [ la, pairs[la] ] for la in larger[1:] ]
+    assert all( len(i)==2 for i in main_chain[2:] )
+
+    for _,pair in _make_groups( main_chain[2:] + ( [[array[-1]]] if len(array) % 2 else [] ) ):
+        if len(pair)==1:
+            item = pair[0]
+            idx = await _bin_insert_index([ i[0] for i in main_chain ], item, comparator)
+        else:
+            assert len(pair)==2
+            pair_idx = main_chain.index(pair)
+            item = pair.pop()
+            idx = await _bin_insert_index([ i[0] for i in main_chain[:pair_idx] ], item, comparator)
+        main_chain.insert(idx, [item])
+    assert all( len(i)==1 for i in main_chain )
+
+    return [ i[0] for i in main_chain ]
+
+def merge_insertion_max_comparisons(n :int) -> int:
+    """Returns the maximum number of comparisons that :func:`merge_insertion_sort` will perform depending on the input length.
+
+    :param n: The number of items in the list to be sorted.
+    :return: The expected maximum number of comparisons.
+    """
+    if n<0:
+        raise ValueError("must specify zero or more items")
+    return n*ceil(log2(3*n/4)) - floor((2**floor(log2(6*n)))/3) + floor(log2(6*n)/2) if n else 0
---
+++ packages/merge-insertion/merge_insertion-1.0.0.tar.gz/merge_insertion-1.0.0/tests/test_merge_insertion.py
@@ -0,0 +1,153 @@
+"""
+Tests for merge-insertion.py
+
+Copyright © 2025 Hauke Dämpfling (haukex@zero-g.net)
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+"""
+import random
+import unittest
+from typing import Literal, Optional
+from itertools import islice, permutations
+from collections import defaultdict
+import merge_insertion as uut
+
+# NOTICE: This file contains very few code comments because it is essentially a port of
+# https://github.com/haukex/merge-insertion.js/blob/main/src/__tests__/merge-insertion.test.ts
+# Please see that file for code comments.
+
+async def _comp(ab :tuple[str,str]) -> Literal[0,1]:
+    return 0 if ab[0] > ab[1] else 1
+
+class TestMergeInsertionSort(unittest.IsolatedAsyncioTestCase):
+
+    def _test_comp(self, comp :uut.Comparator, max_calls :int, log :Optional[list[tuple[uut.T,uut.T]]] = None) -> uut.Comparator:
+        call_count = 0
+        pair_map :dict[uut.T, set[uut.T]] = defaultdict(set)
+        def c(ab :tuple[uut.T,uut.T]):
+            nonlocal call_count
+            a, b = ab
+            self.assertNotEqual(a, b, f"a and b may not be equal ({a!r})")
+            self.assertFalse( a in pair_map and b in pair_map[a] or b in pair_map and a in pair_map[b],
+                f"duplicate comparison of {a!r} and {b!r}" )
+            pair_map[a].add(b)
+            call_count += 1
+            self.assertLessEqual(call_count, max_calls, f"too many Comparator calls ({call_count})")
+            if log is not None:
+                log.append(ab)
+            return comp(ab)
+        return c
+
+    def test_group_sizes(self):
+        group_sizes = uut._group_sizes  # pyright: ignore[reportPrivateUsage]  # pylint: disable=protected-access
+        exp = [ 2, 2, 6, 10, 22, 42, 86, 170, 342, 682, 1366, 2730, 5462, 10922, 21846, 43690,
+            87382, 174762, 349526, 699050, 1398102, 2796202, 5592406, 11184810, 22369622, 44739242,
+            89478486, 178956970, 357913942, 715827882, 1431655766, 2863311530, 5726623062, 11453246122 ]
+        self.assertEqual( exp, list( islice( group_sizes(), len(exp) ) ) )
+
+    def test_make_groups(self):
+        make_groups = uut._make_groups  # pyright: ignore[reportPrivateUsage]  # pylint: disable=protected-access
+        self.assertEqual( make_groups(['y3','y4','y5','y6','y7','y8','y9','y10','y11','y12','y21','y22']),
+            [ (1,'y4'), (0,'y3'),  (3,'y6'), (2,'y5'), (9,'y12'), (8,'y11'), (7,'y10'), (6,'y9'), (5,'y8'), (4,'y7'),  (11,'y22'), (10,'y21') ] )
+        self.assertEqual( make_groups(['b2','b3','b4','b5','b6','b7','b8','b9','b10','b11']),
+            [ (1,'b3'), (0,'b2'),  (3,'b5'), (2,'b4'), (9,'b11'), (8,'b10'), (7,'b9'), (6,'b8'), (5,'b7'), (4,'b6') ] )
+        self.assertEqual( make_groups(['1','2','3','4','5','6','7','8','9']),
+            [ (1,'2'), (0,'1'),  (3,'4'), (2,'3'), (8,'9'), (7,'8'), (6,'7'), (5,'6'), (4,'5') ] )
+
+    async def test_test_comp(self):
+        log :list[tuple[str,str]] = []
+        c = self._test_comp(_comp, 2, log)
+        await c(('x','y'))
+        await c(('x','z'))
+        with self.assertRaisesRegex(AssertionError, 'may not be equal'):
+            await c(('x','x'))
+        with self.assertRaisesRegex(AssertionError, 'duplicate comparison'):
+            await c(('y','x'))
+        with self.assertRaisesRegex(AssertionError, 'duplicate comparison'):
+            await c(('x','y'))
+        with self.assertRaisesRegex(AssertionError, 'duplicate comparison'):
+            await c(('x','z'))
+        with self.assertRaisesRegex(AssertionError, 'too many'):
+            await c(('i','j'))
+        self.assertEqual( log, [('x','y'), ('x','z')] )
+
+    async def test_bin_insert_index(self):
+        bin_insert_index = uut._bin_insert_index  # pyright: ignore[reportPrivateUsage]  # pylint: disable=protected-access
+
+        #              A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+        a :list[str] = ['B','D','F','H','J','L','N','P','R','T','V','X','Z']
+
+        with self.assertRaises(ValueError):
+            await bin_insert_index(a, 'J', _comp)
+
+        self.assertEqual( await bin_insert_index([],    'A', self._test_comp(_comp,0)), 0 )
+        self.assertEqual( await bin_insert_index(a[:1], 'A', self._test_comp(_comp,1)), 0 )
+        self.assertEqual( await bin_insert_index(a[:1], 'C', self._test_comp(_comp,1)), 1 )
+
+        self.assertEqual( await bin_insert_index(a[:2], 'A', self._test_comp(_comp,1)), 0 )
+        self.assertEqual( await bin_insert_index(a[:2], 'C', self._test_comp(_comp,2)), 1 )
+        self.assertEqual( await bin_insert_index(a[:2], 'E', self._test_comp(_comp,2)), 2 )
+
+        self.assertEqual( await bin_insert_index(a[:5], 'A', self._test_comp(_comp,2)), 0 )
+        self.assertEqual( await bin_insert_index(a[:5], 'C', self._test_comp(_comp,3)), 1 )
+        self.assertEqual( await bin_insert_index(a[:5], 'E', self._test_comp(_comp,3)), 2 )
+        self.assertEqual( await bin_insert_index(a[:5], 'G', self._test_comp(_comp,2)), 3 )
+        self.assertEqual( await bin_insert_index(a[:5], 'I', self._test_comp(_comp,3)), 4 )
+        self.assertEqual( await bin_insert_index(a[:5], 'K', self._test_comp(_comp,3)), 5 )
+        self.assertEqual( await bin_insert_index(a[:5], 'M', self._test_comp(_comp,3)), 5 )
+
+        self.assertEqual( await bin_insert_index(a[:6], 'A', self._test_comp(_comp,2)), 0 )
+        self.assertEqual( await bin_insert_index(a[:6], 'G', self._test_comp(_comp,3)), 3 )
+        self.assertEqual( await bin_insert_index(a[:6], 'M', self._test_comp(_comp,3)), 6 )
+
+        self.assertEqual( await bin_insert_index(a[:7], 'A', self._test_comp(_comp,3)), 0 )
+        self.assertEqual( await bin_insert_index(a[:7], 'G', self._test_comp(_comp,3)), 3 )
+        self.assertEqual( await bin_insert_index(a[:7], 'O', self._test_comp(_comp,3)), 7 )
+
+    async def test_merge_insertion_sort_detail(self):
+        log :list[tuple[str,str]] = []
+        self.assertEqual( await uut.merge_insertion_sort('ABCDE', self._test_comp(_comp, 7, log)), ['A','B','C','D','E'] )
+        self.assertEqual( log, [ ('A','B'), ('C','D'), ('B','D'), ('E','B'), ('E','D'), ('C','A'), ('C','B') ] )
+
+        with self.assertRaises(ValueError):
+            await uut.merge_insertion_sort('ABB', _comp)
+
+    async def test_merge_insertion_sort_lengths(self):
+        random.seed(123)
+        for ln in range(95):
+            array = [ chr(x+33) for x in range(ln) ]
+            a = array[:]
+            self.assertEqual( await uut.merge_insertion_sort(a, self._test_comp(_comp, uut.merge_insertion_max_comparisons(ln))), array )
+            if ln<2:
+                continue
+            a.reverse()
+            self.assertEqual( await uut.merge_insertion_sort(a, self._test_comp(_comp, uut.merge_insertion_max_comparisons(ln))), array )
+            for _ in range(10):
+                random.shuffle(a)
+                self.assertEqual( await uut.merge_insertion_sort(a, self._test_comp(_comp, uut.merge_insertion_max_comparisons(ln))), array )
+
+    async def test_merge_insertion_sort_permutations(self):
+        for ln in range(9):  # don't increase! (runtime)
+            array = [ chr(x+65) for x in range(ln) ]
+            for perm in permutations(array):
+                self.assertEqual( await uut.merge_insertion_sort(perm, self._test_comp(_comp, uut.merge_insertion_max_comparisons(ln))), array )
+
+    def test_merge_insertion_max_comparisons(self):
+        exp = [ 0, 0, 1, 3, 5, 7, 10, 13, 16, 19, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62, 66,
+            71, 76, 81, 86, 91, 96, 101, 106, 111, 116, 121, 126, 131, 136, 141, 146, 151, 156, 161,
+            166, 171, 177, 183, 189, 195, 201, 207, 213, 219, 225, 231, 237, 243, 249, 255 ]
+        for i,e in enumerate(exp):
+            self.assertEqual( uut.merge_insertion_max_comparisons(i), e )
+        with self.assertRaises(ValueError):
+            uut.merge_insertion_max_comparisons(-1)
