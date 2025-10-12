2fd8f33694e41ed2b574afcc3100f164027af6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:29 +0000

    Add package PyPtt/1.4.0.dev58619/pyptt-1.4.0.dev58619.tar.gz

---
+++ packages/PyPtt/pyptt-1.4.0.dev58619.tar.gz/pyptt-1.4.0.dev58619/PyPtt/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.4.0.dev58619'
+
+from .PTT import API
+from .data_type import *
+from .exceptions import *
+from .log import LogLevel
+from .service import Service
+
+LOG_LEVEL = LogLevel
+
+_main_version = '1.4'
