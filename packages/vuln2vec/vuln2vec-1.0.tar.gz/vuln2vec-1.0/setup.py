91fe082269912caefb7ce54928ec314581e7c1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:04:49 +0000

    Add package vuln2vec/1.0/vuln2vec-1.0.tar.gz

---
+++ packages/vuln2vec/vuln2vec-1.0.tar.gz/vuln2vec-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vuln2vec",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "flashtext",
+        "gensim==4.3.3",
+        "numpy==1.26.4",
+    ],
+    package_data={
+        "vuln2vec": ["*.json"],  # 👈 include json files
+    },
+)
