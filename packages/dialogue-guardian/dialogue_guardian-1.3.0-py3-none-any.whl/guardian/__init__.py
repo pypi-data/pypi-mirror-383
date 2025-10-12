ebe2aeb2fe177a871629eabdace9b2b52585ba43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:09 +0000

    Add package dialogue-guardian/1.3.0/dialogue_guardian-1.3.0-py3-none-any.whl

---
+++ packages/dialogue-guardian/dialogue_guardian-1.3.0-py3-none-any.whl/guardian/__init__.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2025 Tony Snearly
+# SPDX-License-Identifier: OSL-3.0
+"""
+Dialogue Guardian: Universal Media Censor
+
+A Python package for automatically detecting and censoring profane language
+in video files by processing SRT subtitle files.
+"""
+
+__version__ = "1.3.0"
+__author__ = "Tony Snearly"
+
+from .cli import main
+from .core import GuardianProcessor
+
+__all__ = ["GuardianProcessor", "main"]
