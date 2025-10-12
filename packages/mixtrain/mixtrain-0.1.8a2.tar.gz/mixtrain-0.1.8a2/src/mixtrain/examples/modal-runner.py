5b405e1fb02351451f6c5618651af1708d4581f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 15:06:14 +0000

    Add package mixtrain/0.1.8a2/mixtrain-0.1.8a2.tar.gz

---
+++ packages/mixtrain/mixtrain-0.1.8a2.tar.gz/mixtrain-0.1.8a2/src/mixtrain/examples/modal-runner.py
@@ -0,0 +1,136 @@
+from modal.volume import Volume
+from modal.stream_type import StreamType
+
+
+import modal
+import uuid
+import os
+import shutil
+
+
+from mixtrain import MixClient
+
+# API_KEY = "mix-c213cf62f7ad75e50e4abc03ccd5176356851c32fd091f482934716581fa03ce" # local
+# API_KEY = "mix-8c4d367cdcbafc2fbb9ca8a5ef325955249bd9603ee2db4abb0149ccee0dc067" # morphic-staging
+API_KEY = (
+    "mix-0ffca1a34c351bc3c3e9a19cfbd9e8f3bc72018fa5e8df5a0a295b12d3162187"  # test-prod
+)
+mix = MixClient(api_key=API_KEY)
+app = modal.App.lookup("dk-app", create_if_missing=True)
+
+
+secrets = {"MIXTRAIN_API_KEY": API_KEY}
+
+for secret in mix.get_all_secrets():
+    secrets[secret["name"]] = secret["value"]
+
+uuid = str(uuid.uuid4())
+print(uuid)
+workdir = f"/workdir"
+entrypoint = "/Users/dk/code/mixflow/mixtrain/compare_t2i.py"
+files = [entrypoint]  # TODO add all files in the folder
+
+local_folder = f"/tmp/mixtrain/{uuid}"
+os.makedirs(local_folder, exist_ok=True)
+for file in files:
+    shutil.copy(file, local_folder)
+
+
+workflow_id = 3
+run_id = 8
+overrides = {
+    "limit": 5,
+    "output_dataset_name": f"t2i_eval_{workflow_id}_{run_id}",
+    "evaluation_name": f"t2i_eval_{workflow_id}_{run_id}",
+}
+entrypoint = "compare_t2i.py"
+code = f"""
+import os
+import importlib
+import inspect
+from mixtrain import MixFlow
+workflow_id = {workflow_id}
+run_id = {run_id}
+
+def load_python_modules_from_folder(folder_path: str):
+    modules = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".py") and not filename.startswith("__"):
+            path = os.path.join(folder_path, filename)
+            module_name = os.path.splitext(filename)[0]
+            spec = importlib.util.spec_from_file_location(module_name, path)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            modules.append(module)
+    return modules
+
+def find_workflow_classes_in_folder(folder_path: str):
+    modules = load_python_modules_from_folder(folder_path)
+    for module in modules:
+        for name, cls in inspect.getmembers(module, inspect.isclass):
+            if issubclass(cls, MixFlow) and cls is not MixFlow:
+                return cls
+
+_c = find_workflow_classes_in_folder("{workdir + workdir}")
+_inst_c = _c()
+
+for key, value in {overrides}.items():
+    setattr(_inst_c, key, value)
+
+try:
+    _inst_c.setup()
+except Exception as e:
+    print(e)
+    _inst_c.mix.update_workflow_run_status(workflow_id=workflow_id, run_id=run_id, status="failed")
+    raise e
+_inst_c.mix.update_workflow_run_status(workflow_id=workflow_id, run_id=run_id, status="running")
+try:
+    _inst_c.run()
+except Exception as e:
+    print(e)
+    _inst_c.mix.update_workflow_run_status(workflow_id=workflow_id, run_id=run_id, status="failed")
+    raise e
+_inst_c.mix.update_workflow_run_status(workflow_id=workflow_id, run_id=run_id, status="completed")
+print("done")
+"""
+
+with modal.Volume.ephemeral() as vol:
+    with vol.batch_upload() as f:
+        f.put_directory(local_folder, workdir)  # or dir
+
+    sb = modal.Sandbox.create(
+        app=app,
+        image=modal.Image.debian_slim().uv_pip_install(
+            "mixtrain==0.1.8a1", "pydantic<=2.11.10", "fal-client"
+        ),
+        volumes={workdir: vol},
+        timeout=600,
+        idle_timeout=10,
+        env=secrets,
+    )
+
+    # TODO: add overrides
+    # TODO: api callbacks
+    p = sb.exec(
+        "python",
+        "-c",
+        # "from importlib.metadata import version; print(version('mixtrain'))",
+        code,
+        workdir=workdir + workdir,
+    )
+
+    # p = sb.exec("ls", "-lR", workdir=workdir + workdir)
+    # for line in p.stdout:
+    #     print(line, end="")
+
+    # for line in p.stderr:
+    #     print(line, end="")
+    # p.wait()
+    # print(p.returncode)
+    # p = sb.exec("python", "-c", "import fal_client; print(fal_client.__version__)")
+    for line in p.stdout:
+        print(line, end="")
+    for line in p.stderr:
+        print(line, end="")
+
+    # sb.terminate()
