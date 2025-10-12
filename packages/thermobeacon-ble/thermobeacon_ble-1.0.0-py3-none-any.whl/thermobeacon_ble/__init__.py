e1db6b3eabe2a7d7de106963676acf0ad0cef983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:08 +0000

    Add package thermobeacon-ble/1.0.0/thermobeacon_ble-1.0.0-py3-none-any.whl

---
+++ packages/thermobeacon-ble/thermobeacon_ble-1.0.0-py3-none-any.whl/thermobeacon_ble/__init__.py
@@ -0,0 +1,33 @@
+"""Parser for ThermoBeacon BLE advertisements."""
+
+from __future__ import annotations
+
+from sensor_state_data import (
+    BinarySensorDeviceClass,
+    BinarySensorValue,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceClass,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import ThermoBeaconBluetoothDeviceData
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "ThermoBeaconBluetoothDeviceData",
+    "BinarySensorDeviceClass",
+    "BinarySensorValue",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceClass",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
