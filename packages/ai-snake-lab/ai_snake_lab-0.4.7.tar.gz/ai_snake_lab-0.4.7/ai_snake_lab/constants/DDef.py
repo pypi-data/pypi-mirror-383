935ed147264f0cafa592d3ef39547a0ec77c0f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:25 +0000

    Add package ai-snake-lab/0.4.7/ai_snake_lab-0.4.7.tar.gz

---
+++ packages/ai-snake-lab/ai_snake_lab-0.4.7.tar.gz/ai_snake_lab-0.4.7/ai_snake_lab/constants/DDef.py
@@ -0,0 +1,19 @@
+"""
+constants/DDef.py
+
+    AI Snake Game Simulator
+    Author: Nadim-Daniel Ghaznavi
+    Copyright: (c) 2024-2025 Nadim-Daniel Ghaznavi
+    GitHub: https://github.com/NadimGhaznavi/ai
+    License: GPL 3.0
+"""
+
+from ai_snake_lab.utils.ConstGroup import ConstGroup
+
+
+class DDef(ConstGroup):
+    """Defaults"""
+
+    APP_TITLE: str = "AI Snake Game Lab"
+    DOT_DB: str = ".db"
+    MOVE_DELAY: float = 0.0
