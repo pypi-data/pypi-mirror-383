01b4089aee712243e760e11a72f14e5610509606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 15:06:31 +0000

    Add package cryptg/0.5.2/cryptg-0.5.2.tar.gz

---
+++ packages/cryptg/cryptg-0.5.2.tar.gz/cryptg-0.5.2/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+from setuptools import find_packages, setup
+from setuptools_rust import RustExtension
+
+PACKAGE_NAME = "cryptg"
+PACKAGE_VERSION = "0.5.2"
+ENVVAR_VERSION_SUFFIX = "PYPI_SETUP_VERSION_SUFFIX"
+
+
+def main(args):
+    with open("README.rst", encoding='utf-8') as f:
+        long_description = f.read()
+
+    url = "https://github.com/cher-nov/" + PACKAGE_NAME
+
+    setup(
+        name=PACKAGE_NAME,
+        version=PACKAGE_VERSION+os.environ.get(ENVVAR_VERSION_SUFFIX, ""),
+        description="Cryptographic utilities for Telegram.",
+        long_description=long_description,
+        long_description_content_type="text/x-rst",
+
+        url=url,
+        download_url=url+"/releases",
+
+        author="Dmitry D. Chernov; Lonami E",
+        author_email="blackdoomer@yandex.ru",
+
+        license="CC0",
+
+        # https://pypi.python.org/pypi?:action=list_classifiers
+        classifiers=[
+            "Development Status :: 4 - Beta",
+
+            "Intended Audience :: Developers",
+            "Topic :: Security :: Cryptography",
+
+            "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+        ],
+        keywords="telegram crypto cryptography mtproto aes",
+
+        packages=find_packages(),
+        python_requires=">=3.7",
+        rust_extensions=[RustExtension("cryptg.cryptg")],
+        zip_safe=False,
+    )
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ packages/cryptg/cryptg-0.5.2.tar.gz/cryptg-0.5.2/src/lib.rs
@@ -0,0 +1,56 @@
+use pyo3::{prelude::*, types::PyBytes, wrap_pyfunction};
+
+/// Encrypts the input plain text with the 32 bytes key and IV.
+#[pyfunction]
+#[pyo3(signature = (plain, key, iv))]
+fn encrypt_ige(py: Python, plain: &[u8], key: &[u8], iv: &[u8]) -> PyResult<Py<PyBytes>> {
+    let mut key_array = [0; 32];
+    if key.len() != key_array.len() {
+        return Err(pyo3::exceptions::PyValueError::new_err("len(key) != 32"));
+    }
+    key_array.copy_from_slice(key);
+
+    let mut iv_array = [0; 32];
+    if iv.len() != iv_array.len() {
+        return Err(pyo3::exceptions::PyValueError::new_err("len(iv) != 32"));
+    }
+    iv_array.copy_from_slice(iv);
+
+    let cipher = grammers_crypto::encrypt_ige(plain, &key_array, &iv_array);
+    Ok(PyBytes::new(py, &cipher).into())
+}
+
+/// Decrypts the input cipher text with the 32 bytes key and IV.
+#[pyfunction]
+#[pyo3(signature = (cipher, key, iv))]
+fn decrypt_ige(py: Python, cipher: &[u8], key: &[u8], iv: &[u8]) -> PyResult<Py<PyBytes>> {
+    let mut key_array = [0; 32];
+    if key.len() != key_array.len() {
+        return Err(pyo3::exceptions::PyValueError::new_err("len(key) != 32"));
+    }
+    key_array.copy_from_slice(key);
+
+    let mut iv_array = [0; 32];
+    if iv.len() != iv_array.len() {
+        return Err(pyo3::exceptions::PyValueError::new_err("len(iv) != 32"));
+    }
+    iv_array.copy_from_slice(iv);
+
+    let plain = grammers_crypto::decrypt_ige(cipher, &key_array, &iv_array);
+    Ok(PyBytes::new(py, &plain).into())
+}
+
+/// Factorizes the pair of primes ``pq`` into ``(p, q)``.
+#[pyfunction]
+#[pyo3(text_signature = "(pq)")]
+fn factorize_pq_pair(pq: u64) -> (u64, u64) {
+    grammers_crypto::factorize::factorize(pq)
+}
+
+#[pymodule]
+fn cryptg(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(encrypt_ige))?;
+    m.add_wrapped(wrap_pyfunction!(decrypt_ige))?;
+    m.add_wrapped(wrap_pyfunction!(factorize_pq_pair))?;
+    Ok(())
+}
