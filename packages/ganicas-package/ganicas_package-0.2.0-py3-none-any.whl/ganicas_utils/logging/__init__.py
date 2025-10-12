b8bb85629cfe5f3ad47106cebcc503db41519761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:04:42 +0000

    Add package ganicas-package/0.2.0/ganicas_package-0.2.0-py3-none-any.whl

---
+++ packages/ganicas-package/ganicas_package-0.2.0-py3-none-any.whl/ganicas_utils/logging/__init__.py
@@ -0,0 +1,4 @@
+from ganicas_utils.logging.configuration import get_default_logging_conf
+from ganicas_utils.logging.logger import LoggingConfigurator
+
+__all__ = ["LoggingConfigurator", "get_default_logging_conf"]
