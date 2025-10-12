bc120fdc75d8ebf31c671e336d67ed99a16d5cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:28 +0000

    Add package queus/0.0.2/queus-0.0.2-py3-none-any.whl

---
+++ packages/queus/queus-0.0.2-py3-none-any.whl/Queue/__init__.py
@@ -0,0 +1,22 @@
+appname = 'queus'
+version = '0.0.2'
+pythons = '~=3.10'
+clinton = 'Clinton Abraham'
+mention = ['queue', 'queues', 'telegram']
+profile = 'https://github.com/Clinton-Abraham'
+
+DATA03 = 'Python queue'
+DATA01 = "clintonabrahamc@gmail.com"
+DATA02 = ['Natural Language :: English',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Programming Language :: Python :: 3.14',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Localization',
+          'Topic :: Software Development :: User Interfaces',
+          'Topic :: Software Development :: Version Control',
+         ]
