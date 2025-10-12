ff295269860bce8d812713d50a2b365484d9d108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:05:08 +0000

    Add package vercel-ai-sdk-mate/5.0.1/vercel_ai_sdk_mate-5.0.1-py3-none-any.whl

---
+++ packages/vercel-ai-sdk-mate/vercel_ai_sdk_mate-5.0.1-py3-none-any.whl/vercel_ai_sdk_mate/api.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from .constants import RequestBodyTriggerEnum
+from .constants import MessageRoleEnum
+from .constants import MessagePartTypeEnum
+from .constants import MessagePartStateEnum
+from .type_defs import T_REQUEST_BODY_TRIGGER_TYPE
+from .type_defs import T_MESSAGE_ROLE_TYPE
+from .type_defs import T_MESSAGE_PART_TYPE_TYPE
+from .type_defs import T_MESSAGE_PART_STATE_TYPE
+from .type_defs import T_RECORD_TYPE
+from .model import BaseMessagePart
+from .model import TextUIPart
+from .model import ReasoningUIPart
+from .model import T_PART
+from .model import Message
+from .model import RequestBody
