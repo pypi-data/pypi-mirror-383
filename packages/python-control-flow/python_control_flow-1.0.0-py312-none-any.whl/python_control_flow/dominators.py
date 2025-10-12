912b4c3735e4ac8bbc08b0fde0d034106865877c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:26 +0000

    Add package python-control-flow/1.0.0/python_control_flow-1.0.0-py312-none-any.whl

---
+++ packages/python-control-flow/python_control_flow-1.0.0-py312-none-any.whl/python_control_flow/dominators.py
@@ -0,0 +1,280 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, 2023-2025 by Rocky Bernstein <rb@dustyfeet.com>
+"""
+Computes Dominators and Dominator Regions on a Control-flow Graph.
+
+Copyright (c) 2017-2018, 2021, 2023-2025 by Rocky Bernstein
+Copyright (c) 2014 by Romain Gaucher (@rgaucher)
+"""
+
+from python_control_flow.bb import BasicBlock
+from python_control_flow.graph import TreeGraph
+from python_control_flow.traversals import dfs_postorder_nodes
+
+
+class DominatorSet(set):
+    def __str__(self) -> str:
+        sorted_set = {dom.bb.number for dom in sorted(self)}
+        return f"DominatorSet<{sorted_set}>"
+
+
+class DominatorTree:
+    """Handles the dominator trees, dominator, post-dominator
+    relation, and the computation of the dominance/post-dominance
+    frontier.
+    """
+
+    def __init__(self, cfg, debug=False):
+        self.cfg = cfg
+        self.debug = debug
+        self.root = cfg.entry_node
+        self.max_nesting_depth = -1
+        self.build()
+        cfg.dom_tree = self.build_dom_tree()
+        dfs_forest(cfg.dom_tree)
+        cfg.graph.max_nesting = cfg.max_nesting_depth = cfg.dom_tree.max_nesting
+        build_dom_set(cfg.dom_tree, debug)
+
+    @classmethod
+    def compute_dominators_in_cfg(cls, cfg, debug):
+        return DominatorTree(cfg, debug)
+
+    def build(self):
+        entry = self.cfg.entry_node
+
+        self.doms = {}  # map of node to its dominator
+        self.df = {}  # dominator frontier
+
+        self.build_dominators(entry)
+
+    def build_dominators(self, entry):
+        """
+        Builds the dominator tree based on:
+          http://www.cs.rice.edu/~keith/Embed/dom.pdf
+
+        """
+        doms = self.doms
+        doms[entry] = entry
+        post_order = dfs_postorder_nodes(entry)
+
+        post_order_number = {}
+        for i, n in enumerate(post_order):
+            post_order_number[n] = i
+
+        def intersec(b1, b2):
+            finger1 = b1
+            finger2 = b2
+            po_finger1 = post_order_number[finger1]
+            # FIXME: if finger2 isn't in post_order_number, why not?
+            if finger2 in post_order_number:
+                po_finger2 = post_order_number[finger2]
+                while po_finger1 != po_finger2:
+                    no_solution = False
+                    while po_finger1 < po_finger2:
+                        finger1 = doms.get(finger1, None)
+                        if finger1 is None:
+                            no_solution = True
+                            break
+                        po_finger1 = post_order_number[finger1]
+                        pass
+                    while po_finger2 < po_finger1:
+                        finger2 = doms.get(finger2, None)
+                        try:
+                            po_finger2 = post_order_number[finger2]
+                        except Exception:
+                            no_solution = True
+                            break
+                        if finger2 is None:
+                            no_solution = True
+                            break
+                        pass
+                    if no_solution:
+                        break
+
+            return finger1
+
+        changed = True
+
+        while changed:
+            changed = False
+            order = post_order if reversed(post_order) else post_order
+            for b in order:
+                # Skip start node which doesn't have a predecessor
+                # and was initialized above.
+                if b == entry:
+                    continue
+
+                new_idom = None
+                # Find a processed predecessor.
+                # We want to order predecessors by closeness to
+                # "b" the node we are computing a dominator from.
+                # For dominators this is the number that is closest
+                # and less than b.number.
+                # For post dominators  it is the number that is closest
+                # and greater than b.number.
+                # Sorting also gives deterministic results.
+                predecessors = sorted(
+                    [
+                        p
+                        for p in b.predecessors
+                        if post_order_number.get(p, -1) > post_order_number[b]
+                    ],
+                    key=lambda p: p.number,
+                    reverse=True,
+                )
+
+                # If no predecessors, then nothing to do here.
+                if len(predecessors) == 0:
+                    continue
+
+                new_idom = next(iter(predecessors))
+                for p in predecessors:
+                    if p == new_idom:
+                        continue
+                    if p in doms:
+                        new_idom = intersec(p, new_idom)
+                        if new_idom is not None:
+                            # We found the closest predecessor
+                            break
+                    pass
+
+                if b not in doms or doms[b] != new_idom:
+                    if b in doms:
+                        b_number = doms[b].number
+                        new_idom_number = new_idom.number
+                        do_update = b_number > new_idom_number
+                    else:
+                        do_update = True
+                    if do_update:
+                        # if self.debug:
+                        #     name = "dominator"
+                        #     print(
+                        #         f"debug: {name}[{b.number}] is "
+                        #         "{None if new_idom is None else new_idom.number}"
+                        #     )
+
+                        doms[b] = new_idom
+                        changed = True
+                        pass
+                    pass
+                pass
+            pass
+        return
+
+    def build_dom_tree(self) -> TreeGraph:
+        """Computes and return a dominator tree"""
+        t_nodes = {}
+
+        # We sort dominators to give deterministic results.
+        edge_type = "dom-edge"
+        doms_list = sorted(self.doms, key=lambda x: x.number, reverse=True)
+        doms = self.doms
+
+        root = self.cfg.entry_node
+        t = TreeGraph(root)
+
+        for node in doms_list:
+            if node not in t_nodes:
+                cur_node = t.make_add_node(node)
+                t_nodes[node] = cur_node
+            cur_node = t_nodes[node]
+
+            parent = doms.get(node, None)
+            if parent is not None and parent != node:
+                if parent not in t_nodes:
+                    parent_node = t.make_add_node(parent)
+                    t_nodes[parent] = parent_node
+                parent_node = t_nodes[parent]
+                t.make_add_edge(parent_node, cur_node, edge_type)
+                pass
+            pass
+        return t
+
+    def offset_dominates(self, start_offset: int) -> bool:
+        cfg = self.cfg
+        start_block = cfg.get_block(start_offset)
+        end_block = cfg.get_block(start_offset)
+        # FIXME: is this right
+        return start_block in end_block.doms
+
+
+def build_dom_set(t, debug=False):
+    """Makes the dominator set for each node in the tree"""
+    seen = DominatorSet()
+    for node in t.nodes:
+        if node not in seen:
+            seen.add(node)
+            build_dom_set1(node, debug)
+            pass
+        pass
+
+
+def build_dom_set1(node, debug=False):
+    """Build dominator sets from dominator node"""
+
+    node.bb.dom_set = DominatorSet(node.bb.doms)
+
+    for child in node.children:
+        build_dom_set1(child, debug)
+        node.bb.dom_set |= child.bb.dom_set
+    # We want only proper/non-trivial dominators, so remove `node` from the
+    # set.
+    node.bb.dom_set.remove(node)
+    return
+
+
+# Note: this has to be done after calling build_dom_tree()
+# which builds the dominator tree.
+def dfs_forest(t):
+    """
+    Builds data flow graph using Depth-First search.
+    """
+
+    def dfs(seen, node, nesting_depth: int):
+        # print("XXX", node, nesting_depth)
+        if node in seen:
+            return
+        if nesting_depth > t.max_nesting:
+            t.max_nesting = nesting_depth
+        node.bb.nesting_depth = nesting_depth
+        seen.add(node)
+        node.bb.doms = node.doms = DominatorSet([node])
+        node.bb.reach_offset = node.reach_offset = node.bb.end_offset
+        # print(f"XXX1: {node.bb} reach_offset set to {node.bb.end_offset}")
+
+        for n in node.children:
+            dfs(seen, n, nesting_depth + 1)
+            node.doms |= n.doms
+            node.bb.doms |= node.doms
+            if node.reach_offset < n.reach_offset:
+                # print(f"XXX1: {node.bb} reach_offset set to {n.reach_offset}")
+                node.bb.reach_offset = node.reach_offset = n.reach_offset
+                pass
+            pass
+        # print("node %d has children %s" %
+        #       (node.number, [n.number for n in node.children]))
+
+    seen = set([])
+    for node in t.nodes:
+        if node.bb == t.root:
+            root_node = node
+            break
+    else:
+        raise RuntimeError("Root node not found in dominator tree")
+
+    dfs(seen, root_node, 0)
+
+    for node in t.nodes:
+        if node not in seen:
+            dfs(seen, node.bb, 0)
+    return
+
+
+def dominates(bb1: BasicBlock, bb2: BasicBlock, proper=True) -> bool:
+    """Return true if bb1 dominates bb2. In other words,
+    bb2 is in bb1's dominator set.
+    """
+    if proper:
+        return bb2 in bb1.dom_set
+    else:
+        return bb2 == bb1 or bb2 in bb1.dom_set
---
+++ packages/python-control-flow/python_control_flow-1.0.0-py312-none-any.whl/python_control_flow/graph.py
@@ -0,0 +1,427 @@
+# -*- coding: utf-8 -*-
+"""
+  Graph data structures
+
+  Stripped down and modified from equip routine:
+  :copyright: (c) 2018, 2021, 2023-2024 by Rocky Bernstein
+  :copyright: (c) 2014 by Romain Gaucher (@rgaucher)
+"""
+
+from typing import Optional, Set
+from enum import Enum
+
+# First or Basic block that we entered on. Usually
+# at offset 0.
+# Does this need to be a set?
+BB_ENTRY = 0
+
+# Block is at the end, and doesn't have a following instruction.
+# We have though an edge to the successor *instruction* for assisting displaying
+# the control-flow graph the way the program was written.
+BB_NOFOLLOW = 1
+
+# a SETUP_LOOP instruction marking the beginning of a loop.
+# the jump offset can indicate the range of the loop
+BB_LOOP = 2
+
+# A BREAK instruction breaking out of a loop
+BB_BREAK = 3
+
+# POP_BLOCKs detect presence of scope-defining
+# blocks, and can be used to tell the difference between
+# 'if's and 'whiles'
+BB_POP_BLOCK = 4
+
+# Does the basic block contain a single POP_BLOCK instruction?
+# If so, it can signal the difference between "while else"
+# and "while" (with no "else"). The latter has the single
+# POP_BLOCK instruction.
+BB_SINGLE_POP_BLOCK = 5
+
+# Basic blocks that start out POP_BLOCK but have further
+# instructions are generally in "else" clauses, like "while else"
+BB_STARTS_POP_BLOCK = 6
+
+BB_EXCEPT = 7
+
+# Relative or absolute JUMP instructions. Occurs in "break", "continue",
+# ends of loops, and in "if", "elif" constructs
+BB_JUMP_UNCONDITIONAL = 8
+
+# A FOR instruction which is an indicator of a "for" loop
+BB_FOR = 9
+
+# A "FINALLY" instruction which is an indicator of a "finally'
+BB_FINALLY = 10
+
+# A SETUP_EXCEPT is an indicator of "try"
+BB_TRY = 11
+
+# A "END_FINALLY" instruction which is the indicator of the *beginning*
+# of a "finally' block
+BB_END_FINALLY = 12
+
+# An artificial block where all exits and returns are tied
+# to. There will be only one such block, and it used in
+# postdominsator or reverse dominiator calculation.
+BB_EXIT = 13
+
+# Has a conditional jump of some sort. This would be
+# found in "if" constructs.
+BB_JUMP_FORWARD_IF_FALSE = 14
+BB_JUMP_FORWARD_IF_TRUE = 15
+BB_JUMP_BACKWARD_IF_FALSE = 16
+BB_JUMP_BACKWARD_IF_TRUE = 17
+
+# Jumps to what would be the fallthough.
+# If there were optimization, this instruction would be removed.
+# This crud though can be useful in determining control
+# structures that were used in the same way junk DNA may be
+# useful in determining evolution history.
+
+# We mostly use it in drawing graphs to make
+# sure the jump arrow points straight down.
+BB_JUMP_TO_FALLTHROUGH = 18
+
+# The beginning of the basic block is a join.
+BB_JOIN_POINT = 19
+
+# Basic block ends in a return or an raise that is not inside
+# a "try" block.
+BB_RETURN = 20
+
+# Unreachable block
+BB_DEAD_CODE = 21
+
+FLAG2NAME = {
+    BB_ENTRY: "entry",
+    BB_EXIT: "exit",
+    BB_NOFOLLOW: "no fallthrough",
+    BB_LOOP: "loop",
+    BB_BREAK: "break",
+    BB_POP_BLOCK: "block",
+    BB_SINGLE_POP_BLOCK: "single pop block",
+    BB_STARTS_POP_BLOCK: "starts with pop block",
+    BB_EXCEPT: "except",
+    BB_JOIN_POINT: "join block",
+    BB_JUMP_UNCONDITIONAL: "unconditional",
+    BB_JUMP_BACKWARD_IF_FALSE: "jump backward if false",
+    BB_JUMP_BACKWARD_IF_TRUE: "jump backward if true",
+    BB_JUMP_FORWARD_IF_FALSE: "jump forward if false",
+    BB_JUMP_FORWARD_IF_TRUE: "jump forward if true",
+    BB_JUMP_TO_FALLTHROUGH: "jump to fallthough",
+    BB_FOR: "for",
+    BB_FINALLY: "finally",
+    BB_END_FINALLY: "end finally",
+    BB_TRY: "try",
+    BB_RETURN: "return",
+}
+
+# FIXME: some of the classifications may be overkill.
+ScopeEdgeKind = Enum(
+    "ScopeEdgeKind",
+    [
+        # Edge hasn't been computed yet:
+        "Unknown",
+        # Edge starts a new scope.
+        # Example:
+        #   if <jump-to-then> then <jump-is-here> ... end
+        "NewScope",
+        # Edge jumps from one alternate to the next one
+        # Example:
+        #   if <jump to elif test when condition not true> ... elif ... end
+        "Alternate",
+        # Edge joins from an inner scope to an outer one, e.g.
+        # "if ... <jump to end> else ... end" or
+        # "if ... <falltrough after end> end" or
+        # "while ... break <jump to end> ... end
+        "Join",
+        # Edge jumps to a loop head
+        "Looping",
+    ],
+)
+
+jump_flags = set([BB_JUMP_UNCONDITIONAL, BB_BREAK])
+
+
+def format_flags(flags):
+    return ", ".join([FLAG2NAME[flag] for flag in FLAG2NAME if flag in flags])
+
+
+def format_flags_with_width(flags, max_width, newline):
+    result = ""
+    r = 0
+    sep = ""
+    remain = max_width
+    for flag in FLAG2NAME:
+        if flag in flags:
+            add = sep + FLAG2NAME[flag]
+            a = len(add)
+            remain = max_width - (r + a)
+            if remain <= 1:
+                if result:
+                    result += "," + (" " * (remain - 1))
+                    add = FLAG2NAME[flag]
+                    pass
+                result += newline
+                r = 0
+            r += a
+            result += add
+            sep = ", "
+            pass
+        pass
+
+    return result + "}" + (" " * remain)
+
+
+class Node:
+    GLOBAL_COUNTER = 0
+
+    def __init__(self, bb):
+        Node.GLOBAL_COUNTER += 1
+        if bb.number is None:
+            self.number = Node.GLOBAL_COUNTER
+        else:
+            self.number = bb.number
+        self.flags = bb.flags
+        self.bb = bb
+
+        # After the graph is built, a later pass
+        # fills out the in edges and the out edges,
+        # and whether the Node is a join Node.
+        self.in_edges: Optional[Set[Node]] = None
+        self.out_edges: Optional[Set[Node]] = None
+        self.is_dead_code: Optional[bool] = None
+        self.is_join_node: Optional[bool] = None
+
+    @classmethod
+    def reset(cls):
+        cls.GLOBAL_COUNTER = 0
+
+    def __eq__(self, obj) -> bool:
+        return isinstance(obj, Node) and obj.number == self.number
+
+    def __hash__(self) -> int:
+        return hash("node-" + str(self.number))
+
+    def __lt__(self, obj) -> bool:
+        return not self.number < obj.number
+
+    def __ne__(self, obj):
+        return not self == obj
+
+    def __repr__(self):
+
+        return "Node%d(flags=%s, bb=%s)" % (
+            self.number,
+            repr(self.flags),
+            repr(self.bb),
+        )
+
+    def __str__(self):
+        return "Node%d(flags=%s, bb=%s)" % (
+            self.number,
+            repr(self.flags),
+            str(self.bb),
+        )
+
+
+class Edge:
+    GLOBAL_COUNTER = 0
+
+    def __init__(self, source, dest, kind, data):
+        Edge.GLOBAL_COUNTER += 1
+        self.id = Edge.GLOBAL_COUNTER
+        self.source = source
+        self.dest = dest
+        self.kind = kind
+        self.scoping_kind = ScopeEdgeKind.Unknown
+        self.flags = set()
+        self.data = data
+
+    @classmethod
+    def reset(self):
+        self.GLOBAL_COUNTER = 0
+
+    def __ne__(self, obj):
+        return not self == obj
+
+    def __eq__(self, obj):
+        return isinstance(obj, Edge) and obj.id == self.id
+
+    def __hash__(self):
+        return hash("edge-" + str(self.id))
+
+    def __str__(self):
+        return "Edge%d(source=%s, dest=%s, kind=%s, data=%s)" % (
+            self.id,
+            self.source,
+            self.dest,
+            str(self.kind),
+            str(self.data),
+        )
+
+    def __repr__(self):
+        return "Edge%d(source=%s, dest=%s, kind=%s, data=%s)" % (
+            self.id,
+            self.source,
+            self.dest,
+            repr(self.kind),
+            repr(self.data),
+        )
+
+    def is_conditional_jump(self) -> bool:
+        """Return True is edge is attached to a conditional jump
+        instruction at its source.
+        """
+        return {
+            BB_JUMP_FORWARD_IF_TRUE,
+            BB_JUMP_FORWARD_IF_FALSE,
+            BB_JUMP_BACKWARD_IF_TRUE,
+            BB_JUMP_BACKWARD_IF_FALSE,
+        }.intersection(
+            self.source.flags
+        ) and self.dest.bb.start_offset in self.source.bb.jump_offsets
+
+
+class DiGraph:
+    """
+    A simple directed-graph structure.
+    """
+
+    def __init__(self):
+        Node.reset()
+        Edge.reset()
+        self.nodes = set()
+        self.edges = set()
+
+        # Maximum nesting of graph. -1 means this hasn't been
+        # computed.
+        self.max_nesting: int = -1
+
+    def add_edge(self, edge):
+        if edge in self.edges:
+            raise Exception("Edge already present")
+        source_node, dest_node = edge.source, edge.dest
+
+        self.edges.add(edge)
+        self.add_node(source_node)
+        self.add_node(dest_node)
+
+    def add_node(self, node):
+        self.nodes.add(node)
+
+    def to_dot(self, exit_node, is_dominator_format: bool = False):
+        from python_control_flow.dotio import DotConverter
+
+        return DotConverter.process(self, exit_node, is_dominator_format)
+
+    def add_edge_info_to_nodes(self):
+        """
+        Go through the graph and fill out `self.in_edges` and `self.out_edges`.
+        """
+        for node in self.nodes:
+            node.out_edges = set()
+            node.in_edges = set()
+
+        for edge in self.edges:
+            edge.source.out_edges.add(edge)
+            edge.dest.in_edges.add(edge)
+
+    @staticmethod
+    def make_node(bb):
+        return Node(bb)
+
+    @staticmethod
+    def make_edge(source=None, dest=None, kind=None, data=None):
+        return Edge(source=source, dest=dest, kind=kind, data=data)
+
+    # Some helpers
+    def make_add_node(self, bb):
+        node = DiGraph.make_node(bb)
+        self.add_node(node)
+        return node
+
+    def make_add_edge(self, source=None, dest=None, kind=None, data=None) -> Edge:
+        edge = DiGraph.make_edge(source=source, dest=dest, kind=kind, data=data)
+        self.add_edge(edge)
+        return edge
+
+
+class TreeGraph(DiGraph):
+    """
+    A simple tree structure for basic blocks.
+    """
+
+    def __init__(self, root):
+        Edge.reset()
+        Node.reset()
+        self.edges = set()
+        self.nodes = []
+        self.root = root
+        self.root_node = None
+
+        # Maximum nesting of graph. -1 means this hasn't been
+        # computed.
+        self.max_nesting: int = -1
+
+    def add_edge(self, edge):
+        if edge in self.edges:
+            raise Exception("Edge already present")
+        source_node, dest_node = edge.source, edge.dest
+
+        self.add_node(source_node)
+        self.add_node(dest_node)
+        self.edges.add(edge)
+        source_node.children |= set([dest_node])
+        dest_node.parent = set([source_node])
+
+    def add_node(self, node):
+        if node.bb not in [n.bb for n in self.nodes]:
+            node.children = set([])
+            node.parent = None
+            self.nodes.append(node)
+
+    def postorder_traverse(self):
+        """Traverse the tree in preorder"""
+        if self.nodes:
+            return self.postorder_traverse1(self.nodes[0])
+
+    def postorder_traverse1(self, node):
+        """Traverse the tree in preorder"""
+        for child in node.children:
+            self.postorder_traverse1(child)
+            yield child
+        yield node
+
+
+def write_dot(
+    name: str,
+    prefix: str,
+    graph: Optional[DiGraph],
+    write_png: bool = False,
+    debug=True,
+    is_dominator_format: bool = False,
+    exit_node=None,
+):
+    """Produce and write dot and png files for control-flow graph
+    `cfg`; `func_or_code_name` is the func_or_code_name of the
+    code and `prefix` indicates the file prefix to use.
+    dot is converted to PNG and dumped if `write_bool` is True.
+    """
+
+    if graph is None:
+        return
+
+    path_safe = name.translate(name.maketrans(" <>", "_[]"))
+    dot_path = f"{prefix}-{path_safe}.dot"
+    open(dot_path, "w").write(graph.to_dot(exit_node, is_dominator_format))
+    if debug:
+        print(f"{dot_path} written")
+    if write_png:
+        import os
+
+        png_path = f"{prefix}-{path_safe}.png"
+        os.system(f"dot -Tpng {dot_path} > {png_path}")
+        if debug:
+            print(f"{png_path} written")
