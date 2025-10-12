21e3482f5b2edc474d60ed665bdde590ee54ebc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:43 +0000

    Add package voxcity/0.6.31/voxcity-0.6.31-py3-none-any.whl

---
+++ packages/voxcity/voxcity-0.6.31-py3-none-any.whl/voxcity/__init__.py
@@ -0,0 +1,8 @@
+__author__ = """Kunihiko Fujiwara"""
+__email__ = 'kunihiko@nus.edu.sg'
+__version__ = '0.1.0'
+
+# from .generator import *
+# from .download import mbfp
+# # from .utils.draw import rotate_rectangle, draw_rectangle_map
+# from .geo import draw, utils
