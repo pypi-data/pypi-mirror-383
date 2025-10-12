c40b47eae99c6d9324aa12ec674775e11f79c3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 15:05:04 +0000

    Add package amphi-etl/0.9.0a2/amphi-etl-0.9.0a2.tar.gz

---
+++ packages/amphi-etl/amphi-etl-0.9.0a2.tar.gz/amphi-etl-0.9.0a2/packages/theme-light/src/index.ts
@@ -0,0 +1,66 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { folderIcon, saveIcon, runIcon, newFolderIcon, fileUploadIcon, filterIcon, refreshIcon, codeIcon, fileIcon, pdfIcon, spreadsheetIcon, jsonIcon, notebookIcon, listIcon, homeIcon } from '@jupyterlab/ui-components';
+import { folderAmphiIcon, searchAmphiIcon, saveAmphiIcon, playAmphiIcon, folderPlusAmphiIcon, uploadAmphiIcon, reloadAmphiIcon, fileSourceAmphiIcon, fileAmphiIcon, filePdfAmphiIcon, fileCsvAmphiIcon, fileJsonAmphiIcon, fileNotebookIcon, consoleIcon, homeAmphiIcon, fileParquetAmphiIcon, fileExcelAmphiIcon } from './icons'
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the @amphi/theme-light extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: '@amphi/theme-light',
+  requires: [IThemeManager],
+  autoStart: true,
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    const style = '@amphi/theme-light/index.css';
+
+    manager.register({
+      name: 'Amphi Light',
+      isLight: true,
+      themeScrollbars: false,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+
+    app.docRegistry.addFileType(
+      {
+        name: 'parquet',
+        displayName: 'parquet',
+        extensions: ['.parquet'],
+        icon: fileParquetAmphiIcon,
+        fileFormat: "base64",
+      }
+    );
+
+    app.docRegistry.addFileType(
+      {
+        name: 'excel',
+        displayName: 'Excel',
+        extensions: ['.xlsx', 'xls'],
+        icon: fileExcelAmphiIcon,
+        fileFormat: "base64",
+      }
+    );
+
+    folderIcon.svgstr = folderAmphiIcon.svgstr;
+    saveIcon.svgstr = saveAmphiIcon.svgstr;
+    runIcon.svgstr = playAmphiIcon.svgstr
+    newFolderIcon.svgstr = folderPlusAmphiIcon.svgstr;
+    fileUploadIcon.svgstr = uploadAmphiIcon.svgstr;
+    refreshIcon.svgstr = reloadAmphiIcon.svgstr;
+    codeIcon.svgstr = fileSourceAmphiIcon.svgstr;
+    fileIcon.svgstr = fileAmphiIcon.svgstr;
+    pdfIcon.svgstr = filePdfAmphiIcon.svgstr;
+    spreadsheetIcon.svgstr = fileCsvAmphiIcon.svgstr;
+    jsonIcon.svgstr = fileJsonAmphiIcon.svgstr;
+    notebookIcon.svgstr = fileNotebookIcon.svgstr;
+    listIcon.svgstr = consoleIcon.svgstr;
+    homeIcon.svgstr = homeAmphiIcon.svgstr;
+    filterIcon.svgstr = searchAmphiIcon.svgstr;
+
+  }
+};
+
+export default extension;
