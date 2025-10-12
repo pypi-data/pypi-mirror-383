6785e1213b488dbaf3df5237259f1e08290be8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:56 +0000

    Add package ry-pg-utils/1.0.9/ry_pg_utils-1.0.9-py3-none-any.whl

---
+++ packages/ry-pg-utils/ry_pg_utils-1.0.9-py3-none-any.whl/ry_pg_utils/ipc/channels.py
@@ -0,0 +1,11 @@
+from ry_redis_bus.channels import Channel
+
+from ry_pg_utils.pb_types.database_pb2 import DatabaseConfigPb  # pylint: disable=no-name-in-module
+from ry_pg_utils.pb_types.database_pb2 import (  # pylint: disable=no-name-in-module
+    DatabaseNotificationPb,
+)
+
+# Channels
+DATABASE_CHANNEL = Channel("DATABASE_CHANNEL", DatabaseConfigPb)
+DATABASE_CONFIG_CHANNEL = Channel("DATABASE_CONFIG_CHANNEL", DatabaseConfigPb)
+DATABASE_NOTIFY_CHANNEL = Channel("DATABASE_NOTIFY_CHANNEL", DatabaseNotificationPb)
