7aaac2a3a5995020177730e70bcf77b26340adc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:36 +0000

    Add package streamlit-passwordless/0.17.0/streamlit_passwordless-0.17.0.tar.gz

---
+++ packages/streamlit-passwordless/streamlit_passwordless-0.17.0.tar.gz/streamlit_passwordless-0.17.0/src/streamlit_passwordless/database/crud/user.py
@@ -0,0 +1,259 @@
+r"""Database operations on the user table."""
+
+# Standard library
+import logging
+from collections.abc import Sequence
+from typing import Literal, overload
+
+# Third party
+import pandas as pd
+from sqlalchemy import and_, select
+from sqlalchemy.exc import SQLAlchemyError
+
+# Local
+from streamlit_passwordless import exceptions
+from streamlit_passwordless.database import models
+from streamlit_passwordless.database.core import Session
+from streamlit_passwordless.database.core import commit as db_commit
+from streamlit_passwordless.database.crud.custom_role import get_custom_roles
+from streamlit_passwordless.models import User
+
+logger = logging.getLogger(__name__)
+
+
+@overload
+def get_all_users(
+    session: Session,
+    skip: int = 0,
+    limit: int | None = None,
+    as_df: Literal[False] = False,
+    index_col: str = 'user_id',
+) -> Sequence[models.User]: ...
+
+
+@overload
+def get_all_users(
+    session: Session,
+    skip: int = 0,
+    limit: int | None = None,
+    as_df: Literal[True] = True,
+    index_col: str = 'user_id',
+) -> pd.DataFrame: ...
+
+
+def get_all_users(
+    session: Session,
+    skip: int = 0,
+    limit: int | None = None,
+    as_df: bool = False,
+    index_col: str = 'user_id',
+) -> pd.DataFrame | Sequence[models.User]:
+    r"""Get all users from the database.
+
+    Parameters
+    ----------
+    session : Session
+        An active database session.
+
+    skip : int, default 0
+        The number of users to skip from the beginning of the result set.
+        The users are order by their username.
+
+    limit : int or None, default None
+        Limit the result to desired number of users. If None all users are returned.
+
+    as_df : bool, default False
+        Return the users as a :class:`pandas.DataFrame` instead of a sequence of user models.
+
+    index_col : str, default 'user_id'
+        The column from the user table to use as the index column
+        of the :class:`pandas.DataFrame` if `as_df` is True.
+
+    Returns
+    -------
+    pandas.DataFrame or Sequence[streamlit_passwordless.db.models.User]
+        The users from the database.
+    """
+
+    query = select(models.User).order_by(models.User.username).offset(skip).limit(limit)
+
+    if as_df:
+        return pd.read_sql_query(sql=query, con=session.bind, index_col=index_col)  # type: ignore
+    return session.scalars(query).all()
+
+
+def get_user_by_username(
+    session: Session, username: str, disabled: bool | None = False
+) -> models.User | None:
+    r"""Get a user by its username.
+
+    Parameters
+    ----------
+    session : Session
+        An active database session.
+
+    username : str
+        The username to filter by.
+
+    disabled : bool or None, default False
+        True if filtering for a disabled user and False for an enabled user.
+        If None filtering by a disabled or enabled user is omitted.
+
+    Returns
+    -------
+    streamlit_passwordless.db.models.User or None
+        The user with `username` or None if a user with `username` was not found.
+    """
+
+    if disabled is None:
+        where_clause = models.User.username == username
+    else:
+        where_clause = and_(models.User.username == username, models.User.disabled == disabled)
+
+    query = select(models.User).where(where_clause)
+
+    return session.scalars(query).one_or_none()
+
+
+def get_user_by_user_id(
+    session: Session,
+    user_id: models.UserID,
+    disabled: bool | None = False,
+    is_verified: bool | None = None,
+) -> models.User | None:
+    r"""Get a user by user_id.
+
+    Parameters
+    ----------
+    session : streamlit_passwordless.db.Session
+        An active database session.
+
+    user_id : streamlit_passwordless.UserID
+        The user_id to filter by.
+
+    disabled : bool or None, default False
+        True if filtering for a disabled user and False for an active user.
+        If None filtering by a disabled or enabled user is omitted.
+
+    is_verified : bool or None, default None
+        If True filter by verified users and if False by non-verified users.
+        If None filtering by verified users is omitted.
+
+    Returns
+    -------
+    streamlit_passwordless.db.models.User or None
+        The user matching `user_id` or None if a user with `user_id` was not found.
+
+    Raises
+    ------
+    streamlit_passwordless.DatabaseError
+        If an error occurs while loading the user from the database.
+    """
+
+    if disabled is None:
+        where_clause = models.User.user_id == user_id
+    else:
+        where_clause = and_(models.User.user_id == user_id, models.User.disabled == disabled)
+
+    query = select(models.User).where(where_clause)
+
+    if is_verified is True:
+        query = query.where(models.User.verified_at._is_not(None))
+    elif is_verified is False:
+        query = query.where(models.User.verified_at._is(None))
+    else:
+        pass
+
+    try:
+        return session.scalars(query).one_or_none()
+    except SQLAlchemyError as e:
+        raise exceptions.DatabaseError(
+            f'Error loading user {user_id=} from database!', e=e
+        ) from None
+
+
+def create_user(
+    session: Session,
+    user: User,
+    custom_roles: Sequence[models.CustomRole] | None = None,
+    created_by_user_id: models.UserID | None = None,
+    commit: bool = False,
+) -> models.User:
+    r"""Create a new user in the database.
+
+    Parameters
+    ----------
+    session : streamlit_passwordless.db.Session
+        An active database session.
+
+    user : streamlit_passwordless.User
+        The user to create.
+
+    custom_roles : Sequence[streamlit_passwordless.db.models.CustomRole] or None, default None
+        The custom roles from the active database `session` to associate with the user.
+        If provided these roles will take precedence over the custom roles defined on
+        `user` and avoids a database lookup since the custom roles already exist in the `session`.
+
+    created_by_user_id : streamlit_passwordless.UserID or None, default None
+        The ID of the user that is creating the new user.
+
+    commit : bool, default False
+        True if the added user should be committed after being added to the session and False
+        to commit later. Note that the returned `db_user` object will be in a expired state
+        if committing and will be re-loaded from the database upon next access.
+
+    Returns
+    -------
+    db_user : streamlit_passwordless.db.models.User
+        The database model of the created user.
+
+    Raises
+    ------
+    streamlit_passwordless.DatabaseCreateUserError
+        If an error occurs while saving the user to the database or if the
+        role of the user does not exist in the database.
+    """
+
+    if (role_id := user.role.role_id) is None:
+        error_msg = (
+            f'Cannot create user "{user.username}" because user.role.role_id is not specified!'
+        )
+        logger.error(error_msg)
+        raise exceptions.DatabaseCreateUserError(error_msg)
+
+    db_user = models.User(
+        user_id=user.user_id,
+        username=user.username,
+        ad_username=user.ad_username,
+        displayname=user.displayname,
+        role_id=role_id,
+        verified_at=user.verified_at,
+        disabled=user.disabled,
+        disabled_at=user.disabled_at,
+        created_by=created_by_user_id,
+    )
+
+    if emails := user.emails:
+        db_emails = [models.Email(**email.model_dump()) for email in emails]
+        db_user.emails.extend(db_emails)
+        session.add_all(db_emails)
+
+    if custom_roles:
+        db_user.custom_roles = {model.role_id: model for model in custom_roles}
+    elif _custom_roles := user.custom_roles:
+        db_custom_roles = get_custom_roles(session=session, role_ids=_custom_roles.keys())
+        db_user.custom_roles = {model.role_id: model for model in db_custom_roles}
+
+    session.add(db_user)
+
+    if commit:
+        error_msg = (
+            f'Unable to save user "{user.username}" to database! Check the logs for more details.'
+        )
+        try:
+            db_commit(session=session, error_msg=error_msg)
+        except exceptions.DatabaseError as e:
+            logger.error(e.detailed_message)
+            raise exceptions.DatabaseCreateUserError(error_msg, e=e) from None
+
+    return db_user
---
+++ packages/streamlit-passwordless/streamlit_passwordless-0.17.0.tar.gz/streamlit_passwordless-0.17.0/src/streamlit_passwordless/database/models.py
@@ -0,0 +1,619 @@
+r"""The models that represent tables in the database."""
+
+# Standard library
+import os
+from datetime import datetime
+from enum import StrEnum
+from typing import ClassVar, Self, TypeAlias
+from uuid import UUID, uuid4
+
+# Third party
+from sqlalchemy import (
+    BigInteger,
+    Column,
+    DateTime,
+    ForeignKey,
+    Identity,
+    Index,
+    Integer,
+    MetaData,
+    Table,
+    UniqueConstraint,
+    func,
+)
+from sqlalchemy.dialects.sqlite import INTEGER as SQLITE_INTEGER
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    attribute_keyed_dict,
+    mapped_column,
+    relationship,
+)
+
+UserID: TypeAlias = UUID
+
+SCHEMA: str | None = os.getenv('STP_DB_SCHEMA')
+metadata_obj = MetaData(schema=SCHEMA)
+
+updated_at_column: Mapped[datetime | None] = mapped_column(
+    DateTime(timezone=True), onupdate=func.current_timestamp()
+)
+created_at_column: Mapped[datetime] = mapped_column(
+    DateTime(timezone=True), server_default=func.current_timestamp()
+)
+
+
+def _column_to_str(col: object) -> str:
+    r"""Convert a column value to string format.
+
+    To be used for the `__repr__` methods of the models.
+    """
+
+    return col.isoformat() if isinstance(col, datetime) else repr(col)
+
+
+class Base(DeclarativeBase):
+    r"""The base model all database models will inherit from.
+
+    It defines the schema/user where the tables will be created in the
+    database and the columns, which are common for all tables.
+
+    Class variables
+    ---------------
+    columns__repr__ : ClassVar[tuple[str, ...]], default tuple()
+        The names of the columns that should be part of the `__repr__` method output.
+        Each model should define this variable. If columns are defined on :class:`Base`
+        they should not be part of `columns__repr__`.
+
+    indent_space__repr__ : ClassVar[str], default ' ' * 4
+        The indentation space used for each column in the `__repr__` method.
+    """
+
+    columns__repr__: ClassVar[tuple[str, ...]] = ()
+    indent_space__repr__: ClassVar[str] = ' ' * 4
+
+    metadata = metadata_obj
+
+    def __repr__(self) -> str:
+        r"""A string representation of the model."""
+
+        indent = self.indent_space__repr__
+        output = f'{self.__class__.__name__}(\n'
+
+        for col in self.columns__repr__:
+            value = getattr(self, col)
+            value = _column_to_str(value)
+            output += f'{indent}{col}={value},\n'
+
+        output += ')'
+
+        return output
+
+
+class ModifiedAndCreatedColumnMixin:
+    r"""Add columns for when a record was last updated and created in a table.
+
+    Parameters
+    ----------
+    updated_at : datetime or None
+        The timestamp at which the record was last updated (UTC).
+
+    updated_by : streamlit_passwordless.UserID or None
+        The ID of the user that last updated the record.
+
+    created_at : datetime
+        The timestamp at which the record was created (UTC).
+        Defaults to current timestamp.
+
+    created_by : streamlit_passwordless.UserID or None
+        The ID of the user that created the record.
+
+    A table model should inherit from this class prior to :class:`Base`.
+    E.g. creating the user model:
+
+    .. code-block:: python
+
+        class User(ModifiedAndCreatedColumnMixin, Base):
+            pass
+    """
+
+    updated_at: Mapped[datetime | None] = updated_at_column
+    updated_by: Mapped[UUID | None]
+    created_at: Mapped[datetime] = created_at_column
+    created_by: Mapped[UUID | None]
+
+
+class UserRoleName(StrEnum):
+    r"""The predefined user role names of streamlit-passwordless.
+
+    These roles are created in the database when the database is initialized.
+    The default role of a new user is :attr:`UserRoleName.USER`.
+
+    Members
+    -------
+    VIEWER
+        A user that can only view data within an application.
+
+    USER
+        The standard user with normal privileges. When a user is created it is
+        assigned this role by default.
+
+    SUPERUSER
+        A user with higher privileges that can perform certain
+        operations that a normal `USER` can not.
+
+    ADMIN
+        An admin has full access to everything. Only admin users may sign in to the admin page
+        and manage the users of the application. An application should have at least one admin.
+    """
+
+    VIEWER = 'Viewer'
+    USER = 'User'
+    SUPERUSER = 'SuperUser'
+    ADMIN = 'Admin'
+
+
+class Role(ModifiedAndCreatedColumnMixin, Base):
+    r"""The role of a user.
+
+    A :class:`User` is associated with a role to manage its privileges within an application.
+    The available roles are predefined by streamlit-passwordless, but can be extended if needed.
+
+    Parameters
+    ----------
+    role_id : int
+        The primary key of the table.
+
+    name : str
+        The name of the role. Must be unique.
+
+    rank : int
+        The rank of the role. A role with a higher rank has more privileges. Used
+        for comparing roles against one another. Two roles may have the same rank.
+
+    description : str or None, default None
+        A description of the role.
+
+    updated_at : datetime or None
+        The timestamp at which the role was last updated (UTC).
+
+    updated_by : str or None
+        The ID of the user that last updated the role.
+
+    created_at : datetime
+        The timestamp at which the role was created (UTC).
+        Defaults to current timestamp.
+
+    created_by : str or None
+        The ID of the user that created the role.
+
+    users : list[User]
+        The users that have the role assigned.
+    """
+
+    columns__repr__: ClassVar[tuple[str, ...]] = (
+        'role_id',
+        'name',
+        'rank',
+        'description',
+        'updated_at',
+        'updated_by',
+        'created_at',
+        'created_by',
+    )
+
+    __tablename__ = 'stp_role'
+
+    role_id: Mapped[int] = mapped_column(Integer(), Identity(), primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    rank: Mapped[int]
+    description: Mapped[str | None]
+    users: Mapped[list['User']] = relationship(back_populates='role')
+
+    @classmethod
+    def create_viewer(cls) -> Self:
+        r"""Create the VIEWER role."""
+
+        return cls(
+            role_id=1,
+            name=UserRoleName.VIEWER,
+            rank=1,
+            description='A user that can only view data within an application.',
+        )
+
+    @classmethod
+    def create_user(cls) -> Self:
+        r"""Create the USER role, which is the default for a new user."""
+
+        return cls(
+            role_id=2,
+            name=UserRoleName.USER,
+            rank=2,
+            description=(
+                'The standard user with normal privileges. The default role for a new user.'
+            ),
+        )
+
+    @classmethod
+    def create_superuser(cls) -> Self:
+        r"""Create the SUPERUSER role."""
+
+        return cls(
+            role_id=3,
+            name=UserRoleName.SUPERUSER,
+            rank=3,
+            description=(
+                'A user with higher privileges that can perform certain '
+                'operations that a normal `USER` can not.'
+            ),
+        )
+
+    @classmethod
+    def create_admin(cls) -> Self:
+        r"""Create the ADMIN role."""
+
+        return cls(
+            role_id=4,
+            name=UserRoleName.ADMIN,
+            rank=4,
+            description=(
+                'An admin has full access to everything. Only admin users may sign '
+                'in to the admin page and manage the users of the application. '
+                'An application should have at least one admin.'
+            ),
+        )
+
+
+Index(f'{Role.__tablename__}_name_ix', Role.name)
+
+
+class CustomRole(ModifiedAndCreatedColumnMixin, Base):
+    r"""The custom roles of a user.
+
+    Custom roles can be defined specifically for each application.
+    A user may have none or multiple custom roles, which may grant
+    application specific privileges or hide/expose specific pages.
+
+    Parameters
+    ----------
+    role_id : int
+        The primary key of the table.
+
+    name : str
+        The name of the role.
+
+    rank : int
+        The rank of the role. A role with a higher rank has more privileges. Used
+        for comparing roles against one another. Two roles may have the same rank.
+
+    description : str or None, default None
+        A description of the role.
+
+    updated_at : datetime or None
+        The timestamp at which the custom role was last updated (UTC).
+
+    updated_by : str or None
+        The ID of the user that last updated the custom role.
+
+    created_at : datetime
+        The timestamp at which the custom role was created (UTC).
+        Defaults to current timestamp.
+
+    created_by : str or None
+        The ID of the user that created the custom role.
+
+    users : dict[str, User]
+        A mapping of the users that have the custom role assigned.
+        The key is the username of the user.
+    """
+
+    columns__repr__: ClassVar[tuple[str, ...]] = (
+        'role_id',
+        'name',
+        'rank',
+        'description',
+        'updated_at',
+        'updated_by',
+        'created_at',
+        'created_by',
+    )
+
+    __tablename__ = 'stp_custom_role'
+
+    role_id: Mapped[int] = mapped_column(Integer(), Identity(), primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    rank: Mapped[int]
+    description: Mapped[str | None]
+    users: Mapped[dict[str, 'User']] = relationship(
+        secondary='stp_user_custom_role_link',
+        back_populates='custom_roles',
+        collection_class=attribute_keyed_dict('username'),
+    )
+
+
+Index(f'{CustomRole.__tablename__}_name_ix', CustomRole.name)
+
+# Association table for the many-to-many relationship between User and CustomRole.
+user_custom_role_link = Table(
+    'stp_user_custom_role_link',
+    Base.metadata,
+    Column('user_id', ForeignKey('stp_user.user_id', ondelete='CASCADE'), primary_key=True),
+    Column('role_id', ForeignKey('stp_custom_role.role_id'), primary_key=True),
+)
+
+
+class User(ModifiedAndCreatedColumnMixin, Base):
+    r"""The user table.
+
+    Parameters
+    ----------
+    user_id : streamlit_passwordless.UserID
+        The unique ID of the user. The primary key of the table.
+        If not specified :func:`uuid.uuid4` is used to generate the ID.
+
+    username : str
+        The username of the user. It must be unique across all users.
+
+    ad_username : str or None
+        The active directory username of the user.
+
+    displayname : str or None
+        A descriptive name of the user that is easy to understand for a human.
+
+    role_id : int
+        The unique id of the role associated with the user.
+
+    verified : bool, default False
+        True if a user is verified and False otherwise. A user is verified when
+        at least one verified email address is associated with the user.
+
+    verified_at : datetime or None
+        The timestamp in UTC when the user was verified.
+
+    disabled : bool, default False
+        If False the user is enabled and if True the user is disabled.
+        A disabled user is not able to register credentials or sign in.
+
+    disabled_at : datetime or None
+        The timestamp in UTC when the user was disabled.
+
+    updated_at : datetime or None
+        The timestamp at which the user was last updated (UTC).
+
+    updated_by : str or None
+        The ID of the user that last updated the user.
+
+    created_at : datetime
+        The timestamp at which the user was created (UTC).
+        Defaults to current timestamp.
+
+    created_by : str or None
+        The ID of the user that created the user.
+
+    role : Role
+        The role of the user.
+
+    custom_roles : dict[int, CustomRole]
+        The custom roles of the user. :attr:`CustomRole.role_id` is mapped to :class:`CustomRole`.
+        A user may have none or many custom roles.
+
+    emails : list[Email]
+        The email addresses associated with the user.
+
+    sign_ins : list[UserSignIn]
+        Info about when the user has signed in to the application.
+    """
+
+    columns__repr__: ClassVar[tuple[str, ...]] = (
+        'user_id',
+        'username',
+        'ad_username',
+        'displayname',
+        'role_id',
+        'verified',
+        'verified_at',
+        'disabled',
+        'disabled_at',
+        'updated_at',
+        'updated_by',
+        'created_at',
+        'created_by',
+    )
+
+    __tablename__ = 'stp_user'
+
+    user_id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    ad_username: Mapped[str | None]
+    displayname: Mapped[str | None]
+    role_id: Mapped[int] = mapped_column(ForeignKey(Role.role_id))
+    verified: Mapped[bool] = mapped_column(default=False)
+    verified_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    disabled: Mapped[bool] = mapped_column(default=False)
+    disabled_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    role: Mapped[Role] = relationship(back_populates='users')
+    custom_roles: Mapped[dict[int, CustomRole]] = relationship(
+        secondary='stp_user_custom_role_link',
+        collection_class=attribute_keyed_dict('role_id'),
+        back_populates='users',
+        passive_deletes=True,
+    )
+    emails: Mapped[list['Email']] = relationship(
+        back_populates='user', cascade='delete, delete-orphan', order_by='Email.rank'
+    )
+    sign_ins: Mapped[list['UserSignIn']] = relationship(
+        back_populates='user', cascade='delete, delete-orphan'
+    )
+
+
+Index(f'{User.__tablename__}_username_ix', User.username)
+Index(f'{User.__tablename__}_ad_username_ix', User.ad_username)
+Index(f'{User.__tablename__}_disabled_ix', User.disabled)
+
+
+class Email(ModifiedAndCreatedColumnMixin, Base):
+    r"""Email addresses of a user.
+
+    Parameters
+    ----------
+    email_id : int
+        The primary key of the table.
+
+    user_id : streamlit_passwordless.UserID
+        The unique ID of the user the email address belongs to.
+
+    email : str
+        An email address of a user. Must be unique.
+
+    rank : int
+        The rank of the email, where 1 defines the primary email, 2 the secondary
+        and 3 the tertiary etc ... A user can only have one email of each rank.
+
+    verified : bool, default False
+        True if the email address is verified and False otherwise.
+
+    verified_at : datetime or None
+        The timestamp in UTC when the email address was verified by the user.
+
+    disabled : bool, default False
+        If the email address is disabled or not.
+
+    disabled_at : datetime or None
+        The timestamp in UTC when the email address was disabled.
+
+    updated_at : datetime or None
+        The timestamp at which the email was last updated (UTC).
+
+    updated_by : str or None
+        The ID of the user that last updated the email.
+
+    created_at : datetime
+        The timestamp at which the email was created (UTC).
+        Defaults to current timestamp.
+
+    created_by : str or None
+        The ID of the user that created the email.
+
+    user : User
+        The user object the email address belongs to.
+    """
+
+    columns__repr__: ClassVar[tuple[str, ...]] = (
+        'email_id',
+        'user_id',
+        'email',
+        'rank',
+        'verified',
+        'verified_at',
+        'disabled',
+        'disabled_at',
+        'updated_at',
+        'updated_by',
+        'created_at',
+        'created_by',
+    )
+
+    __tablename__ = 'stp_email'
+    __table_args__ = (UniqueConstraint('user_id', 'rank', name=f'{__tablename__}_user_id_rank_iu'),)
+
+    email_id: Mapped[int] = mapped_column(Integer(), Identity(), primary_key=True)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey(User.user_id, ondelete='CASCADE'))
+    email: Mapped[str] = mapped_column(unique=True)
+    rank: Mapped[int]
+    verified: Mapped[bool] = mapped_column(default=False)
+    verified_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    disabled: Mapped[bool] = mapped_column(default=False)
+    disabled_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    user: Mapped['User'] = relationship(back_populates='emails')
+
+
+Index(f'{Email.__tablename__}_email_ix', Email.email)
+Index(f'{Email.__tablename__}_user_id_ix', Email.user_id)
+
+
+class UserSignIn(Base):
+    r"""A logging table of when a user has signed in to the application.
+
+    Parameters
+    ----------
+    user_sign_in_id : int
+        The primary key of the table.
+
+    user_id : streamlit_passwordless.UserID
+        The unique ID of the user that signed in to the application.
+
+    sign_in_timestamp : datetime
+        The timestamp in timezone UTC when the user signed in.
+
+    success : bool
+        True if the user was successfully signed in and False otherwise.
+
+    origin : str
+        The domain name or IP-address of the application the user signed in to.
+
+    device : str
+        The device the user signed in from. E.g. a web browser.
+
+    country : str
+        The country code of the country that the user signed in from. E.g. SE for Sweden.
+
+    credential_nickname : str
+        The nickname of the passkey that was used to sign in.
+
+    credential_id : str
+        The ID of the passkey credential used to sign in.
+
+    sign_in_type : str
+        The type of sign in method. E.g. 'passkey_signin'.
+
+    rp_id : str or None
+        The ID of the relaying party, which is the server that
+        verifies the credentials during the sign in process.
+
+    created_at : datetime
+        The timestamp at which the user sign in record was created (UTC).
+        Defaults to current timestamp.
+
+    user : User
+        The user object the sign in entry belongs to.
+    """
+
+    columns__repr__: ClassVar[tuple[str, ...]] = (
+        'user_sign_in_id',
+        'user_id',
+        'sign_in_timestamp',
+        'success',
+        'origin',
+        'device',
+        'country',
+        'credential_nickname',
+        'credential_id',
+        'sign_in_type',
+        'rp_id',
+        'created_at',
+    )
+
+    __tablename__ = 'stp_user_sign_in'
+
+    user_sign_in_id: Mapped[int] = mapped_column(
+        BigInteger().with_variant(SQLITE_INTEGER(), 'sqlite'),
+        Identity(),
+        primary_key=True,
+        autoincrement=True,
+    )
+    user_id: Mapped[UUID] = mapped_column(ForeignKey(User.user_id, ondelete='CASCADE'))
+    sign_in_timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    success: Mapped[bool]
+    origin: Mapped[str]
+    device: Mapped[str]
+    country: Mapped[str]
+    credential_nickname: Mapped[str]
+    credential_id: Mapped[str]
+    sign_in_type: Mapped[str]
+    rp_id: Mapped[str | None]
+    created_at: Mapped[datetime] = created_at_column
+    user: Mapped['User'] = relationship(back_populates='sign_ins')
+
+
+Index(f'{UserSignIn.__tablename__}_ix1', UserSignIn.user_id, UserSignIn.sign_in_timestamp)
+Index(f'{UserSignIn.__tablename__}_ix2', UserSignIn.origin)
