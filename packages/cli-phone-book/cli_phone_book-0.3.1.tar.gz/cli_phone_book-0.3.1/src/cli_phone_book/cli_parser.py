76dd22864f6e64c0f1c1cc3794b85c406c5bff8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 15:06:43 +0000

    Add package cli-phone-book/0.3.1/cli_phone_book-0.3.1.tar.gz

---
+++ packages/cli-phone-book/cli_phone_book-0.3.1.tar.gz/cli_phone_book-0.3.1/src/cli_phone_book/cli_parser.py
@@ -0,0 +1,39 @@
+import argparse
+from functools import wraps
+from . import __author__, __version__
+
+
+def with_argparse(main_function):
+    wraps(main_function)
+
+    def wrapper():
+        parser = argparse.ArgumentParser(
+          description="Телефонный справочник",
+          epilog="консольная версия"
+        )
+
+        parser.add_argument(
+          '-v',
+          '--version',
+          action='version',
+          version=f'%(prog)s {__version__}',
+          help='Версия пакета'
+        )
+
+        parser.add_argument(
+          '--author',
+          action='store_true',
+          help='Автор проекта'
+        )
+        args = parser.parse_args()
+
+        if args.author:
+            print(f'{__author__}')
+            return
+
+        if args.version:
+            print(f'{__version__}')
+            return
+
+        main_function()
+    return wrapper
