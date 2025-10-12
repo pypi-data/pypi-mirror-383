d293f44bad0538e67d781dc90ff29cdec3bccbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 05:05:03 +0000

    Add package ml4co-kit/0.4.1/ml4co_kit-0.4.1-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/ml4co-kit/ml4co_kit-0.4.1-cp310-cp310-manylinux2014_x86_64.whl/ml4co_kit/task/__init__.py
@@ -0,0 +1,22 @@
+r"""
+Task Module.
+"""
+
+# Base Task
+from .base import TaskBase, TASK_TYPE
+
+# Graph Task
+from .graph.base import GraphTaskBase
+from .graph.mcl import MClTask
+from .graph.mcut import MCutTask
+from .graph.mis import MISTask
+from .graph.mvc import MVCTask
+
+# Routing Task
+from .routing.base import RoutingTaskBase, DISTANCE_TYPE, ROUND_TYPE
+from .routing.atsp import ATSPTask
+from .routing.cvrp import CVRPTask
+from .routing.op import OPTask
+from .routing.pctsp import PCTSPTask
+from .routing.spctsp import SPCTSPTask
+from .routing.tsp import TSPTask
