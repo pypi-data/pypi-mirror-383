605d244984537944ac9c2164ee3e5175343a5621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:53 +0000

    Add package itk-cudacommon-cuda124/2.0.0/itk_cudacommon_cuda124-2.0.0-cp39-cp39-win_amd64.whl

---
+++ packages/itk-cudacommon-cuda124/itk_cudacommon_cuda124-2.0.0-cp39-cp39-win_amd64.whl/itk/__init_cudacommon__.py
@@ -0,0 +1,41 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_10_0():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'itk_cudacommon_cuda124.libs'))
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-itk_cudacommon_cuda124-2.0.0')
+        if os.path.isfile(load_order_filepath):
+            import ctypes.wintypes
+            with open(os.path.join(libs_dir, '.load-order-itk_cudacommon_cuda124-2.0.0')) as file:
+                load_order = file.read().split()
+            kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+            kernel32.LoadLibraryExW.restype = ctypes.wintypes.HMODULE
+            kernel32.LoadLibraryExW.argtypes = ctypes.wintypes.LPCWSTR, ctypes.wintypes.HANDLE, ctypes.wintypes.DWORD
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(lib_path, None, 8):
+                    raise OSError('Error loading {}; {}'.format(lib, ctypes.FormatError(ctypes.get_last_error())))
+
+
+_delvewheel_patch_1_10_0()
+del _delvewheel_patch_1_10_0
+# end delvewheel patch
+
+import sys
+import importlib
+
+itk_module = sys.modules["itk"]
+cuda_submodules = ["itk.itkCudaImageFromImage", "itk.itkCudaImageFromCudaArray"]
+
+for mod_name in cuda_submodules:
+    mod = importlib.import_module(mod_name)
+    for a in dir(mod):
+        if a[0] != "_":
+            setattr(itk_module, a, getattr(mod, a))
