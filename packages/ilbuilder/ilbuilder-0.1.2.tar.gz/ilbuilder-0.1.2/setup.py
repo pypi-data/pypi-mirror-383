28821f0b64383a2a01b8988eadfcfb1a013de9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:47 +0000

    Add package ilbuilder/0.1.2/ilbuilder-0.1.2.tar.gz

---
+++ packages/ilbuilder/ilbuilder-0.1.2.tar.gz/ilbuilder-0.1.2/setup.py
@@ -0,0 +1,31 @@
+﻿from setuptools import setup, find_packages
+from ilbuilder.version import __version__
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ilbuilder',
+    version=__version__,
+    packages=find_packages(),
+    requires=[
+        'colorama',
+        'rushlib',
+        'rushdata',
+        'rushrunning'
+        'pyinstaller'
+    ],
+    description='python builder',
+    author='ndrzy',
+    author_email='dandan0019@outlook.com',
+    python_requires='>=3.10',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/meatdumplings0019/rushlib",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
