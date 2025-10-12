33afd011e8d3b9774eff1c9db7c11eb66d10516e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 03:18:52 +0000

    Add package eldar-calculator-eliyev/0.1.11/eldar_calculator_eliyev-0.1.11.tar.gz

---
+++ packages/eldar-calculator-eliyev/eldar_calculator_eliyev-0.1.11.tar.gz/eldar_calculator_eliyev-0.1.11/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="eldar-calculator-eliyev",
+    version="0.1.11",  # Sonrakı versiya, əvvəlkindən böyük
+    author="Eldar Eliyev",
+    author_email="liyev8024@gmail.com",
+    description="A powerful calculator library for Python — supports basic, advanced, geometry, algebra, and unit conversions.",
+    py_modules=["calculator"],  # Burada əsas Python faylının adı
+    install_requires=[],
+    python_requires=">=3.7",
+)
