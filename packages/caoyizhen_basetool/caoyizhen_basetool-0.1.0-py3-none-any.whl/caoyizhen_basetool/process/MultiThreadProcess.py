d470132aee54ee659fd5a5579813a38d70dd74c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 04:05:41 +0000

    Add package caoyizhen_basetool/0.1.0/caoyizhen_basetool-0.1.0-py3-none-any.whl

---
+++ packages/caoyizhen_basetool/caoyizhen_basetool-0.1.0-py3-none-any.whl/caoyizhen_basetool/process/MultiThreadProcess.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf-8 -*-
+# @Time    :   2025/10/11 22:16:26
+# @File    :   MultiThreadProcess.py
+# @Author  :   ciaoyizhen
+# @Contact :   yizhen.ciao@gmail.com
+# @Function:   多线程的消费者生产者进程处理
