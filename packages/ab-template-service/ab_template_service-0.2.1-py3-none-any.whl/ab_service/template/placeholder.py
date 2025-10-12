ba12cc4e31f2c854d2bf71494062f5c5e5036073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:53 +0000

    Add package ab-template-service/0.2.1/ab_template_service-0.2.1-py3-none-any.whl

---
+++ packages/ab-template-service/ab_template_service-0.2.1-py3-none-any.whl/ab_service/template/placeholder.py
@@ -0,0 +1,8 @@
+"""Sample entrypoint for template package."""
+
+from typing import Literal
+
+
+def placeholder_func() -> Literal[True]:  # always returns True
+    """Sample function which returns True."""
+    return True
