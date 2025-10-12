c64180056fd998688ef299017a4fc3bda40dbf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-12 14:05:39 +0000

    Add package edsger/0.1.6/edsger-0.1.6.tar.gz

---
+++ packages/edsger/edsger-0.1.6.tar.gz/edsger-0.1.6/tests/test_spiess_florian.py
@@ -0,0 +1,14 @@
+"""Tests for spiess_florian.pyx.
+
+py.test tests/test_spiess_florian.py
+"""
+
+from edsger.spiess_florian import *  # type: ignore
+
+
+def test_compute_SF_in_01():
+    compute_SF_in_01()  # type: ignore
+
+
+def test_compute_SF_in_02():
+    compute_SF_in_02()  # type: ignore
