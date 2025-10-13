b819e1a88c30fbd8802996606102511d53efd960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 04:05:28 +0000

    Add package persist-queue/1.1.0b0/persist_queue-1.1.0b0.tar.gz

---
+++ packages/persist-queue/persist_queue-1.1.0b0.tar.gz/persist_queue-1.1.0b0/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+
+def get_extras():
+    return {
+        "extra": open("extra-requirements.txt").read().splitlines(),
+        "async": [
+            "aiofiles>=0.8.0",
+            "aiosqlite>=0.17.0"
+        ]
+    }
+
+
+setup(
+    name='persist-queue',
+    version=__import__('persistqueue').__version__,
+    description=(
+        'A thread-safe disk based persistent queue in Python.'
+    ),
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    author=__import__('persistqueue').__author__,
+    author_email='wangxu198709@gmail.com',
+    maintainer=__import__('persistqueue').__author__,
+    maintainer_email='wangxu198709@gmail.com',
+    license=__import__('persistqueue').__license__,
+    packages=find_packages(),
+    extras_require=get_extras(),
+    platforms=["all"],
+    url='http://github.com/peter-wangxu/persist-queue',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries'
+    ],
+    package_data={'persistqueue': ['py.typed']}
+)
