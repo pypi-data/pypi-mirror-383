f13db2ce2c720f5d80b83106e32aaf06e4c709bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:24 +0000

    Add package shenko/0.1.87/shenko-0.1.87-py2.py3-none-any.whl

---
+++ packages/shenko/shenko-0.1.87-py2.py3-none-any.whl/shenko-0.1.87.dist-info/AUTHORS.rst
@@ -0,0 +1,14 @@
+=======
+Credits
+=======
+
+Development Team
+----------------
+
+* Danny Dowshenko <dowshenko225@gmail.com>
+* Vic Dowshenko <vic.dowshenko@shenko.org>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ packages/shenko/shenko-0.1.87-py2.py3-none-any.whl/shenko/shenko.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Shenko
+===================
+
+Shenko is a open source platform that uses the Panda3d game engine.
+You can visit us on our website www.shenko.org
+or find this code on github/shenko:
+    https://github.com/shenko/shenko/blob/master/shenko/shenko.py
+
+Authors:
+    SHENKO Development Team
+
+License:
+    # Copyright 2018 SHENKO
+    #
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+    #
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+    #
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
+__author__      = "Shenko Development Team"
+__email__       = "shenko.org@gmail.com"
+__copyright__   = "http://creativecommons.org/licenses/by/3.0/legalcode"
+
+# Python imports
+import os
+import sys
+
+# Panda imports
+from panda3d.core import WindowProperties
+from direct.showbase.ShowBase import ShowBase
+
+# Local imports - conditional to support both script and module execution
+try:
+    # Try relative import (package/module context)
+    from . import s00_init
+    from .utils.assets import get_audio_path
+except ImportError:
+    # Fall back to absolute import (direct script execution)
+    import sys
+    import os
+    sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+    import s00_init
+    from utils.assets import get_audio_path
+
+class shenkoApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # The background color
+        base.setBackgroundColor(0,0,0)
+        # Add a title to the window
+        win_props = WindowProperties()
+        win_props.setTitle('Shenko.org')
+        self.win.requestProperties(win_props)
+
+        self.rollSound = base.loader.loadSfx(get_audio_path("click.ogg"))
+
+
+        state = s00_init.AppState("Application")
+        state.request("Menu")
+
+        # Input
+        self.accept('escape', s00_init.quit)
+        self.accept("f11", s00_init.toggle_fullscreen)
+        #self.accept('m', self.menuToggle)
+        #self.accept('arrow_down-repeat', self.moveCam)
+
+# For making modules"
+if __name__ == '__main__':
+    print('main.py is being run directly')
+    app = shenkoApp()
+    app.run()
+else:
+    print("Shenko main is being imported, with no setup")
+    app = shenkoApp()
+    app.run()
