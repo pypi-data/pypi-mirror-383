26804208c589081166099238dbca42f0f942895b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 04:05:44 +0000

    Add package current/0.5/current-0.5.tar.gz

---
+++ packages/current/current-0.5.tar.gz/current-0.5/run-tests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""Run the tests with https://pytest.org."""
+
+import pathlib
+import platform
+import sys
+
+import pytest
+
+SELF = pathlib.Path(__file__)
+
+ARGS = [#'--verbose',
+        #'--pdb',
+        #'--exitfirst',  # a.k.a. -x
+        #'-W', 'error',
+       ]
+
+if platform.system() == 'Windows' and 'idlelib' in sys.modules:
+    ARGS += ['--capture=sys', '--color=no']
+
+
+print('run', [SELF.name] + sys.argv[1:])
+args = ARGS + sys.argv[1:]
+
+print(f'pytest.main({args!r})')
+sys.exit(pytest.main(args))
