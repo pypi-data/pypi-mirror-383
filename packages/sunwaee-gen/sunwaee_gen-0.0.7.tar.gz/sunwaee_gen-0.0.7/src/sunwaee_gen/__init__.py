e8d7b1e0802fa428588dc9216941f06764f535bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:05 +0000

    Add package sunwaee-gen/0.0.7/sunwaee_gen-0.0.7.tar.gz

---
+++ packages/sunwaee-gen/sunwaee_gen-0.0.7.tar.gz/sunwaee_gen-0.0.7/src/sunwaee_gen/__init__.py
@@ -0,0 +1,34 @@
+# standard
+# third party
+# custom
+from sunwaee_gen import agents
+from sunwaee_gen import models
+from sunwaee_gen import providers
+
+from sunwaee_gen._completion import async_completion
+from sunwaee_gen.agents._registry import AGENTS
+from sunwaee_gen.models._registry import MODELS
+from sunwaee_gen.providers._registry import PROVIDERS
+from sunwaee_gen.tools._registry import TOOLS
+
+from sunwaee_gen.agent import Agent
+from sunwaee_gen.model import Model
+from sunwaee_gen.provider import Provider
+from sunwaee_gen.tool import Tool, T
+
+
+__all__ = [
+    "AGENTS",
+    "MODELS",
+    "PROVIDERS",
+    "TOOLS",
+    "Agent",
+    "Model",
+    "Provider",
+    "Tool",
+    "T",
+    "agents",
+    "models",
+    "providers",
+    "async_completion",
+]
---
+++ packages/sunwaee-gen/sunwaee_gen-0.0.7.tar.gz/sunwaee_gen-0.0.7/tests/test_api.py
@@ -0,0 +1,6 @@
+# standard
+# third party
+# custom
+
+class TestApi:
+    pass
