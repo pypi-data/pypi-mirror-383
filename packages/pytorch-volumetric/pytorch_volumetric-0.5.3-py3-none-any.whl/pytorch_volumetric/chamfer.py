6b8a2ee2f1df35432e7983599be48bdc8bc24aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:35 +0000

    Add package pytorch-volumetric/0.5.3/pytorch_volumetric-0.5.3-py3-none-any.whl

---
+++ packages/pytorch-volumetric/pytorch_volumetric-0.5.3-py3-none-any.whl/pytorch_volumetric/chamfer.py
@@ -0,0 +1,195 @@
+import time
+from typing import NamedTuple
+
+import torch
+import pytorch_kinematics as pk
+from pytorch_kinematics import transforms as tf
+from pytorch_kinematics.transforms.rotation_conversions import matrix_to_pos_rot
+
+from pytorch_volumetric.sdf import ObjectFactory, sample_mesh_points, ObjectFrameSDF
+
+
+def pairwise_distance(world_to_link_tfs: pk.Transform3d):
+    m = world_to_link_tfs.get_matrix()
+    t = m[:, :3, 3]
+    r = pk.rotation_conversions.matrix_to_rotation_6d(m[:, :3, :3])
+    cont_rep = torch.cat((t, r), dim=1)
+    return torch.cdist(cont_rep, cont_rep)
+
+
+def pairwise_distance_chamfer(A_link_to_world_tfs: pk.Transform3d, B_world_to_link_tfs: pk.Transform3d = None,
+                              obj_factory: ObjectFactory = None,
+                              obj_sdf: ObjectFrameSDF = None,
+                              model_points_eval: torch.tensor = None, vis=None, scale=1000):
+    """
+    Compute the pairwise chamfer distance between a set of world to link transforms of an object.
+    :param A_link_to_world_tfs: B x 4 x 4 link frame to world frame transforms
+    :param B_world_to_link_tfs: P x 4 x 4 world frame to link frame transforms, if None, then the inverse of A will be used
+    :param obj_factory: object factory to evaluate against
+    :param obj_sdf: object sdf to evaluate against (accelerates the computation at the cost of accuracy)
+    :param model_points_eval: points to evaluate the chamfer distance on; if None, sample from the object
+    :param vis: visualizer
+    :param scale: units with respect to the position units; e.g. if the position units are in meters, then the scale=1
+    :return: B x B chamfer distance matrix in squared distance units, averaged across the model_points_eval
+    """
+    if model_points_eval is None:
+        model_points_eval, _, _ = sample_mesh_points(obj_factory, num_points=500, name=obj_factory.name,
+                                                     device=A_link_to_world_tfs.device)
+
+    # effectively can apply one transform then take the inverse using the other one; if they are the same, then
+    # we should end up in the base frame if that T == Tp
+    # want pairwise matrix multiplication |T| x |Tp| x 4 x 4 T[0]@Tp[0], T[0]@Tp[1]
+    T = A_link_to_world_tfs.get_matrix()
+    if B_world_to_link_tfs is None:
+        # this is fastor than inverting the 4x4 since we can exploit orthogonality
+        T_inv = A_link_to_world_tfs.inverse().get_matrix()
+    else:
+        T_inv = B_world_to_link_tfs.get_matrix()
+    Iapprox = torch.einsum("bij,pjk->bpik", T_inv, T)
+
+    B = len(T)
+    P = len(T_inv)
+    # the einsum does the multiplication below and is about twice as fast
+    # Iapprox = T.inverse().view(-1, 1, 4, 4) @ T.view(1, -1, 4, 4)
+
+    errors_per_batch = batch_chamfer_dist(Iapprox.reshape(B * P, 4, 4), model_points_eval,
+                                          obj_factory=obj_factory, obj_sdf=obj_sdf,
+                                          viewing_delay=0, vis=vis, scale=scale)
+    errors_per_batch = errors_per_batch.view(B, P)
+    return errors_per_batch
+
+
+def batch_chamfer_dist(world_to_object: torch.tensor, model_points_world_frame_eval: torch.tensor,
+                       obj_factory: ObjectFactory = None, obj_sdf: ObjectFrameSDF = None, viewing_delay=0, scale=1000.,
+                       print_err=False, vis=None):
+    """
+    Compute batched unidirectional chamfer distance between the observed world frame surface points and the
+    surface points of the object transformed by a set of 4x4 rigid transform matrices (from the object frame).
+    :param world_to_object: B x 4 x 4 transformation matrices from world to object frame
+    :param model_points_world_frame_eval: N x 3 points to evaluate the chamfer distance on
+    :param obj_factory: object to evaluate against
+    :param obj_sdf: sdf of the object to evaluate against (potentially much faster than obj_sdf, but less accurate)
+    :param viewing_delay: if a visualizer is given, sleep between plotting successive elements
+    :param scale: units with respect to the position units; e.g. if the position units are in meters, then the scale
+    being 1000 will convert the distance to mm
+    :param print_err: whether to visualize and print the chamfer error
+    :param vis: optional visualizer
+    :return: B chamfer error for each transform, averaged across the N points to evaluate against
+    """
+    B = world_to_object.shape[0]
+    eval_num_points = model_points_world_frame_eval.shape[0]
+    world_to_link = tf.Transform3d(matrix=world_to_object)
+    model_points_object_frame_eval = world_to_link.transform_points(model_points_world_frame_eval)
+
+    if obj_sdf is not None:
+        d, _ = obj_sdf(model_points_object_frame_eval)
+    elif obj_factory is not None:
+        res = obj_factory.object_frame_closest_point(model_points_object_frame_eval)
+        d = res.distance
+    else:
+        raise ValueError("Either obj_sdf or obj_factory must be given")
+    # convert to scale such as mm**2
+    chamfer_distance = (scale * d) ** 2
+    # average across the evaluation points
+    errors_per_batch = chamfer_distance.mean(dim=-1)
+
+    if vis is not None and obj_factory is not None:
+        link_to_world = world_to_link.inverse()
+        res = obj_factory.object_frame_closest_point(model_points_object_frame_eval)
+        closest_pt_world_frame = link_to_world.transform_points(res.closest)
+        m = link_to_world.get_matrix()
+        for b in range(B):
+            pos, rot = matrix_to_pos_rot(m[b])
+            obj_factory.draw_mesh(vis, "chamfer evaluation", (pos, rot), rgba=(0, 0.1, 0.8, 0.1),
+                                  object_id=vis.USE_DEFAULT_ID_FOR_NAME)
+            # vis.draw_point("avgerr", [0, 0, 0], (1, 0, 0), label=f"avgerr: {round(errors_per_batch[b].item())}")
+
+            if print_err:
+                for i in range(eval_num_points):
+                    query = model_points_world_frame_eval[i].cpu()
+                    closest = closest_pt_world_frame[b, i].cpu()
+                    vis.draw_point(f"query.{i}", query, (0, 1, 0))
+                    vis.draw_point(f"closest.{i}", closest, (0, 1, 1))
+                    vis.draw_2d_line(f"qc.{i}", query, closest - query, (0, 1, 0), scale=1)
+
+            time.sleep(viewing_delay)
+
+        # move somewhere far away
+        obj_factory.draw_mesh(vis, "chamfer evaluation", ([0, 0, 100], [0, 0, 0, 1]), rgba=(0, 0.2, 0.8, 0.2),
+                              object_id=vis.USE_DEFAULT_ID_FOR_NAME)
+    return errors_per_batch
+
+
+class PlausibleDiversityReturn(NamedTuple):
+    plausibility: torch.tensor
+    coverage: torch.tensor
+    most_plausible_per_estimated: torch.tensor
+    most_covered_per_plausible: torch.tensor
+
+
+class PlausibleDiversity:
+    """
+    Compute the plausibility and coverage of an estimated set of transforms against a set of plausible transforms.
+    Each transform is a 4x4 homogeneous matrix. The return are in units of squared coordinates (for example if points
+    are given with their coordinates in mm, then the return is in mm^2). In some sense it is a set divergence.
+    """
+
+    def __init__(self, obj_factory: ObjectFactory, model_points_eval: torch.tensor = None, num_model_points_eval=500,
+                 obj_sdf: ObjectFrameSDF = None):
+        self.obj_factory = obj_factory
+        self.obj_sdf = obj_sdf
+        # if model points are not given, sample them from the object
+        if model_points_eval is None:
+            model_points_eval, _, _ = sample_mesh_points(obj_factory, num_points=num_model_points_eval,
+                                                         name=obj_factory.name)
+        self.model_points_eval = model_points_eval
+
+    def __call__(self, T_est_inv, T_p, bidirectional=False, scale=1000.):
+        """
+        Compute the plausibility and coverage of an estimated set of transforms against a set of plausible transforms.
+
+        :param T_est_inv: The inverse of the estimated transforms, in the form of Bx4x4 homogeneous matrices.
+        :param T_p: The plausible transforms, in the form of Px4x4 homogeneous matrices.
+        :return: plausibility score, coverage score, most_plausible_per_estimated, most_covered_per_plausible
+        """
+        errors_per_batch = self.compute_tf_pairwise_error_per_batch(T_est_inv, T_p, scale=scale)
+        ret = self.do_evaluate_plausible_diversity_on_pairwise_chamfer_dist(errors_per_batch)
+        if bidirectional:
+            errors_per_batch_rev = self.compute_tf_pairwise_error_per_batch(T_p, T_est_inv, scale=scale)
+            ret2 = self.do_evaluate_plausible_diversity_on_pairwise_chamfer_dist(errors_per_batch_rev)
+            # the plausibility and coverage are flipped when we reverse the transforms
+            ret = PlausibleDiversityReturn(
+                plausibility=(ret.plausibility + ret2.coverage) / 2,
+                coverage=(ret.coverage + ret2.plausibility) / 2,
+                most_plausible_per_estimated=ret.most_plausible_per_estimated,
+                most_covered_per_plausible=ret.most_covered_per_plausible,
+            )
+        return ret
+
+    def compute_tf_pairwise_error_per_batch(self, T_est_inv, T_p, scale=1000.):
+        # effectively can apply one transform then take the inverse using the other one; if they are the same, then
+        # we should end up in the base frame if that T == Tp
+        # want pairwise matrix multiplication |T| x |Tp| x 4 x 4 T[0]@Tp[0], T[0]@Tp[1]
+        Iapprox = torch.einsum("bij,pjk->bpik", T_est_inv, T_p)
+        # the einsum does the multiplication below and is about twice as fast
+        # Iapprox = T_est_inv.view(-1, 1, 4, 4) @ T_p.view(1, -1, 4, 4)
+
+        B, P = Iapprox.shape[:2]
+        self.model_points_eval = self.model_points_eval.to(device=Iapprox.device, dtype=Iapprox.dtype)
+        errors_per_batch = batch_chamfer_dist(Iapprox.reshape(B * P, 4, 4), self.model_points_eval,
+                                              self.obj_factory, obj_sdf=self.obj_sdf, viewing_delay=0, vis=None,
+                                              scale=scale)
+        errors_per_batch = errors_per_batch.view(B, P)
+        return errors_per_batch
+
+    @staticmethod
+    def do_evaluate_plausible_diversity_on_pairwise_chamfer_dist(errors_per_batch):
+        B, P = errors_per_batch.shape
+
+        best_per_sampled = errors_per_batch.min(dim=1)
+        best_per_plausible = errors_per_batch.min(dim=0)
+
+        bp_plausibility = best_per_sampled.values.sum() / B
+        bp_coverage = best_per_plausible.values.sum() / P
+
+        return PlausibleDiversityReturn(bp_plausibility, bp_coverage, best_per_sampled, best_per_plausible)
---
+++ packages/pytorch-volumetric/pytorch_volumetric-0.5.3-py3-none-any.whl/pytorch_volumetric/model_to_sdf.py
@@ -0,0 +1,171 @@
+import typing
+
+import numpy as np
+import torch
+import pytorch_kinematics as pk
+from pytorch_volumetric import sdf
+import logging
+
+logger = logging.getLogger(__file__)
+
+
+class RobotSDF(sdf.ObjectFrameSDF):
+    """Create an SDF for a robot model described by a pytorch_kinematics Chain.
+    The SDF is conditioned on a joint configuration which must be set."""
+
+    def __init__(self, chain: pk.Chain, default_joint_config=None, path_prefix='',
+                 link_sdf_cls: typing.Callable[[sdf.ObjectFactory], sdf.ObjectFrameSDF] = sdf.MeshSDF):
+        """
+
+        :param chain: Robot description; each link should be a mesh type - non-mesh geometries are ignored
+        :param default_joint_config: values for each joint of the robot by default; None results in all zeros
+        :param path_prefix: path to search for referenced meshes inside the robot description (e.g. URDF) which may use
+        relative paths. This given path is prefixed onto those relative paths in order to find the meshes.
+        :param link_sdf_cls: Factory of each link's SDFs; **kwargs are forwarded to this factory
+        :param kwargs: Keyword arguments fed to link_sdf_cls
+        """
+        self.chain = chain
+        self.dtype = self.chain.dtype
+        self.device = self.chain.device
+        self.q = None
+        self.object_to_link_frames: typing.Optional[pk.Transform3d] = None
+        self.joint_names = self.chain.get_joint_parameter_names()
+        self.frame_names = self.chain.get_frame_names(exclude_fixed=False)
+        self.sdf: typing.Optional[sdf.ComposedSDF] = None
+        self.sdf_to_link_name = []
+        self.configuration_batch = None
+
+        sdfs = []
+        offsets = []
+        # get the link meshes from the frames and create meshes
+        for frame_name in self.frame_names:
+            frame = self.chain.find_frame(frame_name)
+            # TODO create SDF for non-mesh primitives
+            # TODO consider the visual offset transform
+            for link_vis in frame.link.visuals:
+                if link_vis.geom_type == "mesh":
+                    logger.info(f"{frame.link.name} offset {link_vis.offset}")
+                    link_obj = sdf.MeshObjectFactory(link_vis.geom_param[0],
+                                                     scale=link_vis.geom_param[1],
+                                                     path_prefix=path_prefix)
+                    link_sdf = link_sdf_cls(link_obj)
+                    self.sdf_to_link_name.append(frame.link.name)
+                    sdfs.append(link_sdf)
+                    offsets.append(link_vis.offset)
+                else:
+                    logger.warning(f"Cannot handle non-mesh link visual type {link_vis} for {frame.link.name}")
+
+        self.offset_transforms = offsets[0].stack(*offsets[1:]).to(device=self.device, dtype=self.dtype)
+        self.sdf = sdf.ComposedSDF(sdfs, self.object_to_link_frames)
+        self.set_joint_configuration(default_joint_config)
+
+    def surface_bounding_box(self, **kwargs):
+        return self.sdf.surface_bounding_box(**kwargs)
+
+    def link_bounding_boxes(self):
+        """
+        Get the bounding box of each link in the robot's frame under the current configuration.
+        Note that the bounding box is not necessarily axis-aligned, so the returned bounding box is not just
+        the min and max of the points.
+        :return: [A x] [B x] 8 x 3 points of the bounding box for each link in the robot's frame
+        """
+        tfs = self.sdf.obj_frame_to_link_frame.inverse()
+        bbs = []
+        for i in range(len(self.sdf.sdfs)):
+            sdf = self.sdf.sdfs[i]
+            bb = aabb_to_ordered_end_points(sdf.surface_bounding_box(padding=0))
+            bb = tfs.transform_points(torch.tensor(bb, device=tfs.device, dtype=tfs.dtype))[
+                self.sdf.ith_transform_slice(i)]
+            bbs.append(bb)
+        return torch.stack(bbs).squeeze()
+
+    def set_joint_configuration(self, joint_config=None):
+        """
+        Set the joint configuration of the robot
+        :param joint_config: [A x] M optionally arbitrarily batched joint configurations. There are M joints; A can be
+        any number of batched dimensions.
+        :return:
+        """
+        M = len(self.joint_names)
+        if joint_config is None:
+            joint_config = torch.zeros(M, device=self.device, dtype=self.dtype)
+        # Transform3D only works with 1 batch dimension, so we need to manually flatten any additional ones
+        # save the batch dimensions for when retrieving points
+        if len(joint_config.shape) > 1:
+            self.configuration_batch = joint_config.shape[:-1]
+            joint_config = joint_config.reshape(-1, M)
+        else:
+            self.configuration_batch = None
+        tf = self.chain.forward_kinematics(joint_config, end_only=False)
+        tsfs = []
+        for link_name in self.sdf_to_link_name:
+            tsfs.append(tf[link_name].get_matrix())
+        # make offset transforms have compatible batch dimensions
+        offset_tsf = self.offset_transforms.inverse()
+        if self.configuration_batch is not None:
+            # must be of shape (num_links, *self.configuration_batch, 4, 4) before flattening
+            expand_dims = (None,) * len(self.configuration_batch)
+            offset_tsf_mat = offset_tsf.get_matrix()[(slice(None),) + expand_dims]
+            offset_tsf_mat = offset_tsf_mat.repeat(1, *self.configuration_batch, 1, 1)
+            offset_tsf = pk.Transform3d(matrix=offset_tsf_mat.reshape(-1, 4, 4))
+
+        tsfs = torch.cat(tsfs)
+        self.object_to_link_frames = offset_tsf.compose(pk.Transform3d(matrix=tsfs).inverse())
+        if self.sdf is not None:
+            self.sdf.set_transforms(self.object_to_link_frames, batch_dim=self.configuration_batch)
+
+    def __call__(self, points_in_object_frame):
+        """
+        Query for SDF value and SDF gradients for points in the robot's frame
+        :param points_in_object_frame: [B x] N x 3 optionally arbitrarily batched points in the robot frame; B can be
+        any number of batch dimensions.
+        :return: [A x] [B x] N SDF value, and [A x] [B x] N x 3 SDF gradient. A are the configurations' arbitrary
+        number of batch dimensions.
+        """
+        return self.sdf(points_in_object_frame)
+
+
+def cache_link_sdf_factory(resolution=0.01, padding=0.1, **kwargs):
+    def create_sdf(obj_factory: sdf.ObjectFactory):
+        gt_sdf = sdf.MeshSDF(obj_factory)
+        return sdf.CachedSDF(obj_factory.name, resolution, obj_factory.bounding_box(padding=padding), gt_sdf, **kwargs)
+
+    return create_sdf
+
+
+def aabb_to_ordered_end_points(aabb, arrange_in_sequential_order=False):
+    aabbMin = aabb[:, 0]
+    aabbMax = aabb[:, 1]
+    if arrange_in_sequential_order:
+        arr = [
+            [aabbMin[0], aabbMin[1], aabbMin[2]],
+            [aabbMax[0], aabbMin[1], aabbMin[2]],
+            [aabbMax[0], aabbMax[1], aabbMin[2]],
+            [aabbMin[0], aabbMax[1], aabbMin[2]],
+            [aabbMin[0], aabbMin[1], aabbMin[2]],
+            [aabbMin[0], aabbMin[1], aabbMax[2]],
+            [aabbMax[0], aabbMin[1], aabbMax[2]],
+            [aabbMax[0], aabbMin[1], aabbMin[2]],
+            [aabbMax[0], aabbMin[1], aabbMax[2]],
+            [aabbMax[0], aabbMax[1], aabbMax[2]],
+            [aabbMax[0], aabbMax[1], aabbMin[2]],
+            [aabbMax[0], aabbMax[1], aabbMax[2]],
+            [aabbMin[0], aabbMax[1], aabbMax[2]],
+            [aabbMin[0], aabbMax[1], aabbMin[2]],
+            [aabbMin[0], aabbMax[1], aabbMax[2]],
+            [aabbMin[0], aabbMin[1], aabbMax[2]],
+        ]
+    else:
+        arr = [
+            [aabbMin[0], aabbMin[1], aabbMin[2]],
+            [aabbMax[0], aabbMin[1], aabbMin[2]],
+            [aabbMin[0], aabbMax[1], aabbMin[2]],
+            [aabbMin[0], aabbMin[1], aabbMax[2]],
+            [aabbMin[0], aabbMax[1], aabbMax[2]],
+            [aabbMax[0], aabbMin[1], aabbMax[2]],
+            [aabbMax[0], aabbMax[1], aabbMin[2]],
+            [aabbMax[0], aabbMax[1], aabbMax[2]]
+        ]
+    if torch.is_tensor(aabb):
+        return torch.tensor(arr, device=aabb.device, dtype=aabb.dtype)
+    return np.array(arr)
