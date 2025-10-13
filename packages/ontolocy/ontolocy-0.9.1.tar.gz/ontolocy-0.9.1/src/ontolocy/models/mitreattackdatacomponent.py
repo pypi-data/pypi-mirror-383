ea3c01854cc937f4ea2336c6be863be2c638b76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 04:05:28 +0000

    Add package ontolocy/0.9.1/ontolocy-0.9.1.tar.gz

---
+++ packages/ontolocy/ontolocy-0.9.1.tar.gz/ontolocy-0.9.1/src/ontolocy/models/mitreattackdatacomponent.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from typing import ClassVar, Optional
+
+from ..node import OntolocyNode
+from ..relationship import OntolocyRelationship
+from .mitreattacktechnique import MitreAttackTechnique
+
+
+class MitreAttackDataComponent(OntolocyNode):
+    __primaryproperty__: ClassVar[str] = "stix_id"
+    __primarylabel__: ClassVar[str] = "MitreAttackDataComponent"
+
+    stix_id: str
+    stix_type: str
+    stix_created: datetime
+    stix_modified: datetime
+    stix_spec_version: str = "2.1"
+    stix_revoked: Optional[bool] = False
+
+    attack_spec_version: str
+    attack_version: str
+    attack_deprecated: Optional[bool] = False
+
+    name: str
+    description: str
+
+
+#
+# OUTGOING RELATIONSHIPS
+#
+
+
+class MitreAttackDataComponentDetectsTechnique(OntolocyRelationship):
+    source: MitreAttackDataComponent
+    target: MitreAttackTechnique
+
+    __relationshiptype__: ClassVar[str] = (
+        "MITRE_ATTACK_DATA_COMPONENT_DETECTS_TECHNIQUE"
+    )
