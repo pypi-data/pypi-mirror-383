3e8e6126bf588c1012130db31dea90a607024567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 03:23:03 +0000

    Add package oniondctw/1.6.0/oniondctw-1.6.0.tar.gz

---
+++ packages/oniondctw/oniondctw-1.6.0.tar.gz/oniondctw-1.6.0/onion/__init__.py
@@ -0,0 +1,4 @@
+from .core import hello, onionify, onion_quote, onion_wall, summon_onion
+from .utils import version
+
+__all__ = ["hello", "onionify", "onion_quote", "onion_wall", "summon_onion", "version"]
