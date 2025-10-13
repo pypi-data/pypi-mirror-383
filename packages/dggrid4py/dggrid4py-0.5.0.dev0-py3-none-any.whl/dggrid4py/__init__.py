b4c4f8d6cadd46f8e2358214e9a7d0ef592e8abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:24 +0000

    Add package dggrid4py/0.5.0.dev0/dggrid4py-0.5.0.dev0-py3-none-any.whl

---
+++ packages/dggrid4py/dggrid4py-0.5.0.dev0-py3-none-any.whl/dggrid4py/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 - Alexander Kmoch
+# Licenced under GNU AFFERO GENERAL PUBLIC LICENSE. Please consult the LICENCE 
+# file for details.
+#
+# Author: Alexander Kmoch (alexander.kmoch@ut.ee)
+# Date: 07-12-2022 
+#
+
+from .dggrid_runner import DGGRIDv7, DGGRIDv8, Dggs, dgselect, dggs_types
+import dggrid4py.igeo7 as igeo7
+import dggrid4py.tool as tool
+
+__version__ = "0.5.0-dev"
