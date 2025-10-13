90a6119368cc99e947df1d2127a5812202311a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:11 +0000

    Add package apache-tvm-ffi/0.1.0b16/apache_tvm_ffi-0.1.0b16-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/apache-tvm-ffi/apache_tvm_ffi-0.1.0b16-cp310-cp310-macosx_11_0_arm64.whl/tvm_ffi/src/ffi/error.cc
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * \file src/ffi/error.cc
+ * \brief Error handling implementation
+ */
+#include <tvm/ffi/c_api.h>
+#include <tvm/ffi/error.h>
+
+namespace tvm {
+namespace ffi {
+
+class SafeCallContext {
+ public:
+  void SetRaised(TVMFFIObjectHandle error) {
+    last_error_ =
+        details::ObjectUnsafe::ObjectPtrFromUnowned<ErrorObj>(static_cast<TVMFFIObject*>(error));
+  }
+
+  void SetRaisedByCstr(const char* kind, const char* message, const TVMFFIByteArray* backtrace) {
+    Error error(kind, message, backtrace);
+    last_error_ = details::ObjectUnsafe::ObjectPtrFromObjectRef<ErrorObj>(std::move(error));
+  }
+
+  void MoveFromRaised(TVMFFIObjectHandle* result) {
+    result[0] = details::ObjectUnsafe::MoveObjectPtrToTVMFFIObjectPtr(std::move(last_error_));
+  }
+
+  static SafeCallContext* ThreadLocal() {
+    static thread_local SafeCallContext ctx;
+    return &ctx;
+  }
+
+ private:
+  ObjectPtr<ErrorObj> last_error_;
+};
+
+}  // namespace ffi
+}  // namespace tvm
+
+void TVMFFIErrorSetRaisedFromCStr(const char* kind, const char* message) {
+  // NOTE: run backtrace here to simplify the depth of tracekback
+  tvm::ffi::SafeCallContext::ThreadLocal()->SetRaisedByCstr(
+      kind, message, TVMFFIBacktrace(nullptr, 0, nullptr, 0));
+}
+
+void TVMFFIErrorSetRaised(TVMFFIObjectHandle error) {
+  tvm::ffi::SafeCallContext::ThreadLocal()->SetRaised(error);
+}
+
+void TVMFFIErrorMoveFromRaised(TVMFFIObjectHandle* result) {
+  tvm::ffi::SafeCallContext::ThreadLocal()->MoveFromRaised(result);
+}
+
+int TVMFFIErrorCreate(const TVMFFIByteArray* kind, const TVMFFIByteArray* message,
+                      const TVMFFIByteArray* backtrace, TVMFFIObjectHandle* out) {
+  // log other errors to the logger
+  TVM_FFI_LOG_EXCEPTION_CALL_BEGIN();
+  try {
+    tvm::ffi::Error error(std::string(kind->data, kind->size),
+                          std::string(message->data, message->size),
+                          std::string(backtrace->data, backtrace->size));
+    *out = tvm::ffi::details::ObjectUnsafe::MoveObjectRefToTVMFFIObjectPtr(std::move(error));
+    return 0;
+  } catch (const std::bad_alloc& e) {
+    return -1;
+  }
+  TVM_FFI_LOG_EXCEPTION_CALL_END(TVMFFIErrorCreate);
+}
