64807454a1b01bc859a588c7e8a77480c0f03079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 03:22:54 +0000

    Add package ranimate/0.1.2/ranimate-0.1.2.tar.gz

---
+++ packages/ranimate/ranimate-0.1.2.tar.gz/ranimate-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ranimate",
+    version="0.1.2",
+    author="Repaa",
+    author_email="personal@repaa.xyz",
+    description="Animation Library",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/repaa818/ranimate",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pygame",
+        "numpy",
+        "opencv-python",
+        "moviepy",
+        "decorator",
+        "proglog",
+        "imageio",
+        "imageio-ffmpeg"
+    ],
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "ranimate=ranimate.__main__:main"
+        ]
+    }
+)
