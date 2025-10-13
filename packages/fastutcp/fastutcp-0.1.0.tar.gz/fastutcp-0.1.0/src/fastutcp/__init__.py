8eff40f2ad2bfa1615628c5453ba555dcc9d2b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 04:05:36 +0000

    Add package fastutcp/0.1.0/fastutcp-0.1.0.tar.gz

---
+++ packages/fastutcp/fastutcp-0.1.0.tar.gz/fastutcp-0.1.0/src/fastutcp/__init__.py
@@ -0,0 +1,11 @@
+"""
+A2A Store - A simple Python package
+"""
+
+__version__ = "0.1.0"
+__author__ = "whillhill"
+__email__ = "ooooofish@126.com"
+
+from .hello import hello_world
+
+__all__ = ["hello_world"]
