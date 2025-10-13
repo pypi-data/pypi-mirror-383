66ca94138f3e85917e82f93daf2ad096722a834b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:21 +0000

    Add package poweretl-databricks/0.0.1a1/poweretl_databricks-0.0.1a1.tar.gz

---
+++ packages/poweretl-databricks/poweretl_databricks-0.0.1a1.tar.gz/poweretl_databricks-0.0.1a1/src/poweretl/databricks/model/DatabricksMetaProvider.py
@@ -0,0 +1,12 @@
+
+
+
+class DatabricksMetaProvider:
+
+    def __init__(self):
+        pass
+
+    def refresh_meta_model():
+        """Checks the version of meta model and updates it if required
+        """
+        pass
