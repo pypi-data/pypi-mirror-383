1c1cc223b24ba25c47a331cc83b84c52c619e07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:33 +0000

    Add package endevre-id-client/0.1.0.dev20251012201555/endevre_id_client-0.1.0.dev20251012201555-py3-none-any.whl

---
+++ packages/endevre-id-client/endevre_id_client-0.1.0.dev20251012201555-py3-none-any.whl/endevre_id_client/constants.py
@@ -0,0 +1,9 @@
+ID_API_BASE = "https://idapi.endevre.com/services"
+
+EXCHANGE_URL = f"{ID_API_BASE}/exchange"
+USERINFO_URL = f"{ID_API_BASE}/getuserinfo"
+REFRESH_URL = f"{ID_API_BASE}/refreshtoken"
+PUBLIC_KEY_URL = f"{ID_API_BASE}/publickey"
+
+SSO_EXCHANGE_REDIRECT_BASE = "https://justindev.id.endevre.com/ssoexchange"
+SSO_LOGIN_BASE = "https://justindev.id.endevre.com/ssologin"
