ff411f030e34d59e71f78ac5d1361c150bf04a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:13 +0000

    Add package mac-formatter-gui/0.1.2/mac_formatter_gui-0.1.2-py3-none-any.whl

---
+++ packages/mac-formatter-gui/mac_formatter_gui-0.1.2-py3-none-any.whl/mac_formatter/__init__.py
@@ -0,0 +1,8 @@
+"""MAC Address Formatter GUI package.
+
+Provides a simple Tkinter application to normalize and format MAC addresses
+into common styles and copy them to the clipboard.
+"""
+
+__all__ = ["__version__"]
+__version__ = "0.1.0"
