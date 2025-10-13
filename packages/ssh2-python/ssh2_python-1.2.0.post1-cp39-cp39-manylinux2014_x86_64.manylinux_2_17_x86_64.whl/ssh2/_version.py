3f00bff76a90202c3e51469f68fefc369a9c1267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 03:22:58 +0000

    Add package ssh2-python/1.2.0.post1/ssh2_python-1.2.0.post1-cp39-cp39-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/ssh2-python/ssh2_python-1.2.0.post1-cp39-cp39-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/ssh2/_version.py
@@ -0,0 +1,10 @@
+
+import json
+
+version_json = '''
+{"date": "2025-10-12T13:10:00.296940", "dirty": false, "error": null, "full-revisionid": "c92aa3eee8905a3983a80e7105b523b113068438", "version": "1.2.0.post1"}'''  # END VERSION_JSON
+
+
+def get_versions():
+    return json.loads(version_json)
+
