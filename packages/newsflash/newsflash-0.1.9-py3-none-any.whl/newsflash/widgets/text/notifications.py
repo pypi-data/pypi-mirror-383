cac6d775049e7aa37174c9339924236ccd44087c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:15 +0000

    Add package newsflash/0.1.9/newsflash-0.1.9-py3-none-any.whl

---
+++ packages/newsflash/newsflash-0.1.9-py3-none-any.whl/newsflash/widgets/text/notifications.py
@@ -0,0 +1,31 @@
+from django.http import HttpRequest
+
+from pydantic import BaseModel
+from .base import Text
+
+
+class Notification(BaseModel):
+    text: str
+    duration_in_ms: int
+
+
+class NotificationsContext(BaseModel):
+    notifications: list[Notification]
+
+
+class Notifications(Text):
+    template_name: str = "text/notification"
+    notifications: list[Notification] = []
+
+    def push(self, text: str, duration_in_ms: int = 2000) -> None:
+        self.notifications.append(
+            Notification(
+                text=text,
+                duration_in_ms=duration_in_ms,
+            )
+        )
+
+    def _build(self, request: HttpRequest) -> NotificationsContext:
+        return NotificationsContext(
+            notifications=self.notifications
+        )
