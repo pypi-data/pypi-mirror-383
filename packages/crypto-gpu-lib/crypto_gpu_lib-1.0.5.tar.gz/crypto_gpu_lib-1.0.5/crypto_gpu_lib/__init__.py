5938cc15b52d78790fa5efdf85bc024fb0e77e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:23 +0000

    Add package crypto-gpu-lib/1.0.5/crypto_gpu_lib-1.0.5.tar.gz

---
+++ packages/crypto-gpu-lib/crypto_gpu_lib-1.0.5.tar.gz/crypto_gpu_lib-1.0.5/crypto_gpu_lib/__init__.py
@@ -0,0 +1,45 @@
+"""
+CryptoGPU - High-Performance GPU-Accelerated Cryptocurrency Wallet Generation Library
+Supports Bitcoin, Ethereum, Litecoin, Solana and other cryptocurrencies
+Uses CUDA for NVIDIA GPUs for maximum performance
+"""
+
+from .core import GPUWalletGenerator, WalletBatch
+from .networks import NetworkManager, SupportedNetworks
+from .cuda_engine import CUDAEngine
+from .utils import entropy_to_mnemonic, validate_mnemonic
+
+# High-performance components
+try:
+    from .fast_core import UltraFastWalletGenerator, FastWallet, FastWalletBatch, quick_performance_test
+    from .gpu_crypto import HighPerformanceWalletGenerator, GPUCrypto
+    from .ultra_fast_crypto import UltraFastWalletEngine, ultra_fast_performance_test
+    FAST_MODE_AVAILABLE = True
+except ImportError:
+    FAST_MODE_AVAILABLE = False
+
+__version__ = "1.0.3"
+__author__ = "CryptoGPU Team"
+
+__all__ = [
+    'GPUWalletGenerator',
+    'WalletBatch', 
+    'NetworkManager',
+    'SupportedNetworks',
+    'CUDAEngine',
+    'entropy_to_mnemonic',
+    'validate_mnemonic'
+]
+
+# Add fast components if available
+if FAST_MODE_AVAILABLE:
+    __all__.extend([
+        'UltraFastWalletGenerator',
+        'FastWallet',
+        'FastWalletBatch',
+        'HighPerformanceWalletGenerator',
+        'GPUCrypto',
+        'UltraFastWalletEngine',
+        'quick_performance_test',
+        'ultra_fast_performance_test'
+    ])
