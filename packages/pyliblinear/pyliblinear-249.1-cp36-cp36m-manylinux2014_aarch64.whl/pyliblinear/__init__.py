b989545d2845b8283396635f2140a9950c4fa0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 03:22:55 +0000

    Add package pyliblinear/249.1/pyliblinear-249.1-cp36-cp36m-manylinux2014_aarch64.whl

---
+++ packages/pyliblinear/pyliblinear-249.1-cp36-cp36m-manylinux2014_aarch64.whl/pyliblinear/__init__.py
@@ -0,0 +1,47 @@
+# -*- coding: ascii -*-
+u"""
+:Copyright:
+
+ Copyright 2015 - 2025
+ Andr\xe9 Malo or his licensors, as applicable
+
+:License:
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+======================================
+ pyliblinear - a liblinear python API
+======================================
+
+pyliblinear - a liblinear python API
+"""
+__author__ = u"Andr\xe9 Malo"
+__license__ = "Apache License, Version 2.0"
+__version__ = "249.1"
+__all__ = ["FeatureMatrix", "Model", "Solver", "SOLVER_TYPES"]
+
+try:
+    from pyliblinear._liblinear import __version__ as _c_version
+except ImportError:
+    _c_version = None
+if _c_version != __version__:
+    raise ImportError(
+        "Could not import pyliblinear C extension version %r. Found: %r"
+        % (__version__, _c_version)
+    )
+del _c_version
+
+from pyliblinear._liblinear import SOLVER_TYPES
+from pyliblinear._liblinear import FeatureMatrix
+from pyliblinear._liblinear import Model
+from pyliblinear._liblinear import Solver
