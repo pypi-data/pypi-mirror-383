8482473183744831b68803577f8cc80b975e5b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:32 +0000

    Add package axiomos/0.1.10/axiomos-0.1.10.tar.gz

---
+++ packages/axiomos/axiomos-0.1.10.tar.gz/axiomos-0.1.10/axiomos/demo.py
@@ -0,0 +1,6 @@
+﻿def softmax_demo():
+    import numpy as np
+    x = np.array([[1.0, 2.0, 3.0]])
+    e = np.exp(x - np.max(x))
+    s = e / e.sum(axis=1, keepdims=True)
+    print("Softmax demo:", s)
