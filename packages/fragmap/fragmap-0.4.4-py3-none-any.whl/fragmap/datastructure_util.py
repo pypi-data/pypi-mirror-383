60de10c1126cca1b917db3b538e85f67b5258db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 04:05:18 +0000

    Add package fragmap/0.4.4/fragmap-0.4.4-py3-none-any.whl

---
+++ packages/fragmap/fragmap-0.4.4-py3-none-any.whl/fragmap/datastructure_util.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Copyright 2016-2021 Alexander Mollberg
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import List
+
+
+def lzip(*args):
+    """
+    zip(...) but returns list of lists instead of list of tuples
+    """
+    return [list(el) for el in zip(*args)]
+
+
+def flatten(list_of_lists):
+    """
+    Flatten list of lists into a list
+    """
+    return [el for inner in list_of_lists for el in inner]
+
+
+def up_to_and_including(l: List, matcher) -> List:
+    """
+    Cut the list after the first matching element.
+    """
+
+    def generate():
+        for element in l:
+            yield element
+            if matcher(element):
+                break
+
+    return list(generate())
---
+++ packages/fragmap/fragmap-0.4.4-py3-none-any.whl/fragmap/httphelper.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Copyright 2016-2021 Alexander Mollberg
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class HtmlHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path not in ["", "/"]:
+            self.send_response(404)
+            return
+        html = self.server.html_callback()
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(str.encode(html))
+
+
+def start_server(html_callback):
+    # Port 0 means select an arbitrary unused port
+    port = 0
+    server = HTTPServer(("127.0.0.1", port), HtmlHandler)
+    server.html_callback = html_callback
+
+    def serve_requests():
+        server.serve_forever()
+
+    import threading
+
+    threading.Thread(target=serve_requests).start()
+    return server
