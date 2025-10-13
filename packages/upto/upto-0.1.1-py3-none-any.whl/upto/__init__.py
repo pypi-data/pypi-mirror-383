016747179aa8c9349e1763803f9febab279e0091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 04:05:15 +0000

    Add package upto/0.1.1/upto-0.1.1-py3-none-any.whl

---
+++ packages/upto/upto-0.1.1-py3-none-any.whl/upto/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+
+warnings.warn(
+    "This package has been renamed to renmaed to `lupl`."
+    "Please use the actively maintained `lupl` package."
+    DeprecationWarning,
+    stacklevel=2,
+)
