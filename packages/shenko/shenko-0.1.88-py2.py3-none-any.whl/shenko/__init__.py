62987396316631f2053b8580d042a5e4cc0cd29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:18 +0000

    Add package shenko/0.1.88/shenko-0.1.88-py2.py3-none-any.whl

---
+++ packages/shenko/shenko-0.1.88-py2.py3-none-any.whl/shenko/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for shenko."""
+
+__author__ = """Danny Dowshenko"""
+__email__ = 'dowshenko225@gmail.com'
+__version__ = '0.1.88'
+from .s00_init.menu import AppState
+from .s00_init.menu import quit
+from .s00_init.menu import toggle_fullscreen
