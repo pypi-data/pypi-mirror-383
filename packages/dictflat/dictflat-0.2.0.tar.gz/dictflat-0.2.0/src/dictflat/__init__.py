744f69dd7d66ccfacf72c053a9d67f6cf21f9b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 05:05:43 +0000

    Add package dictflat/0.2.0/dictflat-0.2.0.tar.gz

---
+++ packages/dictflat/dictflat-0.2.0.tar.gz/dictflat-0.2.0/src/dictflat/__init__.py
@@ -0,0 +1,17 @@
+from typing import Final, List
+
+from .dictflat import (
+    ALL,
+    CHANGE_ROOT,
+    CONTEXT_DEPTH,
+    CONTEXT_ELEMENT,
+    CONTEXT_PATH,
+    CONTEXT_ROOT_REF,
+    DictFlat,
+)
+from .dicttools import extract_list, get_dict, simple_flat
+
+__author__: Final[str] = 'Arnaud Valmary'
+__email__: Final[str] = 'github@valmary.eu'
+__version_parts__: Final[List[str]] = ['0', '2', '0']
+__version__: Final[str] = '.'.join(__version_parts__)
