0623863f0bd8ab167f00d665ffed71302e65e163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 03:18:06 +0000

    Add package Holamundoplayer-waduswadus/0.0.1/holamundoplayer_waduswadus-0.0.1.tar.gz

---
+++ packages/Holamundoplayer-waduswadus/holamundoplayer_waduswadus-0.0.1.tar.gz/holamundoplayer_waduswadus-0.0.1/holamundoplayer/player.py
@@ -0,0 +1,34 @@
+"""
+Esta es el modulo que incluye la clase de reproductor de musica
+"""
+
+
+class Player:
+    """
+    Esta clase crea un reproductor de musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la cacion que recibio en 
+        el constructor
+
+        Parameters:
+        song(str): este es un strg con el path de la cancion
+
+        Returns:
+        int:devuelve 1 si reproduce con exito, en caso de fracaso da un 0 
+        """
+        print("reproduciendo cancion")
+
+    def stop(self):
+        """
+
+        """
+        print("stopping")
+
+
+for l in range(3):
+    pass
+
+print(l)
