0f6ca07db13d952ce0cd6e3fb28eaea720866869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 17:06:32 +0000

    Add package retrotv-log/0.0.6a0/retrotv_log-0.0.6a0.tar.gz

---
+++ packages/retrotv-log/retrotv_log-0.0.6a0.tar.gz/retrotv_log-0.0.6a0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="retrotv_log",
+    version="v0.0.6-alpha",
+    description="로깅을 위한 패키지",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="RetroTV",
+    author_email="yjj8353@gmail.com",
+    url="https://github.com/retrotv-pypi-repo/log",
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=["retrotv", "RetroTV", "yjj8353", "log", "Log", "logging", "Logging", "pypi"],
+    python_requires=">=3",
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+)
