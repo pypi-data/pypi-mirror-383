39db90caca9917f242364998e13b318cc1ff233b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 05:06:59 +0000

    Add package supervisorex/1.0.5/supervisorex-1.0.5.tar.gz

---
+++ packages/supervisorex/supervisorex-1.0.5.tar.gz/supervisorex-1.0.5/README.md
@@ -0,0 +1,14 @@
+# Supervisorex
+
+**Letest Version:** 1.0.5 
+**Developer:** @NacDevs  
+**Git Repo:** [GitHub](https://github.com/yuvrajmodz/supervisorex)
+
+Supervisorex is a Python CLI tool to easily create and start Supervisor-managed processes in Linux.
+
+## Installation & Usage
+
+```bash
+sudo apt install supervisor -y
+pip install supervisorex
+supervisorex <command>
---
+++ packages/supervisorex/supervisorex-1.0.5.tar.gz/supervisorex-1.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="supervisorex",
+    version="1.0.5",
+    author="@NacDevs",
+    author_email="yuvrajmodz@gmail.com",
+    description="Simple CLI to create supervisor processes easily",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yuvrajmodz/supervisorex",
+    packages=find_packages(),
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'supervisorex=supervisorex.cli:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
+
