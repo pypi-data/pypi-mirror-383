e41345c4ae8a382c7e4b031484814c184825c1b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 03:18:08 +0000

    Add package ut-xls/1.6.3.20251013/ut_xls-1.6.3.20251013-py3-none-any.whl

---
+++ packages/ut-xls/ut_xls-1.6.3.20251013-py3-none-any.whl/ut_xls/dec.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+TyDic = dict[Any, Any]
+TyPath = str
+TyPathK = str
+
+
+def sh_static_path(func):
+    def wrapper(pathk: TyPathK, kwargs: TyDic, *args):
+        print("========================================")
+        print(f"pathk = {pathk}")
+        print(f"kwargs = {kwargs}")
+        print(f"args = {args}")
+        print("========================================")
+        _path: TyPath = PathK.sh_path(pathk, kwargs)
+        return func(_path, kwargs, *args)
+    return wrapper
+
+
+def sh_class_path(func):
+    def wrapper(cls, pathk: TyPathK, kwargs: TyDic, *args):
+        print("========================================")
+        print(f"pathk = {pathk}")
+        print(f"kwargs = {kwargs}")
+        print(f"args = {args}")
+        print("========================================")
+        _path: TyPath = PathK.sh_path(pathk, kwargs)
+        return func(_path, kwargs, *args)
+    return wrapper
