2cd653521890fcdc3efe475f615e7be2637229d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 14:05:55 +0000

    Add package cacophonyapi/0.0.5/cacophonyapi-0.0.5-py2.py3-none-any.whl

---
+++ packages/cacophonyapi/cacophonyapi-0.0.5-py2.py3-none-any.whl/cacophonyapi/apibase.py
@@ -0,0 +1,55 @@
+import json
+import requests
+from urllib.parse import urljoin
+
+
+class APIBase:
+    TIMEOUT = 10
+    DOWNLOAD_TIMEOUT = 100
+
+    def __init__(self, baseurl, loginname, password, logintype):
+        self._baseurl = baseurl
+        self._loginname = loginname
+        self._token = self._get_jwt(password, logintype)
+        self._auth_header = {"Authorization": self._token}
+
+    def _get_jwt(self, password, logintype):
+        url = urljoin(self._baseurl, "/authenticate_" + logintype)
+        if logintype == "user":
+            r = requests.post(
+                url,
+                data={"email": self._loginname, "password": password},
+                timeout=self.TIMEOUT,
+            )
+        else:
+            nameProp = logintype + "name"
+            r = requests.post(
+                url,
+                data={nameProp: self._loginname, "password": password},
+                timeout=self.TIMEOUT,
+            )
+
+        if r.status_code == 200:
+            return r.json().get("token")
+        elif r.status_code == 422:
+            raise ValueError(
+                "Could not log on as '{}'.  Please check {} name.".format(
+                    self._loginname, logintype
+                )
+            )
+        elif r.status_code == 401:
+            raise ValueError(
+                "Could not log on as '{}'.  Please check password.".format(
+                    self._loginname
+                )
+            )
+        else:
+            r.raise_for_status()
+
+    def _check_response(self, r):
+        if r.status_code in (400, 422):
+            j = r.json()
+            messages = j.get("messages", j.get("message", ""))
+            raise IOError("request failed ({}): {}".format(r.status_code, messages))
+        r.raise_for_status()
+        return r.json()
