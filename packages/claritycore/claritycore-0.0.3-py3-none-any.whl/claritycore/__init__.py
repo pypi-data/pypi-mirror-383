423fb3c7069aa9f0045aa874ef4b8801056dbf18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:05:46 +0000

    Add package claritycore/0.0.3/claritycore-0.0.3-py3-none-any.whl

---
+++ packages/claritycore/claritycore-0.0.3-py3-none-any.whl/claritycore/__init__.py
@@ -0,0 +1,26 @@
+"""
+Copyright 2025 Aman Urumbekov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ClarityCore: Next-generation Open Source toolkit for low-level vision.
+
+Engineered for state-of-the-art performance in image and video restoration,
+including Super-Resolution, Denoising, Deblurring, and more.
+"""
+
+__all__ = [
+    "__version__",
+]
+
+__version__ = "0.0.3"
---
+++ packages/claritycore/claritycore-0.0.3-py3-none-any.whl/claritycore/cli/train.py
@@ -0,0 +1,5 @@
+# Copyright (c) Aman Urumbekov and other contributors.
+from claritycore.train import train_main
+
+if __name__ == "__main__":
+    train_main()
