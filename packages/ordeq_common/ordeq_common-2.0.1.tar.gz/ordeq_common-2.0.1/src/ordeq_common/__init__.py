1a2b2d2c8a7540d61d4cfe1828e7adc67e591297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:06:03 +0000

    Add package ordeq_common/2.0.1/ordeq_common-2.0.1.tar.gz

---
+++ packages/ordeq_common/ordeq_common-2.0.1.tar.gz/ordeq_common-2.0.1/src/ordeq_common/__init__.py
@@ -0,0 +1,25 @@
+from ordeq_common.hooks import SpyHook
+from ordeq_common.io import (
+    BytesBuffer,
+    Dataclass,
+    Iterate,
+    Literal,
+    Match,
+    MatchOnLoad,
+    MatchOnSave,
+    Print,
+    StringBuffer,
+)
+
+__all__ = (
+    "BytesBuffer",
+    "Dataclass",
+    "Iterate",
+    "Literal",
+    "Match",
+    "MatchOnLoad",
+    "MatchOnSave",
+    "Print",
+    "SpyHook",
+    "StringBuffer",
+)
