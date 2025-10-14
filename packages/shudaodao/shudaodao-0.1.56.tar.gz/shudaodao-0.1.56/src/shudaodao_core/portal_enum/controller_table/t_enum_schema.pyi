b10636416338d262b5609a77e20573ea2b492bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 09:06:39 +0000

    Add package shudaodao/0.1.56/shudaodao-0.1.56.tar.gz

---
+++ packages/shudaodao/shudaodao-0.1.56.tar.gz/shudaodao-0.1.56/src/shudaodao_core/portal_enum/controller_table/t_enum_schema.pyi
@@ -0,0 +1,11 @@
+from .. import get_engine_name as get_engine_name, get_schema_name as get_schema_name
+from ...controller.table_controller import AuthController as AuthController
+from ..entity_table.t_enum_schema import (
+    TEnumSchema as TEnumSchema,
+    TEnumSchemaCreate as TEnumSchemaCreate,
+    TEnumSchemaResponse as TEnumSchemaResponse,
+    TEnumSchemaUpdate as TEnumSchemaUpdate,
+)
+from _typeshed import Incomplete
+
+TEnumSchema_Router: Incomplete
