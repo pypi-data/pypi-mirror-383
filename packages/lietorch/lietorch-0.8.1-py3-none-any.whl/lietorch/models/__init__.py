8c759f6d8fa1d11461abd8abf076af6c76ac74fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 05:07:16 +0000

    Add package lietorch/0.8.1/lietorch-0.8.1-py3-none-any.whl

---
+++ packages/lietorch/lietorch-0.8.1-py3-none-any.whl/lietorch/models/__init__.py
@@ -0,0 +1,9 @@
+"""
+Models used in various experiments that serve as examples.
+"""
+
+import lietorch.models.dca1
+import lietorch.models.drive
+import lietorch.models.rotnist
+import lietorch.models.fixed_lift
+import lietorch.models.ds_pde
