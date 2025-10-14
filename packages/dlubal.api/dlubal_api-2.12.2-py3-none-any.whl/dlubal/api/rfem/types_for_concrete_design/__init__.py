cbeec1f0a8493b04d8fcbfc41c26a397dd0b224c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:05:48 +0000

    Add package dlubal.api/2.12.2/dlubal_api-2.12.2-py3-none-any.whl

---
+++ packages/dlubal.api/dlubal_api-2.12.2-py3-none-any.whl/dlubal/api/rfem/types_for_concrete_design/__init__.py
@@ -0,0 +1,5 @@
+from .concrete_durability_pb2 import *
+from .reinforcement_direction_pb2 import *
+from .concrete_effective_lengths_pb2 import *
+from .surface_reinforcement_pb2 import *
+from .punching_reinforcement_pb2 import *
---
+++ packages/dlubal.api/dlubal_api-2.12.2-py3-none-any.whl/dlubal/api/rstab/types_for_concrete_design/__init__.py
@@ -0,0 +1,2 @@
+from .concrete_durability_pb2 import *
+from .concrete_effective_lengths_pb2 import *
