8a139efef9cbee4b4425f54b5571f9f07b8e4dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:05:46 +0000

    Add package flink-agents/0.1.0/flink_agents-0.1.0-py3-none-any.whl

---
+++ packages/flink-agents/flink_agents-0.1.0-py3-none-any.whl/flink_agents/runtime/python_java_utils.py
@@ -0,0 +1,37 @@
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+#################################################################################
+from typing import Any
+
+import cloudpickle
+
+from flink_agents.api.events.event import InputEvent
+
+
+def convert_to_python_object(bytesObject: bytes) -> Any:
+    """Used for deserializing Python objects."""
+    return cloudpickle.loads(bytesObject)
+
+
+def wrap_to_input_event(bytesObject: bytes) -> bytes:
+    """Wrap data to python input event and serialize."""
+    return cloudpickle.dumps(InputEvent(input=cloudpickle.loads(bytesObject)))
+
+
+def get_output_from_output_event(bytesObject: bytes) -> Any:
+    """Get output data from OutputEvent and serialize."""
+    return cloudpickle.dumps(convert_to_python_object(bytesObject).output)
