688785b51731d3775dee4f29ad43d530671d0dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 14:06:35 +0000

    Add package liscopelens/0.2.4/liscopelens-0.2.4.tar.gz

---
+++ packages/liscopelens/liscopelens-0.2.4.tar.gz/liscopelens-0.2.4/liscopelens/api/__init__.py
@@ -0,0 +1,37 @@
+"""Convenient re-export of public API helpers."""
+
+from __future__ import annotations
+
+from .base import (
+    BaseCompatiblityParser,
+    BaseExceptionParser,
+    BasePropagateParser,
+    ParserInitArgs,
+    ScancodeParser,
+    check_compatibility,
+    regenerate_knowledge_base,
+    check_license_compatibility,
+    query_license_compatibility,
+)
+from .scancode import (
+    LicenseMap,
+    ScancodeExecutionError,
+    detect_license,
+    detect_license_chunked,
+)
+
+__all__ = [
+    "BaseCompatiblityParser",
+    "BaseExceptionParser",
+    "BasePropagateParser",
+    "ParserInitArgs",
+    "ScancodeParser",
+    "check_compatibility",
+    "regenerate_knowledge_base",
+    "check_license_compatibility",
+    "query_license_compatibility",
+    "LicenseMap",
+    "ScancodeExecutionError",
+    "detect_license",
+    "detect_license_chunked",
+]
