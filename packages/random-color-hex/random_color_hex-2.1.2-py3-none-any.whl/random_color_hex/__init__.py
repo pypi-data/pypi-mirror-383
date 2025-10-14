4ae0dad998ed0400ee7d8629ee51536e170442f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 15:06:53 +0000

    Add package random-color-hex/2.1.2/random_color_hex-2.1.2-py3-none-any.whl

---
+++ packages/random-color-hex/random_color_hex-2.1.2-py3-none-any.whl/random_color_hex/__init__.py
@@ -0,0 +1,19 @@
+from .random_color_hex import RandomColorHex
+
+__version__="2.1.2"
+__author__="Nathan Honn"
+__email__="randomhexman@gmail.com"
+
+__all__=["RandomColorHex", "main", "BasicMain", "Credits", "Help"]
+
+def main(*, SuperLightColorsAllowed=True, SuperDarkColorsAllowed=True, HowDifferentShouldColorsBe='m'):
+    return RandomColorHex().main(SuperLightColorsAllowed=SuperLightColorsAllowed, SuperDarkColorsAllowed=SuperDarkColorsAllowed, HowDifferentShouldColorsBe=HowDifferentShouldColorsBe)
+
+def BasicMain(*, SuperLightColorsAllowed=True, SuperDarkColorsAllowed=True):
+    return RandomColorHex().BasicMain(SuperLightColorsAllowed=SuperLightColorsAllowed, SuperDarkColorsAllowed=SuperDarkColorsAllowed)
+
+def Credits():
+    return RandomColorHex.Credits()
+
+def Help():
+    return RandomColorHex.Help()
