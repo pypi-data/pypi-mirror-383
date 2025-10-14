a9873c49d7aa0db72626a55464244e168f821a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:06:10 +0000

    Add package design-django-theme/2.15.1/design_django_theme-2.15.1.tar.gz

---
+++ packages/design-django-theme/design_django_theme-2.15.1.tar.gz/design_django_theme-2.15.1/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+#  rm -R build/ dist/ *egg-info
+#  python3 setup.py sdist
+#  twine upload dist/*
+
+setup(
+    name='design-django-theme',
+    version='v2.15.1',
+    packages=['bootstrap_italia_template', ],
+    package_data={'bootstrap_italia_template': ['bootstrap_italia_template/*']},
+    include_package_data=True,
+    license='3-clause BSD',
+    description="Bootstrap Italia template for Django",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/italia/design-django-theme',
+    author='Giuseppe De Marco, Francesco Filicetti',
+    author_email='giuseppe.demarco@unical.it, francesco.filicetti@unical.it',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 5.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'django'
+    ]
+)
