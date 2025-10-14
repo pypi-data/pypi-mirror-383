bf7343f4c26080262c26fdc2bb3e3b04d8da9bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 08:06:43 +0000

    Add package arro3-io/0.6.4/arro3_io-0.6.4-pp310-pypy310_pp73-musllinux_1_2_x86_64.whl

---
+++ packages/arro3-io/arro3_io-0.6.4-pp310-pypy310_pp73-musllinux_1_2_x86_64.whl/arro3/io/_io.pyi
@@ -0,0 +1,21 @@
+from ._csv import infer_csv_schema, read_csv, write_csv
+from ._ipc import read_ipc, read_ipc_stream, write_ipc, write_ipc_stream
+from ._json import infer_json_schema, read_json, write_json, write_ndjson
+from ._parquet import read_parquet, read_parquet_async, write_parquet
+
+__all__ = [
+    "infer_csv_schema",
+    "read_csv",
+    "write_csv",
+    "infer_json_schema",
+    "read_json",
+    "write_json",
+    "write_ndjson",
+    "read_ipc",
+    "read_ipc_stream",
+    "write_ipc",
+    "write_ipc_stream",
+    "read_parquet",
+    "read_parquet_async",
+    "write_parquet",
+]
