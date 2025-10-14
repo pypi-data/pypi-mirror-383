855c36da688b8ec7ffd28b8d02a45b935454fa75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 17:06:33 +0000

    Add package fta/0.0.16/fta-0.0.16.tar.gz

---
+++ packages/fta/fta-0.0.16.tar.gz/fta-0.0.16/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='fta',
+    version='0.0.16',
+    description='',
+    url='https://github.com/voidful/FTA',
+    author='Voidful',
+    author_email='voidful.stack@gmail.com',
+    long_description=open("README.md", encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    setup_requires=['setuptools-git'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python"
+    ],
+    license_files="Apache",
+    keywords='finance',
+    packages=find_packages(),
+    install_requires=required,
+    zip_safe=False,
+)
