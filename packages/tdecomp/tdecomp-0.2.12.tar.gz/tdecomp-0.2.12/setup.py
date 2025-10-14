c7ae8ad480ed9c10ed419ea703c0f96f1f04d1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 06:05:27 +0000

    Add package tdecomp/0.2.12/tdecomp-0.2.12.tar.gz

---
+++ packages/tdecomp/tdecomp-0.2.12.tar.gz/tdecomp-0.2.12/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tdecomp",
+    version="0.2.12",
+    description="Library for tensor and matrix decompositions applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Leon Strelkov",
+    author_email="strelllleo@mail.ru",
+    url="https://github.com/leostre/tensor-decompositions.git",
+    packages=find_packages(
+        where=".",
+        include=["tdecomp*"],
+        exclude=["tests*", "experiments*", "docs*", "build*", "dist*"]
+    ),
+    python_requires=">=3.8",
+    install_requires=[
+        "tensorly",
+        "torch",
+    ],
+    license=' BSD License',
+    keywords=[
+        "tensor decompositions",
+        "matrix decompositions", 
+        "randomized algorithms"
+    ],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
