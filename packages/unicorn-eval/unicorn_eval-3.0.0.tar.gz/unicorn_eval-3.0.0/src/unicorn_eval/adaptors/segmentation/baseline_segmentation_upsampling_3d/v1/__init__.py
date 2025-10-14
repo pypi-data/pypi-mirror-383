f9f059d12292be7df82bf8271411cc202c260a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 08:05:30 +0000

    Add package unicorn-eval/3.0.0/unicorn_eval-3.0.0.tar.gz

---
+++ packages/unicorn-eval/unicorn_eval-3.0.0.tar.gz/unicorn_eval-3.0.0/src/unicorn_eval/adaptors/segmentation/baseline_segmentation_upsampling_3d/v1/__init__.py
@@ -0,0 +1,9 @@
+from unicorn_eval.adaptors.segmentation.baseline_segmentation_upsampling_3d.v1.main import (
+    SegmentationUpsampling,
+    SegmentationUpsampling3D,
+)
+
+__all__ = [
+    "SegmentationUpsampling",
+    "SegmentationUpsampling3D",
+]
