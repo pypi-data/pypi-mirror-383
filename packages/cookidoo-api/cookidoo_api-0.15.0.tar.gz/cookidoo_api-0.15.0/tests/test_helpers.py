4096656da4c8d09a2f6c06b11323f7292d806782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 12:07:10 +0000

    Add package cookidoo-api/0.15.0/cookidoo_api-0.15.0.tar.gz

---
+++ packages/cookidoo-api/cookidoo_api-0.15.0.tar.gz/cookidoo_api-0.15.0/tests/test_helpers.py
@@ -0,0 +1,43 @@
+"""Unit tests for cookidoo-api."""
+
+from typing import cast
+
+from dotenv import load_dotenv
+import pytest
+
+from cookidoo_api.helpers import (
+    cookidoo_recipe_details_from_json,
+    get_country_options,
+    get_language_options,
+    get_localization_options,
+)
+from cookidoo_api.raw_types import RecipeDetailsJSON
+from tests.responses import COOKIDOO_TEST_RESPONSE_GET_RECIPE_DETAILS
+
+load_dotenv()
+
+
+class TestLocalization:
+    """Tests for localization functions."""
+
+    async def test_get_localization_options(self) -> None:
+        """Test get localization options."""
+        assert len(await get_localization_options()) == 373
+        assert len(await get_localization_options(country="ch")) == 4
+        assert len(await get_localization_options(language="en")) == 38
+
+    async def test_get_country_options(self) -> None:
+        """Test get country options."""
+        assert len(await get_country_options()) == 54
+
+    async def test_get_language_options(self) -> None:
+        """Test get language options."""
+        assert len(await get_language_options()) == 31
+
+    async def test_cookidoo_recipe_details_from_json_exception(self) -> None:
+        """Test get recipe details from json exception."""
+        JSON = cast(RecipeDetailsJSON, COOKIDOO_TEST_RESPONSE_GET_RECIPE_DETAILS.copy())
+        JSON["times"] = []
+
+        with pytest.raises(StopIteration):
+            cookidoo_recipe_details_from_json(JSON)
