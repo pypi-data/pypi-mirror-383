01be7256d66a8f2952530d28368906afba31bf69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 06:05:56 +0000

    Add package bfabric-scripts/1.13.36/bfabric_scripts-1.13.36.tar.gz

---
+++ packages/bfabric-scripts/bfabric_scripts-1.13.36.tar.gz/bfabric_scripts-1.13.36/src/bfabric_scripts/cli/cli_api.py
@@ -0,0 +1,12 @@
+import cyclopts
+
+from bfabric_scripts.cli.api.create import cmd_api_create
+from bfabric_scripts.cli.api.delete import cmd_api_delete
+from bfabric_scripts.cli.api.read import cmd_api_read
+from bfabric_scripts.cli.api.update import cmd_api_update
+
+cmd_api = cyclopts.App(help="Commands for interacting with B-Fabric API directly.")
+cmd_api.command(cmd_api_create, name="create")
+cmd_api.command(cmd_api_delete, name="delete")
+cmd_api.command(cmd_api_read, name="read")
+cmd_api.command(cmd_api_update, name="update")
