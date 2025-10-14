eb1f2efe6e238b06b2682e1ac2b1019557e6308a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 03:18:05 +0000

    Add package machineconfig/5.89/machineconfig-5.89-py3-none-any.whl

---
+++ packages/machineconfig/machineconfig-5.89-py3-none-any.whl/machineconfig/settings/shells/pwsh/init.ps1
@@ -0,0 +1,96 @@
+
+
+$MAC_ROOT = "$HOME\.config\machineconfig"
+
+function Add-ToPathIfNotAlready {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string[]]$Directories
+    )
+
+    foreach ($dir in $Directories) {
+        if ($env:Path -notlike "*$dir*") {
+            $env:Path += ";$dir"
+        }
+    }
+}
+
+Add-ToPathIfNotAlready -Directories @(
+    "$MAC_ROOT\src\machineconfig\scripts\windows",
+    "$HOME\dotfiles\scripts\windows",
+    "C:\Program Files (x86)\GnuWin32\bin",
+    "C:\Program Files\CodeBlocks\MinGW\bin",
+    "C:\Program Files\nu\bin",
+    "C:\Program Files\Graphviz\bin"
+)
+
+# $machineconfig_path = (python -c "print(__import__('machineconfig').__file__[:-12])")
+#C:\Program Files\Git\bin
+
+
+# sources  ================================================================
+. $MAC_ROOT\src\machineconfig\settings\broot\brootcd.ps1
+. $MAC_ROOT\src\machineconfig\settings\lf\windows\lfcd.ps1
+. $MAC_ROOT\src\machineconfig\settings\tere\terecd.ps1
+
+
+function lsdla { lsd -la }
+Set-Alias -Name l -Value lsdla -Option AllScope
+
+function d { mcfg devops @args }
+function c { mcfg cloud @args }
+function a { mcfg agents @args }
+function s { mcfg sessions @args }
+function ff { mcfg ftpx @args }
+function f { mcfg fire @args }
+function cs { mcfg croshell @args }
+
+
+try {
+    Set-Alias -Name gcs -Value {gh copilot suggest -t shell}
+    Set-Alias -Name gcg -Value {gh copilot suggest -t git}
+    Set-Alias -Name gce -Value {gh copilot explain}
+    # Check for conflicts
+    # Get-Command gcs -ErrorAction SilentlyContinue
+    # Get-Command gcg -ErrorAction SilentlyContinue
+    # Get-Command gce -ErrorAction SilentlyContinue
+}
+catch {
+    # Do nothing
+}
+
+
+# patches ===========================================================
+
+try {
+    # patched by machineconfig from https://github.com/ajeetdsouza/zoxide
+    Invoke-Expression (& {
+        $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+        (zoxide init --hook $hook powershell | Out-String)
+    })
+}
+catch {
+    # Do nothing
+}
+
+# try {
+#     Invoke-Expression -Command $(mcfly init powershell | out-string)
+# }
+# catch {
+#     # Do nothing
+# }
+
+
+# oh-my-posh --init --shell pwsh --config $env:USERPROFILE/AppData/Local/Programs/oh-my-posh/themes/atomicBit.omp.json | Invoke-Expression
+
+# try {
+#     Invoke-Expression (&starship init powershell)
+# }
+# catch {
+#     # Do nothing
+# # oh-my-posh --init --shell pwsh --config $env:USERPROFILE/AppData/Local/Programs/oh-my-posh/themes/atomicBit.omp.json | Invoke-Expression
+# }
+
+# Set-Alias lvim '~/.local/bin/lvim.ps1'
+# function fuck {     $history = (Get-History -Count 1).CommandLine;     if (-not [string]::IsNullOrWhiteSpace($history)) {         $f>
+
