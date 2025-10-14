d1331076ab69c3f2d488194665a9d5ceb203a078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 17:06:32 +0000

    Add package mcush-python/1.5.9/mcush_python-1.5.9-py3-none-any.whl

---
+++ packages/mcush-python/mcush_python-1.5.9-py3-none-any.whl/mcush/rigol/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf8
+__doc__ = 'Products from Beijing RIGOL Technologies Inc.'
+__author__ = 'Peng Shulin <trees_peng@163.com>'
+__license__ = 'MCUSH designed by Peng Shulin, all rights reserved.'
+
+from . import DP700
+from . import DP800
+from . import DG800
+from . import DG1000Z
+from . import DM3058
+from . import DS1000
+
