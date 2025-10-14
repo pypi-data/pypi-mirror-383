8653a66da93213d57fd4130ade9fca6f84a6220a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 04:05:44 +0000

    Add package coogle/0.0.4b2/coogle-0.0.4b2-py3-none-any.whl

---
+++ packages/coogle/coogle-0.0.4b2-py3-none-any.whl/Coogle/functions/function01.py
@@ -0,0 +1,26 @@
+import re
+from .function03 import Tuples
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+from ..exceptions import InvalidToken
+#======================================================================
+
+class MGoogle:
+
+    async def get01(token):
+        if len(token) == 73 and token[1] == "/":
+            pass
+        else:
+            raise InvalidToken("Invalid Token")
+
+    async def get02(link: str, pattern=None):
+        if link.startswith(Tuples.DATA01):
+            moones = re.search(pattern, link)
+            moonus = moones.group(3) if moones else None
+            return moonus
+        else:
+            moones = urlparse(link)
+            moonus = parse_qs(moones.query)['id'][0]
+            return moonus
+
+#======================================================================
