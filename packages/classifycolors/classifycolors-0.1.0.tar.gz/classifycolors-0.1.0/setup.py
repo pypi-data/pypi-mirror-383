d9032d4f4ccbf61230951ef8683ecde544fc656d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 03:18:00 +0000

    Add package classifycolors/0.1.0/classifycolors-0.1.0.tar.gz

---
+++ packages/classifycolors/classifycolors-0.1.0.tar.gz/classifycolors-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="classifycolors",
+    version="0.1.0",
+    author="Elie Fares",
+    packages=find_packages(),
+    install_requires=[
+        "webcolors"
+    ]
+)
