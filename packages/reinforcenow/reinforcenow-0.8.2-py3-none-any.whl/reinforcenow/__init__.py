87209fb7d3c9adc6a96944732b91bc615936891e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 04:05:44 +0000

    Add package reinforcenow/0.8.2/reinforcenow-0.8.2-py3-none-any.whl

---
+++ packages/reinforcenow/reinforcenow-0.8.2-py3-none-any.whl/reinforcenow/__init__.py
@@ -0,0 +1,28 @@
+"""
+ReinforceNow CLI - Command-line interface for ReinforceNow RLHF platform.
+"""
+
+# Export types for user convenience
+from reinforcenow.types import (
+    Sample,
+    RewardFunction,
+    RewardExecutor,
+    GenerationFunction,
+    reward_function,
+    reward_aggregator,
+    reward_executor,
+    generation,
+)
+
+__all__ = [
+    "Sample",
+    "RewardFunction",
+    "RewardExecutor",
+    "GenerationFunction",
+    "reward_function",
+    "reward_aggregator",
+    "reward_executor",
+    "generation",
+]
+
+__version__ = "0.8.2"
