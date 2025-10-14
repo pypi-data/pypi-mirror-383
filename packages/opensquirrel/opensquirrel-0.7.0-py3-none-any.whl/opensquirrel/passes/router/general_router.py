7b0a9c2827d2b99ccf56902cf1a84f9ab21a48e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:05:47 +0000

    Add package opensquirrel/0.7.0/opensquirrel-0.7.0-py3-none-any.whl

---
+++ packages/opensquirrel/opensquirrel-0.7.0-py3-none-any.whl/opensquirrel/passes/router/general_router.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from opensquirrel.ir import IR
+
+
+class Router(ABC):
+    def __init__(self, **kwargs: Any) -> None:
+        """Generic router class"""
+
+    @abstractmethod
+    def route(self, ir: IR) -> IR:
+        raise NotImplementedError
