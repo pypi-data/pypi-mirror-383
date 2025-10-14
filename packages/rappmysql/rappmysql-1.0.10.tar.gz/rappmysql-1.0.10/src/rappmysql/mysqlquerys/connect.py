6cb418b61c2855af706fe6b8749b547fb27c8226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 08:05:57 +0000

    Add package rappmysql/1.0.10/rappmysql-1.0.10.tar.gz

---
+++ packages/rappmysql/rappmysql-1.0.10.tar.gz/rappmysql-1.0.10/src/rappmysql/mysqlquerys/connect.py
@@ -0,0 +1,57 @@
+from configparser import ConfigParser
+import sys
+import rappmysql
+
+
+class Config:
+    def __init__(self, fileName):
+        #print('Module: {}, {}, Class: {}, Def: {}'.format(__name__, __class__, sys._getframe().f_code.co_name))
+        self.fileName = fileName
+        if len(self.sections) == 1:
+            self.credentials = 0
+
+    @property
+    def credentials(self):
+        #print('Module: {}, {}, Class: {}, Def: {}'.format(__name__, __class__, sys._getframe().f_code.co_name))
+        parser = ConfigParser(empty_lines_in_values=False)
+        parser.read(self.fileName)
+        sections = parser.sections()
+        sect = sections[self.sec_no]
+        params = dict(parser.items(sect))
+        return params
+
+    @credentials.setter
+    def credentials(self, sec_no):
+        #print('Module: {}, {}, Class: {}, Def: {}'.format(__name__, __class__, sys._getframe().f_code.co_name))
+        self.sec_no = sec_no
+
+    @property
+    def sections(self):
+        #print('Module: {}, {}, Class: {}, Def: {}'.format(__name__, __class__, sys._getframe().f_code.co_name))
+        parser = ConfigParser(empty_lines_in_values=False)
+        parser.read(self.fileName)
+        sections = parser.sections()
+        return sections
+
+    @property
+    def db_type(self):
+        #print('Module: {}, {}, Class: {}, Def: {}'.format(__name__, __class__, sys._getframe().f_code.co_name))
+        parser = ConfigParser(empty_lines_in_values=False)
+        parser.read(self.fileName)
+        sections = parser.sections()
+        type = sections[self.sec_no].split('_')[0]
+        return type
+
+
+def main():
+
+    auto_app = Config(rappmysql.ini_masina)
+    print(auto_app)
+    print(type(auto_app))
+    print(auto_app.credentials)
+    # return
+
+
+
+if __name__ == '__main__':
+    main()
