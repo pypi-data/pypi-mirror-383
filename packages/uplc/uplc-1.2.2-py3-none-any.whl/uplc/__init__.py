064f9fc02251231484f33292daf097f87b5ef020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:05:56 +0000

    Add package uplc/1.2.2/uplc-1.2.2-py3-none-any.whl

---
+++ packages/uplc/uplc-1.2.2-py3-none-any.whl/uplc/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import logging
+
+
+__version__ = "1.2.1"
+__author__ = "nielstron"
+__author_email__ = "n.muendler@web.de"
+__copyright__ = "Copyright (C) 2023 nielstron"
+__license__ = "MIT"
+__url__ = "https://github.com/opshin/uplc"
+
+try:
+    from .tools import parse, eval, dumps, UPLCDialect, flatten, unflatten, apply
+
+except ImportError as e:
+    logging.error(
+        "Error, trying to import dependencies. Should only occur upon package installation",
+        exc_info=e,
+    )
