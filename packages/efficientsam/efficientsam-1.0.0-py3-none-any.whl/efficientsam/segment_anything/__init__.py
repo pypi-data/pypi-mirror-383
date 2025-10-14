dc5724f8ca20fbe8418e20abb5851b8f843d7c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 06:06:15 +0000

    Add package efficientsam/1.0.0/efficientsam-1.0.0-py3-none-any.whl

---
+++ packages/efficientsam/efficientsam-1.0.0-py3-none-any.whl/efficientsam/segment_anything/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .automatic_mask_generator import SamAutomaticMaskGenerator
+from .predictor import SamPredictor
