bf6345dfe3829dc661baece2374b74e7d4899c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 15:07:12 +0000

    Add package dj-oauth-clients/0.1.7/dj_oauth_clients-0.1.7.tar.gz

---
+++ packages/dj-oauth-clients/dj_oauth_clients-0.1.7.tar.gz/dj_oauth_clients-0.1.7/tests/test_models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_dj-oauth-clients
+------------
+
+Tests for `dj-oauth-clients` models module.
+"""
+
+from django.test import TestCase
+
+from oauth_clients import models
+
+
+class TestOauth_clients(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
