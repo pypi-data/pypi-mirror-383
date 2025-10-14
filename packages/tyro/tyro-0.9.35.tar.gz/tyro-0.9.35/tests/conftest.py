1c73f92506467fa8e050eff8e0b894f3da0090a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 05:06:50 +0000

    Add package tyro/0.9.35/tyro-0.9.35.tar.gz

---
+++ packages/tyro/tyro-0.9.35.tar.gz/tyro-0.9.35/tests/conftest.py
@@ -0,0 +1,41 @@
+import sys
+from typing import List
+
+collect_ignore_glob: List[str] = []
+
+if not sys.version_info >= (3, 9):
+    collect_ignore_glob.append("*min_py39*.py")
+
+if not sys.version_info >= (3, 10):
+    collect_ignore_glob.append("*min_py310*.py")
+
+if not sys.version_info >= (3, 11):
+    collect_ignore_glob.append("*min_py311*.py")
+
+if not sys.version_info >= (3, 12):
+    collect_ignore_glob.append("*min_py312*.py")
+
+if not sys.version_info >= (3, 13):
+    collect_ignore_glob.append("*min_py313*.py")
+
+if not sys.version_info >= (3, 11):
+    collect_ignore_glob.append("test_py311_generated/*.py")
+
+if sys.version_info >= (3, 13):
+    collect_ignore_glob.append("*_exclude_py313*.py")
+
+try:
+    import flax  # noqa: I001,F401 # type: ignore
+except ImportError:
+    collect_ignore_glob.append("*_flax*.py")
+
+try:
+    import numpy  # noqa: I001,F401 # type: ignore
+except ImportError:
+    collect_ignore_glob.append("*_custom_constructors*.py")
+
+try:
+    import torch  # noqa: I001,F401 # type: ignore
+except ImportError:
+    collect_ignore_glob.append("*_torch*.py")
+    collect_ignore_glob.append("*_base_configs_nested*.py")
