fbc460711de76064d17f8af9d4b3a1eb9f482727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 12:06:12 +0000

    Add package copyjindu/1.0.0/copyjindu-1.0.0.tar.gz

---
+++ packages/copyjindu/copyjindu-1.0.0.tar.gz/copyjindu-1.0.0/README.md
@@ -0,0 +1,8 @@
+# CopyProgress
+
+一个带进度显示的文件复制Python库。
+
+## 安装
+
+```bash
+pip install copyprogress
---
+++ packages/copyjindu/copyjindu-1.0.0.tar.gz/copyjindu-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="copyjindu",
+    version="1.0.0",
+    author="pengmin",
+    author_email="877419534@qq.com",
+    description="A file copy library with progress bar",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "tqdm>=4.60.0",
+    ],
+)
