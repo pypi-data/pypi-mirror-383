5a6b91def03e2032b4c9e07e9fa874e212f6a300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 12:06:28 +0000

    Add package p2d2/0.1.117/p2d2-0.1.117.tar.gz

---
+++ packages/p2d2/p2d2-0.1.117.tar.gz/p2d2-0.1.117/src/p2d2/__init__.py
@@ -0,0 +1,3 @@
+from .mod_analytics import Analytics
+from .mod_types import dtype_to_sql_type, python_to_dtype, empty_dataframe_from_type, python_to_sql_type, migrate_table_schema
+from .database import Database
