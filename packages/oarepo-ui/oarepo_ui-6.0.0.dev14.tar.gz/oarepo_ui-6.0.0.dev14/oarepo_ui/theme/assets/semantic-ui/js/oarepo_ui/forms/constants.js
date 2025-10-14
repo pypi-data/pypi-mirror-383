60c41ff5a02bba502eb777e07387ed7d102123d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 17:06:44 +0000

    Add package oarepo-ui/6.0.0.dev14/oarepo_ui-6.0.0.dev14.tar.gz

---
+++ packages/oarepo-ui/oarepo_ui-6.0.0.dev14.tar.gz/oarepo_ui-6.0.0.dev14/oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/constants.js
@@ -0,0 +1,9 @@
+export const overridableComponentIds = [
+  "Errors.container",
+  "FormActions.container",
+  "FormApp.layout",
+  "FormFields.container",
+  "CustomFields.container",
+];
+
+export const DEFAULT_SUGGESTION_SIZE = 20;
