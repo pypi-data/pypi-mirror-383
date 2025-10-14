a6ce37a951d8530b19d7ec84959db205965ad7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 03:18:15 +0000

    Add package degenerate-dna/0.1.0/degenerate_dna-0.1.0.tar.gz

---
+++ packages/degenerate-dna/degenerate_dna-0.1.0.tar.gz/degenerate_dna-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="degenerate-dna",
+    version="0.1.0",
+    url="https://github.com/carlosp420/degenerate-dna",
+
+    author="Carlos Pena",
+    author_email="mycalesis@gmail.com",
+
+    description="Python implementation of the Degen Perl package by Zwick et al.",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    license='BSD-3-Clause',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
