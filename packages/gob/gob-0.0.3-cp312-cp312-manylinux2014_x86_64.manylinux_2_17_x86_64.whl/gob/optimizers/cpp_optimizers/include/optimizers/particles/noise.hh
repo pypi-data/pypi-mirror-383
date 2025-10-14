dabe3019841ac67c54c3eb1f272134ad973a1f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 06:05:32 +0000

    Add package gob/0.0.3/gob-0.0.3-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/gob/gob-0.0.3-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/gob/optimizers/cpp_optimizers/include/optimizers/particles/noise.hh
@@ -0,0 +1,9 @@
+/*
+ * Created in 2025 by Gaëtan Serré
+ */
+
+#include "utils.hh"
+
+extern Eigen::MatrixXd zero_noise(const int &rows, const int &cols);
+
+extern Eigen::MatrixXd normal_noise(const int &rows, const int &cols, std::mt19937_64 re, double mean = 0, double stddev = 1);
