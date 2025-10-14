94c85caf7ca1ab9db534a179b713e328a4adbde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 15:06:17 +0000

    Add package tejasayya/1.0.2/tejasayya-1.0.2.tar.gz

---
+++ packages/tejasayya/tejasayya-1.0.2.tar.gz/tejasayya-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tejasayya",
+    version="1.0.2",
+    packages=find_packages(include=["tejasayya", "tejasayya.*"]),
+    entry_points={
+        "console_scripts": [
+            "tejasayya=tejasayya.__main__:main",  # CLI command
+        ],
+    },
+    description="CLI package that introduces Teja Swaroop Sayya 🚀",
+    author="Teja Swaroop Sayya",
+    author_email="tejasayya@gmail.com",
+    license="MIT",
+    python_requires=">=3.7",
+)
