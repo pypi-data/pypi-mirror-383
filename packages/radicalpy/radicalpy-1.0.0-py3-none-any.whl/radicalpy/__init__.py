8bdb0f5d381da090e5c32949f7ea9b55cefbe850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 17:05:55 +0000

    Add package radicalpy/1.0.0/radicalpy-1.0.0-py3-none-any.whl

---
+++ packages/radicalpy/radicalpy-1.0.0-py3-none-any.whl/radicalpy/__init__.py
@@ -0,0 +1,23 @@
+"""Radicalpy package root."""
+
+from pint import UnitRegistry
+
+from . import plot  # noqa: F401 F403
+from . import (
+    classical,
+    data,
+    estimations,
+    experiments,
+    kinetics,
+    relaxation,
+    shared,
+    simulation,
+    tensornetwork,
+    utils,
+)
+
+# pylint: disable=unused-import wildcard-import
+
+
+ureg = UnitRegistry()
+Q_ = ureg.Quantity
