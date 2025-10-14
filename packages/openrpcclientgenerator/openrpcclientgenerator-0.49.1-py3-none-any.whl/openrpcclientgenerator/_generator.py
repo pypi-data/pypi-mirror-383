f61d88865b742808702eca299cf955461a9b7f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 11:06:20 +0000

    Add package openrpcclientgenerator/0.49.1/openrpcclientgenerator-0.49.1-py3-none-any.whl

---
+++ packages/openrpcclientgenerator/openrpcclientgenerator-0.49.1-py3-none-any.whl/openrpcclientgenerator/_generator.py
@@ -0,0 +1,15 @@
+"""Client generator top-level."""
+
+from pathlib import Path
+
+from openrpc import OpenRPC
+
+from openrpcclientgenerator import _python, _typescript
+from openrpcclientgenerator._common import Language
+
+
+def generate(openrpc: OpenRPC, language: Language, url: str, out: Path) -> str:
+    """Generate an RPC client."""
+    transport = "WS" if url.startswith("ws") else "HTTP"
+    lang = _python if language is Language.PYTHON else _typescript
+    return lang.generate_client(openrpc, url, transport, out)
