4119f8213e131a9e0108aa376485f51f0bc147b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 03:18:24 +0000

    Add package climax-rfglab/2025.10.1/climax_rfglab-2025.10.1-cp310-abi3-macosx_11_0_arm64.whl

---
+++ packages/climax-rfglab/climax_rfglab-2025.10.1-cp310-abi3-macosx_11_0_arm64.whl/climax/__init__.py
@@ -0,0 +1,6 @@
+from climax import climax, _imagepanel, _renderer, _slider
+from climax.version import __version__
+
+name = "climax"
+
+__all__ = [__version__, climax.climax, _imagepanel.ImagePanel, _renderer.Renderer, _slider.Slider]
