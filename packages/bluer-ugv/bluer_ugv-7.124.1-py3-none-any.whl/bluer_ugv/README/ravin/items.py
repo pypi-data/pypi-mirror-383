ddc903c688bf6ee775e0ae498a31efc756f54808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 15:06:15 +0000

    Add package bluer-ugv/7.124.1/bluer_ugv-7.124.1-py3-none-any.whl

---
+++ packages/bluer-ugv/bluer_ugv-7.124.1-py3-none-any.whl/bluer_ugv/README/ravin/items.py
@@ -0,0 +1,25 @@
+from bluer_objects.README.items import ImageItems
+
+from bluer_ugv.README.ravin.consts import ravin_assets2, ravin_design
+
+
+items = ImageItems(
+    {
+        f"{ravin_assets2}/20250712_114819.jpg": "",
+        f"{ravin_design}/wiring.png": f"{ravin_design}/wiring.svg",
+        f"{ravin_assets2}/20250713_172325.jpg": "",
+        f"{ravin_assets2}/20250713_172413.jpg": "",
+        f"{ravin_assets2}/20250713_172442_1.gif": "",
+        f"{ravin_assets2}/20250723_095022.jpg": "",
+        f"{ravin_assets2}/20250723_095155~2_1.gif": "",
+        f"{ravin_assets2}/20250728_112123.jpg": "",
+        f"{ravin_assets2}/20250726_134024.jpg": "",
+        f"{ravin_assets2}/20250807_103534.jpg": "",
+        f"{ravin_assets2}/20250807_171354.jpg": "",
+        f"{ravin_assets2}/20250813_172023.jpg": "",
+        f"{ravin_assets2}/20250827_170346.jpg": "",
+        f"{ravin_assets2}/20250827_170319~3_1.gif": "",
+        f"{ravin_assets2}/20250914_175506-2.jpg": "",
+        f"{ravin_assets2}/20251013_154125-2.jpg": "",
+    }
+)
