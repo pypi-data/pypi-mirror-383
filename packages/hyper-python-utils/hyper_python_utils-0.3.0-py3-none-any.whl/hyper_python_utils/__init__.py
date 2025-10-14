9e5d2c359ef9d6e93a5e816e5a7354693b75e5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 04:05:24 +0000

    Add package hyper-python-utils/0.3.0/hyper_python_utils-0.3.0-py3-none-any.whl

---
+++ packages/hyper-python-utils/hyper_python_utils-0.3.0-py3-none-any.whl/hyper_python_utils/__init__.py
@@ -0,0 +1,17 @@
+"""
+Hyper Python Utils - AWS S3 and Athena utilities for data processing with Polars
+"""
+
+from .file_handler import FileHandler
+from .query_manager import QueryManager, EmptyResultError, AthenaQueryError
+from .wrapper import query, query_unload
+
+__version__ = "0.3.0"
+__all__ = [
+    "FileHandler",
+    "QueryManager",
+    "EmptyResultError",
+    "AthenaQueryError",
+    "query",
+    "query_unload"
+]
