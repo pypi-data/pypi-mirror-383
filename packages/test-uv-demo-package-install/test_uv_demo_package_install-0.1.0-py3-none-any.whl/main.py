65c383c0aa98e2bf7c7e8089a7bcf06ed45dd31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 14:05:53 +0000

    Add package test-uv-demo-package-install/0.1.0/test_uv_demo_package_install-0.1.0-py3-none-any.whl

---
+++ packages/test-uv-demo-package-install/test_uv_demo_package_install-0.1.0-py3-none-any.whl/main.py
@@ -0,0 +1,11 @@
+import is_uv_demo as isp
+
+def main():
+    print("Hello from test-uv-demo-package-install!")
+    # print(isp.demo_stats.design_stats_test(0.5, 0.5))
+    
+
+
+
+if __name__ == "__main__":
+    main()
