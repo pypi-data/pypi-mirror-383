824ec73fcdcf000c20c96c24b7ec6ddd37513fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 04:05:44 +0000

    Add package roleft/1.0.37/roleft-1.0.37.tar.gz

---
+++ packages/roleft/roleft-1.0.37.tar.gz/roleft-1.0.37/tests/test_11.py
@@ -0,0 +1,29 @@
+from typing import Iterable, TypeVar
+
+import sys
+
+
+# print(sys.path)
+
+import roleft.Enumerable
+
+print(roleft.Enumerable.__file__)
+
+from Utilities.AU_defs import AU
+from Utilities.CU_defs import CU
+
+
+from roleft.Enumerable.RoleftList import xList
+from Enumerable.xlist import Fuck
+
+
+hehe = CU.gen_random()
+pass
+
+hoho = xList[int]()
+# hoho = xdatetime.Standard()
+
+# hehe = Fuck.xfrom()
+# nums = [1, 2, 3]
+# nums_ex = xlist.xfrom(nums)
+pass
