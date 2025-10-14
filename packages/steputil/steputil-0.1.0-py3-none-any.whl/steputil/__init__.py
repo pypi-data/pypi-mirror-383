76a7b39a88808df1b0f0758b0cc83757a3126563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 15:06:47 +0000

    Add package steputil/0.1.0/steputil-0.1.0-py3-none-any.whl

---
+++ packages/steputil/steputil-0.1.0-py3-none-any.whl/steputil/__init__.py
@@ -0,0 +1,12 @@
+"""steputil - Utilities to simplify creation of pipeline steps."""
+
+__version__ = "0.1.0"
+
+
+def info() -> str:
+    """Return a hello world string.
+
+    Returns:
+        str: A greeting message.
+    """
+    return "Hello World"
