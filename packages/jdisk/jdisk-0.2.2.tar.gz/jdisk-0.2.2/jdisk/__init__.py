4746a14dca0b118b0b3606b6f4c1bb3ed9a16552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 17:05:55 +0000

    Add package jdisk/0.2.2/jdisk-0.2.2.tar.gz

---
+++ packages/jdisk/jdisk-0.2.2.tar.gz/jdisk-0.2.2/jdisk/__init__.py
@@ -0,0 +1,38 @@
+"""jdisk.
+
+A simple Python implementation for Shanghai Jiao Tong University Netdisk client.
+"""
+
+__version__ = "0.2.2"
+__author__ = "chengjilai"
+
+from .auth import SJTUAuth
+from .client import SJTUNetdiskClient
+from .download import FileDownloader
+from .exceptions import (
+    APIError,
+    AuthenticationError,
+    DownloadError,
+    SJTUNetdiskError,
+    UploadError,
+)
+from .jdisk import main
+from .models import DirectoryInfo, FileInfo, Session, UploadResult
+from .upload import FileUploader
+
+__all__ = [
+    "SJTUAuth",
+    "SJTUNetdiskClient",
+    "FileUploader",
+    "FileDownloader",
+    "FileInfo",
+    "DirectoryInfo",
+    "UploadResult",
+    "Session",
+    "SJTUNetdiskError",
+    "AuthenticationError",
+    "UploadError",
+    "DownloadError",
+    "APIError",
+    "main",
+]
