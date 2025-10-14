2852466fee480ae0235ee94cd2d03e63311cbf07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 04:05:33 +0000

    Add package django-salestrack/1.0.1/django_salestrack-1.0.1.tar.gz

---
+++ packages/django-salestrack/django_salestrack-1.0.1.tar.gz/django_salestrack-1.0.1/salestrack/__init__.py
@@ -0,0 +1,9 @@
+"""
+Django SalesTrack - Sales personnel and distributor tracking with Google Maps
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Parijat Srivastava'
+__email__ = 'parijat.shrivastava@sortstring.com'
+
+default_app_config = 'salestrack.apps.SalestrackConfig'
