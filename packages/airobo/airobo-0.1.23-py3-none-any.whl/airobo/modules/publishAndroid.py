cc4610bc594aa05db95347708273e63d51b86727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 04:05:53 +0000

    Add package airobo/0.1.23/airobo-0.1.23-py3-none-any.whl

---
+++ packages/airobo/airobo-0.1.23-py3-none-any.whl/airobo/modules/publishAndroid.py
@@ -0,0 +1,15 @@
+"""
+Android App Publishing Module  
+Handles building and publishing Android apps using Capacitor and Google Play Console API
+"""
+
+def publish_android(app_path):
+
+    print("""
+    TODO: 
+        - Capacitor build for Android
+        - Upload to Google Play Console
+
+    """)
+
+    print("=======================================")
