447e948c394bce0f3c7a3b2691fa322f6fa4f609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 09:05:41 +0000

    Add package ai-snake-lab/0.8.0/ai_snake_lab-0.8.0.tar.gz

---
+++ packages/ai-snake-lab/ai_snake_lab-0.8.0.tar.gz/ai_snake_lab-0.8.0/ai_snake_lab/constants/DEpsilon.py
@@ -0,0 +1,19 @@
+"""
+constants/DEpsilon.py
+
+    AI Snake Game Simulator
+    Author: Nadim-Daniel Ghaznavi
+    Copyright: (c) 2024-2025 Nadim-Daniel Ghaznavi
+    GitHub: https://github.com/NadimGhaznavi/ai
+    License: GPL 3.0
+"""
+
+from ai_snake_lab.utils.ConstGroup import ConstGroup
+
+
+class DEpsilon(ConstGroup):
+    """Epsilon Defaults"""
+
+    EPSILON_INITIAL: float = 0.99  # Initial value for Epsilon
+    EPSILON_MIN: float = 0.0  # Minimum value for Epsilon
+    EPSILON_DECAY: float = 0.95  # How quickly Epsilon decays
