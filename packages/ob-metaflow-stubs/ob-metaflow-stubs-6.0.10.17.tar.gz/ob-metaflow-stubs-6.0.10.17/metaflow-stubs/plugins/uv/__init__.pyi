13f1bc1eca3f1f52dc1158736c4f18bf1e568af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 12:07:05 +0000

    Add package ob-metaflow-stubs/6.0.10.17/ob-metaflow-stubs-6.0.10.17.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.10.17.tar.gz/ob-metaflow-stubs-6.0.10.17/metaflow-stubs/plugins/uv/__init__.pyi
@@ -0,0 +1,11 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.11.1+obcheckpoint(0.2.8);ob(v1)                                                   #
+# Generated on 2025-10-13T21:06:57.843533                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import uv_environment as uv_environment
+
