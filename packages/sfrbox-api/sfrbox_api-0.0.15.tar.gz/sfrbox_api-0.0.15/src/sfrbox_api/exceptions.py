d536d3cf404d432b907cb8faa1965d27b6a8c903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 05:06:48 +0000

    Add package sfrbox-api/0.0.15/sfrbox_api-0.0.15.tar.gz

---
+++ packages/sfrbox-api/sfrbox_api-0.0.15.tar.gz/sfrbox_api-0.0.15/src/sfrbox_api/exceptions.py
@@ -0,0 +1,13 @@
+"""SFR Box exceptions."""
+
+
+class SFRBoxError(Exception):
+    """SFR Box base exception."""
+
+
+class SFRBoxApiError(Exception):
+    """SFR Box api exception."""
+
+
+class SFRBoxAuthenticationError(Exception):
+    """SFR Box authentication exception."""
---
+++ packages/sfrbox-api/sfrbox_api-0.0.15.tar.gz/sfrbox_api-0.0.15/src/sfrbox_api/helpers.py
@@ -0,0 +1,18 @@
+"""SFR Box helpers."""
+
+import hashlib
+import hmac
+
+
+def _compute_hash(token: str, value: str) -> str:
+    """Compute single value hash."""
+    hash = hashlib.sha256(value.encode()).hexdigest()
+    return hmac.new(token.encode(), hash.encode(), hashlib.sha256).hexdigest()
+
+
+def compute_hash(token: str, username: str, password: str) -> str:
+    """Compute full username/password hash."""
+    hmac_username = _compute_hash(token, username)
+    hmac_password = _compute_hash(token, password)
+
+    return f"{hmac_username}{hmac_password}"
