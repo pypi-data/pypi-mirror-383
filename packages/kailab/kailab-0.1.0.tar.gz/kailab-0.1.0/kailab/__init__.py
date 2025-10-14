35b2d8f449fd80f7211012b487c08afc9b5747b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 08:06:46 +0000

    Add package kailab/0.1.0/kailab-0.1.0.tar.gz

---
+++ packages/kailab/kailab-0.1.0.tar.gz/kailab-0.1.0/kailab/__init__.py
@@ -0,0 +1,3 @@
+from .core import basiean, cartpole, basieanTamil, basieanImg,astar,simAn,crypt,tictac,NQGA,NQHC,GAFX
+
+__all__ = ["basiean", "cartpole", "basieanTamil", "basieanImg","astar","simAn","crypt","tictac","NQGA","NQHC","GAFX"]
