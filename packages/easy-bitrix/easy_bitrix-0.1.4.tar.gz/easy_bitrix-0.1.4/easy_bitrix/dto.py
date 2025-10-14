5f0c0c1b585203ded9cd5cf3ec2c4b94fcb4000b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:06:21 +0000

    Add package easy-bitrix/0.1.4/easy_bitrix-0.1.4.tar.gz

---
+++ packages/easy-bitrix/easy_bitrix-0.1.4.tar.gz/easy_bitrix-0.1.4/easy_bitrix/dto.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass, field
+from typing import Literal
+
+
+@dataclass
+class SelectData:
+    method: str
+
+
+@dataclass
+class SelectListData(SelectData):
+    select: list[str] = field(default_factory=['*'])
+    filter: dict[str, str | int | float | list[str | int | float]] = field(default_factory=dict())
+    order: dict[str, str | int | float] = field(default_factory=dict())
+    start: int = field(default=0)
+
+
+@dataclass
+class SelectGetData(SelectData):
+    id: dict[str, int]
+
+
+@dataclass
+class GetFieldsData(SelectData):
+    ...
+
+
+@dataclass
+class AddData(SelectData):
+    fields: dict[str, str | int | float]
+    params: dict[str, str] = field(default_factory=dict())
+
+
+@dataclass
+class UpdateData(SelectData):
+    id: dict[str, int]
+    fields: dict[str, str | int | float] = field(default_factory=dict())
+    params: dict[str, str] = field(default_factory=dict())
+
+
+@dataclass
+class DeleteData(SelectData):
+    id: dict[str, int]
+
+
+@dataclass
+class OAuthData:
+    grant_type: Literal['authorization_code', 'refresh_token']
+    client_id: str
+    client_secret: str
+
+
+@dataclass
+class RequestOAuthAssessToken(OAuthData):
+    code: str
+
+
+@dataclass
+class RequestOAuthRefreshToken(OAuthData):
+    refresh_token: str
