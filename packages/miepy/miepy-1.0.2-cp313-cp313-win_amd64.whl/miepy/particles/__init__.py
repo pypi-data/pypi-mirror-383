85a2371935f4e6004fb3d41f9fe54b3793775e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 15:06:47 +0000

    Add package miepy/1.0.2/miepy-1.0.2-cp313-cp313-win_amd64.whl

---
+++ packages/miepy/miepy-1.0.2-cp313-cp313-win_amd64.whl/miepy/particles/__init__.py
@@ -0,0 +1,8 @@
+from .core_shell import core_shell
+from .cylinder import cylinder
+from .ellipsoid import ellipsoid
+from .particle_base import particle
+from .regular_prism import cube, regular_prism
+from .sphere import sphere
+from .sphere_cluster_particle import sphere_cluster_particle
+from .spheroid import spheroid
