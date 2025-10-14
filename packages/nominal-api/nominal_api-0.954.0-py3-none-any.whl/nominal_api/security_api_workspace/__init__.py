403b4ef141c706eae482f4176c24fe2105940a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 12:06:40 +0000

    Add package nominal-api/0.954.0/nominal_api-0.954.0-py3-none-any.whl

---
+++ packages/nominal-api/nominal_api-0.954.0-py3-none-any.whl/nominal_api/security_api_workspace/__init__.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+from .._impl import (
+    security_api_workspace_PreferredRefNameConfiguration as PreferredRefNameConfiguration,
+    security_api_workspace_PreferredRefNameConfigurationVisitor as PreferredRefNameConfigurationVisitor,
+    security_api_workspace_RemoveType as RemoveType,
+    security_api_workspace_UpdateOrRemoveWorkspaceDisplayName as UpdateOrRemoveWorkspaceDisplayName,
+    security_api_workspace_UpdateOrRemoveWorkspaceDisplayNameVisitor as UpdateOrRemoveWorkspaceDisplayNameVisitor,
+    security_api_workspace_UpdateOrRemoveWorkspaceSymbol as UpdateOrRemoveWorkspaceSymbol,
+    security_api_workspace_UpdateOrRemoveWorkspaceSymbolVisitor as UpdateOrRemoveWorkspaceSymbolVisitor,
+    security_api_workspace_UpdateWorkspaceRequest as UpdateWorkspaceRequest,
+    security_api_workspace_Workspace as Workspace,
+    security_api_workspace_WorkspaceService as WorkspaceService,
+    security_api_workspace_WorkspaceSettings as WorkspaceSettings,
+    security_api_workspace_WorkspaceSymbol as WorkspaceSymbol,
+    security_api_workspace_WorkspaceSymbolVisitor as WorkspaceSymbolVisitor,
+)
+
+__all__ = [
+    'PreferredRefNameConfiguration',
+    'PreferredRefNameConfigurationVisitor',
+    'RemoveType',
+    'UpdateOrRemoveWorkspaceDisplayName',
+    'UpdateOrRemoveWorkspaceDisplayNameVisitor',
+    'UpdateOrRemoveWorkspaceSymbol',
+    'UpdateOrRemoveWorkspaceSymbolVisitor',
+    'UpdateWorkspaceRequest',
+    'Workspace',
+    'WorkspaceSettings',
+    'WorkspaceSymbol',
+    'WorkspaceSymbolVisitor',
+    'WorkspaceService',
+]
+
