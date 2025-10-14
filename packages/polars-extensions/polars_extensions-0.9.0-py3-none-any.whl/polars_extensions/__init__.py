804e8ac92e095211422ffc1739a996afcb65f187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 15:07:11 +0000

    Add package polars-extensions/0.9.0/polars_extensions-0.9.0-py3-none-any.whl

---
+++ packages/polars-extensions/polars_extensions-0.9.0-py3-none-any.whl/polars_extensions/__init__.py
@@ -0,0 +1,14 @@
+from .io import *
+from .name import *
+from .numeric import *
+from .string import *
+from .geo import *
+
+__all__ = [
+    "NameExtensionNameSpace",
+    "NumericExtensionNamespace",
+    "StringExtensionNamespace",
+    "GeometryExtensionNamespace",
+    "write_schema",
+    "read_schema",
+]
