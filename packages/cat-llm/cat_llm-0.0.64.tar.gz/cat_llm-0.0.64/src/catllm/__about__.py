cd59ca8ee789bca5c6b7f6791f3f88e7c213e217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 04:05:56 +0000

    Add package cat-llm/0.0.64/cat_llm-0.0.64.tar.gz

---
+++ packages/cat-llm/cat_llm-0.0.64.tar.gz/cat_llm-0.0.64/src/catllm/__about__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2025-present Christopher Soria <chrissoria@berkeley.edu>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.64"
+__author__ = "Chris Soria"
+__email__ = "chrissoria@berkeley.edu"
+__title__ = "cat-llm"
+__description__ = "A tool for categorizing and exploring text data and images using LLMs and vision models"
+__url__ = "https://github.com/chrissoria/cat-llm" 
+__license__ = "GPL-3.0"
