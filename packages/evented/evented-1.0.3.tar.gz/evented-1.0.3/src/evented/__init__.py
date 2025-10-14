b18ff25c9ff40a4806a2a78918bd50458112c4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 14:06:41 +0000

    Add package evented/1.0.3/evented-1.0.3.tar.gz

---
+++ packages/evented/evented-1.0.3.tar.gz/evented-1.0.3/src/evented/__init__.py
@@ -0,0 +1,21 @@
+"""Evented: main package.
+
+Event emitters.
+"""
+
+from __future__ import annotations
+
+from importlib.metadata import version
+
+__version__ = version("evented")
+__title__ = "Evented"
+
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2025 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/evented"
+
+__all__ = [
+    "__version__",
+]
