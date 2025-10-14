1009dc3ecbb921b5bbca3e4ce19367d416aa237e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 04:05:46 +0000

    Add package puyapy/5.1.0/puyapy-5.1.0-py3-none-any.whl

---
+++ packages/puyapy/puyapy-5.1.0-py3-none-any.whl/puyapy/options.py
@@ -0,0 +1,20 @@
+import typing
+from collections.abc import Sequence
+from pathlib import Path
+
+import attrs
+
+from puya.log import LogLevel
+from puya.options import PuyaOptions
+
+
+@attrs.frozen(kw_only=True)
+class PuyaPyOptions(PuyaOptions):
+    paths: Sequence[Path] = attrs.field(default=(), repr=lambda p: str(list(map(str, p))))
+    resource_encoding: typing.Literal["index", "value"] = "value"
+    output_awst: bool = False
+    output_awst_json: bool = False
+    output_source_annotations_json: bool = False
+    output_client: bool = False
+    out_dir: Path | None = attrs.field(default=None, repr=False)
+    log_level: LogLevel = LogLevel.info
