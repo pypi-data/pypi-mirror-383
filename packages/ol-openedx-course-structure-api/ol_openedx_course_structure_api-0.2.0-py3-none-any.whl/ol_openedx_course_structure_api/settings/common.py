f43a9e9f33593be293b170a8927e50d6dba0705a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:05:53 +0000

    Add package ol-openedx-course-structure-api/0.2.0/ol_openedx_course_structure_api-0.2.0-py3-none-any.whl

---
+++ packages/ol-openedx-course-structure-api/ol_openedx_course_structure_api-0.2.0-py3-none-any.whl/ol_openedx_course_structure_api/settings/common.py
@@ -0,0 +1,5 @@
+"""Common settings unique to the course structure API plugin."""
+
+
+def plugin_settings(settings):
+    """Settings for the course structure API plugin"""  # noqa: D401
---
+++ packages/ol-openedx-course-structure-api/ol_openedx_course_structure_api-0.2.0-py3-none-any.whl/ol_openedx_course_structure_api/settings/production.py
@@ -0,0 +1,5 @@
+"""Production settings unique to the course structure API."""
+
+
+def plugin_settings(settings):
+    """Settings for the course structure API."""  # noqa: D401
