d89c2f04d4b5ee6a5568915196539b60d3f58072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 08:06:15 +0000

    Add package dask-sphinx-theme/4.0.0/dask_sphinx_theme-4.0.0.tar.gz

---
+++ packages/dask-sphinx-theme/dask_sphinx_theme-4.0.0.tar.gz/dask_sphinx_theme-4.0.0/docs/conf.py
@@ -0,0 +1,66 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "Dask Sphinx Theme"
+copyright = "2020, Dask Contributors"
+author = "Dask Contributors"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinx_autosummary_accessors",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.viewcode",
+    "numpydoc",
+    "sphinx_click.ext",
+    "dask_sphinx_theme.ext.dask_config_sphinx_ext",
+    "sphinx_tabs.tabs",
+    "sphinx_remove_toctrees",
+    "sphinx_copybutton",
+    "sphinx_design",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "dask_sphinx_theme"
+
+intersphinx_mapping = {
+    "dask": ("https://docs.dask.org/en/latest", None),
+}
---
+++ packages/dask-sphinx-theme/dask_sphinx_theme-4.0.0.tar.gz/dask_sphinx_theme-4.0.0/docs/index.rst
@@ -0,0 +1,24 @@
+Welcome to Dask Sphinx Theme's documentation!
+=============================================
+
+This is the official Sphinx theme for Dask documentation.  It extends the
+``sphinx-book-theme`` project, but adds custom styling and a navigation bar to
+additional Dask subprojects.
+
+When creating a Dask subproject you can include this theme by changing this
+line in your conf.py file
+
+.. code-block:: python
+
+   html_theme = 'dask_sphinx_theme'
+
+And by including ``dask_sphinx_theme`` as a requirement in your documentation
+installation.
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   self
+   10-minutes-to-dask
