7d7dfb3c89d165f11d6760a880d9b49a30dd5928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 06:05:47 +0000

    Add package delta-rest-api-india/2.3/delta_rest_api_india-2.3.tar.gz

---
+++ packages/delta-rest-api-india/delta_rest_api_india-2.3.tar.gz/delta_rest_api_india-2.3/delta_rest_api_india/__init__.py
@@ -0,0 +1,26 @@
+# delta_rest_api_india/__init__.py
+
+from .api import (
+    PLACE_BRACKET_ORDER,
+    PLACE_ORDER,
+    BALANCE_CHECK,
+    EMA,
+    MOVING_AVG,
+    get_support_resistance,
+    pivot_support_resistance,
+    CURRENT_PRICE,
+    EMA_MA_CROSSOVER
+)
+
+__all__ = [
+    "PLACE_BRACKET_ORDER",
+    "PLACE_ORDER",
+    "BALANCE_CHECK",
+    "EMA",
+    "MOVING_AVG",
+    "get_support_resistance",
+    "pivot_support_resistance",
+    "CURRENT_PRICE",
+    "EMA_MA_CROSSOVER"
+]
+
