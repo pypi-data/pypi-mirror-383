58954fb565abaea5986561e62f13b9a2bfb8a430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 15:06:16 +0000

    Add package tejasayya/1.0.2/tejasayya-1.0.2-py3-none-any.whl

---
+++ packages/tejasayya/tejasayya-1.0.2-py3-none-any.whl/tejasayya/__main__.py
@@ -0,0 +1,32 @@
+def main():
+    border = "═" * 90
+    print(f"╔{border}╗")
+    print("║{:^90}║".format("👋 Hello, I'm Teja Sayya"))
+    print("║{:^90}║".format("🚀 Software Engineer @ CVS Health"))
+    print("║{:^90}║".format("💼 Prev Verizon & CVS Health Clients | Full Stack Developer (AWS, GCP)"))
+    print("║{:^90}║".format("🧠 AI/ML | LLM | RAG | CNN | RNN | SaaS Builder | Machine Learning"))
+    print("║{:^90}║".format("I build intelligent, scalable applications leveraging GenAI + Cloud"))
+    print("║{:^90}║".format(" "))
+    print("║{:^90}║".format("------------ 🌐 My Online Presence ------------"))
+    print("║{:^90}║".format("📧 Email: teja.sayya@gmail.com"))
+    print("║{:^90}║".format("💻 GitHub: github.com/tejasayya"))
+    print("║{:^90}║".format("🔗 LinkedIn: linkedin.com/in/tejaswop9/"))
+    print("║{:^90}║".format("👨‍💻 LeetCode: leetcode.com/Teja_Sayya"))
+    print("║{:^90}║".format("🤗 HuggingFace: huggingface.co/TejaSayya"))
+    print("║{:^90}║".format("🌐 Portfolio: tejasayya.github.io/portfolio/"))
+    print("║{:^90}║".format(" "))
+    print("║{:^90}║".format("------------ 🧩 Featured Projects ------------"))
+
+    print("║{:^90}║".format("🔹 Chat with Teja - RAG Chatbot | Retrieval-Augmented LLM Knowledge Assistant"))
+    print("║{:^90}║".format("   🔗 https://vext-ai-agent-pcbe5guo6kzwsukxusepwp.streamlit.app/"))
+    print("║{:^90}║".format("🔹 Real-Time Chat Application "))
+    print("║{:^90}║".format("   🔗 https://chatfront-latest.onrender.com/"))
+    print("║{:^90}║".format("🔹 MakeItTalk — Deep Learning, Facial Animations"))
+    print("║{:^90}║".format("   🔗 https://huggingface.co/spaces/TejaSayya/makeittalk-spaces"))
+    print("║{:^90}║".format("🔹 Connect4 with AI — Alpha-Beta Pruning Search Algorithm"))
+    print("║{:^90}║".format("   🔗 https://github.com/tejasayya/connect4game"))
+    print(f"╚{border}╝")
+
+
+if __name__ == "__main__":
+    main()
