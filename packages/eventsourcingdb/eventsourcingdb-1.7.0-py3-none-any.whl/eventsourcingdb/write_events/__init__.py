ab0dfee3c32facbd3d71b4646989601999876dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:06:09 +0000

    Add package eventsourcingdb/1.7.0/eventsourcingdb-1.7.0-py3-none-any.whl

---
+++ packages/eventsourcingdb/eventsourcingdb-1.7.0-py3-none-any.whl/eventsourcingdb/write_events/__init__.py
@@ -0,0 +1,8 @@
+from .preconditions import IsEventQlQueryTrue, IsSubjectOnEventId, IsSubjectPristine, Precondition
+
+__all__ = [
+    "IsEventQlQueryTrue",
+    "IsSubjectOnEventId",
+    "IsSubjectPristine",
+    "Precondition",
+]
