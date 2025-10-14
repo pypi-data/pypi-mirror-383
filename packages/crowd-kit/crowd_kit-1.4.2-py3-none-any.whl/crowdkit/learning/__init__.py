07de0e9c54491379b994baa5119631de5b0506d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 06:05:39 +0000

    Add package crowd-kit/1.4.2/crowd_kit-1.4.2-py3-none-any.whl

---
+++ packages/crowd-kit/crowd_kit-1.4.2-py3-none-any.whl/crowdkit/learning/__init__.py
@@ -0,0 +1,9 @@
+"""
+Routines for deep learning for crowds.
+"""
+
+from .conal import CoNAL
+from .crowd_layer import CrowdLayer
+from .text_summarization import TextSummarization
+
+__all__ = ["CoNAL", "CrowdLayer", "TextSummarization"]
