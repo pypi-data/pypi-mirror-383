c9fb021fc7ba9f67ae0e30ddb18b0dc943745025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 08:06:47 +0000

    Add package pyrtools/1.0.10/pyrtools-1.0.10-py3-none-macosx_11_0_arm64.whl

---
+++ packages/pyrtools/pyrtools-1.0.10-py3-none-macosx_11_0_arm64.whl/pyrtools/pyramids/c/internal_pointOp.h
@@ -0,0 +1,5 @@
+#include "meta.h"
+
+PYRTOOLS_EXPORT void internal_pointop(double *im, double *res, int size, double *lut,
+									  int lutsize, double origin, double increment,
+									  int warnings);
