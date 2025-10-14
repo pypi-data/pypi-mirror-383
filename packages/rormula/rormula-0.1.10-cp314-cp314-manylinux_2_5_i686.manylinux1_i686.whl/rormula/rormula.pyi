c15836008a39495fa12967a9b338fff2bc3be7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 17:06:38 +0000

    Add package rormula/0.1.10/rormula-0.1.10-cp314-cp314-manylinux_2_5_i686.manylinux1_i686.whl

---
+++ packages/rormula/rormula-0.1.10-cp314-cp314-manylinux_2_5_i686.manylinux1_i686.whl/rormula/rormula.pyi
@@ -0,0 +1,35 @@
+from typing import List, NamedTuple, Optional, Sequence, Tuple
+import numpy as np
+
+class Wilkinson:
+    pass
+
+class SeparatedData(NamedTuple):
+    numerical_cols: List[str]
+    numerical_data: np.ndarray
+    categorical_cols: List[str]
+    categorical_data: np.ndarray
+
+def parse_wilkinson(s: str) -> Wilkinson: ...
+def eval_wilkinson(
+    ror: Wilkinson,
+    numerical_data: np.ndarray,
+    numerical_cols: Sequence[str],
+    cat_data: np.ndarray,
+    cat_cols: Sequence[str],
+    skip_names: bool = False,
+) -> Tuple[Optional[List[str]], np.ndarray]: ...
+
+class Arithmetic:
+    def has_row_change_op(self) -> bool: ...
+    def unparse(self) -> str: ...
+    def operator_reprs(self) -> Sequence[str]: ...
+    def binary_reprs(self) -> Sequence[str]: ...
+    def unary_reprs(self) -> Sequence[str]: ...
+
+def parse_arithmetic(s: str) -> Arithmetic: ...
+def eval_arithmetic(
+    ror: Arithmetic,
+    numerical_data: np.ndarray,
+    numerical_cols: Sequence[str],
+) -> np.ndarray: ...
