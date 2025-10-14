83f614d5d0059bb8601ab5b97b8e7d59bff70e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 03:18:04 +0000

    Add package minikai/0.1.0/minikai-0.1.0-py3-none-any.whl

---
+++ packages/minikai/minikai-0.1.0-py3-none-any.whl/minikai/models/add_attachments_body.py
@@ -0,0 +1,98 @@
+from collections.abc import Mapping
+from typing import Any, TypeVar, Union, cast
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from .. import types
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="AddAttachmentsBody")
+
+
+@_attrs_define
+class AddAttachmentsBody:
+    """
+    Attributes:
+        files (Union[None, Unset, list[Any]]):
+    """
+
+    files: Union[None, Unset, list[Any]] = UNSET
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> dict[str, Any]:
+        files: Union[None, Unset, list[Any]]
+        if isinstance(self.files, Unset):
+            files = UNSET
+        elif isinstance(self.files, list):
+            files = self.files
+
+        else:
+            files = self.files
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if files is not UNSET:
+            field_dict["files"] = files
+
+        return field_dict
+
+    def to_multipart(self) -> types.RequestFiles:
+        files: types.RequestFiles = []
+
+        if not isinstance(self.files, Unset):
+            if isinstance(self.files, list):
+                for files_type_0_item_element in self.files:
+                    files.append(("files", (None, str(files_type_0_item_element).encode(), "text/plain")))
+            else:
+                files.append(("files", (None, str(self.files).encode(), "text/plain")))
+
+        for prop_name, prop in self.additional_properties.items():
+            files.append((prop_name, (None, str(prop).encode(), "text/plain")))
+
+        return files
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: Mapping[str, Any]) -> T:
+        d = dict(src_dict)
+
+        def _parse_files(data: object) -> Union[None, Unset, list[Any]]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, list):
+                    raise TypeError()
+                files_type_0 = cast(list[Any], data)
+
+                return files_type_0
+            except:  # noqa: E722
+                pass
+            return cast(Union[None, Unset, list[Any]], data)
+
+        files = _parse_files(d.pop("files", UNSET))
+
+        add_attachments_body = cls(
+            files=files,
+        )
+
+        add_attachments_body.additional_properties = d
+        return add_attachments_body
+
+    @property
+    def additional_keys(self) -> list[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
