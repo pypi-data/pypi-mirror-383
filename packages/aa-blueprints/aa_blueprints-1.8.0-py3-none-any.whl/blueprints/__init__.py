1f1795e2eca4bf6b3a9ad719d4e0b4550b1a2338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 09:05:38 +0000

    Add package aa-blueprints/1.8.0/aa_blueprints-1.8.0-py3-none-any.whl

---
+++ packages/aa-blueprints/aa_blueprints-1.8.0-py3-none-any.whl/blueprints/__init__.py
@@ -0,0 +1,7 @@
+"""Blueprint Library plugin for Alliance Auth."""
+
+# pylint: disable = invalid-name
+default_app_config = "blueprints.apps.BlueprintsConfig"
+
+__version__ = "1.8.0"
+__title__ = "Blueprints"
