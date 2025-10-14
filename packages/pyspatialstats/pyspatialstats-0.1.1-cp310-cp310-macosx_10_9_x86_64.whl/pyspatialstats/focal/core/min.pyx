cc33000f8e20401fcd00572a87ec1a524faaa7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 05:07:24 +0000

    Add package pyspatialstats/0.1.1/pyspatialstats-0.1.1-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/pyspatialstats/pyspatialstats-0.1.1-cp310-cp310-macosx_10_9_x86_64.whl/pyspatialstats/focal/core/min.pyx
@@ -0,0 +1,42 @@
+# cython: cdivision=True
+# cython: boundscheck=False
+# cython: wraparound=False
+
+cimport numpy as np
+from libc.math cimport isnan
+
+
+cpdef void _focal_min(
+    double[:, :, :, :] a,
+    np.npy_uint8[:, ::1] mask,
+    double[:, :] r,
+    int[:] fringe,
+    double threshold,
+    bint reduce
+):
+    cdef:
+        size_t i, j, p, q, count_values
+        double[:, :] window
+        double curr_min
+
+    with nogil:
+        for i in range(a.shape[0]):
+            for j in range(a.shape[1]):
+                window = a[i, j]
+
+                if not reduce and isnan(window[fringe[0], fringe[1]]):
+                    continue
+
+                count_values = 0
+
+                for p in range(mask.shape[0]):
+                    for q in range(mask.shape[1]):
+                        if not isnan(window[p, q]) and mask[p, q]:
+                            if window[p, q] < curr_min or count_values == 0:
+                                curr_min = window[p, q]
+                            count_values = count_values + 1
+
+                if count_values < threshold:
+                    continue
+
+                r[i, j] = curr_min
