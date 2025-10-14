2d1bf5370e956a546f27cb2e85261e37cd34caf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 07:06:10 +0000

    Add package easyfederated/0.1.2/easyfederated-0.1.2-py3-none-any.whl

---
+++ packages/easyfederated/easyfederated-0.1.2-py3-none-any.whl/easyfederated/lib/__init__.py
@@ -0,0 +1,15 @@
+'''
+This module enables easyfed
+2
+'''
+from src.lib.easyfed import Federated
+
+__all__ = ["Federated"]
+
+
+def my_func(a: int) -> None:
+    '''
+    This is my func!
+    How good it is
+    '''
+    pass
