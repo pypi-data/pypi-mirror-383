d054c91b7b5ece3f7436d0d9116e387aee76fb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 04:05:54 +0000

    Add package UtilityLibAPI/0.1.0/utilitylibapi-0.1.0.tar.gz

---
+++ packages/UtilityLibAPI/utilitylibapi-0.1.0.tar.gz/utilitylibapi-0.1.0/README.md
@@ -0,0 +1,5 @@
+#UtilityLibAPI Classes
+
+## History of version
+Version 0.1.0: 2025/10/13<BR>
+Create UtilityLibAPI -- EnDeCodeLib + SessionVARLib<BR>
---
+++ packages/UtilityLibAPI/utilitylibapi-0.1.0.tar.gz/utilitylibapi-0.1.0/src/__init__.py
@@ -0,0 +1,6 @@
+from .EnDeCodeLib   import CLASS_EnDeCodeLib
+from .SessionVARLib import TIniFile
+
+__all__ = ["CLASS_EnDeCodeLib", 
+           "CLASS_SessionVAR"
+          ]
