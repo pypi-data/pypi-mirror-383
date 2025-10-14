16ca353e588dc849c659c0e176df32803a979cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 05:06:58 +0000

    Add package ellippy/0.1.1/ellippy-0.1.1-cp314-cp314-win32.whl

---
+++ packages/ellippy/ellippy-0.1.1-cp314-cp314-win32.whl/ellippy/__about__.py
@@ -0,0 +1,10 @@
+# EllipPy is licensed under The 3-Clause BSD, see LICENSE.
+# Copyright 2025 Sira Pornsiriprasert <code@psira.me>
+
+__all__ = ["__name__", "__version__", "__author__", "__license__", "__copyright__"]
+
+__name__ = "ellippy"
+__version__ = "0.1.1"
+__author__ = "Sira Pornsiriprasert"
+__license__ = "BSD-3-Clause"
+__copyright__ = f"2025 {__author__}"
