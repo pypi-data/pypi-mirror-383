a4c02bc5537906276fcc71528ba4bd9628fdcd36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 03:18:19 +0000

    Add package esphome-dashboard/20251013.0/esphome_dashboard-20251013.0.tar.gz

---
+++ packages/esphome-dashboard/esphome_dashboard-20251013.0.tar.gz/esphome_dashboard-20251013.0/esphome_dashboard/static/js/esphome/c.D7SdLOTV.js
@@ -0,0 +1,9 @@
+import{_ as e,n as l,s,x as a}from"./index-Bt5Cdm1t.js";import"./c.CyL_D25b.js";import"./c.BU1vbuah.js";let o=class extends s{render(){return a`
+      <esphome-process-dialog
+        heading="Clean All"
+        .type=${"clean-all"}
+        .spawnParams=${{clean_build_dir:!0}}
+        @closed=${this._handleClose}
+      ></esphome-process-dialog>
+    `}_handleClose(){this.parentNode.removeChild(this)}};o=e([l("esphome-clean-all-dialog")],o);
+//# sourceMappingURL=c.D7SdLOTV.js.map
---
+++ packages/esphome-dashboard/esphome_dashboard-20251013.0.tar.gz/esphome_dashboard-20251013.0/esphome_dashboard/static/js/esphome/c.DNcqQXn0.js
@@ -0,0 +1,2 @@
+const e=(e,o)=>{import("./c.B-EHIPmf.js");const t=document.createElement("esphome-install-server-dialog");t.configuration=e,t.target=o,document.body.append(t)};export{e as o};
+//# sourceMappingURL=c.DNcqQXn0.js.map
