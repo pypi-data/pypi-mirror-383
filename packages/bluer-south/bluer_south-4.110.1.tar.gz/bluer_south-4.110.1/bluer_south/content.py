63b5b7077889e455dc5e940517339cf71396c778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 17:06:44 +0000

    Add package bluer-south/4.110.1/bluer_south-4.110.1.tar.gz

---
+++ packages/bluer-south/bluer_south-4.110.1.tar.gz/bluer_south-4.110.1/bluer_south/content.py
@@ -0,0 +1,52 @@
+import blueness
+import bluer_ai
+import abadpour
+import bluer_algo
+import bluer_options
+import bluer_objects
+import bluer_flow
+import bluer_geo
+import bluer_journal
+import bluer_plugin
+import bluer_sandbox
+import bluer_sbc
+import bluer_ugv
+import vancouver_watching
+from bluer_objects import README
+import gizai
+
+
+items = README.Items(
+    [
+        {
+            "name": module.NAME,
+            "marquee": module.MARQUEE,
+            "description": " ".join(
+                [
+                    module.DESCRIPTION.replace(module.ICON, "").strip(),
+                    " [![PyPI version](https://img.shields.io/pypi/v/{}.svg)](https://pypi.org/project/{}/)".format(
+                        module.NAME, module.NAME
+                    ),
+                ]
+            ),
+            "url": f"https://github.com/kamangir/{module.REPO_NAME}",
+        }
+        for module in [
+            bluer_ai,
+            bluer_sbc,
+            bluer_geo,
+            bluer_ugv,
+            bluer_algo,
+            bluer_flow,
+            vancouver_watching,
+            bluer_journal,
+            bluer_options,
+            bluer_objects,
+            bluer_plugin,
+            bluer_sandbox,
+            blueness,
+            gizai,
+            abadpour,
+        ]
+    ]
+)
