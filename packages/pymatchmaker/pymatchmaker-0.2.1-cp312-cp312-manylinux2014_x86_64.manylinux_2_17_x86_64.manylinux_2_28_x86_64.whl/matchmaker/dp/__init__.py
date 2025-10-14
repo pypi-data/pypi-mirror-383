ce1c1ef85d6a360578ec0c97cb56b288463a01e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-14 09:07:06 +0000

    Add package pymatchmaker/0.2.1/pymatchmaker-0.2.1-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pymatchmaker/pymatchmaker-0.2.1-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/matchmaker/dp/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Top module for dynamic programming-based alignment methods
+"""
+
+from .oltw_arzt import OnlineTimeWarpingArzt
+from .oltw_dixon import *
+
+# Alias
+OLTWArzt = OnlineTimeWarpingArzt
---
+++ packages/pymatchmaker/pymatchmaker-0.2.1-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/matchmaker/prob/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Probabilistic methods for music alignment
+"""
+
+from .hmm import PitchIOIHMM
