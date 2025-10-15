cf53e5091d7053ec6213814aa48bd2bc006e47d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:51 +0000

    Add package kantris/0.4.2/kantris-0.4.2-py3-none-any.whl

---
+++ packages/kantris/kantris-0.4.2-py3-none-any.whl/kantris/__init__.py
@@ -0,0 +1,14 @@
+from .plotter import Plotter, StackedPlotter
+from .PCA import PCA, PCA_Signalum
+from .FFT import FFT, CWT
+from .Progressive_CWT import progressive_cwt_plot
+from .STFT import STFT
+from .Basics import Basics
+from .Calc import Calc
+from .HTTP import HTTP
+
+from .DataManipulation import DataManipulation
+from .DataManipulation import Dataframe, Array
+
+
+__version__ = "0.4.2"
