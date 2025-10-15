52a9343184aa57f81241dbf588e1c88af5ee5be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:49 +0000

    Add package capytaine/2.3.1/capytaine-2.3.1-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/capytaine/capytaine-2.3.1-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/capytaine/tools/optional_imports.py
@@ -0,0 +1,27 @@
+"""Tool to import optional dependencies. Inspired by similar code in pandas."""
+
+import importlib
+
+def import_optional_dependency(module_name: str, package_name: str = None):
+    try:
+        module = importlib.import_module(module_name)
+    except ImportError:
+        if package_name is None:
+            package_name = module_name
+
+        message = (
+            f"Missing optional dependency '{module_name}'. "
+            f"Use pip or conda to install {package_name}."
+        )
+        raise ImportError(message) from None
+
+    return module
+
+def silently_import_optional_dependency(module_name: str):
+    # Same as above, except it does not raise a exception when the module is not found.
+    # Instead, simply returns None.
+    try:
+        module = importlib.import_module(module_name)
+    except ImportError:
+        module = None
+    return module
