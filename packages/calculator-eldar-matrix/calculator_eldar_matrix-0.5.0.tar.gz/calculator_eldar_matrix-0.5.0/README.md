e02f4d2e2a22bff88dd5db0f0c53730eaf59246e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:51 +0000

    Add package calculator-eldar-matrix/0.5.0/calculator_eldar_matrix-0.5.0.tar.gz

---
+++ packages/calculator-eldar-matrix/calculator_eldar_matrix-0.5.0.tar.gz/calculator_eldar_matrix-0.5.0/README.md
@@ -0,0 +1,8 @@
+# Calculator Library
+Python kitabxanasi butun esas, elave, statistik, trigonometriya ve hendese funksiyalari ehtiva edir.
+Istifade numunesi:
+`python
+import calculator
+print(calculator.add(5, 3))
+print(calculator.sqrt(16))
+print(calculator.sin(30))
---
+++ packages/calculator-eldar-matrix/calculator_eldar_matrix-0.5.0.tar.gz/calculator_eldar_matrix-0.5.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calculator-eldar-matrix',
+    version='0.5.0',
+    packages=find_packages(),
+    description='Python calculator library with math, trig, stats, and geometry functions',
+    author='Eldar',
+    author_email='eldar2005matrix@gmail.com',
+    url='https://github.com/eldar/calculator',
+    python_requires='>=3.12',
+)
