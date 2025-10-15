1a9383bba7e17ea14d1a3d9a6a6673a20d2be666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:56 +0000

    Add package hello-brahim/0.1.0/hello_brahim-0.1.0-py3-none-any.whl

---
+++ packages/hello-brahim/hello_brahim-0.1.0-py3-none-any.whl/hello_brahim/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# (C) 2025 EL MEKKAOUI BRAHIM : elmekkaoui.brahim@gmail.com
+
+from .core import say_hello
+
+__all__ = ["say_hello"]
+__version__ = "0.1.0"
---
+++ packages/hello-brahim/hello_brahim-0.1.0-py3-none-any.whl/hello_brahim/core.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# (C) 2025 EL MEKKAOUI BRAHIM : elmekkaoui.brahim@gmail.com
+
+def say_hello(name: str) -> str:
+    return f"Hello, {name}!""
+
+
