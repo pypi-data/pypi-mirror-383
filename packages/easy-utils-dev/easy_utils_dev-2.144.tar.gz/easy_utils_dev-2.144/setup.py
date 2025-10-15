e5aef9c8dd4bf1926002dba65dde9220af51722e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:55 +0000

    Add package easy-utils-dev/2.144/easy_utils_dev-2.144.tar.gz

---
+++ packages/easy-utils-dev/easy_utils_dev-2.144.tar.gz/easy_utils_dev-2.144/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+VERSION = '2.144'
+
+# Setting up
+setup(
+    name="easy_utils_dev",
+    version=VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+            'psutil' ,
+            'ping3' , 
+            'snakebite-py3',
+            'flask' , 
+            'flask_cors' , 
+            'xmltodict' , 
+            'paramiko' ,
+            'oracledb' ,
+            'requests',
+            'flask_socketio',
+            'python-dotenv',
+            'gevent',
+            'pyzipper',
+            "shutil",
+            "bs4",
+            'pyjwt',
+            'authlib',
+            'kafka-python'
+        ],
+    keywords=['python3'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
