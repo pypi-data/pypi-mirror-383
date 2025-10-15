c7b3b39fdeb917398271573f1b97c3934e0a191c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:50 +0000

    Add package capytaine/2.3.1/capytaine-2.3.1-cp310-cp310-win_amd64.whl

---
+++ packages/capytaine/capytaine-2.3.1-cp310-cp310-win_amd64.whl/capytaine/__init__.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2017-2019 Matthieu Ancellin
+# See LICENSE file at <https://github.com/mancellin/capytaine>
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'capytaine.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from .__about__ import (
+    __title__, __description__, __version__, __author__, __uri__, __license__
+)
+
+from capytaine.meshes.geometry import Axis, Plane, xOz_Plane, yOz_Plane, xOy_Plane
+from capytaine.meshes.meshes import Mesh
+from capytaine.meshes.collections import CollectionOfMeshes
+from capytaine.meshes.symmetric import ReflectionSymmetricMesh, TranslationalSymmetricMesh, AxialSymmetricMesh
+from capytaine.meshes.predefined.cylinders import mesh_disk, mesh_horizontal_cylinder, mesh_vertical_cylinder
+from capytaine.meshes.predefined.spheres import mesh_sphere
+from capytaine.meshes.predefined.rectangles import mesh_rectangle, mesh_parallelepiped
+
+from capytaine.bodies.bodies import FloatingBody
+from capytaine.bodies.dofs import rigid_body_dofs
+
+from capytaine.bodies.predefined.spheres import Sphere
+from capytaine.bodies.predefined.cylinders import VerticalCylinder, HorizontalCylinder, Disk
+from capytaine.bodies.predefined.rectangles import Rectangle, RectangularParallelepiped, OpenRectangularParallelepiped
+
+from capytaine.bem.problems_and_results import RadiationProblem, DiffractionProblem
+from capytaine.bem.solver import BEMSolver
+from capytaine.bem.engines import BasicMatrixEngine, HierarchicalToeplitzMatrixEngine, HierarchicalPrecondMatrixEngine
+from capytaine.green_functions.delhommeau import Delhommeau, XieDelhommeau
+from capytaine.green_functions.hams import LiangWuNoblesseGF, FinGreen3D, HAMS_GF
+
+from capytaine.post_pro.free_surfaces import FreeSurface
+
+from capytaine.io.mesh_loaders import load_mesh
+from capytaine.io.xarray import assemble_dataframe, assemble_dataset, assemble_matrices, export_dataset
+
+from capytaine.ui.rich import set_logging
+
+set_logging(level="WARNING")
---
+++ packages/capytaine/capytaine-2.3.1-cp310-cp310-win_amd64.whl/capytaine/ui/vtk/__init__.py
@@ -0,0 +1,3 @@
+from capytaine.ui.vtk.mesh_viewer import MeshViewer
+from capytaine.ui.vtk.body_viewer import FloatingBodyViewer
+from capytaine.ui.vtk.animation import Animation
