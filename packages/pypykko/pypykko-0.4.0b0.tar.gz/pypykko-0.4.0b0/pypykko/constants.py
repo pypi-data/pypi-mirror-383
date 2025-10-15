d16a988743867ddcd6d60f8f89043e32a859220c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-15 14:05:35 +0000

    Add package pypykko/0.4.0b0/pypykko-0.4.0b0.tar.gz

---
+++ packages/pypykko/pypykko-0.4.0b0.tar.gz/pypykko-0.4.0b0/pypykko/constants.py
@@ -0,0 +1,98 @@
+import pathlib
+import os
+scripts_path = pathlib.Path(__file__).parent.resolve()
+
+ALPHA_UPPER_EXTENDED = \
+	'ABCDEFGHIJKLMNOPQRSTUVWXYZ' \
+	'ÁÉÍÓÚÝ' \
+	'ÀÈÌÒÙỲ' \
+	'ÂÊÎÔÛŶĈĜĴĤ' \
+	'ÄËÏÖÜŸ' \
+	'ĀĒĪŌŪȲ' \
+	'ŠŽČŇĽŘŤĎĚ' \
+	'ŚŹĆŃĹŔ' \
+	'ÃÕÑ' \
+	'ÅŮ' \
+	'ŞÇ' \
+	'ĄĘŲĢ' \
+	'ĖŻ' \
+	'ȘŅĻŖȚĶ' \
+	'ØßÐĐÆŒŁĞŐŊÞ'
+
+ALPHA_LOWER_BASIC = 'abcdefghijklmnopqrstuvwxyzåäö'
+ALPHA_LOWER = 'abcdefghijklmnopqrstuvwxyzåäöüšžčćı'
+ALPHA_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖÜŠŽČĆ'
+
+PARSER_FST_PATH = os.path.join(scripts_path, 'fi-parser.kfst')
+GENERATOR_FST_PATH = os.path.join(scripts_path, 'fi-generator.kfst')
+
+LINE_BREAK = '@_LINEBREAK_@'
+SENT_BREAK = '@_SENTBREAK_@'
+ZERO = '@_ZERO_@'
+TAB = '^TAB'
+
+OPENING_TAGS = [f'<{tag}>' for tag in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']]
+
+POS_TAGS = [
+	'noun',
+	'noun-pl',
+	'verb',
+	'adverb',
+	'ordinal',
+	'numeral',
+	'pronoun',
+	'pronoun-pl',
+	'adjective',
+	'adposition',
+	'conjunction',
+	'interjection',
+	'adverb+verb',
+	'conjunction+verb',
+	'proper',
+	'proper-pl',
+	'participle',
+	'none',
+]
+
+CLITICS = {
+	"+han",
+	"+ka",
+	"+kaan",
+	"+kin",
+	"+ko",
+	"+pa",
+	"+poss1pl",
+	"+poss1sg",
+	"+poss2pl",
+	"+poss2sg",
+	"+poss3",
+	"+poss3",
+	"+s",
+}
+
+STYLE_TAGS = [
+	'+arch',
+	'+child',
+	'+coll',
+	'+dated',
+	'+dial',
+	'+foreign',
+	'+jocul',
+	'+nstd',
+	'+poet',
+	'+rare',
+	'+slang',
+	'+vulg',
+]
+STYLE_TAG_REGEX = '|'.join(tag[1:] for tag in STYLE_TAGS)
+
+FIELDS = [
+	'',    # 1. source
+	'%s',  # 2. lemma
+	'',    # 3. pos
+	'',    # 4. homonym
+	'',    # 5. style
+	''     # 6. morph.tags
+]
+
+FIELD_STRING = '\t'.join(FIELDS)
